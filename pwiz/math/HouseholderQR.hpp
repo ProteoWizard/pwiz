e2897f7371fe20aed73a17008524350b45e11331
dkessner <dkessner@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2008-03-07 23:52:04 +0000

    added Apache license

--- pwiz/math/HouseholderQR.hpp
+++ pwiz/math/HouseholderQR.hpp
@@ -1,8 +1,19 @@
 //
-// HouseholderQR.hpp
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
 //
 //
-// Robert Burke <robert.burke@gmail.com>
+// Original author: Robert Burke <robert.burke@gmail.com>
 //
 // This code taken from the following site:
 // http://www.crystalclearsoftware.com/cgi-bin/boost_wiki/wiki.pl?Effective_UBLAS/Matrix_Inversion
