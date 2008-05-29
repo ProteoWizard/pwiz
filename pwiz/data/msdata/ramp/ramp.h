e8688f4c6942d4562eb06577d0206ce0ea162c3d
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2008-05-29 06:04:44 +0000

    - fixed internalization of boost::program_options - fixed MinGW compatibility for the msconvert path (i.e. most of the project) - moved Image.cpp to its own library so utility_misc can be used without depending on GD

--- pwiz/data/msdata/ramp/ramp.h
+++ pwiz/data/msdata/ramp/ramp.h
@@ -32,7 +32,7 @@ Non sequential parser for mzXML files
 #define RAP_EXTERN_C 
 #endif
 
-#ifdef _MSC_VER
+#if defined(_MSC_VER) || defined(_MINGW_)
 #include <winsock2.h>
 #include <sys/types.h>
 #include <fcntl.h>
