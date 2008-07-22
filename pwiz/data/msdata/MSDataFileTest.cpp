1a3c62556d609f9c6eccec4889db160c688bfc43
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2008-07-22 22:27:29 +0000

    - updating pwiz project file (TODO: slap Microsoft for not making the sorting and filepaths in a Makefile project stable) - fixed variable scope oversight in Brian's gzip unit test

--- pwiz/data/msdata/MSDataFileTest.cpp
+++ pwiz/data/msdata/MSDataFileTest.cpp
@@ -75,18 +75,21 @@ void validateWriteRead(const MSDataFile::WriteConfig& writeConfig,
         diff(tiny, msd2);
         if (diff && os_) *os_ << diff << endl;
         unit_assert(!diff);
-    }
 
-	// now give the gzip read a workout
-	std::string cmd("gzip ");
-	cmd += filename1;
-	if (!system(cmd.c_str())) { // don't fret if gzip command isn't present
-		filename1+=".gz";
-		MSDataFile msd1(filename1);
-        // compare
-        Diff<MSData> diff(tiny, msd1, diffConfig);
-        if (diff && os_) *os_ << diff << endl;
-        unit_assert(!diff);
+
+	    // now give the gzip read a workout
+	    std::string cmd("gzip ");
+	    cmd += filename1;
+	    if (!system(cmd.c_str())) // don't fret if gzip command isn't present
+        {
+		    filename1+=".gz";
+		    MSDataFile msd1(filename1);
+
+            // compare
+            Diff<MSData> diff(tiny, msd1, diffConfig);
+            if (diff && os_) *os_ << diff << endl;
+            unit_assert(!diff);
+        }
 	}
 
     // remove temp files
