4a354b20cc1b18bd3bc79cbc79b9e43a2970cdd9
dkessner <dkessner@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2008-07-24 18:44:38 +0000

    untar_boost.sh: bsdtar->tar

--- libraries/untar_boost.sh
+++ libraries/untar_boost.sh
@@ -3,5 +3,5 @@
 cd $1/libraries;
 if [ ! -e boost_1_34_1/boost/algorithm/string.hpp ]; then
 	echo "Extracting tarballs...";
-	bsdtar -xkvjf boost_1_34_1.tar.bz2 boost_1_34_1/libs boost_1_34_1/boost;
+	tar -xkvjf boost_1_34_1.tar.bz2 boost_1_34_1/libs boost_1_34_1/boost;
 fi;
