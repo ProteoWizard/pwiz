c95829ca78b69b6d26c034e93f5c6aafeb642aca
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2008-07-30 02:55:40 +0000

    - added a nightly build of boost-build to libraries for VC9 compatibility - added quickbuild.bat for one-click building of ProteoWizard on Windows

--- libraries/untar_boost.bat
+++ libraries/untar_boost.bat
@@ -1,5 +1,6 @@
-cd %1/libraries
+pushd %1\libraries
 IF EXIST boost_1_34_1/boost/algorithm/string.hpp GOTO SKIP
-echo "Extracting tarballs..."
-bsdtar.exe -xkjvf boost_1_34_1.tar.bz2 boost_1_34_1/libs boost_1_34_1/boost
+echo Extracting tarballs...
+bsdtar.exe -xkjvf boost_1_34_1.tar.bz2 boost_1_34_1/libs boost_1_34_1/boost boost_1_34_1/tools/jam boost_1_34_1/tools/build
 :SKIP
+popd
---
+++ quickbuild.bat
@@ -0,0 +1,34 @@
+@echo off
+setlocal
+@echo off
+
+REM # Get the location of quickbuild.bat and drop trailing slash
+set PWIZ_ROOT=%~dp0
+set PWIZ_ROOT=%PWIZ_ROOT:~0,-1%
+
+REM # Extract Boost distro
+call %PWIZ_ROOT%\libraries\untar_boost.bat  %PWIZ_ROOT%
+
+REM # Extract Boost.Build (for VC9 support)
+pushd %PWIZ_ROOT%\libraries
+IF EXIST boost-build GOTO SKIP_BB
+bsdtar.exe -xkjvf boost-build.tar.bz2
+:SKIP_BB
+popd
+
+set PWIZ_BJAM=%PWIZ_ROOT%\libraries\boost-build\jam_src\bin.ntx86\bjam.exe
+REM set PWIZ_BJAM=%PWIZ_ROOT%\libraries\bjam.exe
+
+REM # Build local copy of bjam
+IF EXIST %PWIZ_BJAM% GOTO SKIP_BJAM
+echo Building bjam...
+pushd %PWIZ_ROOT%\libraries\boost-build\jam_src
+call build.bat
+popd
+:SKIP_BJAM
+
+set BOOST_BUILD_PATH=%PWIZ_ROOT%\libraries\boost-build
+
+REM # Do full build of ProteoWizard, passing quickbuild's arguments to bjam
+echo Building pwiz...
+%PWIZ_BJAM% %PWIZ_ROOT%/pwiz/build//all %*
