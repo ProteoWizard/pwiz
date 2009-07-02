9ca8a57c9be11c7b741fd536c0b95c8481e04ab7
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2009-07-02 21:47:29 +0000

    - converted Reader_ABI_Test to use VendorReaderTestHarness - updated test data to fix absolute path mangling and non-UTC startTimeStamps

--- pwiz_aux/msrc/data/vendor_readers/ABI/Reader_ABI_Test.cpp
+++ pwiz_aux/msrc/data/vendor_readers/ABI/Reader_ABI_Test.cpp
@@ -19,166 +19,29 @@
 // limitations under the License.
 //
 
-
 #include "Reader_ABI.hpp"
-#include "pwiz/data/msdata/TextWriter.hpp"
-#include "pwiz/data/msdata/MSDataFile.hpp"
-#include "pwiz/data/msdata/Diff.hpp"
-#include "pwiz/data/msdata/SpectrumListBase.hpp"
-#include "pwiz/data/msdata/ChromatogramListBase.hpp"
-#include "pwiz/utility/misc/unit.hpp"
-#include "pwiz/utility/misc/String.hpp"
+#include "pwiz/utility/misc/VendorReaderTestHarness.hpp"
 #include "pwiz/utility/misc/Filesystem.hpp"
-#include <iostream>
-#include <fstream>
+#include "pwiz/utility/misc/String.hpp"
 
-
-using namespace std;
-using namespace pwiz::util;
-using namespace pwiz;
-using namespace pwiz::msdata;
-
-
-ostream* os_ = 0;
-
-
-void testAccept(const string& rawpath)
+struct IsWiffFile : public pwiz::util::TestPathPredicate
 {
-    if (os_) *os_ << "testAccept(): " << rawpath << endl;
-
-    Reader_ABI reader;
-    bool accepted = reader.accept(rawpath, "");
-    if (os_) *os_ << "accepted: " << boolalpha << accepted << endl;
-
-    unit_assert(accepted);
-}
-
-
-void hackInMemoryMSData(MSData& msd)
-{
-    // remove metadata ptrs appended on read
-    vector<SourceFilePtr>& sfs = msd.fileDescription.sourceFilePtrs;
-    if (!sfs.empty()) sfs.erase(sfs.end()-1);
-    //vector<SoftwarePtr>& sws = msd.softwarePtrs;
-    //if (!sws.empty()) sws.erase(sws.end()-1);
-
-    // remove current DataProcessing created on read
-    SpectrumListBase* sl = dynamic_cast<SpectrumListBase*>(msd.run.spectrumListPtr.get());
-    ChromatogramListBase* cl = dynamic_cast<ChromatogramListBase*>(msd.run.chromatogramListPtr.get());
-    if (sl) sl->setDataProcessingPtr(DataProcessingPtr());
-    if (cl) cl->setDataProcessingPtr(DataProcessingPtr());
-}
-
-
-void testRead(const string& rawpath)
-{
-    if (os_) *os_ << "testRead(): " << rawpath << endl;
-
-    // read file into MSData object
-    Reader_ABI reader;
-    vector<MSDataPtr> msds;
-    reader.read(rawpath, "dummy", msds);
-
-    // WIFFs can represent multiple samples (runs), so test each one
-    for (size_t i=0; i < msds.size(); ++i)
+    bool operator() (const string& rawpath) const
     {
-        MSData& msd = *msds[i];
-        if (os_) TextWriter(*os_,0)(msd);
-
-        string targetResultFilename;
-        if (msds.size() == 1)
-            targetResultFilename = bfs::change_extension(rawpath, ".mzML").string();
-        else
-            targetResultFilename = bfs::change_extension(rawpath, "-" + msd.run.id + ".mzML").string();
-
-        MSDataFile targetResult(targetResultFilename);
-        hackInMemoryMSData(targetResult);
-
-        // test for 1:1 equality
-        Diff<MSData> diff(msd, targetResult);
-        if (os_ && diff) *os_ << diff << endl; 
-        unit_assert(!diff);
+        return bfs::path(rawpath).extension() == ".wiff";
     }
-}
-
-
-void test(const string& rawpath)
-{
-    testAccept(rawpath);
-    
-    #ifdef _MSC_VER
-    testRead(rawpath);
-    #else
-    if (os_) *os_ << "Not MSVC -- nothing to do.\n";
-    #endif // _MSC_VER
-}
-
-
-void generate(const string& rawpath)
-{
-    #ifdef _MSC_VER
-    // read file into MSData object
-    Reader_ABI reader;
-    vector<MSDataPtr> msds;
-    reader.read(rawpath, "dummy", msds);
-    MSDataFile::WriteConfig config;
-    config.indexed = false;
-    config.binaryDataEncoderConfig.compression = BinaryDataEncoder::Compression_Zlib;
-    if (os_) *os_ << "Writing mzML(s) for " << rawpath << endl;
-    for (size_t i=0; i < msds.size(); ++i)
-    {
-        string outputFilename;
-        if (msds.size() == 1)
-            outputFilename = bfs::change_extension(rawpath, ".mzML").string();
-        else
-            outputFilename = bfs::change_extension(rawpath, "-" + msds[i]->run.id + ".mzML").string();
-        MSDataFile::write(*msds[i], outputFilename, config);
-    }
-    #else
-    if (os_) *os_ << "Not MSVC -- nothing to do.\n";
-    #endif // _MSC_VER
-}
-
+};
 
 int main(int argc, char* argv[])
 {
-    try
-    {
-        bool generateMzML = false;
-        vector<string> rawpaths;
+    #ifdef PWIZ_READER_ABI
+    const bool testAcceptOnly = false;
+    #else
+    const bool testAcceptOnly = true;
+    #endif
 
-        for (int i=1; i<argc; i++)
-        {
-            if (!strcmp(argv[i],"-v")) os_ = &cout;
-            else if (!strcmp(argv[i],"--generate-mzML")) generateMzML = true;
-            else rawpaths.push_back(argv[i]);
-        }
-
-        vector<string> args(argv, argv+argc);
-        if (rawpaths.empty())
-            throw runtime_error(string("Invalid arguments: ") + bal::join(args, " ") +
-                                "\nUsage: Reader_ABI_Test [-v] [--generate-mzML] <source path 1> [source path 2] ..."); 
-
-        for (size_t i=0; i < rawpaths.size(); ++i)
-            for (bfs::directory_iterator itr(rawpaths[i]); itr != bfs::directory_iterator(); ++itr)
-            {
-                if (itr->path().extension() != ".wiff")
-                    continue;
-                else if (generateMzML)
-                    generate(itr->path().string());
-                else
-                    test(itr->path().string());
-            }
-        return 0;
-    }
-    catch (exception& e)
-    {
-        cerr << e.what() << endl;
-    }
-    catch (...)
-    {
-        cerr << "Caught unknown exception.\n";
-    }
-    
-    return 1;
+    return pwiz::util::testReader(pwiz::msdata::Reader_ABI(),
+                                  vector<string>(argv, argv+argc),
+                                  testAcceptOnly,
+                                  IsWiffFile());
 }
