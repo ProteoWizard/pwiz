a66d4ee0904e3ee69e8e65f9ac865b304ab7bb19
pcbrefugee <pcbrefugee@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2009-07-08 18:51:07 +0000

    untar boost_1_39_0 completely (attempt as selective directories seemed to leave out some needed components), and silently (reduced screen IO compensates for increased disk IO), go ahead an delete boost 1_36 tarball for smaller code downloads.

--- libraries/untar_boost.bat
+++ libraries/untar_boost.bat
@@ -1,6 +1,6 @@
 pushd %1\libraries
 IF EXIST boost_1_39_0/boost/algorithm/string.hpp GOTO SKIP
 echo Extracting boost tarball...
-bsdtar.exe -xkjvf boost_1_39_0.tar.bz2 boost_1_39_0/libs boost_1_39_0/boost boost_1_39_0/tools/jam boost_1_39_0/tools/build
+bsdtar.exe -xkjf boost_1_39_0.tar.bz2
 :SKIP
 popd
--- libraries/untar_boost.sh
+++ libraries/untar_boost.sh
@@ -3,5 +3,5 @@
 cd $1/libraries;
 if [ ! -e boost_1_39_0/boost/algorithm/string.hpp ]; then
 	echo "Extracting boost tarball...";
-	tar -xkvjf boost_1_39_0.tar.bz2 boost_1_39_0/libs boost_1_39_0/boost;
+	tar -xkjf boost_1_39_0.tar.bz2 ;
 fi;
