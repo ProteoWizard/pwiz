1b4ff97b0741aeff954636dc34fc3c1fdfd7ef62
pcbrefugee <pcbrefugee@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2009-07-08 18:35:53 +0000

    first cut at mingw/linux/darwin equivalent of quickbuild.bat to bootstrap the building of ProteoWizard (including building own boost/bjam system).  Works in linux (ubuntu 8.04 x86 and x86_64, anyway), completely untested in Darwin, borken in mingw (but this does not appear to be the fault of the script, but something about bjam discerning msvc vs gcc on windows)

---
+++ quickbuild.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+# script for getting bjam, boost and ProteoWizard up and running
+#
+# Get the location of quickbuild.sh and drop trailing slash
+PWIZ_ROOT=$(pwd)
+if [ ! -e $PWIZ_ROOT/quickbuild.sh ]
+then
+echo "quickbuild.sh must be run from the directory it resides in - quitting"
+exit 1
+fi
+
+case "$1" in
+  "mingw" ) 
+	BJAM_BIN=bin.ntx86 
+	BJAM_TOOLSET="gcc"
+	;;
+  "linuxx86" ) 
+	BJAM_BIN=bin.linuxx86 
+	BJAM_TOOLSET="gcc"
+	;;
+  "linuxx86_64" ) 
+	BJAM_BIN=bin.linuxx86_64 
+	BJAM_TOOLSET="gcc"
+	;;
+  "darwin" ) 
+	BJAM_BIN=bin.darwin 
+	BJAM_TOOLSET="darwin"
+	;;
+   *  ) 
+	echo "usage: quickbuild.sh mingw|linuxx86|linuxx86_64|darwin [optional_bjam_args]"
+	exit 1
+	;;
+esac
+
+# Extract Boost distro
+$PWIZ_ROOT/libraries/untar_boost.sh  $PWIZ_ROOT
+
+# Extract Boost.Build
+if [ ! -e $PWIZ_ROOT/libraries/boost-build/jam_src/build.sh ]
+then
+cd $PWIZ_ROOT/libraries ; tar -xkjvf boost-build.tar.bz2
+fi
+cp $PWIZ_ROOT/libraries/msvc.jam $PWIZ_ROOT/libraries/boost-build/tools
+
+PWIZ_BJAM=$PWIZ_ROOT/libraries/boost-build/jam_src/$BJAM_BIN/bjam
+
+# Build local copy of bjam
+if [ ! -e $PWIZ_BJAM ]
+then
+echo "Building bjam..."
+cd $PWIZ_ROOT/libraries/boost-build/jam_src; sh build.sh $BJAM_TOOLSET
+fi
+
+# Do full build of ProteoWizard, passing quickbuild's arguments to bjam
+echo "Building pwiz..."
+echo "export BOOST_BUILD_PATH=$PWIZ_ROOT/libraries/boost-build ; $PWIZ_BJAM $2 $3 $4 $5 --toolset=$BJAM_TOOLSET $PWIZ_ROOT"
+export BOOST_BUILD_PATH=$PWIZ_ROOT/libraries/boost-build ; $PWIZ_BJAM $2 $3 $4 $5 "--toolset=$BJAM_TOOLSET" $PWIZ_ROOT
+
+exit 0
+
