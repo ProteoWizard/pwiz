c4c096e3e277b9b18be1d7258857a25c811c9998
pcbrefugee <pcbrefugee@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2009-07-09 22:31:41 +0000

    restore the selective untarring of boost - whatever my problem building under minGW is, it wasn't that.  Still leaving out the verbose switch, though, as it's noisy and just slows things down.

--- libraries/untar_boost.sh
+++ libraries/untar_boost.sh
@@ -3,5 +3,5 @@
 cd $1/libraries;
 if [ ! -e boost_1_39_0/boost/algorithm/string.hpp ]; then
 	echo "Extracting boost tarball...";
-	tar -xkjf boost_1_39_0.tar.bz2 ;
+	tar -xkjf boost_1_39_0.tar.bz2 boost_1_39_0/libs boost_1_39_0/boost ;
 fi;
