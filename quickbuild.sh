7ec711edf79eaceece7d7dc4000dd8e6d67ee7e8
pcbrefugee <pcbrefugee@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2009-07-10 00:49:22 +0000

    a couple of tweaks to quickbuild.sh (tested on linux and mingw), and advertise its existence on the build doc page.

--- quickbuild.sh
+++ quickbuild.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 # script for getting bjam, boost and ProteoWizard up and running
 #
@@ -13,19 +13,23 @@ fi
 case "$1" in
   "mingw" ) 
 	BJAM_BIN=bin.ntx86 
-	BJAM_TOOLSET="gcc"
+	BJAM_TOOLSET="mingw"
+	PWIZ_TOOLSET="gcc"
 	;;
   "linuxx86" ) 
 	BJAM_BIN=bin.linuxx86 
 	BJAM_TOOLSET="gcc"
+	PWIZ_TOOLSET="gcc"
 	;;
   "linuxx86_64" ) 
 	BJAM_BIN=bin.linuxx86_64 
 	BJAM_TOOLSET="gcc"
+	PWIZ_TOOLSET="gcc"
 	;;
   "darwin" ) 
 	BJAM_BIN=bin.darwin 
 	BJAM_TOOLSET="darwin"
+	PWIZ_TOOLSET="darwin"
 	;;
    *  ) 
 	echo "usage: quickbuild.sh mingw|linuxx86|linuxx86_64|darwin [optional_bjam_args]"
@@ -39,7 +43,8 @@ $PWIZ_ROOT/libraries/untar_boost.sh  $PWIZ_ROOT
 # Extract Boost.Build
 if [ ! -e $PWIZ_ROOT/libraries/boost-build/jam_src/build.sh ]
 then
-cd $PWIZ_ROOT/libraries ; tar -xkjvf boost-build.tar.bz2
+echo "extracting boost-build..."
+cd $PWIZ_ROOT/libraries ; tar -xkjf boost-build.tar.bz2
 fi
 cp $PWIZ_ROOT/libraries/msvc.jam $PWIZ_ROOT/libraries/boost-build/tools
 
@@ -54,8 +59,8 @@ fi
 
 # Do full build of ProteoWizard, passing quickbuild's arguments to bjam
 echo "Building pwiz..."
-echo "export BOOST_BUILD_PATH=$PWIZ_ROOT/libraries/boost-build ; $PWIZ_BJAM $2 $3 $4 $5 --toolset=$BJAM_TOOLSET $PWIZ_ROOT"
-export BOOST_BUILD_PATH=$PWIZ_ROOT/libraries/boost-build ; $PWIZ_BJAM $2 $3 $4 $5 "--toolset=$BJAM_TOOLSET" $PWIZ_ROOT
+echo "cd $PWIZ_ROOT ; export BOOST_BUILD_PATH=$PWIZ_ROOT/libraries/boost-build ; $PWIZ_BJAM $2 $3 $4 $5 toolset=$PWIZ_TOOLSET $PWIZ_ROOT"
+cd $PWIZ_ROOT ; export BOOST_BUILD_PATH=$PWIZ_ROOT/libraries/boost-build ; $PWIZ_BJAM $2 $3 $4 $5 "toolset=$PWIZ_TOOLSET" $PWIZ_ROOT
 
 exit 0
 
