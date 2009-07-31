dad45b9c8b2163ac795cc02f4d0dad5d8d8e5b42
dstearns <dstearns@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2009-07-31 00:18:44 +0000

    Correction to TeamCity progress message format and minor cleanup of error messages

--- scripts/misc/autobuild.bat
+++ scripts/misc/autobuild.bat
@@ -1,6 +1,6 @@
 @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
-if %ERRORLEVEL% NEQ 0 set ERROR_TEXT="Error setting up Visual C++ environment variables" & goto error
+if %ERRORLEVEL% NEQ 0 set ERROR_TEXT=Error setting up Visual C++ environment variables & goto error
 setlocal
 @echo off
 
@@ -10,22 +10,22 @@ set SCRIPTS_MISC_ROOT=%SCRIPTS_MISC_ROOT:~0,-1%
 pushd %SCRIPTS_MISC_ROOT%\..\..
 
 REM # call bootstrap
-echo ##teamcity[message text='Running bootstrap...']
-echo ##teamcity[progressMessage text='Running bootstrap...']
+echo "##teamcity[message text='Running bootstrap...']"
+echo "##teamcity[progressMessage text='Running bootstrap...']"
 call bootstrap.bat
-if %ERRORLEVEL% NEQ 0 set ERROR_TEXT="Error performing bootstrap" & goto error
+if %ERRORLEVEL% NEQ 0 set ERROR_TEXT=Error performing bootstrap & goto error
 
 REM # call quickbuild to build only the bindings first
-echo ##teamcity[message text='Building bindings...']
-echo ##teamcity[progressMessage text='Building bindings...']
+echo "##teamcity[message text='Building bindings...']"
+echo "##teamcity[progressMessage text='Building bindings...']"
 call quickbuild.bat -j4 pwiz\utility\bindings\CLI//pwiz_bindings_cli
-if %ERRORLEVEL% NEQ 0 set ERROR_TEXT="Error building bindings" & goto error
+if %ERRORLEVEL% NEQ 0 set ERROR_TEXT=Error building bindings & goto error
 
 REM # call quickbuild again to build the rest and run tests
-echo ##teamcity[message text='Running quickbuild...']
-echo ##teamcity[progressMessage text='Running quickbuild...']
+echo "##teamcity[message text='Running quickbuild...']"
+echo "##teamcity[progressMessage text='Running quickbuild...']"
 call quickbuild.bat -j4
-if %ERRORLEVEL% NEQ 0 set ERROR_TEXT="Error performing quickbuild" & goto error
+if %ERRORLEVEL% NEQ 0 set ERROR_TEXT=Error performing quickbuild & goto error
 
 popd
 goto :EOF
