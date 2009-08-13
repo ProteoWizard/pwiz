698abc83cd59e000d2fd8ae069dc08fd296887cd
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2009-08-13 21:55:17 +0000

    - fixed bad path for clean.bat and added missing clean.sh

--- clean.bat
+++ clean.bat
@@ -3,7 +3,8 @@ setlocal
 @echo off
 
 REM # Get the location of quickbuild.bat and drop trailing slash
-set PWIZ_ROOT=%~dp0..\..
+set PWIZ_ROOT=%~dp0
+set PWIZ_ROOT=%PWIZ_ROOT:~0,-1%
 pushd %PWIZ_ROOT%
 
 echo Cleaning project...
