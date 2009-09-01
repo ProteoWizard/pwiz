d1a4659ff602b52c053fe0f738c159444fc7b638
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2009-09-01 20:46:16 +0000

    - fixed backslashes in clean.sh - added svnrev.hpp to clean scripts - fixed <location> of svnrev.hpp targets

--- clean.bat
+++ clean.bat
@@ -15,8 +15,14 @@ IF EXIST libraries\boost-build rmdir /s /q libraries\boost-build
 IF EXIST libraries\gd-2.0.33 rmdir /s /q libraries\gd-2.0.33
 IF EXIST libraries\zlib-1.2.3 rmdir /s /q libraries\zlib-1.2.3
 IF EXIST libraries\fftw-3.1.2 rmdir /s /q libraries\fftw-3.1.2
+
 IF EXIST libraries\libfftw3-3.def del /q libraries\libfftw3-3.def
 IF EXIST libraries\libfftw3-3.dll del /q libraries\libfftw3-3.dll
+IF EXIST pwiz\svnrev.hpp del /q pwiz\svnrev.hpp; fi;
+IF EXIST pwiz\data/msdata\svnrev.hpp del /q pwiz\data\msdata\svnrev.hpp; fi;
+IF EXIST pwiz\analysis\svnrev.hpp del /q pwiz\analysis\svnrev.hpp; fi;
+IF EXIST pwiz\utility\proteome\svnrev.hpp del /q pwiz/utility/proteome/svnrev.hpp; fi;
+
 IF EXIST pwiz\data\vendor_readers\Thermo\Reader_Thermo_Test.data rmdir /s /q pwiz\data\vendor_readers\Thermo\Reader_Thermo_Test.data
 IF EXIST pwiz\data\vendor_readers\Agilent\Reader_Agilent_Test.data rmdir /s /q pwiz\data\vendor_readers\Agilent\Reader_Agilent_Test.data
 IF EXIST pwiz_aux\msrc\data\vendor_readers\ABI\Reader_ABI_Test.data rmdir /s /q pwiz_aux\msrc\data\vendor_readers\ABI\Reader_ABI_Test.data
--- clean.sh
+++ clean.sh
@@ -5,18 +5,22 @@ pushd $pwiz_root > /dev/null
 
 echo "Cleaning project..."
 if [ -d build ]; then rm -fdr build; fi;
-if [ -d libraries\boost_1_36_0 ]; then rm -fdr libraries\boost_1_36_0; fi;
-if [ -d libraries\boost_1_39_0 ]; then rm -fdr libraries\boost_1_39_0; fi;
-if [ -d libraries\boost-build ]; then rm -fdr libraries\boost-build; fi;
-if [ -d libraries\gd-2.0.33 ]; then rm -fdr libraries\gd-2.0.33; fi;
-if [ -d libraries\zlib-1.2.3 ]; then rm -fdr libraries\zlib-1.2.3; fi;
-if [ -d libraries\fftw-3.1.2 ]; then rm -fdr libraries\fftw-3.1.2; fi;
-if [ -f libraries\libfftw3-3.def ]; then rm -f libraries\libfftw3-3.def; fi;
-if [ -f libraries\libfftw3-3.dll ]; then rm -f libraries\libfftw3-3.dll; fi;
-if [ -d pwiz\data\vendor_readers\Thermo\Reader_Thermo_Test.data ]; then rm -fdr pwiz\data\vendor_readers\Thermo\Reader_Thermo_Test.data; fi;
-if [ -d pwiz\data\vendor_readers\Agilent\Reader_Agilent_Test.data ]; then rm -fdr pwiz\data\vendor_readers\Agilent\Reader_Agilent_Test.data; fi;
-if [ -d pwiz_aux\msrc\data\vendor_readers\ABI\Reader_ABI_Test.data ]; then rm -fdr pwiz_aux\msrc\data\vendor_readers\ABI\Reader_ABI_Test.data; fi;
-if [ -d pwiz_aux\msrc\data\vendor_readers\Waters\Reader_Waters_Test.data ]; then rm -fdr pwiz_aux\msrc\data\vendor_readers\Waters\Reader_Waters_Test.data; fi;
-if [ -d pwiz_aux\msrc\data\vendor_readers\Bruker\Reader_Bruker_Test.data ]; then rm -fdr pwiz_aux\msrc\data\vendor_readers\Bruker\Reader_Bruker_Test.data; fi;
+if [ -d libraries/boost_1_36_0 ]; then rm -fdr libraries/boost_1_36_0; fi;
+if [ -d libraries/boost_1_39_0 ]; then rm -fdr libraries/boost_1_39_0; fi;
+if [ -d libraries/boost-build ]; then rm -fdr libraries/boost-build; fi;
+if [ -d libraries/gd-2.0.33 ]; then rm -fdr libraries/gd-2.0.33; fi;
+if [ -d libraries/zlib-1.2.3 ]; then rm -fdr libraries/zlib-1.2.3; fi;
+if [ -d libraries/fftw-3.1.2 ]; then rm -fdr libraries/fftw-3.1.2; fi;
+if [ -f libraries/libfftw3-3.def ]; then rm -f libraries/libfftw3-3.def; fi;
+if [ -f libraries/libfftw3-3.dll ]; then rm -f libraries/libfftw3-3.dll; fi;
+if [ -f pwiz/svnrev.hpp ]; then rm -f pwiz/svnrev.hpp; fi;
+if [ -f pwiz/data/msdata/svnrev.hpp ]; then rm -f pwiz/data/msdata/svnrev.hpp; fi;
+if [ -f pwiz/analysis/svnrev.hpp ]; then rm -f pwiz/analysis/svnrev.hpp; fi;
+if [ -f pwiz/utility/proteome/svnrev.hpp ]; then rm -f pwiz/utility/proteome/svnrev.hpp; fi;
+if [ -d pwiz/data/vendor_readers/Thermo/Reader_Thermo_Test.data ]; then rm -fdr pwiz/data/vendor_readers/Thermo/Reader_Thermo_Test.data; fi;
+if [ -d pwiz/data/vendor_readers/Agilent/Reader_Agilent_Test.data ]; then rm -fdr pwiz/data/vendor_readers/Agilent/Reader_Agilent_Test.data; fi;
+if [ -d pwiz_aux/msrc/data/vendor_readers/ABI/Reader_ABI_Test.data ]; then rm -fdr pwiz_aux/msrc/data/vendor_readers/ABI/Reader_ABI_Test.data; fi;
+if [ -d pwiz_aux/msrc/data/vendor_readers/Waters/Reader_Waters_Test.data ]; then rm -fdr pwiz_aux/msrc/data/vendor_readers/Waters/Reader_Waters_Test.data; fi;
+if [ -d pwiz_aux/msrc/data/vendor_readers/Bruker/Reader_Bruker_Test.data ]; then rm -fdr pwiz_aux/msrc/data/vendor_readers/Bruker/Reader_Bruker_Test.data; fi;
 
 popd > /dev/null
