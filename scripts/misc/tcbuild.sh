79a69615a4f752f5b3dd4fb826b5c4b92b67a34f
dstearns <dstearns@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2009-09-02 18:23:35 +0000

    build script wrapper for team city

---
+++ scripts/misc/tcbuild.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function echo_info()
+{
+    echo "##teamcity[message text='$*']"
+    echo "##teamcity[progressMessage '$*']"
+}
+
+function echo_error()
+{
+    echo "##teamcity[message text='$*' status='ERROR']"
+    exit 1
+}
+
+echo_info "Cleaning project..."
+if ! sh clean.sh
+then
+	echo_error "Error cleaning project!"
+fi
+
+echo_info "Running quickbuild.sh..."
+if ! sh quickbuild.sh $1
+then
+	echo "Error running quickbuild!"
+	echo_error "Error running quickbuild! See full build log for more details."
+fi
+
+exit 0
