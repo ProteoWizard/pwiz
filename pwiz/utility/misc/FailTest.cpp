161c7f5e3d446187305be93282afd0d94c6be70d
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2009-09-05 03:31:23 +0000

    - added a simple test to check that building with <ci>teamcity talks correctly to the TC agent when a test fails

---
+++ pwiz/utility/misc/FailTest.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cerr << "This is a test of the emergency test failure system." << endl;
+    cout << "DON'T PANIC!" << endl;
+    cerr << "This is only a test." << endl;
+    return 42;
+}
