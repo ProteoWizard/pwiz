8389ae10bfae0c37adb232ba6824d6a7713eab36
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2009-09-08 16:33:04 +0000

    - made FailTest explicit and moved it to a separate quickbuild invocation which can be easily commented out

--- scripts/misc/tcbuild.bat
+++ scripts/misc/tcbuild.bat
@@ -15,17 +15,22 @@ echo ##teamcity[progressMessage 'Cleaning project...']
 call clean.bat
 if %ERRORLEVEL% NEQ 0 set ERROR_TEXT=Error performing clean & goto error
 
+REM # the -p1 argument overrides bjam's default behavior of merging stderr into stdout
+
 REM # call quickbuild to generate SVN revision info
 echo ##teamcity[message text='Generating revision info...']
 echo ##teamcity[progressMessage 'Generating revision info...']
-call quickbuild.bat -j4 pwiz//svnrev.hpp
+call quickbuild.bat -j4 -p1 pwiz//svnrev.hpp
 if %ERRORLEVEL% NEQ 0 set ERROR_TEXT=Error generating revision info & goto error
 
 REM # call quickbuild to build and run tests
 echo ##teamcity[message text='Running quickbuild...']
 echo ##teamcity[progressMessage 'Running quickbuild...']
-call quickbuild.bat -j4 ci=teamcity
+call quickbuild.bat -j4 -p1 ci=teamcity
 if %ERRORLEVEL% NEQ 0 set ERROR_TEXT=Error performing quickbuild & goto error
+
+REM # uncomment this to test that test failures and error output are handled properly
+call quickbuild.bat -p1 ci=teamcity pwiz/utility/misc//FailTest
 
 popd
 goto :EOF
--- scripts/misc/tcbuild.sh
+++ scripts/misc/tcbuild.sh
@@ -18,18 +18,23 @@ then
 	echo_error "Error cleaning project!"
 fi
 
+# the -p1 argument overrides bjam's default behavior of merging stderr into stdout
+
 echo_info "Generating revision info..."
-if ! sh quickbuild.sh $1 pwiz//svnrev.hpp
+if ! /bin/bash quickbuild.sh $1 -p1 pwiz//svnrev.hpp
 then
 	echo "Error generating revision info!"
 	echo_error "Error generating revision info! See full build log for more details."
 fi
 
 echo_info "Running quickbuild.sh..."
-if ! /bin/bash quickbuild.sh $1 ci=teamcity -j4
+if ! /bin/bash quickbuild.sh $1 -p1 ci=teamcity -j4
 then
 	echo "Error running quickbuild!"
 	echo_error "Error running quickbuild! See full build log for more details."
 fi
+
+# uncomment this to test that test failures and error output are handled properly
+/bin/bash quickbuild.sh $1 -p1 ci=teamcity pwiz/utility/misc//FailTest
 
 exit 0
