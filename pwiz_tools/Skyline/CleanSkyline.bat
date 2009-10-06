9e9bcbdbb91d211f3d83bcf41c146a4ac3dd108f
brendanx <brendanx@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2009-10-06 21:39:08 +0000

    Build updates to include Skyline in the mainline ProteoWizard full build.

--- pwiz_tools/Skyline/CleanSkyline.bat
+++ pwiz_tools/Skyline/CleanSkyline.bat
@@ -10,9 +10,9 @@ pushd %PWIZ_ROOT%
 IF EXIST bin rmdir /s /q bin
 IF EXIST obj rmdir /s /q obj
 IF EXIST Test\bin rmdir /s /q Test\bin
-IF EXIST Test\bin rmdir /s /q Test\obj
+IF EXIST Test\obj rmdir /s /q Test\obj
 IF EXIST TestFunctional\bin rmdir /s /q TestFunctional\bin
-IF EXIST TestFunctional\bin rmdir /s /q TestFunctional\obj
+IF EXIST TestFunctional\obj rmdir /s /q TestFunctional\obj
 IF EXIST TestResults rmdir /s /q TestResults
 IF EXIST ..\Shared\ProteomeDb\bin rmdir /s /q ..\Shared\ProteomeDb\bin
 IF EXIST ..\Shared\ProteomeDb\obj rmdir /s /q ..\Shared\ProteomeDb\obj
@@ -20,7 +20,6 @@ IF EXIST ..\Shared\ProteowizardWrapper\bin rmdir /s /q ..\Shared\ProteowizardWra
 IF EXIST ..\Shared\ProteowizardWrapper\obj rmdir /s /q ..\Shared\ProteowizardWrapper\obj
 IF EXIST ..\Shared\MSGraph\bin rmdir /s /q ..\Shared\MSGraph\bin
 IF EXIST ..\Shared\MSGraph\obj rmdir /s /q ..\Shared\MSGraph\obj
-IF EXIST ..\Shared\Crawdad\Debug rmdir /s /q ..\Shared\Crawdad\Debug
-IF EXIST ..\Shared\Crawdad\Release rmdir /s /q ..\Shared\Crawdad\Release
+IF EXIST ..\Shared\Crawdad\obj rmdir /s /q ..\Shared\Crawdad\obj
 
 popd
