28ae87708a1eb3976b08b335ac9e420886919752
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2009-10-30 00:49:33 +0000

    - refactored automatic versioning system to auto-generate AssemblyInfo (which means that the executable has detailed version/product info available in the OS) - added svn:keyword properties to all user-created .cs files (not sure what to do with the .Designer.cs files) - added license headers to all user-created .cs files (not sure what to do with the .Designer.cs files)

--- pwiz_tools/SeeMS/Dialogs/AboutForm.Designer.cs
+++ pwiz_tools/SeeMS/Dialogs/AboutForm.Designer.cs
@@ -30,7 +30,7 @@ namespace seems
         {
             this.componentListView = new System.Windows.Forms.ListView();
             this.Component = new System.Windows.Forms.ColumnHeader();
-            this.Version = new System.Windows.Forms.ColumnHeader();
+            this.VersionColumn = new System.Windows.Forms.ColumnHeader();
             this.aboutTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
@@ -40,14 +40,14 @@ namespace seems
                         | System.Windows.Forms.AnchorStyles.Right ) ) );
             this.componentListView.Columns.AddRange( new System.Windows.Forms.ColumnHeader[] {
             this.Component,
-            this.Version} );
+            this.VersionColumn} );
             this.componentListView.FullRowSelect = true;
             this.componentListView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
             this.componentListView.LabelWrap = false;
             this.componentListView.Location = new System.Drawing.Point( 12, 117 );
             this.componentListView.Name = "componentListView";
             this.componentListView.Scrollable = false;
-            this.componentListView.Size = new System.Drawing.Size( 262, 108 );
+            this.componentListView.Size = new System.Drawing.Size( 262, 136 );
             this.componentListView.TabIndex = 0;
             this.componentListView.UseCompatibleStateImageBehavior = false;
             this.componentListView.View = System.Windows.Forms.View.Details;
@@ -57,10 +57,10 @@ namespace seems
             this.Component.Text = "Component";
             this.Component.Width = 121;
             // 
-            // Version
+            // VersionColumn
             // 
-            this.Version.Text = "Version";
-            this.Version.Width = 136;
+            this.VersionColumn.Text = "Version";
+            this.VersionColumn.Width = 136;
             // 
             // aboutTextBox
             // 
@@ -73,15 +73,15 @@ namespace seems
             this.aboutTextBox.Name = "aboutTextBox";
             this.aboutTextBox.Size = new System.Drawing.Size( 261, 97 );
             this.aboutTextBox.TabIndex = 1;
-            this.aboutTextBox.Text = "SeeMS <<version>>\r\n© 2008 Vanderbilt University\r\n\r\nAuthor: Matt Chambers\r\n\r\nThank" +
-                "s to: David Tabb, the ProteoWizard Team,\r\n                  Digital Rune, Joe Wo" +
-                "odbury";
+            this.aboutTextBox.Text = "SeeMS <<version>>\r\n© <<date>> Vanderbilt University\r\n\r\nAuthor: Matt Chambers\r\n\r\nT" +
+                "hanks to: David Tabb, the ProteoWizard Team,\r\n                  Digital Rune, Jo" +
+                "e Woodbury";
             // 
             // AboutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 13F );
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size( 286, 237 );
+            this.ClientSize = new System.Drawing.Size( 286, 265 );
             this.Controls.Add( this.aboutTextBox );
             this.Controls.Add( this.componentListView );
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -102,7 +102,7 @@ namespace seems
 
         private System.Windows.Forms.ListView componentListView;
         private System.Windows.Forms.ColumnHeader Component;
-        private System.Windows.Forms.ColumnHeader Version;
+        private System.Windows.Forms.ColumnHeader VersionColumn;
         private System.Windows.Forms.TextBox aboutTextBox;
 
 
--- pwiz_tools/SeeMS/Dialogs/AboutForm.cs
+++ pwiz_tools/SeeMS/Dialogs/AboutForm.cs
@@ -1,3 +1,24 @@
+//
+// $Id$
+//
+//
+// Original author: Matt Chambers <matt.chambers .@. vanderbilt.edu>
+//
+// Copyright 2009 Vanderbilt University - Nashville, TN 37232
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+//
+
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -6,6 +27,7 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.Reflection;
 using System.IO;
 using System.Text.RegularExpressions;
 
@@ -18,24 +40,16 @@ namespace seems
             InitializeComponent();
 
             aboutTextBox.Text = aboutTextBox.Text.Replace( "<<version>>",
-                String.Format( "{0} ({1})", seemsForm.Version, seemsForm.LastModified ) );
+                String.Format( "{0} ({1})", Version, LastModified.ToShortDateString() ) );
+            aboutTextBox.Text = aboutTextBox.Text.Replace( "<<date>>", LastModified.Year.ToString() );
 
-            foreach( System.Reflection.Assembly a in AppDomain.CurrentDomain.GetAssemblies() )
-            {
-                if( a.FullName.Contains( "DigitalRune" ) )
-                {
-                    Match versionMatch = Regex.Match( a.ToString(), @"Version=([\d.]+)" );
-                    string version = versionMatch.Groups[1].Success ? versionMatch.Groups[1].Value : "unknown";
-                    componentListView.Items.Add( "DR Docking Windows" ).SubItems.Add(
-                        String.Format( "{0} ({1})", version, File.GetLastWriteTime( a.Location ).ToShortDateString() ) );
-                } else if( a.FullName.Contains( "ZedGraph" ) )
-                {
-                    Match versionMatch = Regex.Match( a.ToString(), @"Version=([\d.]+)" );
-                    string version = versionMatch.Groups[1].Success ? versionMatch.Groups[1].Value : "unknown";
-                    componentListView.Items.Add( "ZedGraph" ).SubItems.Add(
-                     String.Format( "{0} ({1})", version, File.GetLastWriteTime( a.Location ).ToShortDateString() ) );
-                }
-            }
+                componentListView.Items.Add( "DR Docking Windows" ).SubItems.Add(
+                    String.Format( "{0} ({1})", GetAssemblyVersion(GetAssemblyByName("DigitalRune")),
+                                                GetAssemblyLastModified(GetAssemblyByName("DigitalRune")).ToShortDateString() ) );
+
+            componentListView.Items.Add( "ZedGraph" ).SubItems.Add(
+                    String.Format( "{0} ({1})", GetAssemblyVersion(GetAssemblyByName("ZedGraph")),
+                                                GetAssemblyLastModified(GetAssemblyByName("ZedGraph")).ToShortDateString() ) );
 
             componentListView.Items.Add( "ProteoWizard MSData" ).SubItems.Add(
                 String.Format( "{0} ({1})", pwiz.CLI.msdata.Version.ToString(), pwiz.CLI.msdata.Version.LastModified() ) );
@@ -43,6 +57,33 @@ namespace seems
                 String.Format( "{0} ({1})", pwiz.CLI.analysis.Version.ToString(), pwiz.CLI.analysis.Version.LastModified() ) );
             componentListView.Items.Add( "ProteoWizard Proteome" ).SubItems.Add(
                 String.Format( "{0} ({1})", pwiz.CLI.proteome.Version.ToString(), pwiz.CLI.proteome.Version.LastModified() ) );
+        }
+
+        public static string Version { get { return GetAssemblyVersion( Assembly.GetExecutingAssembly().GetName() ); } }
+        public static DateTime LastModified { get { return GetAssemblyLastModified( Assembly.GetExecutingAssembly().GetName() ); } }
+
+        public static AssemblyName GetAssemblyByName( string assemblyName )
+        {
+            if( Assembly.GetCallingAssembly().GetName().FullName.Contains( assemblyName ) )
+                return Assembly.GetCallingAssembly().GetName();
+
+            foreach( AssemblyName a in Assembly.GetCallingAssembly().GetReferencedAssemblies() )
+            {
+                if( a.FullName.Contains( assemblyName ) )
+                    return a;
+            }
+            return null;
+        }
+
+        public static string GetAssemblyVersion( AssemblyName assembly )
+        {
+            Match versionMatch = Regex.Match( assembly.ToString(), @"Version=([\d.]+)" );
+            return versionMatch.Groups[1].Success ? versionMatch.Groups[1].Value : "unknown";
+        }
+
+        public static DateTime GetAssemblyLastModified( AssemblyName assembly )
+        {
+            return File.GetLastWriteTime( Assembly.ReflectionOnlyLoad( assembly.FullName ).Location );
         }
     }
 }
