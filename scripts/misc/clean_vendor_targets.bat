c503e170196af4cdae665d44b018f3058f4b3e8e
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2009-11-23 17:59:12 +0000

    - fixed path to build directory

--- scripts/misc/clean_vendor_targets.bat
+++ scripts/misc/clean_vendor_targets.bat
@@ -7,17 +7,17 @@ set PWIZ_ROOT=%~dp0..\..
 pushd %PWIZ_ROOT%
 
 echo Cleaning project...
-IF EXIST build\msvc-release rmdir /s /q build\msvc-release
-IF EXIST build\msvc-debug rmdir /s /q build\msvc-debug
-IF EXIST build\gcc-release rmdir /s /q build\gcc-release
-IF EXIST build\gcc-debug rmdir /s /q build\gcc-debug
-IF EXIST build\pwiz\data\vendor_readers rmdir /s /q build\pwiz\data\vendor_readers
-IF EXIST build\pwiz\utility\vendor_api rmdir /s /q build\pwiz\utility\vendor_api
-IF EXIST build\pwiz\utility\bindings rmdir /s /q build\pwiz\utility\bindings
-IF EXIST build\pwiz_aux\msrc\data\vendor_readers rmdir /s /q build\pwiz_aux\msrc\data\vendor_readers
-IF EXIST build\pwiz_aux\msrc\utility\vendor_api rmdir /s /q build\pwiz_aux\msrc\utility\vendor_api
-IF EXIST build\pwiz\analysis\spectrum_processing rmdir /s /q build\pwiz\analysis\spectrum_processing
-IF EXIST build\pwiz\analysis\chromatogram_processing rmdir /s /q build\pwiz\analysis\chromatogram_processing
+IF EXIST build-nt-x86\msvc-release rmdir /s /q build-nt-x86\msvc-release
+IF EXIST build-nt-x86\msvc-debug rmdir /s /q build-nt-x86\msvc-debug
+IF EXIST build-nt-x86\gcc-release rmdir /s /q build-nt-x86\gcc-release
+IF EXIST build-nt-x86\gcc-debug rmdir /s /q build-nt-x86\gcc-debug
+IF EXIST build-nt-x86\pwiz\data\vendor_readers rmdir /s /q build-nt-x86\pwiz\data\vendor_readers
+IF EXIST build-nt-x86\pwiz\utility\vendor_api rmdir /s /q build-nt-x86\pwiz\utility\vendor_api
+IF EXIST build-nt-x86\pwiz\utility\bindings rmdir /s /q build-nt-x86\pwiz\utility\bindings
+IF EXIST build-nt-x86\pwiz_aux\msrc\data\vendor_readers rmdir /s /q build-nt-x86\pwiz_aux\msrc\data\vendor_readers
+IF EXIST build-nt-x86\pwiz_aux\msrc\utility\vendor_api rmdir /s /q build-nt-x86\pwiz_aux\msrc\utility\vendor_api
+IF EXIST build-nt-x86\pwiz\analysis\spectrum_processing rmdir /s /q build-nt-x86\pwiz\analysis\spectrum_processing
+IF EXIST build-nt-x86\pwiz\analysis\chromatogram_processing rmdir /s /q build-nt-x86\pwiz\analysis\chromatogram_processing
 IF EXIST pwiz\data\vendor_readers\Thermo\Reader_Thermo_Test.data rmdir /s /q pwiz\data\vendor_readers\Thermo\Reader_Thermo_Test.data
 IF EXIST pwiz\data\vendor_readers\Agilent\Reader_Agilent_Test.data rmdir /s /q pwiz\data\vendor_readers\Agilent\Reader_Agilent_Test.data
 IF EXIST pwiz_aux\msrc\data\vendor_readers\ABI\Reader_ABI_Test.data rmdir /s /q pwiz_aux\msrc\data\vendor_readers\ABI\Reader_ABI_Test.data
