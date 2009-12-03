491fda71d6a86843d8e9f540ef7ede86c9dce817
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2009-12-03 16:48:59 +0000

    - fixed bug with setting release/debug error level

--- scripts/misc/tcbuild.bat
+++ scripts/misc/tcbuild.bat
@@ -7,11 +7,14 @@ set SCRIPTS_MISC_ROOT=%~dp0
 set SCRIPTS_MISC_ROOT=%SCRIPTS_MISC_ROOT:~0,-1%
 pushd %SCRIPTS_MISC_ROOT%\..\..
 
+set EXIT=0
+
 REM # call clean
 echo ##teamcity[message text='Cleaning project...']
 echo ##teamcity[progressMessage 'Cleaning project...']
 call clean.bat
-if %ERRORLEVEL% NEQ 0 set ERROR_TEXT=Error performing clean & goto error
+set EXIT=%ERRORLEVEL%
+if %EXIT% NEQ 0 set ERROR_TEXT=Error performing clean & goto error
 
 REM # the -p1 argument overrides bjam's default behavior of merging stderr into stdout
 
@@ -19,18 +22,20 @@ REM # call quickbuild to build and run tests with variant=release
 echo ##teamcity[message text='Running quickbuild for release variant...']
 echo ##teamcity[progressMessage 'Running quickbuild for release variant...']
 call quickbuild.bat -j4 -p1 --teamcity-test-decoration release
-set EXIT=%ERRORLEVEL%
-if %EXIT% NEQ 0 echo ##teamcity[message text='Error running quickbuild for release variant! See full build log for more details.' status='ERROR']
+set RELEASE_ERRORLEVEL=%ERRORLEVEL%
+if %EXIT% EQU 0 set EXIT=%RELEASE_ERRORLEVEL%
+if %RELEASE_ERRORLEVEL% NEQ 0 echo ##teamcity[message text='Error running quickbuild for release variant! See full build log for more details.' status='ERROR']
 
 REM # call quickbuild to build and run tests with variant=debug
 echo ##teamcity[message text='Running quickbuild for debug variant...']
 echo ##teamcity[progressMessage 'Running quickbuild for debug variant...']
 call quickbuild.bat -j4 -p1 --teamcity-test-decoration debug
-if %EXIT% EQU 0 set EXIT=%ERRORLEVEL%
-if %EXIT% NEQ 0 echo ##teamcity[message text='Error running quickbuild for debug variant! See full build log for more details.' status='ERROR']
+set DEBUG_ERRORLEVEL=%ERRORLEVEL%
+if %EXIT% EQU 0 set EXIT=%DEBUG_ERRORLEVEL%
+if %DEBUG_ERRORLEVEL% NEQ 0 echo ##teamcity[message text='Error running quickbuild for debug variant! See full build log for more details.' status='ERROR']
 
 REM # uncomment this to test that test failures and error output are handled properly
-REM call quickbuild.bat -p1 ci=teamcity pwiz/utility/misc//FailUnitTest pwiz/utility/misc//FailRunTest
+REM call quickbuild.bat -p1 --teamcity-test-decoration pwiz/utility/misc//FailUnitTest pwiz/utility/misc//FailRunTest
 
 popd
 
