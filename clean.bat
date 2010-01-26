ecd659f269af0138382a8c8b03941040f6ae5839
brendanx <brendanx@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2010-01-26 23:59:50 +0000

    Fix copy-paste error.

--- clean.bat
+++ clean.bat
@@ -38,6 +38,6 @@ IF EXIST pwiz_aux\msrc\data\vendor_readers\Bruker\Reader_Bruker_Test.data rmdir 
 
 call pwiz_tools\SeeMS\CleanSeeMS.bat
 call pwiz_tools\Skyline\CleanSkyline.bat
-call pwiz_tools\Skyline\CleanTopograph.bat
+call pwiz_tools\Topograph\CleanTopograph.bat
 
 popd
