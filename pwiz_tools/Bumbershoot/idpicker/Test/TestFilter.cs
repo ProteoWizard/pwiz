272514a8a3120251f7131c42f12ce6d0ec863126
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2010-07-21 16:25:31 +0000

    - added license headers and svn:keyword properties
    Former-commit-id: 2779b30e4bb0ec303658c305c52f03bc7e259dd5

--- pwiz_tools/Bumbershoot/idpicker/Test/TestFilter.cs
+++ pwiz_tools/Bumbershoot/idpicker/Test/TestFilter.cs
@@ -1,4 +1,26 @@
-﻿using System;
+﻿//
+// $Id$
+//
+// The contents of this file are subject to the Mozilla Public License
+// Version 1.1 (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+// http://www.mozilla.org/MPL/
+//
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+// License for the specific language governing rights and limitations
+// under the License.
+//
+// The Original Code is the IDPicker project.
+//
+// The Initial Developer of the Original Code is Matt Chambers.
+//
+// Copyright 2010 Vanderbilt University
+//
+// Contributor(s): Surendra Dasari
+//
+
+using System;
 using System.Text;
 using System.Collections.Generic;
 using System.Linq;
@@ -508,7 +530,7 @@ namespace Test
             // clear session so objects are loaded from database
             session.Clear();
 
-            Map<long, long> additionalPeptidesByProteinId = dataFilter.calculateAdditionalPeptides(session);
+            Map<long, long> additionalPeptidesByProteinId = DataFilter_Accessor.CalculateAdditionalPeptides(session);
 
             // 1 protein to 1 peptide to 1 spectrum = 1 additional peptide
             Assert.AreEqual(1, additionalPeptidesByProteinId[session.UniqueResult<Protein>(o => o.Accession == "PRO1").Id.GetValueOrDefault()]);
