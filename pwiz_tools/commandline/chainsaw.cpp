9d4b54e762299ddc4803a3dfda402bcfd4c74796
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2010-09-01 17:14:44 +0000

    - added --proteinSummary for creating a protein summary table - moved modal behavior to separate functions - stopped usage being printed when filename arguments don't exist

--- pwiz_tools/commandline/chainsaw.cpp
+++ pwiz_tools/commandline/chainsaw.cpp
@@ -29,13 +29,15 @@ struct Config
     size_t precision;
     bool benchmark;
     bool indexOnly;
+    bool proteinSummary;
 
     Config()
         :   proteolyticEnzyme(ProteolyticEnzyme_Trypsin),
             digestionConfig(0,0,100000),
             precision(12),
             benchmark(false),
-            indexOnly(false)
+            indexOnly(false),
+            proteinSummary(false)
     {}
 
 };
@@ -81,12 +83,13 @@ Config parseCommandLine(int argc, const char* argv[])
         ("digestionMotif,d", po::value<string>(&config.digestionMotif), " : specify a digestion motif (e.g. trypsin = \"[KR]|[^P]\")")
         ("cleavageAgentRegex,r", po::value<string>(&config.cleavageAgentRegex), " : specify a cleavage agent regex (e.g. trypsin = \"(?<=[KR])(?!P)\")")
         ("numMissedCleavages,n", po::value<int>(&config.digestionConfig.maximumMissedCleavages)->default_value(config.digestionConfig.maximumMissedCleavages), " : specify number of missed cleavages to allow.")
-        ("specificity,s", po::value<string>(&tempSpecificity)," : specify minimum specificity. Options: none, semi, fully. \nDefault: fully")
+        ("specificity,s", po::value<string>(&tempSpecificity), " : specify minimum specificity. Options: none, semi, fully. \nDefault: fully")
         ("minLength,m",po::value<int>(&config.digestionConfig.minimumLength)->default_value(config.digestionConfig.minimumLength), " : specify minimum length of digested peptides")
         ("maxLength,M",po::value<int>(&config.digestionConfig.maximumLength)->default_value(config.digestionConfig.maximumLength), " : specify maximum length of digested peptides")
-        ("massPrecison,p", po::value<size_t>(&config.precision)->default_value(config.precision)," : specify precision of calculated mass of digested peptides")
-        ("benchmark", po::value<bool>(&config.benchmark)->zero_tokens()," : do not write results")
-        ("indexOnly", po::value<bool>(&config.indexOnly)->zero_tokens()," : create database index (if necessary)");
+        ("massPrecison,p", po::value<size_t>(&config.precision)->default_value(config.precision), " : specify precision of calculated mass of digested peptides")
+        ("benchmark", po::value<bool>(&config.benchmark)->zero_tokens(), " : do not write results")
+        ("indexOnly", po::value<bool>(&config.indexOnly)->zero_tokens(), " : create database index (if necessary)")
+        ("proteinSummary", po::value<bool>(&config.proteinSummary)->zero_tokens(), " : print a table with index, id, MW, and description for each protein");
     
     
     // append options to usage string
@@ -104,6 +107,9 @@ Config parseCommandLine(int argc, const char* argv[])
           << endl
           << "# create an index file for database.fasta\n"
           << "chainsaw --indexOnly database.fasta\n"
+          << endl
+          << "# create a summary table for database.fasta\n"
+          << "chainsaw --proteinSummary database.fasta\n"
           << endl
           << endl
 
@@ -143,14 +149,16 @@ Config parseCommandLine(int argc, const char* argv[])
         {
             expand_pathmask(bfs::path(filename), globbedFilenames);
             if (!globbedFilenames.size())
-            {
-                 cout <<  "[chainsaw] no files found matching \"" << filename << "\"";
-            }
+                 cout <<  "[chainsaw] no files found matching \"" << filename << "\"" << endl;
         }
 
         config.filenames.clear();
         BOOST_FOREACH(const bfs::path& filename, globbedFilenames)
             config.filenames.push_back(filename.string());
+
+        // skip usage if user passed some files but none existed
+        if (config.filenames.empty())
+            throw exception("");
     }
 
     // usage if incorrect
@@ -165,27 +173,145 @@ Config parseCommandLine(int argc, const char* argv[])
 
 }
 
+void writeSummary(const Config& config, const ProteomeData& pd)
+{
+    ofstream ofs;
+    if (!config.benchmark)
+    {
+        ofs.open((pd.id + "_summary.tsv").c_str());
+        ofs << "index" 
+            << "\t" << "id" 
+            << "\t" << "length" 
+            << "\t" << "MW"
+            << "\t" << "description" 
+            << "\n";
+
+        ofs.precision(config.precision);
+    }
+
+    const ProteinList& pl = *pd.proteinListPtr;
+    cout << "Summarizing " << pl.size() << " proteins..." << endl;
+    bpt::ptime start = bpt::microsec_clock::local_time();
+    for(size_t index = 0, end=pl.size(); index < end; ++index)
+    {
+        if (index > 0 && (index % 10) == 0)
+        {
+            bpt::ptime stop = bpt::microsec_clock::local_time();
+            bpt::time_duration duration = stop - start;
+            double perSecond = index / (double) duration.total_milliseconds() * 1000;
+            cout << std::fixed << setprecision(0) << index << " (" << perSecond << " per second)\r" << flush;
+        }
+
+        string id;
+        try
+        {
+            // get protein with sequence for the length column
+            ProteinPtr proteinPtr = pl.protein(index, true);
+            id = proteinPtr->id;
+
+            // skip output if benchmarking
+            if (config.benchmark)
+                continue;
+
+            ofs << index
+                << "\t" << id
+                << "\t" << proteinPtr->sequence().length()
+                << "\t" << proteinPtr->molecularWeight()
+                << "\t" << proteinPtr->description
+                << "\n";
+        }
+        catch (runtime_error& e)
+        {
+            cerr << "Error summarizing protein " << index << " (" << id << "): " << e.what() << endl;
+        }
+    }
+    bpt::ptime stop = bpt::microsec_clock::local_time();
+    bpt::time_duration duration = stop - start;
+    cout << "Summary finished. Time elapsed: " << bpt::to_simple_string(duration) << endl;
+}
+
+void writeDigestion(const Config& config, const ProteomeData& pd)
+{
+    ofstream ofs;
+    if (!config.benchmark)
+    {
+        ofs.open((pd.id + "_digestedPeptides.tsv").c_str());
+        ofs << "sequence" 
+            << "\t" << "protein" 
+            << "\t" << "mass" 
+            << "\t" << "missedCleavages" 
+            << "\t" << "specificity"
+            << "\t" << "nTerminusIsSpecific"
+            << "\t" << "cTerminusIsSpecific"
+            << "\n";
+
+        ofs.precision(config.precision);
+    }
+
+    const ProteinList& pl = *pd.proteinListPtr;
+    cout << "Digesting " << pl.size() << " proteins..." << endl;
+    bpt::ptime start = bpt::microsec_clock::local_time();
+    for(size_t index = 0, end=pl.size(); index < end; ++index)
+    {
+        if (index > 0 && (index % 10) == 0)
+        {
+            bpt::ptime stop = bpt::microsec_clock::local_time();
+            bpt::time_duration duration = stop - start;
+            double perSecond = index / (double) duration.total_milliseconds() * 1000;
+            cout << std::fixed << setprecision(0) << index << " (" << perSecond << " per second)\r" << flush;
+        }
+
+        string id;
+        try
+        {
+            // digest
+            ProteinPtr proteinPtr = pl.protein(index, true);
+            id = proteinPtr->id;
+
+            shared_ptr<Digestion> digestion;
+            if (!config.cleavageAgentRegex.empty())
+                digestion.reset(new Digestion(*proteinPtr, boost::regex(config.cleavageAgentRegex), config.digestionConfig));
+            else if (!config.digestionMotif.empty())
+                digestion.reset(new Digestion(*proteinPtr, config.digestionMotif, config.digestionConfig));
+            else
+                digestion.reset(new Digestion(*proteinPtr, config.proteolyticEnzyme, config.digestionConfig));
+
+            // iterate through digested peptides (and, if not benchmarking, write output)
+            if (config.benchmark)
+            {
+                for (Digestion::const_iterator jt = digestion->begin(); jt != digestion->end(); ++jt)
+                {
+                    const DigestedPeptide& p = *jt; // instantiate the peptide
+                    volatile size_t offset = p.offset(); // prevent compiler optimizing the loop away
+                }
+                continue;
+            }
+
+            for (Digestion::const_iterator jt = digestion->begin(); jt != digestion->end(); ++jt)
+                ofs << jt->sequence() 
+                    << "\t" << proteinPtr->id
+                    << "\t" << jt->monoisotopicMass(0, false) /* unmodified neutral mass + h2o*/ 
+                    << "\t" << jt->missedCleavages() 
+                    << "\t" << jt->specificTermini() 
+                    << "\t" << jt->NTerminusIsSpecific() 
+                    << "\t" << jt->CTerminusIsSpecific() 
+                    << "\n";
+        }
+        catch (runtime_error& e)
+        {
+            cerr << "Error digesting protein " << index << " (" << id << "): " << e.what() << endl;
+        }
+    }
+    bpt::ptime stop = bpt::microsec_clock::local_time();
+    bpt::time_duration duration = stop - start;
+    cout << "Digestion finished. Time elapsed: " << bpt::to_simple_string(duration) << endl;
+}
+
 void go(const Config& config)
 {
     vector<string>::const_iterator file_it = config.filenames.begin();
     for( ; file_it != config.filenames.end(); ++file_it)
     {
-        ofstream ofs;
-        if (!config.benchmark && !config.indexOnly)
-        {
-            ofs.open((*file_it + "_digestedPeptides.txt").c_str());
-            ofs << "sequence" 
-                << "\t" << "protein" 
-                << "\t" << "mass" 
-                << "\t" << "missedCleavages" 
-                << "\t" << "specificity"
-                << "\t" << "nTerminusIsSpecific"
-                << "\t" << "cTerminusIsSpecific"
-                << "\n";
-
-            ofs.precision(config.precision);
-        }
-
         cout << "Reading database: " << *file_it << endl;
         ProteomeDataFile pd(*file_it, true);
         cout << "Finished reading database." << endl;
@@ -193,64 +319,15 @@ void go(const Config& config)
         if (config.indexOnly)
             continue;
 
-        const ProteinList& pl = *pd.proteinListPtr;
-        cout << "Digesting " << pl.size() << " proteins..." << endl;
-        bpt::ptime digestStart = bpt::microsec_clock::local_time();
-        for(size_t index = 0, end=pl.size(); index < end; ++index)
-        {
-            if (index > 0 && (index % 10) == 0)
-            {
-                bpt::ptime digestStop = bpt::microsec_clock::local_time();
-                bpt::time_duration digestDuration = digestStop - digestStart;
-                double digestedPerSecond = index / (double) digestDuration.total_seconds();
-                cout << std::fixed << setprecision(0) << index << " (" << digestedPerSecond << " per second)\r" << flush;
-            }
-
-            string id;
-            try
-            {
-                // digest
-                ProteinPtr proteinPtr = pl.protein(index, true);
-                id = proteinPtr->id;
-
-                shared_ptr<Digestion> digestion;
-                if (!config.cleavageAgentRegex.empty())
-                    digestion.reset(new Digestion(*proteinPtr, boost::regex(config.cleavageAgentRegex), config.digestionConfig));
-                else if (!config.digestionMotif.empty())
-                    digestion.reset(new Digestion(*proteinPtr, config.digestionMotif, config.digestionConfig));
-                else
-                    digestion.reset(new Digestion(*proteinPtr, config.proteolyticEnzyme, config.digestionConfig));
-
-                // iterate through digested peptides and output
-                vector<DigestedPeptide> digestedPeptides(digestion->begin(), digestion->end());
-                if (!config.benchmark)
-                {
-                    vector<DigestedPeptide>::iterator jt = digestedPeptides.begin();
-                    for(; jt!= digestedPeptides.end(); ++jt)                
-                        ofs << jt->sequence() 
-                            << "\t" << proteinPtr->id
-                            << "\t" << jt->monoisotopicMass(0, false) /* unmodified neutral mass + h2o*/ 
-                            << "\t" << jt->missedCleavages() 
-                            << "\t" << jt->specificTermini() 
-                            << "\t" << jt->NTerminusIsSpecific() 
-                            << "\t" << jt->CTerminusIsSpecific() 
-                            << "\n";
-                }
-            }
-            catch (runtime_error& e)
-            {
-                cerr << "Error digesting protein " << index << " (" << id << "): " << e.what() << endl;
-            }
-        }
-        bpt::ptime digestStop = bpt::microsec_clock::local_time();
-        bpt::time_duration digestDuration = digestStop - digestStart;
-        cout << "Digestion finished. Time elapsed: " << bpt::to_simple_string(digestDuration) << endl;
+        if (config.proteinSummary)
+            writeSummary(config, pd);
+        else
+            writeDigestion(config, pd);
     }
 }
 
 int main(int argc, const char* argv[])
 {
-    
     try
     {
         Config config = parseCommandLine(argc, argv);
@@ -258,13 +335,10 @@ int main(int argc, const char* argv[])
 
         return 0;
     }
-
     catch (exception& e)
     {
         cout << e.what() << endl;
     }
 
-    
     return 0;
-
 }
