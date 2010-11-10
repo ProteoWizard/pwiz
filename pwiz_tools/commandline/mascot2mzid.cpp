89a74b7332ed0fa85a7929740cade94cfc31a625
broter <broter@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2010-11-10 20:06:52 +0000

    Added better commandline param handling for mascot2mzid Updated usage message Added proteowizard version to usage
    Former-commit-id: 8600bf7f530da57ce4c4cc90d3ec704a04703378

--- pwiz_tools/commandline/mascot2mzid.cpp
+++ pwiz_tools/commandline/mascot2mzid.cpp
@@ -23,6 +23,7 @@
 #include "pwiz/data/mziddata/MzIdentMLFile.hpp"
 #include "pwiz/data/mziddata/MascotReader.hpp"
 #include "pwiz/data/mziddata/TextWriter.hpp"
+#include "pwiz/Version.hpp"
 
 #include <boost/program_options.hpp>
 #include <stdexcept>
@@ -36,7 +37,33 @@ struct Config
     string mascotfile;
     string mzidfile;
     string usageOptions;
+
+    vector<string> files;
 };
+
+string usage(const Config& config)
+{
+    ostringstream oss;
+    
+    oss << "Usage: mascot2mzid [-m] <mascot_filename> [-z] <mzid_filename>\n"
+        << "Mascot to mzIdentML - translates a mascot \"dat\" file into an mzid file.\n"
+        << "\n"
+        << "Options:\n" 
+        << "\n"
+        << config.usageOptions
+        << "\n";
+    
+    
+    oss << endl
+        << "Questions, comments, and bug reports:\n"
+        << "http://proteowizard.sourceforge.net\n"
+        << "support@proteowizard.org\n"
+        << "\n"
+        << "ProteoWizard release: " << pwiz::Version::str() << endl
+        << "Build date: " << __DATE__ << " " << __TIME__ << endl;
+
+    return oss.str();
+}
 
 Config processCommandline(int argc, const char** argv)
 {
@@ -58,25 +85,56 @@ Config processCommandline(int argc, const char** argv)
     temp << od_config;
     usageOptions = temp.str();
 
+    config.usageOptions = usageOptions;
+
     // parse command line
 
+    po::options_description od_file_args;
+
     const char* label_files = "files";
-    vector<string> args;
+    od_file_args.add_options()(label_files,
+                               po::value< vector<string> >(&config.files),
+                               "");
 
     po::positional_options_description pod_args;
     pod_args.add(label_files, -1);
 
     po::options_description od_parse;
-    od_parse.add(od_config);
+    od_parse.add(od_config).add(od_file_args);
 
     po::variables_map vm;
     char** argv_hack = (char**)argv;
     po::store(po::command_line_parser(argc, argv_hack).
               options(od_parse).positional(pod_args).run(), vm);
     po::notify(vm);
-
-    config.usageOptions = usageOptions;
-
+    
+    if (vm.count(label_files))
+    {
+        if (vm.count("mascot") && vm.count("mzid"))
+        {
+            ostringstream oss;
+            oss << "Too many input/output files\n";
+            oss << usage(config);
+            throw runtime_error(oss.str().c_str());
+        }
+        else if (vm.count("mascot") && !vm.count("mzid"))
+        {
+            config.mzidfile = vm[label_files].as< vector<string> >().at(0);
+        }
+        else if (!vm.count("mascot") && !vm.count("mzid") &&
+                 vm[label_files].as< vector<string> >().size())
+        {
+            config.mascotfile = vm[label_files].as< vector<string> >().at(0);
+            config.mzidfile = vm[label_files].as< vector<string> >().at(1);
+        }
+        else if (!vm.count("mascot") && vm.count("mzid"))
+        {
+            config.mascotfile = vm[label_files].as< vector<string> >().at(0);
+        }
+        else
+            throw runtime_error(usage(config).c_str());
+    }
+    
     return config;
 }
 
@@ -86,7 +144,7 @@ int main(int argc, const char**argv)
         Config config = processCommandline(argc, argv);
 
         if (config.mascotfile.empty() || config.mzidfile.empty())
-            throw invalid_argument(config.usageOptions);
+            throw invalid_argument(usage(config));
         
         ReaderPtr  mr(new MascotReader());
         MzIdentML mzid;
