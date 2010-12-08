cbc311064f707e3459d89f3504f7305238dac2ce
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2010-12-08 21:13:57 +0000

    - added hackish "Extracting dependency tarballs..." step to tcbuild.bat
    Former-commit-id: 0b0d2f58db4a27319bbb1c621196105e6d64a2e9

--- pwiz_tools/Bumbershoot/scanranker/tcbuild.bat
+++ pwiz_tools/Bumbershoot/scanranker/tcbuild.bat
@@ -2,28 +2,29 @@
 setlocal
 @echo off
 
-REM # Get to the pwiz root directory
+REM # Get to the root directory
 set FREICORE_ROOT=%~dp0
 set FREICORE_ROOT=%FREICORE_ROOT:~0,-1%
 pushd %FREICORE_ROOT%
 
 REM # call clean
-echo ##teamcity[message text='Cleaning project...']
 echo ##teamcity[progressMessage 'Cleaning project...']
 call clean.bat
 if %ERRORLEVEL% NEQ 0 set ERROR_TEXT=Error performing clean & goto error
 
 REM # the -p1 argument overrides bjam's default behavior of merging stderr into stdout
 
+REM # HACK: this will get the tarballs extracted so the parse-time globbing works properly
+echo ##teamcity[progressMessage 'Extracting dependency tarballs...']
+call quickbuild.bat -p1 --clean
+
 REM # call quickbuild to build and run tests with variant=release
-echo ##teamcity[message text='Running quickbuild for release variant...']
 echo ##teamcity[progressMessage 'Running quickbuild for release variant...']
 call quickbuild.bat %* -p1 --teamcity-test-decoration release
 set EXIT=%ERRORLEVEL%
 if %EXIT% NEQ 0 echo ##teamcity[message text='Error running quickbuild for release variant! See full build log for more details.' status='ERROR']
 
 REM # call quickbuild to build and run tests with variant=debug
-echo ##teamcity[message text='Running quickbuild for debug variant...']
 echo ##teamcity[progressMessage 'Running quickbuild for debug variant...']
 call quickbuild.bat %* -p1 --teamcity-test-decoration debug
 set EXIT=%ERRORLEVEL%
