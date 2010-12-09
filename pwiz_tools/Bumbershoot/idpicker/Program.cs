bf1850882ec09ce6a7f7fabc71330f2251f80a71
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2010-12-09 21:21:04 +0000

    - improved handling of unhandled exceptions (oxymoron mode!)
    Former-commit-id: 2c00184587a1b936c5c48705cc74d17e5ac2b9e1

--- pwiz_tools/Bumbershoot/idpicker/Program.cs
+++ pwiz_tools/Bumbershoot/idpicker/Program.cs
@@ -24,6 +24,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace IDPicker
 {
@@ -35,9 +36,55 @@ namespace IDPicker
         [STAThread]
         static void Main (string[] args)
         {
+
+            // Add the event handler for handling UI thread exceptions to the event.
+            Application.ThreadException += new ThreadExceptionEventHandler(UIThread_UnhandledException);
+
+            // Set the unhandled exception mode to force all Windows Forms errors to go through
+            // our handler.
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+
+            // Add the event handler for handling non-UI thread exceptions to the event. 
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
             Application.Run(new IDPickerForm(args));
+        }
+
+        private static void UIThread_UnhandledException (object sender, ThreadExceptionEventArgs e)
+        {
+            /*Process newSeems = new Process();
+            newSeems.StartInfo.FileName = Application.ExecutablePath;
+            if( MainForm.CurrentFilepath.Length > 0 )
+                newSeems.StartInfo.Arguments = "\"" + MainForm.CurrentFilepath + "\" " + MainForm.CurrentScanIndex;
+            newSeems.Start();
+            Process.GetCurrentProcess().Kill();*/
+
+            string message = e.Exception.ToString();
+            if (e.Exception.InnerException != null)
+                message += "\n\nAdditional information: " + e.Exception.InnerException.ToString();
+            MessageBox.Show(message,
+                            "Unhandled Exception",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1,
+                            0, false);
+        }
+
+        private static void CurrentDomain_UnhandledException (object sender, UnhandledExceptionEventArgs e)
+        {
+            /*Process newSeems = new Process();
+            newSeems.StartInfo.FileName = Application.ExecutablePath;
+            if( MainForm.CurrentGraphForm.CurrentSourceFilepath.Length > 0 )
+                newSeems.StartInfo.Arguments = "\"" + MainForm.CurrentGraphForm.CurrentSourceFilepath + "\" " + MainForm.CurrentGraphForm.CurrentGraphItemIndex;
+            newSeems.Start();
+            Process.GetCurrentProcess().Kill();*/
+
+            MessageBox.Show((e.ExceptionObject is Exception ? (e.ExceptionObject as Exception).Message : "Unknown error."),
+                            "Unhandled Exception",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1,
+                            0, false);
         }
     }
 }
