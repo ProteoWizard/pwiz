d1bcd33ee4eab2e2d1580192a56089782e6b00fa
zeqiangma <zeqiangma@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2010-12-23 03:57:48 +0000

    add two files to svn, fix IonMatcher
    Former-commit-id: 323f4eba12c4dd3a69817aa9168911028be8c5dd

---
+++ pwiz_tools/Bumbershoot/scanranker/GUI/IonMatcher/RunPepNovoAction.cs
@@ -0,0 +1,144 @@
+﻿//
+// $Id: RunDirecTagAction.cs 18 2010-12-06 19:05:26Z zeqiangma $
+//
+// The contents of this file are subject to the Mozilla Public License
+// Version 1.1 (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+// http://www.mozilla.org/MPL/
+//
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+// License for the specific language governing rights and limitations
+// under the License.
+//
+// The Initial Developer of the DirecTag peptide sequence tagger is Matt Chambers.
+// Contributor(s): Surendra Dasaris
+//
+// The Initial Developer of the ScanRanker GUI is Zeqiang Ma.
+// Contributor(s): 
+//
+// Copyright 2009 Vanderbilt University
+//
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace IonMatcher
+{
+    class RunPepNovoAction
+    {
+        private string inFile;
+        public string InFile
+        {
+            get { return inFile; }
+            set { inFile = value; }
+        }
+        //private string outputDir;
+        //public string OutputDir
+        //{
+        //    get { return outputDir; }
+        //    set { outputDir = value; }
+        //}
+        private string workingDir;
+        public string WorkingDir
+        {
+            get { return workingDir; }
+            set { workingDir = value; }
+        }
+        //pepnovo parameter
+        private float precursorTolerance;
+        public float PrecursorTolerance
+        {
+            get { return precursorTolerance; }
+            set { precursorTolerance = value; }
+        }
+        private float fragmentTolerance;
+        public float FragmentTolerance
+        {
+            get { return fragmentTolerance; }
+            set { fragmentTolerance = value; }
+        }
+        private string ptms;
+        public string PTMs
+        {
+            get { return ptms; }
+            set { ptms = value; }
+        }
+        private string useSpectrumCharge;
+        public string UseSpectrumCharge
+        {
+            get { return useSpectrumCharge; }
+            set { useSpectrumCharge = value; }
+        }
+        private string useSpectrumMz;
+        public string UseSpectrumMz
+        {
+            get { return useSpectrumMz; }
+            set { useSpectrumMz = value; }
+        }
+
+        private MainForm mainForm;
+        public RunPepNovoAction(MainForm mainForm)
+        {
+            this.mainForm = mainForm;
+        }
+
+        /// <summary>
+        /// run pepnovo
+        /// </summary
+        public void RunPepNovo()
+        {
+            //string fileBaseName = Path.GetFileNameWithoutExtension(file.FullName);
+            //string outMetricsFileName = fileBaseName + outMetricsSuffix + ".txt";
+            //string outHighQualSpecFileName = fileBaseName + outputFilenameSuffixForRemoval + "." + outputFormat;
+            //                string outLabelFileName = fileBaseName + outputFilenameSuffixForRecovery + ".txt";
+
+            if (!File.Exists(inFile))
+            {
+                MessageBox.Show("Error: Cannot find the spectrum file for de novo sequencing!");
+                return;
+            }
+
+            string EXE = @"PepNovo.exe";
+            string BIN_DIR = Path.GetDirectoryName(Application.ExecutablePath) + "\\" + "PepNovo";
+            string pathAndExeFile = BIN_DIR + "\\" + EXE;
+            string args = " -model CID_IT_TRYP " + " -model_dir " + BIN_DIR + "\\" + "Models"
+                + " -pm_tolerance " + precursorTolerance + " -fragment_tolerance " + fragmentTolerance
+                + " -no_quality_filter " + " -num_solutions 10 " + " -PTMs " + ptms
+                + useSpectrumCharge + useSpectrumMz + " -file " + " \"" + inFile + "\"";
+
+            MainForm.SetText(mainForm, "\r\nStart running PepNovo ...\r\n\r\n");
+            //MainForm.SetText(mainForm, "PepNovo command: " + pathAndExeFile + args + "\r\n\r\n");
+
+            try
+            {
+                Workspace workspace = new Workspace(mainForm);
+                workspace.RunProcess(pathAndExeFile, args, workingDir);
+            }
+            catch (Exception exc)
+            {
+                //Workspace.SetText("\r\nError in running PepNovo\r\n");
+                MainForm.SetText(mainForm, "\r\nError in running PepNovo\r\n");
+                throw new Exception(exc.Message);
+            }
+
+            //if (!File.Exists(outMetricsFile))
+            //{
+            //    MessageBox.Show("Error in running DirecTag, no metrics file generated");
+            //}
+
+            MainForm.SetText(mainForm, "\r\nFinished PepNovo! \r\n");
+            if (File.Exists(inFile))
+            {
+                File.Delete(inFile);
+                MainForm.SetText(mainForm, "Deleted " + inFile + "\r\n");
+            }
+            
+        }
+    }
+}
---
+++ pwiz_tools/Bumbershoot/scanranker/GUI/IonMatcher/Workspace.cs
@@ -0,0 +1,173 @@
+﻿//
+// $Id: Workspace.cs 29 2010-12-17 16:55:36Z zeqiangma $
+//
+// The contents of this file are subject to the Mozilla Public License
+// Version 1.1 (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+// http://www.mozilla.org/MPL/
+//
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+// License for the specific language governing rights and limitations
+// under the License.
+//
+// The Initial Developer of the DirecTag peptide sequence tagger is Matt Chambers.
+// Contributor(s): Surendra Dasaris
+//
+// The Initial Developer of the ScanRanker GUI is Zeqiang Ma.
+// Contributor(s): 
+//
+// Copyright 2009 Vanderbilt University
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.ComponentModel;
+//using System.Threading;
+
+namespace IonMatcher
+{
+    class Workspace
+    {
+        //public static TextBoxForm statusForm;
+
+        /// <summary>
+        /// send text to status form
+        /// </summary>
+        /// <param name="text"></param>
+ //       delegate void SetTextCallback(string text);
+ //       public static void SetText(string text)
+ //       {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            //if (statusForm.tbStatus.InvokeRequired)
+            //{
+            //    SetTextCallback d = new SetTextCallback(SetText);
+            //    statusForm.Invoke(d, new object[] { text });
+            //}
+            //else
+            //{
+            //    statusForm.tbStatus.AppendText(text);
+            //}
+//        }
+
+        private MainForm mainForm;
+        public Workspace(MainForm mainForm)
+        {
+            this.mainForm = mainForm;
+        }
+
+        /// <summary>
+        /// run a process and send standard output and standard error to status form
+        /// </summary>
+        /// <param name="pathAndExeFile"></param>
+        /// <param name="args"></param>
+        public void RunProcess(string pathAndExeFile, string args, string workingDir)
+        {
+            Process RunProc = new Process();
+            RunProc.EnableRaisingEvents = true;
+            RunProc.StartInfo.CreateNoWindow = true;
+            RunProc.StartInfo.WorkingDirectory = workingDir;
+            RunProc.StartInfo.UseShellExecute = false;
+            RunProc.StartInfo.RedirectStandardError = true;
+            RunProc.StartInfo.RedirectStandardOutput = true;
+
+            RunProc.ErrorDataReceived += new DataReceivedEventHandler(StdErrorHandler);
+            //RunProc.OutputDataReceived += new DataReceivedEventHandler( StdOutputHandler );
+            //RunProc.ErrorDataReceived += new DataReceivedEventHandler(StdErrorHandler);
+
+            RunProc.StartInfo.FileName = pathAndExeFile;
+            RunProc.StartInfo.Arguments = args;
+            
+            RunProc.Start();
+
+            //RunProc.BeginOutputReadLine();
+            RunProc.BeginErrorReadLine();
+
+            //string stdOutput = RunProc.StandardOutput.ReadToEnd();
+            //statusForm.tbStatus.AppendText(stdOutput);
+            //RunProc.WaitForExit();
+
+            StreamReader srOut = RunProc.StandardOutput;
+            //StreamReader srErr = RunProc.StandardError;
+            //while (!RunProc.HasExited)
+
+            string outLine = string.Empty;
+            //string errLine = string.Empty;
+            //while (((outLine = srOut.ReadLine()) != null) || ((errLine = srErr.ReadLine()) != null))
+            //while (((outLine = srOut.ReadLine()) != null) && (!RunProc.HasExited))
+
+            while ((outLine = srOut.ReadLine()) != null)
+            {
+                //SetText(outLine+"\r\n");
+                MainForm.SetText(mainForm, outLine + "\r\n");
+                //lock(statusForm.tbStatus)
+                //statusForm.tbStatus.AppendText(outLine + "\n");
+                // RunProc.WaitForExit(500);
+            }
+            //SetText("\r\nError/warning message from " + pathAndExeFile + " :\r\n");          
+            //string srErr = RunProc.StandardError.ReadToEnd();
+            //SetText(srErr.ReadToEnd().ToString()+"\r\n");
+            //SetText(srErr + "\r\n");
+            RunProc.WaitForExit();
+            RunProc.Close();
+
+        }
+
+        private void StdErrorHandler(object sendingProcess,
+            DataReceivedEventArgs errLine)
+        {
+            
+            //SetText(errLine.Data + "\r\n");
+            MainForm.SetText(mainForm, errLine.Data + "\r\n");
+
+            // Write the error text to the file if there is something
+            // to write and an error file has been specified.
+
+            //if (!String.IsNullOrEmpty(errLine.Data))
+            //{
+            //    if (!errorsWritten)
+            //    {
+            //        if (streamError == null)
+            //        {
+            //            // Open the file.
+            //            try
+            //            {
+            //                streamError = new StreamWriter(netErrorFile, true);
+            //            }
+            //            catch (Exception e)
+            //            {
+            //                Console.WriteLine("Could not open error file!");
+            //                Console.WriteLine(e.Message.ToString());
+            //            }
+            //        }
+
+            //        if (streamError != null)
+            //        {
+            //            // Write a header to the file if this is the first
+            //            // call to the error output handler.
+            //            streamError.WriteLine();
+            //            streamError.WriteLine(DateTime.Now.ToString());
+            //            streamError.WriteLine("Net View error output:");
+            //        }
+            //        errorsWritten = true;
+            //    }
+
+            //    if (streamError != null)
+            //    {
+            //        // Write redirected errors to the file.
+            //        streamError.WriteLine(errLine.Data);
+            //        streamError.Flush();
+            //    }
+            //}
+        }
+
+
+    }//workspace
+}
