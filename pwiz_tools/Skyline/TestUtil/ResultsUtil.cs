996e89fc3cba02573c3b7f5718a3f2720a60fd28
brendanx <brendanx@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2011-03-14 04:30:05 +0000

    Skyline: Fix for broken build.
    Former-commit-id: 3e1b90229ee2e5b2f36e6029e6cd18dcde080159

--- pwiz_tools/Skyline/TestUtil/ResultsUtil.cs
+++ pwiz_tools/Skyline/TestUtil/ResultsUtil.cs
@@ -18,6 +18,7 @@
  */
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using pwiz.Skyline.Model;
 using pwiz.Skyline.Model.Results;
