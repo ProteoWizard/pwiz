1e669233fd60c1599c247322bd335a05fff8cbe1
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2011-03-25 22:59:44 +0000

    - added CLI bindings for IterationListener (it's not as simple as I'd like, but I don't know how to simplify it)
    Former-commit-id: 9af70b858b96d15d5ec39bde7b8d9716ea322c38

---
+++ pwiz/utility/bindings/CLI/common/IterationListener.hpp
@@ -0,0 +1,79 @@
+//
+// $Id$
+//
+//
+// Original author: Matt Chambers <matt.chambers .@. vanderbilt.edu>
+//
+// Copyright 2011 Vanderbilt University - Nashville, TN 37232
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+//
+
+#ifndef _ITERATIONLISTENER_HPP_CLI_
+#define _ITERATIONLISTENER_HPP_CLI_
+
+#pragma warning( push )
+#pragma warning( disable : 4634 4635 )
+#include "SharedCLI.hpp"
+#include "pwiz/utility/misc/IterationListener.hpp"
+#pragma warning( pop )
+
+
+namespace pwiz {
+namespace CLI {
+namespace util {
+
+
+public ref struct IterationEventArgs : System::EventArgs
+{
+    property System::String^ Message;
+    property int IterationIndex;
+    property int IterationCount;
+    property bool Cancel;
+};
+
+public delegate void IterationEventHandler(System::Object^ sender, IterationEventArgs^ e);
+
+
+struct IterationListenerForwarder : public pwiz::util::IterationListener
+{
+    typedef void (__stdcall *IterationListenerCallback)(System::Object^ sender, IterationEventArgs^ e);
+    IterationListenerCallback managedFunctionPtr;
+
+    IterationListenerForwarder(void* managedFunctionPtr)
+        : managedFunctionPtr(static_cast<IterationListenerCallback>(managedFunctionPtr))
+    {}
+
+    virtual Status update(const UpdateMessage& updateMessage)
+    {
+        if (managedFunctionPtr != NULL)
+        {
+            IterationEventArgs^ args = gcnew IterationEventArgs();
+            args->Message = ToSystemString(updateMessage.message);
+            args->IterationIndex = updateMessage.iterationIndex;
+            args->IterationCount = updateMessage.iterationCount;
+            managedFunctionPtr(nullptr, args);
+            return args->Cancel ? Status_Cancel : Status_Ok;
+        }
+
+        return Status_Ok;
+    }
+};
+
+
+} // namespace util
+} // namespace CLI
+} // namespace pwiz
+
+
+#endif // _ITERATIONLISTENER_HPP_CLI_
