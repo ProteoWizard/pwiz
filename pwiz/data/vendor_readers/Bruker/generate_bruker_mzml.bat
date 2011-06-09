9bb460e9e75fa9777b8663fdfcb9e2dac47e4334
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2011-06-09 17:41:19 +0000

    - updated vendor data to reflect changed double precision in XMLWriter - added generate_*_mzml.bat scripts for each vendor so they can be updated individually
    Former-commit-id: 7a88a9c2a607769662afe927a87bdf0517cbfc7a

---
+++ pwiz/data/vendor_readers/Bruker/generate_bruker_mzml.bat
@@ -0,0 +1,8 @@
+@echo off
+setlocal
+@echo off
+
+REM # Find the location of Jamroot.jam relative to this script.
+set PWIZ_ROOT=%~dp0..\..\..\..
+call %PWIZ_ROOT%\scripts\test\generate_vendor_mzml.bat Bruker %~dp0
+
--- scripts/test/generate_vendor_mzml.bat
+++ scripts/test/generate_vendor_mzml.bat
@@ -2,62 +2,33 @@
 setlocal
 @echo off
 
-REM # Use this script to regenerate the vendor unit test data tarballs iff they are out of date.
+REM # Each vendor reader has a script that calls this script.
+REM # This script regenerates the vendor unit test data tarballs iff they are out of date.
 
 REM # Find the location of Jamroot.jam relative to this script.
 set PWIZ_ROOT=%~dp0..\..
 pushd %PWIZ_ROOT%
 
 REM # clean the vendor readers
-call clean_vendor_targets.bat
+call scripts\misc\clean_vendor_targets.bat
 
-:Agilent
-set name=Agilent
-set readerpath=pwiz\data\vendor_readers\Agilent
-set next=Thermo
-GOTO READER_TEST
+set name=%1
+set readerpath=%2
 
-:Thermo
-set name=Thermo
-set readerpath=pwiz\data\vendor_readers\Thermo
-set next=ABI
-GOTO READER_TEST
-
-:ABI
-set name=ABI
-set readerpath=pwiz\data\vendor_readers\ABI
-set next=Bruker
-GOTO READER_TEST
-
-:Bruker
-set name=Bruker
-set readerpath=pwiz\data\vendor_readers\Bruker
-set next=Waters
-GOTO READER_TEST
-
-:Waters
-set name=Waters
-set readerpath=pwiz\data\vendor_readers\Waters
-set next=end
-GOTO READER_TEST
-
-
-:READER_TEST
 echo Building Reader_%name% and running its unit test...
-call quickbuild.bat -q %* toolset=msvc %readerpath% > nul
+call quickbuild.bat -q toolset=msvc --i-agree-to-the-vendor-licenses secure-scl=off %readerpath% > nul
 if %ERRORLEVEL% EQU 0 GOTO SKIP
 echo The mzML for Reader_%name% is out of date. Regenerating mzML...
-call quickbuild.bat -q %* toolset=msvc --generate-mzML %readerpath% > nul
+call quickbuild.bat -q toolset=msvc --i-agree-to-the-vendor-licenses secure-scl=off --generate-mzML %readerpath% > nul
 if %ERRORLEVEL% NEQ 0 GOTO ERROR
 echo Tarballing Reader_%name% test data...
 call scripts\test\tar_test_data.bat %readerpath% %PWIZ_ROOT% Reader_%name%_Test.data > nul
 :SKIP
 echo Reader_%name% test data is up to date.
-GOTO %next%
+GOTO end
 :ERROR
 echo Error occured while generating the mzML. Use the regular build commands to fix it.
-GOTO %next%
-
+GOTO end
 
 :end
 REM # pop PWIZ_ROOT
