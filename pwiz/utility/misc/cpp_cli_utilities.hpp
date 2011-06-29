473b63da04a610b9761c6eb6d1ef64459af3dc38
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2011-06-29 18:37:53 +0000

    - added __FUNCTION__ context to CATCH_AND_FORWARD macro
    Former-commit-id: 973b56a83a6ef2b55a239aae515ede54630bb4fa

--- pwiz/utility/misc/cpp_cli_utilities.hpp
+++ pwiz/utility/misc/cpp_cli_utilities.hpp
@@ -29,6 +29,8 @@
 #include <vector>
 #include <string>
 #include <stdexcept>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/range/algorithm/copy.hpp>
 #include "automation_vector.h"
 
 
@@ -91,10 +93,33 @@ void ToAutomationVector(cli::array<managed_value_type>^ managedArray, automation
 } // namespace pwiz
 
 
-/// forwards managed exception to unmanaged code
+/// forwards managed exception to unmanaged code;
+/// prepends function with a single level of scope,
+/// e.g. "Reader::read()" instead of "pwiz::data::msdata::Reader::read()"
 #define CATCH_AND_FORWARD \
     catch (System::Exception^ e) \
-{throw std::runtime_error(pwiz::util::ToStdString(e->Message));}
+    { \
+        std::vector<boost::iterator_range<std::string::const_iterator> > tokens; \
+        std::string function(__FUNCTION__); \
+        boost::algorithm::split(tokens, function, boost::is_any_of(":"), boost::algorithm::token_compress_on); \
+        std::string what("["); \
+        if (tokens.size() > 1) \
+        { \
+            boost::range::copy(*(tokens.rbegin()+1), std::back_inserter(what)); \
+            what += "::"; \
+            if (boost::range::equal(*(tokens.rbegin()+1), *tokens.rbegin())) \
+                what += "ctor"; \
+            else if (tokens.rbegin()->front() == '~') \
+                what += "dtor"; \
+            else \
+                boost::range::copy(*tokens.rbegin(), std::back_inserter(what)); \
+        } \
+        else if (tokens.size() > 0) \
+            boost::range::copy(*tokens.rbegin(), std::back_inserter(what)); \
+        what += "] "; \
+        what += pwiz::util::ToStdString(e->Message); \
+        throw std::runtime_error(what); \
+    }
 
 
 #endif // _CPP_CLI_UTILITIES_HPP_
