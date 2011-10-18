1209cd882a94a3cb796592ff23b01aaa58df324d
vsharmauw <vsharmauw@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2011-10-18 07:48:08 +0000

    Refactored class BuildQTRAPMethod. It is now a subclass of BuildAnalystMethod. Added a test for building a QTRAP method.
    Former-commit-id: 9143868e8f01523cc5fb6ea33cd5bcd6ebdf3f74

--- pwiz_tools/Skyline/Executables/BuildMethod/BuildQTRAPMethod/Program.cs
+++ pwiz_tools/Skyline/Executables/BuildMethod/BuildQTRAPMethod/Program.cs
@@ -20,10 +20,8 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Text;
 using AcqMethodSvrLib;
-using Analyst;
+using BuildAnalystMethod;
 using MSMethodSvrLib;
 using ParameterSvrLib;
 
@@ -33,14 +31,15 @@ namespace BuildQTRAPMethod
 
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             try
             {
                 Environment.ExitCode = -1;  // Failure until success
 
                 var builder = new BuildQtrapMethod();
-                builder.ParseCommandArgs(args);
+                var listArgs = new List<string>(args);
+                builder.ParseCommandArgs(listArgs);
                 builder.build();
 
                 Environment.ExitCode = 0;
@@ -85,90 +84,23 @@ namespace BuildQTRAPMethod
         }
     }
 
-    internal sealed class MethodTransitions
+
+    class BuildQtrapMethod : BuildAnalystMethod.BuildAnalystMethod
     {
-        public MethodTransitions(string outputMethod, string finalMethod, string transitionList)
-        {
-            OutputMethod = outputMethod;
-            FinalMethod = finalMethod;
-            TransitionList = transitionList;
-        }
-
-        public string OutputMethod { get; private set; }
-        public string FinalMethod { get; private set; }
-        public string TransitionList { get; private set; }
-
-        public IEnumerable<MethodTransition> Transitions
-        {
-            get
-            {
-                var reader = new StringReader(TransitionList);
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                    yield return new MethodTransition(line);
-            }
-        }
-    }
-
-    internal sealed class MethodTransition
-    {
-        public MethodTransition(string transitionListLine)
-        {
-            var values = transitionListLine.Split(',');
-            if (values.Length < 6)
-                throw new IOException("Invalid transition list format.  Each line must have 6 values.");
-            try
-            {
-                int i = 0;
-                PrecursorMz = double.Parse(values[i++], CultureInfo.InvariantCulture);
-                ProductMz = double.Parse(values[i++], CultureInfo.InvariantCulture);
-                Dwell = double.Parse(values[i++], CultureInfo.InvariantCulture);
-                Label = values[i++];
-                DP = double.Parse(values[i++], CultureInfo.InvariantCulture);
-                CE = double.Parse(values[i], CultureInfo.InvariantCulture);
-            }
-            catch (FormatException)
-            {
-                throw new IOException("Invalid transition list format.  Failure parsing numeric value.");
-            }
-        }
-
-        public double PrecursorMz { get; private set; }
-        public double ProductMz { get; private set; }
-        public double Dwell { get; private set; }
-        public string Label { get; private set; }
-        public double CE { get; private set; }
-        public double DP { get; private set; }
-    }
-
-    internal sealed class BuildQtrapMethod
-    {
-        private const string ANALYST_METHOD_EXT = ".dam";
-
-        private string TemplateMethod { get; set; }
 
         private int? RTWindow { get; set; }
 
-        private List<MethodTransitions> MethodTrans { get; set; }
-
-        public BuildQtrapMethod()
+        public void ParseCommandArgs(List<string> args)
         {
-            MethodTrans = new List<MethodTransitions>();
-        }
-
-        public void ParseCommandArgs(string[] args)
-        {
-            // Default to stdin for transition list input
-            string outputMethod = null;
-            bool readStdin = false;
-            bool multiFile = false;
-
-            int i = 0;
-            while (i < args.Length && args[i][0] == '-')
+            var args2 = new List<string>();
+            for (int i = 0; i < args.Count; i++)
             {
-                switch (args[i++][1])
+                var arg = args[i];
+                if (arg.Length < 2)
+                    continue;
+                switch (arg.Substring(1, arg.Length - 1))
                 {
-                    case 'w':
+                    case "w":
                         try
                         {
                             RTWindow = (int)Math.Round(double.Parse(args[i++], CultureInfo.InvariantCulture));
@@ -178,162 +110,19 @@ namespace BuildQTRAPMethod
                             Usage(string.Format("The value {0} is not a retention time window.", args[i - 1]));
                         }
                         break;
-
-                    case 'o':
-                        if (i >= args.Length)
-                            throw new UsageException();
-                        outputMethod = Path.GetFullPath(args[i++]);
-                        break;
-                    case 's':
-                        readStdin = true;
-                        break;
-                    case 'm':
-                        multiFile = true;
-                        break;
                     default:
-                        throw new UsageException();
+                        args2.Add(arg);
+                        break;
                 }
             }
 
-            if (multiFile && !string.IsNullOrEmpty(outputMethod))
-                Usage("Multi-file and specific output are not compatibile.");
-
-            int argcLeft = args.Length - i;
-            if (argcLeft < 1 || (!readStdin && argcLeft < 2))
-                Usage();
-
-            TemplateMethod = Path.GetFullPath(args[i++]);
-
-            // Read input into a list of lists of fields
-            if (readStdin)
-            {
-                if (!multiFile && string.IsNullOrEmpty(outputMethod))
-                    Usage("Reading from standard in without multi-file format must specify an output file.");
-
-                readTransitions(Console.In, outputMethod);
-            }
-            else
-            {
-                for (; i < args.Length; i++)
-                {
-                    string inputFile = Path.GetFullPath(args[i]);
-                    string filter = null;
-                    if (inputFile.Contains('*'))
-                        filter = Path.GetFileName(inputFile);
-                    else if (Directory.Exists(inputFile))
-                        filter = "*.csv";
-
-                    if (string.IsNullOrEmpty(filter))
-                        readFile(inputFile, outputMethod, multiFile);
-                    else
-                    {
-                        string dirName = Path.GetDirectoryName(filter) ?? ".";
-                        foreach (var fileName in Directory.GetFiles(dirName, filter))
-                        {
-                            readFile(Path.Combine(dirName, fileName), null, multiFile);
-                        }
-                    }
-                }
-            }
+            ParseCommandArgs(args2.ToArray());
+            
         }
 
-        private static void Usage(string message)
+        public override void ValidateMethod(MassSpecMethod method)
         {
-            Console.Error.WriteLine(message);
-            Console.Error.WriteLine();
-            Usage();
-        }
-
-        private static void Usage()
-        {
-            throw new UsageException();
-        }
-
-        private void readFile(string inputFile, string outputMethod, bool multiFile)
-        {
-            if (!multiFile && string.IsNullOrEmpty(outputMethod))
-            {
-                string methodFileName = Path.GetFileNameWithoutExtension(inputFile) + ANALYST_METHOD_EXT;
-                string dirName = Path.GetDirectoryName(inputFile);
-                outputMethod = (dirName != null ? Path.Combine(dirName, methodFileName) : inputFile);
-            }
-
-            using (var infile = new StreamReader(inputFile))
-            {
-                readTransitions(infile, outputMethod);
-            }
-        }
-
-        private void readTransitions(TextReader instream, string outputMethod)
-        {
-            string outputMethodCurrent = outputMethod;
-            string finalMethod = outputMethod;
-            StringBuilder sb = new StringBuilder();
-
-            string line;
-            while ((line = instream.ReadLine()) != null)
-            {
-                line = line.Trim();
-
-                if (string.IsNullOrEmpty(outputMethodCurrent))
-                {
-                    if (!string.IsNullOrEmpty(outputMethod))
-                    {
-                        // Only one file, if outputMethod specified
-                        throw new IOException(string.Format("Failure creating method file {0}. Transition lists may not contain blank lines.", outputMethod));
-                    }
-
-                    // Read output file path from a line in the file
-                    outputMethodCurrent = line;
-                    finalMethod = instream.ReadLine();
-                    if (finalMethod == null)
-                        throw new IOException(string.Format("Empty transition list found."));
-                    
-                    sb = new StringBuilder();
-                }
-                else if (string.IsNullOrEmpty(line))
-                {
-                    MethodTrans.Add(new MethodTransitions(outputMethodCurrent, finalMethod, sb.ToString()));
-                    outputMethodCurrent = null;
-                }
-                else
-                {
-                    sb.AppendLine(line);
-                }
-            }
-
-            // Add the last method, if there is one
-            if (!string.IsNullOrEmpty(outputMethodCurrent))
-            {
-                MethodTrans.Add(new MethodTransitions(outputMethodCurrent, finalMethod, sb.ToString()));
-            }
-
-            // Read remaining contents of stream, in case it is stdin
-            while (instream.ReadLine() != null)
-            {
-            }
-        }
-
-        public void build()
-        {
-            ApplicationClass analyst = new ApplicationClass();
-
-            // Make sure that Analyst is fully started
-            IAcqMethodDirConfig acqMethodDir = (IAcqMethodDirConfig)analyst.Acquire();
-            if (acqMethodDir == null)
-                throw new IOException("Failed to initialize.  Analyst may need to be started.");
-
-            object acqMethodObj;
-            acqMethodDir.LoadNonUIMethod(TemplateMethod, out acqMethodObj);
-            IAcqMethod templateAcqMethod = (IAcqMethod)acqMethodObj;
-
-            MassSpecMethod templateMsMethod = ExtractMSMethod(templateAcqMethod);
-
-            if (templateMsMethod == null)
-                throw new IOException(string.Format("Failed to open template method {0}.  Analyst may need to be started.", TemplateMethod));
-            if (templateMsMethod.PeriodCount != 1)
-                throw new IOException(string.Format("Invalid template method {0}.  Expecting only one period.", TemplateMethod));
-            var msPeriod = (Period)templateMsMethod.GetPeriod(0);
+            var msPeriod = (Period)method.GetPeriod(0);
             if (msPeriod.ExperimCount != 1)
                 throw new IOException(string.Format("Invalid template method {0}.  Expecting only one experiment.", TemplateMethod));
             var msExperiment = (Experiment)msPeriod.GetExperiment(0);
@@ -341,7 +130,7 @@ namespace BuildQTRAPMethod
             if (experimentType != 4)
                 throw new IOException(string.Format("Invalid template method {0}.  Experiment type must be MRM.", TemplateMethod));
 
-            var msExperiment7 = (IExperiment7) msExperiment;
+            var msExperiment7 = (IExperiment7)msExperiment;
             if (RTWindow.HasValue)
             {
                 if (msExperiment7.KnownRetentionTimes == 0)
@@ -352,93 +141,40 @@ namespace BuildQTRAPMethod
                 if (msExperiment7.KnownRetentionTimes != 0)
                     throw new IOException(string.Format("Invalid template method {0}.  Template is for scheduled MRM.", TemplateMethod));
             }
+        }
 
-            foreach (var methodTranList in MethodTrans)
+
+        public override void WriteToTemplate(IAcqMethod acqMethod, MethodTransitions transitions)
+        {
+         
+            var method = ExtractMsMethod(acqMethod);
+            var period = (Period)method.GetPeriod(0);
+            var msExperiment = (Experiment)period.GetExperiment(0);
+
+            msExperiment.DeleteAllMasses();
+
+            foreach (var transition in transitions.Transitions)
             {
-                Console.Error.WriteLine(string.Format("MESSAGE: Exporting method {0}", Path.GetFileName(methodTranList.FinalMethod)));
-                if (string.IsNullOrEmpty(methodTranList.TransitionList))
-                    throw new IOException(string.Format("Failure creating method file {0}.  The transition list is empty.", methodTranList.FinalMethod));
+                int i;
+                var msMassRange = (MassRange) msExperiment.CreateMassRange(out i);
+                var msMassRange3 = (IMassRange3) msMassRange;
 
-                try
-                {
-                    msExperiment.DeleteAllMasses();
-
-                    foreach (var transition in methodTranList.Transitions)
-                    {
-                        int i;
-                        var msMassRange = (MassRange) msExperiment.CreateMassRange(out i);
-                        var msMassRange3 = (IMassRange3) msMassRange;
-
-                        msMassRange.SetMassRange(transition.PrecursorMz, 0, transition.ProductMz);
-                        msMassRange.DwellTime = transition.Dwell;
-                        msMassRange3.CompoundID = transition.Label;
-                        var massRangeParams = (ParamDataColl) msMassRange.MassDepParamTbl;
-                        short s;
-                        massRangeParams.Description = transition.Label;
-                        massRangeParams.AddSetParameter("DP", (float) transition.DP, (float) transition.DP, 0, out s);
-                        massRangeParams.AddSetParameter("EP", 10, 10, 0, out s);
-                        massRangeParams.AddSetParameter("CE", (float) transition.CE, (float) transition.CE, 0, out s);
-                        massRangeParams.AddSetParameter("CXP", 15, 15, 0, out s);
-                    }
-
-                    templateAcqMethod.SaveAcqMethodToFile(methodTranList.OutputMethod, 1);
-                }
-                catch (Exception x)
-                {
-                    throw new IOException(string.Format("Failure creating method file {0}.  {1}", methodTranList.FinalMethod, x.Message));
-                }
-
-                if (!File.Exists(methodTranList.OutputMethod))
-                    throw new IOException(string.Format("Failure creating method file {0}.", methodTranList.FinalMethod));
-
-                // Skyline uses a segmented progress status, which expects 100% for each
-                // segment, with one segment per file.
-                Console.Error.WriteLine("100%");
+                msMassRange.SetMassRange(transition.PrecursorMz, 0, transition.ProductMz);
+                msMassRange.DwellTime = transition.Dwell;
+                msMassRange3.CompoundID = transition.Label;
+                var massRangeParams = (ParamDataColl) msMassRange.MassDepParamTbl;
+                short s;
+                massRangeParams.Description = transition.Label;
+                massRangeParams.AddSetParameter("DP", (float) transition.DP, (float) transition.DP, 0, out s);
+                massRangeParams.AddSetParameter("EP", 10, 10, 0, out s);
+                massRangeParams.AddSetParameter("CE", (float) transition.CE, (float) transition.CE, 0, out s);
+                massRangeParams.AddSetParameter("CXP", 15, 15, 0, out s);
             }
+
+            acqMethod.SaveAcqMethodToFile(transitions.OutputMethod, 1);
+            
         }
 
-        public MassSpecMethod ExtractMSMethod(IAcqMethod dataAcqMethod)
-        {
-            if (dataAcqMethod == null)
-                return null;
-
-            const int kMSMethodDeviceType = 0; // device type for MassSpecMethod
-            int devType; // device type
-            EnumDeviceMethods enumMethod = (EnumDeviceMethods) dataAcqMethod;
-            enumMethod.Reset();
-            do
-            {
-                object devMethod; // one of the sub-methods
-                int devModel; // device model
-                int devInst; // device instrument type
-                enumMethod.Next(out devMethod,out devType,out devModel,out devInst);
-                if (devType == kMSMethodDeviceType)
-                    return (MassSpecMethod) devMethod;
-                else if (devType == -1)
-                {
-                    //Call Err.Raise(1, "ExtractMSMethod", "No MS method found")
-                    return null;
-                }
-            }
-            while (devType == kMSMethodDeviceType);
-
-            return null;
-        }
-    }
-
-    internal sealed class TempFile : IDisposable
-    {
-        public TempFile()
-        {
-            Name = Path.GetTempFileName();
-        }
-
-        public string Name { get; private set; }
-
-        public void Dispose()
-        {
-            File.Delete(Name);
-        }
     }
 }
 
---
+++ pwiz_tools/Skyline/Executables/BuildMethod/BuildQTRAPMethod/Test/BuildMethodTest.cs
@@ -0,0 +1,93 @@
+﻿using System.IO;
+using AcqMethodSvrLib;
+using Analyst;
+using MSMethodSvrLib;
+using NUnit.Framework;
+
+
+
+namespace BuildQTRAPMethod.Test
+{
+    [TestFixture]
+    class BuildMethodTest
+    {
+        private const string METHOD_FILE = "bovine_std2.dam";
+       
+        [Test]
+        public void TestMRMMethod()
+        {
+            string projectDirectory = GetProjectDirectory();
+
+            var args = new[] { projectDirectory+METHOD_FILE, projectDirectory+"Study 7 sched.csv" };
+            Program.Main(args);
+
+            string methodFilePath = Path.GetFullPath(projectDirectory+"Study 7 sched.dam");
+
+            // read the updated method and make sure that the
+            // transition list has been included
+            MassSpecMethod method = GetMethod(methodFilePath);
+
+            var period = (Period)method.GetPeriod(0);
+            var msExperiment = (Experiment)period.GetExperiment(0);
+
+            Assert.AreEqual(60, msExperiment.MassRangesCount);
+
+            // Check the first one
+            // 744.839753,1087.49894,19.49,APR.AGLCQTFVYGGCR.y9.light,85.4,38.2
+            var massRange = (MassRange) msExperiment.GetMassRange(0);
+            Assert.AreEqual(19.49, massRange.DwellTime, 0.000001);
+            Assert.AreEqual(744.839753, massRange.QstartMass, 0.00005);
+            Assert.AreEqual(0, massRange.QstopMass);
+            Assert.AreEqual(1087.49894, massRange.QstepMass, 0.00005);
+
+            var msMassRange3 = (IMassRange3)massRange;
+            Assert.AreEqual("APR.AGLCQTFVYGGCR.y9.light", msMassRange3.CompoundID);
+          
+            // Check the last one
+            // 572.791863,724.375566,20.48,CRP.GYSIFSYATK.y6.heavy,72.6,28.2
+            massRange = (MassRange)msExperiment.GetMassRange(59);
+            Assert.AreEqual(20.48, massRange.DwellTime, 0.000001);
+            Assert.AreEqual(572.791863, massRange.QstartMass, 0.00005);
+            Assert.AreEqual(0, massRange.QstopMass);
+            Assert.AreEqual(724.375566, massRange.QstepMass, 0.00005);
+
+            msMassRange3 = (IMassRange3)massRange;
+            Assert.AreEqual("CRP.GYSIFSYATK.y6.heavy", msMassRange3.CompoundID);
+        }
+
+        private static string GetProjectDirectory()
+        {
+            string projectDirectory = Directory.GetCurrentDirectory();
+            int idx = projectDirectory.IndexOf("bin");
+            if (idx != -1)
+            {
+                projectDirectory = projectDirectory.Substring(0, idx);
+            }
+            return projectDirectory;
+        }
+
+        private static MassSpecMethod GetMethod(string methodFilePath)
+        {
+
+            var analyst = new ApplicationClass();
+
+            // Make sure that Analyst is fully started
+            var acqMethodDir = (IAcqMethodDirConfig)analyst.Acquire();
+            if (acqMethodDir == null)
+                throw new IOException("Failed to initialize.  Analyst may need to be started.");
+
+
+
+            object acqMethodObj;
+            acqMethodDir.LoadNonUIMethod(methodFilePath, out acqMethodObj);
+            var acqMethod = (IAcqMethod)acqMethodObj;
+
+
+            var builder = new BuildQtrapMethod();
+            var method = builder.ExtractMsMethod(acqMethod);
+
+            return method;
+        }
+
+    }
+}
