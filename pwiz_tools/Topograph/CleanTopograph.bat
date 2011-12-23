bdd58689a52a098575bc723ea165d9d68b532e8a
nickshulman <nickshulman@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2011-12-23 00:57:48 +0000

    Topograph: rename directory that was called "turnover" to "TopographApp" and rename directory called "turnover_lib" to "TopographLib"
    Former-commit-id: 18b5f63167280dd2a574f4509c1d8f6634927993

--- pwiz_tools/Topograph/CleanTopograph.bat
+++ pwiz_tools/Topograph/CleanTopograph.bat
@@ -7,9 +7,11 @@ set PWIZ_ROOT=%~dp0
 set PWIZ_ROOT=%PWIZ_ROOT:~0,-1%
 pushd %PWIZ_ROOT%
 
+REM # TODO(nicksh):12/22/2011 Remove lines relating to "turnover" after it has been deleted from people's machines.
+
 IF EXIST bin rmdir /s /q bin
 IF EXIST obj rmdir /s /q obj
-IF EXIST topograph.sln.cache rmdir /s /q topograph.sln.cache
+IF EXIST topograph.sln.cache del /q topograph.sln.cache
 IF EXIST turnover\Microsoft.VC90.MFC rmdir /s /q turnover\Microsoft.VC90.MFC
 IF EXIST turnover\ClearCore.dll del /q turnover\ClearCore.dll
 IF EXIST turnover\ClearCore.Storage.dll del /q turnover\ClearCore.Storage.dll
@@ -17,6 +19,13 @@ IF EXIST turnover\EULA.MHDAC del /q turnover\EULA.MHDAC
 IF EXIST turnover\EULA.MSFileReader del /q turnover\EULA.MSFileReader
 IF EXIST turnover\Interop.EDAL.SxS.manifest del /q turnover\Interop.EDAL.SxS.manifest
 IF EXIST turnover\MassLynxRaw.dll del /q turnover\MassLynxRaw.dll
+IF EXIST TopographApp\Microsoft.VC90.MFC rmdir /s /q TopographApp\Microsoft.VC90.MFC
+IF EXIST TopographApp\ClearCore.dll del /q TopographApp\ClearCore.dll
+IF EXIST TopographApp\ClearCore.Storage.dll del /q TopographApp\ClearCore.Storage.dll
+IF EXIST TopographApp\EULA.MHDAC del /q TopographApp\EULA.MHDAC
+IF EXIST TopographApp\EULA.MSFileReader del /q TopographApp\EULA.MSFileReader
+IF EXIST TopographApp\Interop.EDAL.SxS.manifest del /q TopographApp\Interop.EDAL.SxS.manifest
+IF EXIST TopographApp\MassLynxRaw.dll del /q TopographApp\MassLynxRaw.dll
 IF EXIST TestResults rmdir /s /q TestResults
 IF EXIST TopographTestProject\bin rmdir /s /q TopographTestProject\bin
 IF EXIST TopographTestProject\obj rmdir /s /q TopographTestProject\obj
@@ -24,6 +33,10 @@ IF EXIST turnover\bin rmdir /s /q turnover\bin
 IF EXIST turnover\obj rmdir /s /q turnover\obj
 IF EXIST turnover_lib\bin rmdir /s /q turnover_lib\bin
 IF EXIST turnover_lib\obj rmdir /s /q turnover_lib\obj
+IF EXIST TopographApp\bin rmdir /s /q TopographApp\bin
+IF EXIST TopographApp\obj rmdir /s /q TopographApp\obj
+IF EXIST TopographLib\bin rmdir /s /q TopographLib\bin
+IF EXIST TopographLib\obj rmdir /s /q TopographLib\obj
 IF EXIST ..\Shared\Common\bin rmdir /s /q ..\Shared\Common\bin
 IF EXIST ..\Shared\Common\obj rmdir /s /q ..\Shared\Common\obj
 IF EXIST ..\Shared\ProteomeDb\bin rmdir /s /q ..\Shared\ProteomeDb\bin
