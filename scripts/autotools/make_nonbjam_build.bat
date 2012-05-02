cfa0060b92ad99274db000a6bccdf2e42d80044f
pcbrefugee <pcbrefugee@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2012-05-02 23:27:49 +0000

    use provided 7za.exe instead of provided bsdtar.exe - bsdtar seems not to have built with zip deflate capability
    Former-commit-id: 925e3f56b47c1a28a8e5dcc87c54f6d0c10fd832

--- scripts/autotools/make_nonbjam_build.bat
+++ scripts/autotools/make_nonbjam_build.bat
@@ -4,4 +4,4 @@ cd ..\..
 rmdir /s /q msvc_test
 mkdir msvc_test
 cd msvc_test
-..\libraries\bsdtar.exe -xf ..\scripts\autotools\libpwiz_msvc.zip
+..\libraries\7za.exe x ..\scripts\autotools\libpwiz_msvc.zip
