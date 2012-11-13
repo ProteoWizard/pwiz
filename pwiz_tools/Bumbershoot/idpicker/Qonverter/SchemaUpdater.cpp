d29f830091d43576523e05b9142b45a5735f6049
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2012-11-13 22:24:32 +0000

    * fixed include path * added logic to prevent some targets from building on Linux with the 'all' metatarget
    Former-commit-id: d3d19cacfd4107f90be30cca6a1f612775218785

--- pwiz_tools/Bumbershoot/idpicker/Qonverter/SchemaUpdater.cpp
+++ pwiz_tools/Bumbershoot/idpicker/Qonverter/SchemaUpdater.cpp
@@ -24,7 +24,7 @@
 
 
 #include "SchemaUpdater.hpp"
-#include "../Lib/SQLite/sqlite3pp.h"
+#include "sqlite3pp.h"
 #include "pwiz/utility/misc/Std.hpp"
 #include "pwiz/utility/misc/Filesystem.hpp"
 #include "pwiz/utility/misc/DateTime.hpp"
