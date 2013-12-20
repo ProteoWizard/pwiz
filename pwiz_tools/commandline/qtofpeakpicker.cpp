bffd792b963975b5d4021678b4247357e44cdcc4
witek96 <witek96@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2013-12-20 13:41:48 +0000

    pwiz - adding vendor file format support to qtofpeakpicker
    Former-commit-id: d58fc4f51dab943d7d1dfb6854539d8f13db111d

--- pwiz_tools/commandline/qtofpeakpicker.cpp
+++ pwiz_tools/commandline/qtofpeakpicker.cpp
@@ -4,8 +4,7 @@
 //
 // Original author: Witold Wolski <wewolski@gmail.com>
 //
-// Copyright 2008 Spielberg Family Center for Applied Proteomics
-//   Cedars-Sinai Medical Center, Los Angeles, California  90048
+// Copyright : ETH Zurich 
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); 
 // you may not use this file except in compliance with the License. 
@@ -14,7 +13,7 @@
 // http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software 
-// distributed under the License is distributed on an "AS IS" BASIS, 
+// distributed under the License is distributed  on an "AS IS" BASIS, 
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
 // See the License for the specific language governing permissions and 
 // limitations under the License.
@@ -26,6 +25,8 @@
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
 #include "pwiz/analysis/findmf/qtofpeakpickerfilter.hpp"
+#include "pwiz/data/vendor_readers/ExtendedReaderList.hpp"
+
 
 namespace b_po = boost::program_options;
 namespace b_fs = boost::filesystem;
@@ -192,8 +193,8 @@ int main(int argc, char *argv[])
     }
   PPParams aparam;
   analysisParameters(aparam,vmgeneral);
-
-  pwiz::msdata::MSDataPtr msdataptr_ = pwiz::msdata::MSDataPtr(new pwiz::msdata::MSDataFile(aparam.infile_));
+  pwiz::msdata::ExtendedReaderList readers;
+  pwiz::msdata::MSDataPtr msdataptr_ = pwiz::msdata::MSDataPtr(new pwiz::msdata::MSDataFile(aparam.infile_,&readers));
   pwiz::msdata::SpectrumListPtr sl = msdataptr_->run.spectrumListPtr;
   pwiz::msdata::SpectrumListPtr mp(
         new ralab::QTOFPeakPickerFilter(sl,
