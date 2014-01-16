1857b697b6ee2061e89643b5e1db12334d10156f
donmarsh <donmarsh@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2014-01-16 23:04:12 +0000

    SkylineTester: Cosmetic fixes for delete build window.
    Former-commit-id: 14aeeb39eea8aedba766f78cc0ba563f6ad63848

--- pwiz_tools/Skyline/SkylineTester/DeleteWindow.Designer.cs
+++ pwiz_tools/Skyline/SkylineTester/DeleteWindow.Designer.cs
@@ -84,11 +84,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(539, 87);
             this.ControlBox = false;
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.progressBarDelete);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
