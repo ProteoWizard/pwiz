9158cc508b7d2e03f580d132cbb6fd8293eadad7
yuvalboss <yuvalboss@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2014-01-17 22:44:23 +0000

    MSstats:First commit of QC form, changes to csproj structure removed Program.cs because was unnecessary.  Tested in Skyline and worked.
    Former-commit-id: 59ae6817f66fb621ccc0c58190b1feaedfd3fe77

---
+++ pwiz_tools/Skyline/Executables/Tools/MSstats/ArgsCollectors/MSStatGroupComparisonUI/QualityControlUI.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace MSStatArgsCollector
+{
+    partial class QualityControlUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.comboBoxNoramilzeTo = new System.Windows.Forms.ComboBox();
+            this.labelNormalizeTo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(182, 41);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(3, 3, 15, 3);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.Location = new System.Drawing.Point(182, 12);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(3, 3, 15, 3);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // comboBoxNoramilzeTo
+            // 
+            this.comboBoxNoramilzeTo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxNoramilzeTo.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.comboBoxNoramilzeTo.FormattingEnabled = true;
+            this.comboBoxNoramilzeTo.Items.AddRange(new object[] {
+            "None",
+            "Constant",
+            "Quantile",
+            "Global Standards"});
+            this.comboBoxNoramilzeTo.Location = new System.Drawing.Point(12, 30);
+            this.comboBoxNoramilzeTo.Name = "comboBoxNoramilzeTo";
+            this.comboBoxNoramilzeTo.Size = new System.Drawing.Size(144, 21);
+            this.comboBoxNoramilzeTo.TabIndex = 1;
+            // 
+            // labelNormalizeTo
+            // 
+            this.labelNormalizeTo.AutoSize = true;
+            this.labelNormalizeTo.Location = new System.Drawing.Point(12, 12);
+            this.labelNormalizeTo.Name = "labelNormalizeTo";
+            this.labelNormalizeTo.Size = new System.Drawing.Size(68, 13);
+            this.labelNormalizeTo.TabIndex = 0;
+            this.labelNormalizeTo.Text = "Normalize to:";
+            // 
+            // QualityControlUI
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(268, 79);
+            this.Controls.Add(this.comboBoxNoramilzeTo);
+            this.Controls.Add(this.labelNormalizeTo);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "QualityControlUI";
+            this.ShowInTaskbar = false;
+            this.Text = "MSstats QC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.ComboBox comboBoxNoramilzeTo;
+        private System.Windows.Forms.Label labelNormalizeTo;
+    }
+}
---
+++ pwiz_tools/Skyline/Executables/Tools/MSstats/ArgsCollectors/MSStatGroupComparisonUI/QualityControlUI.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * Original author: Yuval Boss <yuval .at. uw.edu>,
+ *                  MacCoss Lab, Department of Genome Sciences, UW
+ *
+ * Copyright 2013 University of Washington - Seattle, WA
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace MSStatArgsCollector
+{
+    public partial class QualityControlUI : Form
+    {
+        private enum Args { normalize_to}
+
+        public string[] Arguments { get; private set; }
+        public QualityControlUI(string[] oldArgs)
+        {
+            InitializeComponent();
+
+            comboBoxNoramilzeTo.SelectedIndex = 1;
+
+            if (oldArgs != null && oldArgs.Length == 1)
+                Arguments = oldArgs;
+        }
+        private void GenerateArguments()
+        {
+            Arguments = Arguments ?? new string[1];
+            Arguments[(int)Args.normalize_to] = (comboBoxNoramilzeTo.SelectedIndex).ToString(CultureInfo.InvariantCulture);
+          
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+          DialogResult = DialogResult.Cancel;        
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            GenerateArguments();
+            DialogResult = DialogResult.OK;
+     
+        }
+
+    }
+    public class MSstatsQualityControlCollector
+    {
+
+        public static string[] CollectArgs(IWin32Window parent, string report, string[] args)
+        {
+            using (var dlg = new QualityControlUI(args))
+            {
+                if (parent != null)
+                {
+                    return (dlg.ShowDialog(parent) == DialogResult.OK) ? dlg.Arguments : null;
+                }
+                return (dlg.ShowDialog() == DialogResult.OK) ? dlg.Arguments : null;
+            }
+        }
+    }   
+}
