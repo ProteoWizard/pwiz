e91df886556e666c31e3a88db5b8c07d0d587589
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2014-04-08 20:32:59 +0000

    * committed missing files
    Former-commit-id: 243f04d0c3b702918e2f1ca0caf0b09025216d16

---
+++ pwiz_tools/Bumbershoot/idpicker/Forms/NetGestaltTrackSampleInfoForm.Designer.cs
@@ -0,0 +1,193 @@
+﻿//
+// $Id$
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+//
+// The Original Code is the IDPicker project.
+//
+// The Initial Developer of the Original Code is Matt Chambers.
+//
+// Copyright 2014 Vanderbilt University
+//
+// Contributor(s): 
+//
+
+namespace IDPicker.Forms
+{
+    partial class NetGestaltTrackSampleInfoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.pivotGroupNameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.createButton = new System.Windows.Forms.Button();
+            this.addColumnButton = new System.Windows.Forms.Button();
+            this.columnContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.renameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fillByRegExToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.columnContextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AllowUserToOrderColumns = true;
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.pivotGroupNameColumn});
+            this.dataGridView.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView.Size = new System.Drawing.Size(572, 448);
+            this.dataGridView.TabIndex = 0;
+            this.dataGridView.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView_CellMouseClick);
+            // 
+            // pivotGroupNameColumn
+            // 
+            this.pivotGroupNameColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.pivotGroupNameColumn.HeaderText = "Group Name";
+            this.pivotGroupNameColumn.Name = "pivotGroupNameColumn";
+            this.pivotGroupNameColumn.ReadOnly = true;
+            this.pivotGroupNameColumn.Width = 92;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(509, 471);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // createButton
+            // 
+            this.createButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.createButton.Location = new System.Drawing.Point(428, 471);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(75, 23);
+            this.createButton.TabIndex = 2;
+            this.createButton.Text = "Create";
+            this.createButton.UseVisualStyleBackColor = true;
+            this.createButton.Click += new System.EventHandler(this.createButton_Click);
+            // 
+            // addColumnButton
+            // 
+            this.addColumnButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.addColumnButton.Location = new System.Drawing.Point(334, 471);
+            this.addColumnButton.Name = "addColumnButton";
+            this.addColumnButton.Size = new System.Drawing.Size(88, 23);
+            this.addColumnButton.TabIndex = 3;
+            this.addColumnButton.Text = "Add Column";
+            this.addColumnButton.UseVisualStyleBackColor = true;
+            this.addColumnButton.Click += new System.EventHandler(this.addColumnButton_Click);
+            // 
+            // columnContextMenu
+            // 
+            this.columnContextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.renameToolStripMenuItem,
+            this.removeToolStripMenuItem,
+            this.fillByRegExToolStripMenuItem});
+            this.columnContextMenu.Name = "columnContextMenu";
+            this.columnContextMenu.Size = new System.Drawing.Size(149, 70);
+            // 
+            // renameToolStripMenuItem
+            // 
+            this.renameToolStripMenuItem.Name = "renameToolStripMenuItem";
+            this.renameToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.renameToolStripMenuItem.Text = "Rename...";
+            this.renameToolStripMenuItem.Click += new System.EventHandler(this.renameToolStripMenuItem_Click);
+            // 
+            // removeToolStripMenuItem
+            // 
+            this.removeToolStripMenuItem.Name = "removeToolStripMenuItem";
+            this.removeToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.removeToolStripMenuItem.Text = "Remove";
+            this.removeToolStripMenuItem.Click += new System.EventHandler(this.removeToolStripMenuItem_Click);
+            // 
+            // fillByRegExToolStripMenuItem
+            // 
+            this.fillByRegExToolStripMenuItem.Name = "fillByRegExToolStripMenuItem";
+            this.fillByRegExToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.fillByRegExToolStripMenuItem.Text = "Fill by RegEx...";
+            this.fillByRegExToolStripMenuItem.Click += new System.EventHandler(this.fillByRegExToolStripMenuItem_Click);
+            // 
+            // NetGestaltTrackSampleInfoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(596, 506);
+            this.Controls.Add(this.addColumnButton);
+            this.Controls.Add(this.createButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "NetGestaltTrackSampleInfoForm";
+            this.ShowIcon = false;
+            this.Text = "Create NetGestalt Track Sample Info";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.columnContextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pivotGroupNameColumn;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button createButton;
+        private System.Windows.Forms.Button addColumnButton;
+        private System.Windows.Forms.ContextMenuStrip columnContextMenu;
+        private System.Windows.Forms.ToolStripMenuItem renameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fillByRegExToolStripMenuItem;
+    }
+}
---
+++ pwiz_tools/Bumbershoot/idpicker/Forms/NetGestaltTrackSampleInfoForm.cs
@@ -0,0 +1,204 @@
+﻿//
+// $Id$
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+//
+// The Original Code is the IDPicker project.
+//
+// The Initial Developer of the Original Code is Matt Chambers.
+//
+// Copyright 2014 Vanderbilt University
+//
+// Contributor(s): 
+//
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace IDPicker.Forms
+{
+    public partial class NetGestaltTrackSampleInfoForm : Form
+    {
+        string _defaultFilepath;
+
+        public IList<string> PivotGroupNames { get; set; }
+
+        public NetGestaltTrackSampleInfoForm(string defaultFilepath)
+        {
+            InitializeComponent();
+
+            _defaultFilepath = defaultFilepath;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            if (PivotGroupNames.IsNullOrEmpty())
+                throw new Exception("empty or null PivotGroupNames property");
+
+            foreach (string name in PivotGroupNames)
+                dataGridView.Rows.Add(name);
+            base.OnLoad(e);
+        }
+
+        private void addColumnButton_Click(object sender, EventArgs e)
+        {
+            using (var input = new TextInputPrompt("Attribute Name", false, ""))
+            {
+                if (input.ShowDialog(this) == DialogResult.Cancel)
+                    return;
+
+                var column = new DataGridViewTextBoxColumn
+                {
+                    HeaderText = input.GetText(),
+                    Name = input.GetText().Replace(" ", "").Trim() + "Attribute"
+                };
+                dataGridView.Columns.Add(column);
+                dataGridView.Refresh();
+            }
+        }
+
+        private void dataGridView_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.ColumnIndex <= 0 || e.Button != MouseButtons.Right)
+                return;
+
+            columnContextMenu.Tag = e.ColumnIndex;
+            columnContextMenu.Show(MousePosition);
+        }
+
+        private void renameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var input = new TextInputPrompt("Rename Attribute", false, ""))
+            {
+                if (input.ShowDialog(this) == DialogResult.Cancel)
+                    return;
+
+                int columnIndex = (int)columnContextMenu.Tag;
+                var column = dataGridView.Columns[columnIndex];
+                column.HeaderText = input.GetText();
+                column.Name = input.GetText().Replace(" ", "").Trim() + "Attribute";
+                dataGridView.Refresh();
+            }
+        }
+
+        private void removeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int columnIndex = (int) columnContextMenu.Tag;
+            dataGridView.Columns.RemoveAt(columnIndex);
+        }
+
+        private void fillByRegExToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var input = new TextInputPrompt("Fill Column by Regular Expression on Group Name", false, "") { InputFormat = null })
+            using (var replace = new TextInputPrompt("Enter Replacement Expression", false, "\\1") { InputFormat = null })
+            {
+                Regex regex;
+                while (true)
+                {
+                    if (input.ShowDialog(this) == DialogResult.Cancel)
+                        return;
+
+                    try
+                    {
+                        regex = new Regex(input.GetText());
+                    }
+                    catch (Exception ex)
+                    {
+                        Program.HandleUserError(ex);
+                        continue;
+                    }
+
+                    if (replace.ShowDialog(this) == DialogResult.Cancel)
+                        return;
+
+                    break;
+                }
+
+                int columnIndex = (int)columnContextMenu.Tag;
+                foreach (DataGridViewRow row in dataGridView.Rows)
+                {
+                    string inputString = (string) row.Cells[0].Value;
+                    string value = regex.Replace(inputString, replace.GetText());
+                    if (value != inputString)
+                        row.Cells[columnIndex].Value = value;
+                }
+            }
+        }
+
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            /* Barcode	Sample
+               data_type	CAT
+               Broad-20120603-iTRAQ-114	WHIM-16
+               Broad-20120603-iTRAQ-115	WHIM-2
+               Broad-20120603-iTRAQ-116	WHIM-16
+               Broad-20120603-iTRAQ-117	WHIM-2
+            */
+
+            string outputFilepath = _defaultFilepath;
+
+            while (true)
+                using (var sfd = new SaveFileDialog()
+                                    {
+                                        OverwritePrompt = true,
+                                        FileName = Path.GetFileNameWithoutExtension(outputFilepath),
+                                        DefaultExt = ".tsi",
+                                        Filter = "NetGestalt TSI|*.tsi|All files|*.*"
+                                    })
+                {
+                    if (sfd.ShowDialog(Program.MainWindow) == DialogResult.Cancel)
+                        return;
+
+                    outputFilepath = sfd.FileName;
+                    if (Path.GetFileName(outputFilepath).Contains(" "))
+                    {
+                        MessageBox.Show("NetGestalt does not support spaces in imported filenames.",
+                                        "Invalid NetGestalt Filename",
+                                        MessageBoxButtons.OK,
+                                        MessageBoxIcon.Error);
+                        continue;
+                    }
+                    break;
+                }
+
+            using (var outputStream = new StreamWriter(outputFilepath, false, Encoding.ASCII))
+            {
+                outputStream.Write("GroupName");
+                for (int i = 1; i < dataGridView.Columns.Count; ++i)
+                    if (dataGridView.Columns[i].Name.EndsWith("Attribute"))
+                        outputStream.Write("\t{0}", dataGridView.Columns[i].HeaderText);
+                outputStream.WriteLine();
+
+                // write data types for each column, right now only categorical is supported
+                outputStream.Write("data_type");
+                for (int i = 1; i < dataGridView.Columns.Count; ++i)
+                    outputStream.Write("\tCAT"); // categorical
+                outputStream.WriteLine();
+
+                // write each group and its columns
+                foreach(DataGridViewRow row in dataGridView.Rows)
+                    outputStream.WriteLine(String.Join("\t", row.Cells.Cast<DataGridViewCell>().Select(o => o.Value)));
+            }
+
+            Close();
+        }
+    }
+}
