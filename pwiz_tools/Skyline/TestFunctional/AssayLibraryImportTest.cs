15e338341ec6c986259d5eeeb5a18a0c098b30c1
brendanx <brendanx@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2014-04-24 06:07:48 +0000

    Skyline: Fix TestAssayLibraryImport failure
    Former-commit-id: f35e3c5eb069a3412a567cacfac1c8d761502033

--- pwiz_tools/Skyline/TestFunctional/AssayLibraryImportTest.cs
+++ pwiz_tools/Skyline/TestFunctional/AssayLibraryImportTest.cs
@@ -173,7 +173,7 @@ namespace pwiz.SkylineTestFunctional
                 var expectedMessage = string.Format(Resources.SkylineWindow_ImportFastaFile_Failed_reading_the_file__0__1__,
                                               textIrtGroupConflict,
                                               string.Format(Resources.PeptideGroupBuilder_AppendTransition_Two_transitions_of_the_same_peptide___0____have_different_iRT_values___1__and__2___iRT_values_must_be_assigned_consistently_in_an_imported_transition_list_,
-                                                            "AAAAAAAAAAAAAAAGAAGK", 53, 54));
+                                                            "AAAAAAAAAAAAAAAGAAGK", 54, 53));
                 Assert.AreEqual(messageDlg.Message, expectedMessage);
                 messageDlg.OkDialog();
             });
