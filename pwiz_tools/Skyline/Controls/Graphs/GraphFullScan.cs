8f18519c75f6a72a52532f8ffe5be29356ae12b2
pcbrefugee <pcbrefugee@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2014-06-05 22:59:29 +0000

    Skyline: tweak to Don's last change for hiding the filter button when there is no drift time filtering in use.
    Former-commit-id: 6f25dd2ca10349235d60481e203e651a57184e4e

--- pwiz_tools/Skyline/Controls/Graphs/GraphFullScan.cs
+++ pwiz_tools/Skyline/Controls/Graphs/GraphFullScan.cs
@@ -79,13 +79,6 @@ namespace pwiz.Skyline.Controls.Graphs
         private void SetScans(MsDataSpectrum[] scans)
         {
             _fullScans = scans;
-            double minDriftTime, maxDriftTime;
-            GetDriftRange(out minDriftTime, out maxDriftTime);
-            if (maxDriftTime <= 0)
-            {
-                filterBtn.Visible = false;
-                filterBtn.Checked = false;
-            }
             CreateGraph();
             if (_zoomXAxis)
             {
@@ -236,6 +229,18 @@ namespace pwiz.Skyline.Controls.Graphs
             filterBtn.Visible = spectrumBtn.Visible = hasDriftDimension;
             graphControl.IsEnableVPan = graphControl.IsEnableVZoom = useHeatMap;
             GraphPane.Legend.IsVisible = useHeatMap;
+
+            if (hasDriftDimension)
+            {
+                // Is there actually any drift time filtering available?
+                double minDriftTime, maxDriftTime;
+                GetDriftRange(out minDriftTime, out maxDriftTime);
+                if ((minDriftTime == double.MinValue) && (maxDriftTime == double.MaxValue))
+                {
+                    filterBtn.Visible = false;
+                    filterBtn.Checked = false;
+                }
+            }
 
             if (useHeatMap)
                 CreateDriftTimeHeatmap();
