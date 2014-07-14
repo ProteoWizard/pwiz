4e58e552bc412d07a20468b1aae50e13d8c5abaf
pcbrefugee <pcbrefugee@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2014-07-14 23:34:39 +0000

    Skyline: add a couple of perf tests for ion mobility data handling in Agilent and Waters.
    Former-commit-id: cb560c0c54c09c5d1115f3d661395aae102ed258

---
+++ pwiz_tools/Skyline/TestPerf/PerfImportResultsAgilentIMSTest.cs
@@ -0,0 +1,132 @@
+/*
+ * Original author: Brian Pratt <bspratt .at. u.washington.edu>,
+ *                  MacCoss Lab, Department of Genome Sciences, UW
+ *
+ * Copyright 2014 University of Washington - Seattle, WA
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pwiz.Common.SystemUtil;
+using pwiz.ProteowizardWrapper;
+using pwiz.Skyline.FileUI;
+using pwiz.Skyline.Model.Results;
+using pwiz.Skyline.Util;
+using pwiz.SkylineTestUtil;
+
+namespace TestPerf
+{
+    /// <summary>
+    /// Verify consistent import of Agilent IMS data as we work on various code optimizations.
+    /// </summary>
+    [TestClass]
+    public class ImportAgilentIMSTest : AbstractFunctionalTest, IPerfTest // IPerfTests only run when the global "allow perf tests" flag is set
+    {
+        private string _skyFile;
+        private TestFilesDir _testFilesDir;
+
+
+        [TestMethod] 
+        public void AgilentIMSImportTest()
+        {
+            Log.AddMemoryAppender();
+
+            TestFilesZip = "https://skyline.gs.washington.edu/perftests/PerfImportResultsAgilentIMS.zip";
+            TestFilesPersistent = new[] { "19pep_1700V_pos_3May14_Legolas.d", "19pep_1700V_CE22_pos_5May14_Legolas.d" }; // list of files that we'd like to unzip alongside parent zipFile, and (re)use in place
+            _testFilesDir = new TestFilesDir(TestContext, TestFilesZip, null, TestFilesPersistent);
+            _skyFile = _testFilesDir.GetTestPath("Erin 19pep test subset.sky");
+
+            MsDataFileImpl.PerfUtilFactory.IssueDummyPerfUtils = false; // turn on performance measurement
+
+            RunFunctionalTest();
+            
+            var logs = Log.GetMemoryAppendedLogEvents();
+            var stats = PerfUtilFactory.SummarizeLogs(logs, TestFilesPersistent); // show summary
+            var log = new Log("Summary");
+            if (_testFilesDir != null)
+                log.Info(stats.Replace(_testFilesDir.PersistentFilesDir, ""));
+        }
+
+        protected override void DoTest()
+        {
+            RunUI(() => SkylineWindow.OpenFile(_skyFile));
+
+            const int chromIndex = 1;
+            var doc0 = WaitForDocumentLoaded();
+            AssertEx.IsDocumentState(doc0, null, 19, 19, 28, 607);
+            float tolerance = (float)doc0.Settings.TransitionSettings.Instrument.MzMatchTolerance;
+            double maxHeight0 = 0;
+            var results0 = doc0.Settings.MeasuredResults;
+            var intensities = new List<List<float>>();
+            foreach (var pair in doc0.PeptidePrecursorPairs)
+            {
+                ChromatogramGroupInfo[] chromGroupInfo;
+                Assert.IsTrue(results0.TryLoadChromatogram(chromIndex, pair.NodePep, pair.NodeGroup,
+                    tolerance, true, out chromGroupInfo));
+                foreach (var chromGroup in chromGroupInfo)
+                {
+                    foreach (var tranInfo in chromGroup.TransitionPointSets)
+                    {
+                        maxHeight0 = Math.Max(maxHeight0, tranInfo.MaxIntensity);
+                        intensities.Add(new List<float>(tranInfo.Intensities));
+                    }
+                }
+            }
+
+            Stopwatch loadStopwatch = new Stopwatch();
+            loadStopwatch.Start();
+            var manageResults = ShowDialog<ManageResultsDlg>(SkylineWindow.ManageResults);
+            RunUI(() =>
+            {
+                // Just reload the CE22 stuff, using the RT info from the MS1 scan
+                manageResults.SelectedChromatograms = new[] { SkylineWindow.Document.Settings.MeasuredResults.Chromatograms[chromIndex] };
+                manageResults.ReimportResults();
+                manageResults.OkDialog();
+            });
+            WaitForCondition(10 * 60 * 1000, () => SkylineWindow.Document.Settings.MeasuredResults.IsLoaded); // 10 minutes
+            loadStopwatch.Stop();
+
+            var doc1 = WaitForDocumentLoaded(400000);
+            AssertEx.IsDocumentState(doc1, null, 19, 19, 28, 607);
+             
+            var chroms0 = doc0.Settings.MeasuredResults.Chromatograms[chromIndex];
+            var chroms1 = doc1.Settings.MeasuredResults.Chromatograms[chromIndex];
+            Assert.AreEqual(chroms0, chroms1);
+
+            int intensityIndex = 0;
+            var results1 = doc1.Settings.MeasuredResults;
+            foreach (var pair in doc1.PeptidePrecursorPairs)
+            {
+                ChromatogramGroupInfo[] chromGroupInfo;
+                Assert.IsTrue(results1.TryLoadChromatogram(chromIndex, pair.NodePep, pair.NodeGroup,
+                    tolerance, true, out chromGroupInfo));
+                foreach (var chromGroup in chromGroupInfo)
+                {
+                    foreach (var tranInfo in chromGroup.TransitionPointSets)
+                    {
+                        AssertEx.AreEqualDeep(intensities[intensityIndex++],new List<float>(tranInfo.Intensities));
+                    }
+                }
+            }
+            
+            DebugLog.Info("load time = {1}", loadStopwatch.ElapsedMilliseconds);
+
+
+        }  
+    }
+}
