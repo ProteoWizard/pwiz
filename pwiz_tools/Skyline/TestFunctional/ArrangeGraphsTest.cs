6e2a0838f0d5a109805b1c2cde1795ac7ba4ce8b
yuvalboss <yuvalboss@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2014-09-16 20:54:46 +0000

    Fixed issue with window sizes on different computers when running ArrangeGraphsTest.
    Former-commit-id: d61f7d4a2667ca8ad1f300c1936e3a04a63d4a68

--- pwiz_tools/Skyline/TestFunctional/ArrangeGraphsTest.cs
+++ pwiz_tools/Skyline/TestFunctional/ArrangeGraphsTest.cs
@@ -45,6 +45,17 @@ namespace pwiz.SkylineTestFunctional
             // Tests that elements show up in correct groups and order as type "tiled" in grouped window.
             RunGroupDlg(9, DisplayGraphsType.Tiled, GroupGraphsType.separated, GroupGraphsOrder.Acquired_Time);
             List<GraphChromatogram> graphChromatograms = SkylineWindow.GraphChromatograms.ToList();
+            var beginningWidth = 0;
+            var beginningHeight = 0;
+            RunUI(() =>
+            {
+                beginningWidth = graphChromatograms.First().Parent.Parent.Parent.Width;
+                beginningHeight = graphChromatograms.First().Parent.Parent.Parent.Height;
+                // Need to set dimensions of panel containing chromatograms so dimensions get set the same each run.
+                // Dimension are set to those I had in my settings when I build the test. (Yuval)
+                graphChromatograms.First().Parent.Parent.Parent.Height = 443;
+                graphChromatograms.First().Parent.Parent.Parent.Width = 736;
+            });
             Assert.IsTrue(graphChromatograms.Count == 27);
             var dictGraphPositions = GetDictionaryGraphChromatogram();
             foreach (KeyValuePair<Point, List<GraphChromatogram>> dictGraphGroup in dictGraphPositions)
@@ -52,7 +63,7 @@ namespace pwiz.SkylineTestFunctional
                 Assert.IsTrue(dictGraphGroup.Value.Count == 3);
             }
             List<int> order = new List<int>();
-            for (var i = 0; i < 600; i++)
+            for (var i = 0; i < beginningWidth; i++)
             {
                 if (dictGraphPositions.ContainsKey(new Point(i, 1)))
                 {
@@ -62,7 +73,7 @@ namespace pwiz.SkylineTestFunctional
                     }
                 }
             }
-            for (var i = 0; i < 600; i++)
+            for (var i = 0; i < beginningWidth; i++)
             {
                 if (dictGraphPositions.ContainsKey(new Point(i,128)))
                 {
@@ -81,7 +92,7 @@ namespace pwiz.SkylineTestFunctional
             RunGroupDlg(9, DisplayGraphsType.Column, GroupGraphsType.distributed, GroupGraphsOrder.Acquired_Time);
             dictGraphPositions = GetDictionaryGraphChromatogram();
             var orderedByPosition = new Dictionary<Point, List<GraphChromatogram>>();
-            for (int i = 0; i < 600; i++)
+            for (int i = 0; i < beginningWidth; i++)
             {
                 if (dictGraphPositions.ContainsKey(new Point(i, 1)))
                 {
@@ -141,6 +152,12 @@ namespace pwiz.SkylineTestFunctional
                 Assert.IsTrue(dictGraphGroup.Value.Count == 1);
                 Assert.IsTrue(dictGraphGroup.Key.X == 1);
             }
+            RunUI(() =>
+            {
+                // Sets dimesnions back to what they were at the beginning;
+                graphChromatograms.First().Parent.Parent.Parent.Height = beginningHeight;
+                graphChromatograms.First().Parent.Parent.Parent.Width = beginningWidth;
+            });
         }
 
         // Returns a dictionary of all groups and their containing chromatograms.  Key is top-left point of window.
