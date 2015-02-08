d9298e1f4ef362b3488d3cd7d35bf971e6749358
brendanx <brendanx@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2015-02-08 04:47:12 +0000

    Skyline (2.6): Skyline runner changes for update to web site.

--- pwiz/pwiz_tools/Skyline/Executables/SkylineRunner/Properties/AssemblyInfo.cs
+++ pwiz/pwiz_tools/Skyline/Executables/SkylineRunner/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("University of Washington")]
 [assembly: AssemblyProduct("Skyline")]
-[assembly: AssemblyCopyright("Copyright © University of Washington 2011-2013")]
+[assembly: AssemblyCopyright("Copyright © University of Washington 2011-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0.4421")]
-[assembly: AssemblyFileVersion("1.4.0.4421")]
+[assembly: AssemblyVersion("2.6.0.4481")]
+[assembly: AssemblyFileVersion("2.6.0.4481")]
