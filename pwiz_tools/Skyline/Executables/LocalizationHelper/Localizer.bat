c5e5a72805e695c4401f3bdd4c48e414d7792aa4
yuvalboss <yuvalboss@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2015-02-27 00:46:57 +0000

    additional debug messages added
    Former-commit-id: 4ee734ac7b281522c576d6308e908edae0405c8b

--- pwiz_tools/Skyline/Executables/LocalizationHelper/Localizer.bat
+++ pwiz_tools/Skyline/Executables/LocalizationHelper/Localizer.bat
@@ -1,50 +1,50 @@
 cd
 echo %1
-dir C:\Windows\system32\config\systemprofile\AppData\Local/JetBrains/commandline8.2/
-dir C:\Windows\system32\config\systemprofile\AppData\Local/JetBrains/commandline9.0/
-dir C:\Windows\system32\config\systemprofile\AppData\Local/JetBrains/
-dir C:\Windows\system32\config\systemprofile\AppData\Local/
-dir C:\Windows\system32\config\systemprofile\AppData\
-dir C:\Windows\system32\config\systemprofile\
+dir /a C:\Windows\system32\config\systemprofile\AppData\Local\JetBrains\commandline8.2\
+dir /a C:\Windows\system32\config\systemprofile\AppData\Local\JetBrains\commandline9.0\
+dir /a C:\Windows\system32\config\systemprofile\AppData\Local\JetBrains\
+dir /a C:\Windows\system32\config\systemprofile\AppData\Local\
+dir /a C:\Windows\system32\config\systemprofile\AppData\
+dir /a C:\Windows\system32\config\systemprofile\
 if not exist C:\Windows\system32\config\systemprofile\AppData\Local/JetBrains/commandline8.2/(
   Echo "Debug 1"
 )
-if not exist C:\Windows\system32\config\systemprofile\AppData\Local/JetBrains/commandline9.0/(
+if Exist C:\Windows\system32\config\systemprofile\AppData\Local/JetBrains/commandline9.0/(
   Echo "Debug 2"
 )
-if not exist C:\Windows\system32\config\systemprofile\AppData\Local\JetBrains\commandline8.2\(
+if Exist C:\Windows\system32\config\systemprofile\AppData\Local\JetBrains\commandline8.2\(
   Echo "Debug 3"
 )
-if not exist C:\Windows\system32\config\systemprofile\AppData\Local\JetBrains\commandline9.0\(
+if Exist C:\Windows\system32\config\systemprofile\AppData\Local\JetBrains\commandline9.0\(
   Echo "Debug 4"
 )
-if not exist Z:\Windows\system32\config\systemprofile\AppData\Local\JetBrains\commandline8.2\(
+if Exist Z:\Windows\system32\config\systemprofile\AppData\Local\JetBrains\commandline8.2\(
   Echo "Debug 5"
 )
-if not exist Z:\Windows\system32\config\systemprofile\AppData\Local\JetBrains\commandline9.0\(
+if Exist Z:\Windows\system32\config\systemprofile\AppData\Local\JetBrains\commandline9.0\(
   Echo "Debug 6"
 )
 
-if exist %LocalAPPDATA%/JetBrains/commandline8.2/ (
+if exist %LocalAPPDATA%\JetBrains\commandline8.2\ (
     Echo "InspectCode command line package already installed."
  ) ELSE (
  	Echo "Installing ReSharper Command Line Tools."
     %1\scripts\misc\wget http://skyline.gs.washington.edu/downloads/jb-commandline-8.2.0.2151.zip
-    move %1\jb-commandline-8.2.0.2151.zip %LocalAPPDATA%/JetBrains/
-	%1\libraries\7za.exe x  %LocalAPPDATA%/JetBrains/jb-commandline-8.2.0.2151.zip -o%LocalAPPDATA%/JetBrains/commandline8.2/
+    move %1\jb-commandline-8.2.0.2151.zip %LocalAPPDATA%\JetBrains\
+	%1\libraries\7za.exe x  %LocalAPPDATA%/JetBrains/jb-commandline-8.2.0.2151.zip -o%LocalAPPDATA%\JetBrains\commandline8.2\
 )
-if exist %LocalAPPDATA%/JetBrains/commandline9.0/ (
+if exist %LocalAPPDATA%\JetBrains\commandline9.0\ (
     Echo "InspectCode command line package already installed."
  ) ELSE (
  	Echo "Installing ReSharper Command Line Tools."
-    %1\scripts\misc\wget http://skyline.gs.washington.edu/downloads/ReSharperCommandLineTools01Update1.zip
-    move %1\ReSharperCommandLineTools01Update1.zip %LocalAPPDATA%/JetBrains/
-	%1\libraries\7za.exe x  %LocalAPPDATA%/JetBrains/ReSharperCommandLineTools01Update1.zip -o%LocalAPPDATA%/JetBrains/commandline9.0/
+    %1\scripts\misc\wget http://skyline.gs.washington.edu\downloads\ReSharperCommandLineTools01Update1.zip
+    move %1\ReSharperCommandLineTools01Update1.zip %LocalAPPDATA%\JetBrains\
+	%1\libraries\7za.exe x  %LocalAPPDATA%\JetBrains\ReSharperCommandLineTools01Update1.zip -o%LocalAPPDATA%\JetBrains\commandline9.0\
 )
 
-if not exist %LocalAPPDATA%/JetBrains/commandline8.2/InspectCode.exe (
+if not exist %LocalAPPDATA%\JetBrains\commandline8.2\InspectCode.exe (
   Echo "There was an issue with your InspectCode 8.2 directory"
-  Echo "Delete %LocalAPPDATA%/JetBrains/commandline8.2/ and rebuild."
+  Echo "Delete %LocalAPPDATA%\JetBrains\commandline8.2\ and rebuild."
 )
 if not exist %LocalAPPDATA%/JetBrains/commandline9.0/InspectCode.exe (
   Echo "There was an issue with your InspectCode 9.0 directory"
