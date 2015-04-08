2dbf12972caf20d864fd159f22bbc0b09012ba1a
kaipot <kaipot@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2015-04-08 10:50:54 +0000

    ms2 reading fix
    Former-commit-id: 5085d822694b60fab9c58fc6fac3e20ceb336d0b

--- pwiz/data/msdata/SpectrumList_MSn.cpp
+++ pwiz/data/msdata/SpectrumList_MSn.cpp
@@ -251,7 +251,7 @@ class SpectrumList_MSnImpl : public SpectrumList_MSn
         // read in the scan number
         size_t first_num_pos = lineStr.find_first_of("123456789");
         size_t second_space_pos = lineStr.find_first_of(" \t", first_num_pos);
-        int scanNum = lexical_cast<int>(lineStr.substr(first_num_pos, second_space_pos-first_num_pos+1));
+        int scanNum = lexical_cast<int>(lineStr.substr(first_num_pos, second_space_pos-first_num_pos));
         spectrum.id = "scan=" + lexical_cast<string>(scanNum);
 
         // read in the precursor mz
@@ -259,7 +259,7 @@ class SpectrumList_MSnImpl : public SpectrumList_MSn
         {
             size_t last_num_pos = lineStr.find_last_of("0123456789");
             size_t last_space_pos = lineStr.find_last_of(" \t", last_num_pos);
-            precursor_mz = lexical_cast<double>(lineStr.substr(last_space_pos, last_num_pos-last_space_pos+1));
+            precursor_mz = lexical_cast<double>(lineStr.substr(last_space_pos+1, last_num_pos-last_space_pos));
             // store precursor in the first selected ion if we do not have accurate mass data (below)
             Precursor& precursor = spectrum.precursors.back();
             precursor.isolationWindow.set(MS_isolation_window_target_m_z, precursor_mz, MS_m_z);
@@ -311,7 +311,7 @@ class SpectrumList_MSnImpl : public SpectrumList_MSn
             size_t last_num_pos = lineStr.find_last_of("0123456789");
             size_t last_space_pos = lineStr.find_last_of(" \t", last_num_pos);
             double z_precursor_mz = calculateMassOverCharge(
-                lexical_cast<double>(lineStr.substr(last_space_pos, last_num_pos-last_space_pos+1)), charge, 1);
+                lexical_cast<double>(lineStr.substr(last_space_pos+1, last_num_pos-last_space_pos)), charge, 1);
             stringstream ss;
             ss << charge << ' ' << z_precursor_mz;
             // Store Z line information in UserParams, in the format "<charge> <m/z calculated from Z line mass>"
