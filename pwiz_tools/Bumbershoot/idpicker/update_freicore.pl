4e52259d56b88b29b25629771a68caef4fc13c62
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2015-06-25 23:18:28 +0000

    - removed IDPicker's svn:external (again) and its quickbuild/clean files
    Former-commit-id: 67cad4e43b107c127872690ab2ca8062b6cb1ddd

--- pwiz_tools/Bumbershoot/idpicker/update_freicore.pl
+++
@@ -1,6 +0,0 @@
-# get directory the script is running from
-use File::Basename;
-my ($scriptFilename, $scriptPath) = fileparse($0);
-
-# update freicore external
-system("$scriptPath/freicore/update_externals.pl", "$scriptPath");
