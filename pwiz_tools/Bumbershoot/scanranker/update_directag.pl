ad28656d9dc999718cddf8372a03edab74862e6c
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2015-06-29 19:14:30 +0000

    - removed svn:externals and redundant libraries after importing ScanRanker
    Former-commit-id: 4934f6f7dab888f97e05ea751f99e06eab46a643

--- pwiz_tools/Bumbershoot/scanranker/update_directag.pl
+++
@@ -1,6 +0,0 @@
-# get directory the script is running from
-use File::Basename;
-my ($scriptFilename, $scriptPath) = fileparse($0);
-
-# update freicore external
-system("$scriptPath/DirecTag/freicore/update_externals.pl", "$scriptPath");
