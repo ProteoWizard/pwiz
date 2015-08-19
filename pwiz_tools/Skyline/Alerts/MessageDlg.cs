e548176463c92bf23f7bfa0e48d4e997518ffef0
nickshulman <nickshulman@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2015-08-19 21:45:12 +0000

    Missed file from last checkin
    Former-commit-id: 4f7ba256789da926ae807a499af0a6b5aeb49fce

---
+++ pwiz_tools/Skyline/Alerts/MessageDlg.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Original author: Nick Shulman <nicksh .at. u.washington.edu>,
+ *                  MacCoss Lab, Department of Genome Sciences, UW
+ *
+ * Copyright 2015 University of Washington - Seattle, WA
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Windows.Forms;
+
+namespace pwiz.Skyline.Alerts
+{
+    public class MessageDlg : AlertDlg
+    {
+        public static void Show(IWin32Window parent, string message)
+        {
+            new MessageDlg(message).ShowAndDispose(parent);
+        }
+
+        public static void ShowException(IWin32Window parent, Exception exception)
+        {
+            ShowWithException(parent, exception.Message, exception);
+        }
+
+        public static void ShowWithException(IWin32Window parent, string message, Exception exception)
+        {
+            new MessageDlg(message) { Exception = exception }.ShowAndDispose(parent);
+        }
+
+        private MessageDlg(string message) : base(message, MessageBoxButtons.OK)
+        {
+        }
+    }
+}
