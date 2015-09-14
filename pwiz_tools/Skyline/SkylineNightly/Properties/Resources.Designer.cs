ab423c44b55920584abefcabe895d9ee05f21af2
pcbrefugee <pcbrefugee@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2015-09-14 23:17:59 +0000

    Skyline: update SkylineNightly to use .NET 4.5.1 - not sure why this worked with 4.0 locally but not on TC.
    Former-commit-id: 8edf7beee3d5d0febb70f4141eb8e22c5b293d53

--- pwiz_tools/Skyline/SkylineNightly/Properties/Resources.Designer.cs
+++ pwiz_tools/Skyline/SkylineNightly/Properties/Resources.Designer.cs
@@ -8,8 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SkylineNightly.Properties 
-{
+namespace SkylineNightly.Properties {
     using System;
     
     
@@ -23,28 +22,23 @@ namespace SkylineNightly.Properties
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
         
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager 
-        {
-            get 
-            {
-                if (object.ReferenceEquals(resourceMan, null)) 
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SkylineNightly.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
@@ -57,14 +51,11 @@ namespace SkylineNightly.Properties
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
             }
         }
