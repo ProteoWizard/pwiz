401216bb322aa5e0d36eab704cb0eec6e5b73029
pcbrefugee <pcbrefugee@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2015-11-27 20:51:05 +0000

    Skyline (3_5): get rid of extra "using" directive for quiet compile
    Former-commit-id: b5ed3aef8f562cb36258cbfc20201b7b87bce9be

--- pwiz_tools/Skyline/Model/Databinding/SkylineViewContext.cs
+++ pwiz_tools/Skyline/Model/Databinding/SkylineViewContext.cs
@@ -24,7 +24,6 @@ using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using System.Xml.Serialization;
 using pwiz.Common.Collections;
