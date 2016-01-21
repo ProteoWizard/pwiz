c24640acaeba780051f9b0e20470fd918c0a2051
brendanx <brendanx@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2016-01-21 01:18:18 +0000

    Skyline: Command-line EXE for measuring import performance for multiple files with threads and processes
    Former-commit-id: 4157e6b8bfd9811e61d841d55f0cb81d138f2e22

---
+++ pwiz_tools/Skyline/Executables/ImportPerf/CommandArgs.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ImportPerf
+{
+    class CommandArgs
+    {
+        private const string ARG_THREADS = "threads";
+        private const string ARG_PROCESSES = "processes";
+        private const string ARG_UI = "ui";
+        private const string ARG_ACG = "acg";
+        private const string ARG_SKYLINE_PATH = "skyline-path";
+        private const string ARG_FILE_PATH = "file-path";
+        private const string ARG_DATA_DIR = "data-dir";
+        private const string ARG_DATA_FILTER = "data-filter";
+
+        public int Threads { get; private set; }
+        public int Processes { get; private set; }
+        public string SkylinePath { get; private set; }
+        public string FilePath { get; private set; }
+        public string DataDir { get; private set; }
+        public string DataFilter { get; private set; }
+
+        private enum AcgState { show, hide, none }
+
+        private bool IsUi { get; set; }
+        private AcgState Acg { get; set; }
+
+        public string[] UiArgs
+        {
+            get
+            {
+                if (!IsUi)
+                    return new string[0];
+                var args = new List<string> {"--ui"};
+                switch (Acg)
+                {
+                    case AcgState.hide:
+                        args.Add("--hideacg");
+                        break;
+                    case AcgState.none:
+                        args.Add("--noacg");
+                        break;
+                }
+                return args.ToArray();
+            }
+        }
+
+        public CommandArgs()
+        {
+            Threads = 1;
+            Processes = 1;
+        }
+
+        public bool ParseArgs(string[] args)
+        {
+            try
+            {
+                return ParseArgsInternal(args);
+            }
+            catch (UsageException x)
+            {
+                Console.Error.WriteLine("Error: {0}", x.Message); // Not L10N
+                return false;
+            }
+            catch (Exception x)
+            {
+                // Unexpected behavior, but better to output the error then appear to crash, and
+                // have Windows write it to the application event log.
+                Console.Error.WriteLine("Error: {0}", x.Message); // Not L10N
+                Console.Error.WriteLine(x.StackTrace);
+                return false;
+            }
+        }
+
+        private bool ParseArgsInternal(IEnumerable<string> args)
+        {
+            foreach (string s in args)
+            {
+                var pair = new NameValuePair(s);
+                if (string.IsNullOrEmpty(pair.Name))
+                    continue;
+
+                if (IsNameOnly(pair, ARG_UI))
+                {
+                    IsUi = true;
+                }
+                else if (IsNameValue(pair, ARG_ACG))
+                {
+                    if (pair.Value.ToLower() == AcgState.hide.ToString())
+                        Acg = AcgState.hide;
+                    else if (pair.Value.ToLower() == AcgState.none.ToString())
+                        Acg = AcgState.none;
+                    else if (pair.Value.ToLower() == AcgState.show.ToString())
+                        Acg = AcgState.show;
+                    else
+                    {
+                        Console.Error.WriteLine("Error: The value {0} for --{1} is not valid", pair.Value, pair.Name);
+                    }
+                }
+                else if (IsNameValue(pair, ARG_THREADS))
+                {
+                    int threads;
+                    if (!int.TryParse(pair.Value, out threads))
+                    {
+                        Console.Error.WriteLine("Error: The value {0} for --{1} must be an integer", pair.Value, pair.Name); // Not L10N
+                        return false;
+                    }
+                    Threads = threads;
+                }
+                else if (IsNameValue(pair, ARG_PROCESSES)) // Not L10N
+                {
+                    int processes;
+                    if (!int.TryParse(pair.Value, out processes))
+                    {
+                        Console.Error.WriteLine("Error: The value {0} for --{1} must be an integer", pair.Value, pair.Name); // Not L10N
+                        return false;
+                    }
+                    Processes = processes;
+                }
+                else if (IsNameValue(pair, ARG_SKYLINE_PATH))
+                {
+                    SkylinePath = pair.Value;
+                }
+                else if (IsNameValue(pair, ARG_FILE_PATH))
+                {
+                    FilePath = pair.Value;
+                }
+                else if (IsNameValue(pair, ARG_DATA_DIR))
+                {
+                    DataDir = pair.Value;
+                }
+                else if (IsNameValue(pair, ARG_DATA_FILTER))
+                {
+                    DataFilter = pair.Value;
+                }
+                else
+                {
+                    Console.Error.WriteLine("Error: Unexpected argument --{0}", pair.Name); // Not L10N
+                    return false;
+                }
+            }
+
+            if (string.IsNullOrEmpty(SkylinePath))
+            {
+                SkylinePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "SkylineDailyRunner.exe");
+            }
+
+            return true;
+        }
+
+        private bool IsNameOnly(NameValuePair pair, string name)
+        {
+            if (!pair.Name.Equals(name))
+                return false;
+            if (!string.IsNullOrEmpty(pair.Value))
+                throw new ValueUnexpectedException(name);
+            return true;
+        }
+
+        private static bool IsNameValue(NameValuePair pair, string name)
+        {
+            if (!pair.Name.Equals(name))
+                return false;
+            if (string.IsNullOrEmpty(pair.Value))
+                throw new ValueMissingException(name);
+            return true;
+        }
+
+        // ReSharper disable once UnusedMember.Local
+        private static bool IsName(NameValuePair pair, string name)
+        {
+            return pair.Name.Equals(name);
+        }
+
+        private class ValueMissingException : UsageException
+        {
+            public ValueMissingException(string name)
+                : base(string.Format("The argument --{0} requires a value and must be specified in the format --{0}=value", name)) // Not L10N
+            {
+            }
+        }
+
+        private class ValueUnexpectedException : UsageException
+        {
+            public ValueUnexpectedException(string name)
+                : base(string.Format("The argument --{0} should not have a value specified", name)) // Not L10N
+            {
+            }
+        }
+
+        private class UsageException : ArgumentException
+        {
+            protected UsageException(string message)
+                : base(message)
+            {
+            }
+        }
+
+        public struct NameValuePair
+        {
+            public NameValuePair(string arg)
+                : this()
+            {
+                if (arg.StartsWith("--")) // Not L10N
+                {
+                    arg = arg.Substring(2);
+                    int indexEqualsSign = arg.IndexOf('=');
+                    if (indexEqualsSign >= 0)
+                    {
+                        Name = arg.Substring(0, indexEqualsSign);
+                        Value = arg.Substring(indexEqualsSign + 1);
+                    }
+                    else
+                    {
+                        Name = arg;
+                    }
+                }
+            }
+
+            public string Name { get; private set; }
+            public string Value { get; private set; }
+            public int ValueInt { get { return int.Parse(Value); } }
+            public double ValueDouble { get { return double.Parse(Value); } }
+        }
+    }
+}
---
+++ pwiz_tools/Skyline/Executables/ImportPerf/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace ImportPerf
+{
+    class Program
+    {
+        private static CommandArgs _cmdArgs;
+        private static QueueWorker<List<string>> _queue;
+        private static Stopwatch _stopwatch;
+        
+        static void Main(string[] args)
+        {
+            _cmdArgs = new CommandArgs();
+            if (!_cmdArgs.ParseArgs(args))
+                return;
+
+            // Remove all SKYD files
+            foreach (var skydFile in Directory.EnumerateFiles(Path.GetDirectoryName(_cmdArgs.FilePath) ?? string.Empty, "*.sky"))
+            {
+                try
+                {
+                    File.Delete(skydFile);
+                }
+                catch (Exception)
+                {
+                    Console.Error.WriteLine("Error: Failed to remove existing file {0}", skydFile);
+                    return;
+                }
+            }
+
+            _stopwatch = new Stopwatch();
+            _stopwatch.Start();
+            _queue = new QueueWorker<List<string>>(Run);
+            _queue.RunAsync(_cmdArgs.Processes, "Start Skyline");
+
+            string dataFilter = "*.*";
+            if (!string.IsNullOrEmpty(_cmdArgs.DataFilter))
+                dataFilter = _cmdArgs.DataFilter;
+            string dataDir = ".";
+            if (!string.IsNullOrEmpty(_cmdArgs.DataDir))
+                dataDir = _cmdArgs.DataDir;
+            var fileGroupCurrent = new List<string>();
+            var fileGroups = new List<List<string>> {fileGroupCurrent};
+            foreach (var dataFile in Directory.EnumerateFiles(dataDir, dataFilter))
+            {
+                if (fileGroupCurrent.Count >= _cmdArgs.Threads)
+                {
+                    fileGroupCurrent = new List<string>();
+                    fileGroups.Add(fileGroupCurrent);
+                }
+                fileGroupCurrent.Add(dataFile);
+            }
+            _queue.Add(fileGroups, true);
+
+            Console.WriteLine("Elapsed time: " + _stopwatch.Elapsed.ToString(@"mm\:ss"));        
+        }
+
+        private static void Run(List<string> dataFiles, int threadIndex)
+        {
+            AddToLog(threadIndex, "Start " + GetFileNames(dataFiles));
+            var args = new List<string>(_cmdArgs.UiArgs)
+            {
+                "--timestamp",
+                "--in=\"" + _cmdArgs.FilePath + "\"",
+                "--import-no-join",
+                "--import-threads=" + dataFiles.Count
+            };
+            args.AddRange(dataFiles.Select(f => "--import-file=\"" + f + "\""));
+            string argsText = string.Join(" ", args);
+            AddToLog(threadIndex, argsText);
+            var psi = new ProcessStartInfo
+            {
+                FileName = _cmdArgs.SkylinePath,
+                Arguments = argsText,
+                UseShellExecute = false,
+            };
+            RunProcess(psi, threadIndex);
+            AddToLog(threadIndex, "End   " + GetFileNames(dataFiles));
+        }
+
+        private static string GetFileNames(List<string> dataFiles)
+        {
+            return string.Join(", ", dataFiles.Select(Path.GetFileName).ToArray());
+        }
+
+        private static void RunProcess(ProcessStartInfo psi, int threadIndex)
+        {
+            // Make sure required streams are redirected.
+            psi.RedirectStandardOutput = true;
+            psi.RedirectStandardError = true;
+
+            var proc = Process.Start(psi);
+            if (proc == null)
+                throw new IOException(string.Format("Failure starting {0} command.", psi.FileName)); // Not L10N
+
+            var reader = new ProcessStreamReader(proc);
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                AddToLog(threadIndex, line);
+            }
+            proc.WaitForExit();
+        }
+
+        private static readonly object LogLock = new object();
+
+        private static void AddToLog(int threadIndex, string line)
+        {
+            lock (LogLock)
+            {
+                Console.WriteLine(threadIndex + "> " + line);
+            }
+        }
+    }
+}
