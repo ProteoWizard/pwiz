f0ae34d9e383b9f80c494bee716e6d912f7a5695
pcbrefugee <pcbrefugee@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2016-04-04 21:49:25 +0000

    Skyline: adding a test to check whether or not the current test directory has been excluded from virus scanning or not.  When the test directory is being scanned it can lead to file locking problems, and is generally likely to slow down the test cycle.
    The test works by creating a little file that, while harmless, contains a standard string used for checking whether an antivirus program is active or not.  If the file disappears after being created, it's assumed that this is due to antivirus quarantine and the following failure message is emitted:
    
    Could not read contents of the (completely harmless!) antivirus test file "eicar_fake_threat.com", probably because it was quarantined by antivirus software.  If your antivirus flagged on "eicar_fake_threat.com", don't panic - that's part of the test (see http://www.eicar.org/86-0-Intended-use.html).  Now go exclude the test directory from further antivirus scrutiny, as it causes file locking problems in the tests.
    
    
    Former-commit-id: 99a84b6df11d39eee25b78f7d6858537789f75ab

---
+++ pwiz_tools/Skyline/Test/AntivirusExclusionTest.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * Original author: Brian Pratt <bspratt .at. proteinms dot net>,
+ *                  MacCoss Lab, Department of Genome Sciences, UW
+ *
+ * Copyright 2016 University of Washington - Seattle, WA
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pwiz.SkylineTestUtil;
+
+namespace pwiz.SkylineTest
+{
+    [TestClass]
+
+    // Test to make sure antivirus software is not monitoring the test directory by 
+    // creating a fake threat file containing a standard test string that's used to 
+    // test antivirus systems (See http://www.eicar.org/86-0-Intended-use.html for details).
+    // If the file is not there after we create it, we know we're being watched.
+    //
+    // This is necessary because antivirus systems slow down and even interfere with testing.
+    //
+    // On a system that's not configured properly this may set off alarms, but that's just as well
+
+    public class AntivirusExclusionTest : AbstractUnitTest
+    {
+        [TestMethod]
+        public void AntivirusTestExclusion()
+        {
+            var eicarTestString = @"X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"; // See http://www.eicar.org/86-0-Intended-use.html
+            var eicarTestFile = "eicar_fake_threat.com";
+            if (!File.Exists(eicarTestFile))
+            {
+                File.WriteAllText(eicarTestFile, eicarTestString); // If we are being watched, this should get removed immediately
+            }
+            string test = string.Empty;
+            try
+            {
+                test = File.ReadAllText(eicarTestFile); // This should succeed - if not we are probably under antivirus scrutiny, which can mess with other tests
+            }
+            catch
+            {
+                 // Do nothing
+            }
+            if (!eicarTestString.Equals(test))
+            {
+                Assert.Fail("Could not read contents of the (completely harmless!) antivirus test file \"eicar_fake_threat.com\", probably because it was quarantined by antivirus software.  If your antivirus flagged on \"eicar_fake_threat.com\", don't panic - that's part of the test (see http://www.eicar.org/86-0-Intended-use.html).  Now go exclude the test directory from further antivirus scrutiny, as it causes file locking problems in the tests.");
+            }
+        }
+    }
+}
