a4619fd7415754db64d2a6c2f8b14a4853d499fa
brendanx <brendanx@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2016-04-09 17:01:05 +0000

    Skyline: Update AssemblyInfo.cs for Skyline and SkylineNightly - SkylineNightly AssemblyCompany to University of Washington
    Former-commit-id: ac6db16dc8fb9164a0db64312a99ab010e5e7bce

--- pwiz_tools/Skyline/SkylineNightly/Properties/AssemblyInfo.cs
+++ pwiz_tools/Skyline/SkylineNightly/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SkylineNightly")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Nightly Test Runner")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("University of Washington")]
 [assembly: AssemblyProduct("SkylineNightly")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © University of Washington 2014-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
