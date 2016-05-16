4678078fc683bc70efc47b3c712e935a9a1e9da3
pcbrefugee <pcbrefugee@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2016-05-16 22:07:59 +0000

    Skyline: remove a redundant "using" for quiet code inspection
    Former-commit-id: 2b6976510050243ff564849811f9bc4b2a77644e

--- pwiz_tools/Skyline/TestFunctional/ExplicitRTTest.cs
+++ pwiz_tools/Skyline/TestFunctional/ExplicitRTTest.cs
@@ -20,7 +20,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using pwiz.Skyline.Controls.Graphs;
 using pwiz.Skyline.Model;
 using pwiz.Skyline.Model.Results;
 using pwiz.SkylineTestUtil;
