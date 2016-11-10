99efcca92e6fed5c690397dc7a363fb01de2a6fe
pcbrefugee <pcbrefugee@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2016-11-10 00:12:51 +0000

    Skyline (3.6 branch) - these tests were depending on default fullscan settings, which should be set explicitly for best reliability
    Former-commit-id: 93003ecd75176f2c4f748c0696f748cdfbd46514

--- pwiz_tools/Skyline/TestPerf/PerfImportResultsAgilentSpectrumMillIMSTest.cs
+++ pwiz_tools/Skyline/TestPerf/PerfImportResultsAgilentSpectrumMillIMSTest.cs
@@ -26,6 +26,7 @@ using pwiz.Common.SystemUtil;
 using pwiz.ProteowizardWrapper;
 using pwiz.Skyline.FileUI;
 using pwiz.Skyline.FileUI.PeptideSearch;
+using pwiz.Skyline.Model.DocSettings;
 using pwiz.Skyline.Model.Lib;
 using pwiz.Skyline.Model.Results;
 using pwiz.Skyline.SettingsUI;
@@ -131,6 +132,7 @@ namespace TestPerf // Note: tests in the "TestPerf" namespace only run when the 
             WaitForClosedForm(importResultsNameDlg);
             // Modifications are already set up, so that page should get skipped.
             RunUI(() => importPeptideSearchDlg.FullScanSettingsControl.PrecursorCharges = new []{2,3,4,5});
+            RunUI(() => importPeptideSearchDlg.FullScanSettingsControl.PrecursorMassAnalyzer = FullScanMassAnalyzerType.tof);
             RunUI(() => importPeptideSearchDlg.ClickNextButton()); // Accept the full scan settings
 
             // We're on the "Import FASTA" page of the wizard.
@@ -167,7 +169,7 @@ namespace TestPerf // Note: tests in the "TestPerf" namespace only run when the 
                 }
             }
             // ReSharper disable once ConditionIsAlwaysTrueOrFalse
-            Assert.AreEqual(useDriftTimes ? 1389115 : 1643104 , maxHeight, 1);
+            Assert.AreEqual(useDriftTimes ? 972186 : 1643104 , maxHeight, 1);
         }  
     }
 }
