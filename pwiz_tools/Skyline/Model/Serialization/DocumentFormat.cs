6c033ef0ebf49f9a827809042226f49cd13e7204
pcbrefugee <pcbrefugee@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2017-06-12 22:33:34 +0000

    Merged revision(s) 10939-10940 from branches/skyline_3_7: Fix NullReferenceException in MidasLibrary.Load (merge from trunk rev#10896) ........ Skyline (3_7): roll the schema version to 3.7 for release ........ Skyline (3.7): Change 0.05 to 0.01 in Chinese and Japanese translations for ComparePeakPickingDlg
    Former-commit-id: d91dba3d82f17ac63d2a2fcf2a8c829c116380b7

--- pwiz_tools/Skyline/Model/Serialization/DocumentFormat.cs
+++ pwiz_tools/Skyline/Model/Serialization/DocumentFormat.cs
@@ -48,9 +48,10 @@ namespace pwiz.Skyline.Model.Serialization
         public static readonly DocumentFormat  VERSION_3_55 = new DocumentFormat(3.55); // Adds linear width option for drift time filtering
         public static readonly DocumentFormat  VERSION_3_6 = new DocumentFormat(3.6); // Improved full-scan settings for DIA isolation schemes and add selective extraction
         public static readonly DocumentFormat  VERSION_3_61 = new DocumentFormat(3.61); //  Semi-cleavage enzymes and ion mobility CCS work, points across peak
-        public static readonly DocumentFormat VERSION_3_62 = new DocumentFormat(3.62);
-        public static readonly DocumentFormat  CURRENT = VERSION_3_62;
-        public static readonly DocumentFormat BINARY_RESULTS = VERSION_3_62;
+        public static readonly DocumentFormat VERSION_3_62 = new DocumentFormat(3.62); // Adds binary results 
+        public static readonly DocumentFormat BINARY_RESULTS = VERSION_3_62; // First version that had binary results
+        public static readonly DocumentFormat VERSION_3_7 = new DocumentFormat(3.7); // Release format
+        public static readonly DocumentFormat CURRENT = VERSION_3_7; 
 
 
         private readonly double _versionNumber;
