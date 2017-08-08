ebf75d6aa4a69c99a57e5515111c5a143542b5ae
pcbrefugee <pcbrefugee@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2017-08-08 01:09:47 +0000

    Yet another attempt at including libraries/doctest.h in the autotools bundle (debugging via TeamCity is tedious!)
    Former-commit-id: e47aa3a0bedca800e04ed16089f6032f9bfccf85

--- scripts/autotools/generate_autoconf.py
+++ scripts/autotools/generate_autoconf.py
@@ -278,10 +278,6 @@ if (not dryrun) :
 				addShipDir(shipdir,addTree=True)
 				break
 			
-	# make sure doctest.h etc get shipped
-	for inc in ac.explicitIncludes: 
-		addShipDir(ac.get_pwizroot()+"/"+inc)
-			
 	for d in ac.subtleIncludes : # any others not mentioned?
 		addShipDir(ac.get_pwizroot()+"/"+d,addTree=True)
 
@@ -303,6 +299,16 @@ if (not dryrun) :
 					if dbug :
 						print ("add "+f+" as "+tarname)
 					z.add(f,tarname)
+
+	# make sure doctest.h etc get shipped
+	for inc in ac.explicitIncludes: 
+		f = ac.get_pwizroot()+"/"+inc
+		if (os.path.exists(f)) :
+			tarname = ac.replace_pwizroot(f,"pwiz")
+			if dbug :
+				print ("add "+f+" as "+tarname)
+			z.add(f,tarname)
+					
 	for file in os.listdir(logdir) : # assume we also did the autoconf stuff in this dir
 		f = logdir+"/"+file
 		ext = file.partition(".")[2]
