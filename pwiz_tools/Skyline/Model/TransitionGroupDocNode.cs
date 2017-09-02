27456de0da21c9a42863e62b6c0178b586d4604a
pcbrefugee <pcbrefugee@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2017-09-02 22:14:58 +0000

    Skyline (3_7 branch): more general fix for https://skyline.ms/announcements/home/issues/exceptions/thread.view?rowId=31265 , where Skyline could erroneously throw an Assume exception when reimporting results with an altered drift time filter.
    Former-commit-id: 2d977f605ef85794c7b998c8df8056f15485a591

--- pwiz_tools/Skyline/Model/TransitionGroupDocNode.cs
+++ pwiz_tools/Skyline/Model/TransitionGroupDocNode.cs
@@ -2099,7 +2099,6 @@ namespace pwiz.Skyline.Model
                 OptimizationStep = optimizationStep;
                 TransitionCount = transitionCount;
                 UserSet = UserSet.FALSE;
-                DriftTimeInfo = TransitionGroupDriftTimeInfo.EMPTY;   // This will repopulate by inspection
 
                 // Use existing ratios, annotations and peak scores
                 if (chromInfo != null)
@@ -2109,11 +2108,15 @@ namespace pwiz.Skyline.Model
 
                     Ratios = chromInfo.Ratios;
                     Annotations = chromInfo.Annotations;
+
+                    DriftTimeInfo = chromInfo.DriftInfo;
                 }
                 else
                 {
                     Ratios = TransitionGroupChromInfo.GetEmptyRatios(ratioCount);
                     Annotations = Annotations.EMPTY;
+
+                    DriftTimeInfo = TransitionGroupDriftTimeInfo.EMPTY; 
                 }
 
                 if (reintegratePeak != null)
