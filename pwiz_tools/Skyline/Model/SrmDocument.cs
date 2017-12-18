3615f4585234299c1f68c091a7f2b7aa95e8fd3b
brendanx <brendanx@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2017-12-18 22:43:52 +0000

    Skyline: Further reduce max transition count to pass all tests on 32-bit build
    Former-commit-id: 22321c57de31f56dddf1821a6ad4597ac0d5828d

--- pwiz_tools/Skyline/Model/SrmDocument.cs
+++ pwiz_tools/Skyline/Model/SrmDocument.cs
@@ -242,7 +242,7 @@ namespace pwiz.Skyline.Model
         public const int MAX_PEPTIDE_COUNT = 200 * 1000;
         public const int MAX_TRANSITION_COUNT = 5 * 1000 * 1000;
 
-        public static readonly int _maxTransitionCount = Install.Is64Bit ? MAX_TRANSITION_COUNT : MAX_TRANSITION_COUNT/2;   // To keep from running out of memory on 32-bit
+        public static readonly int _maxTransitionCount = Install.Is64Bit ? MAX_TRANSITION_COUNT : MAX_TRANSITION_COUNT/5;   // To keep from running out of memory on 32-bit
 
         public static int MaxTransitionCount
         {
