2eb0635f88fb22f557f8cc35220ae5d0c2778081
chambm <chambm@ca36993f-311d-43bf-b7bd-f3fd426d09d2>
2018-01-15 16:52:55 +0000

    * added UNIFI browser form files
    Former-commit-id: 6be22bdc4e248f0bd4cc15d5ae5a384834442c25

---
+++ pwiz_tools/MSConvertGUI/UnifiBrowserForm.Designer.cs
@@ -0,0 +1,267 @@
+﻿//
+// $Id$
+//
+//
+// Original author: Matt Chambers <matt.chambers42 .@. gmail.com>
+//
+// Copyright 2018 Matt Chambers - Nashville, TN 37221
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+//
+
+namespace MSConvertGUI
+{
+    partial class UnifiBrowserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UnifiBrowserForm));
+            this.VertSplit = new System.Windows.Forms.SplitContainer();
+            this.FileTree = new System.Windows.Forms.TreeView();
+            this.FolderViewList = new System.Windows.Forms.ListView();
+            this.SourceName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SourceSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SourceType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DateModified = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.labelSourcePath = new System.Windows.Forms.Label();
+            this.serverLocationTextBox = new System.Windows.Forms.TextBox();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.openButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sampleResultTextBox = new System.Windows.Forms.TextBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.treeViewImageList = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.VertSplit)).BeginInit();
+            this.VertSplit.Panel1.SuspendLayout();
+            this.VertSplit.Panel2.SuspendLayout();
+            this.VertSplit.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // VertSplit
+            // 
+            this.VertSplit.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.VertSplit.Location = new System.Drawing.Point(12, 31);
+            this.VertSplit.Name = "VertSplit";
+            // 
+            // VertSplit.Panel1
+            // 
+            this.VertSplit.Panel1.Controls.Add(this.FileTree);
+            // 
+            // VertSplit.Panel2
+            // 
+            this.VertSplit.Panel2.Controls.Add(this.FolderViewList);
+            this.VertSplit.Size = new System.Drawing.Size(919, 549);
+            this.VertSplit.SplitterDistance = 289;
+            this.VertSplit.TabIndex = 44;
+            this.VertSplit.TabStop = false;
+            // 
+            // FileTree
+            // 
+            this.FileTree.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FileTree.LabelEdit = true;
+            this.FileTree.Location = new System.Drawing.Point(0, 0);
+            this.FileTree.Name = "FileTree";
+            this.FileTree.Size = new System.Drawing.Size(289, 549);
+            this.FileTree.TabIndex = 33;
+            this.FileTree.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.FileTree_AfterSelect);
+            // 
+            // FolderViewList
+            // 
+            this.FolderViewList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.SourceName,
+            this.SourceSize,
+            this.SourceType,
+            this.DateModified});
+            this.FolderViewList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FolderViewList.Location = new System.Drawing.Point(0, 0);
+            this.FolderViewList.Name = "FolderViewList";
+            this.FolderViewList.Size = new System.Drawing.Size(626, 549);
+            this.FolderViewList.TabIndex = 24;
+            this.FolderViewList.UseCompatibleStateImageBehavior = false;
+            this.FolderViewList.View = System.Windows.Forms.View.Details;
+            this.FolderViewList.SelectedIndexChanged += new System.EventHandler(this.FolderViewList_SelectedIndexChanged);
+            this.FolderViewList.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.FolderViewList_MouseDoubleClick);
+            // 
+            // SourceName
+            // 
+            this.SourceName.Text = "Name";
+            this.SourceName.Width = 200;
+            // 
+            // SourceSize
+            // 
+            this.SourceSize.Text = "Size";
+            // 
+            // SourceType
+            // 
+            this.SourceType.Text = "Type";
+            this.SourceType.Width = 80;
+            // 
+            // DateModified
+            // 
+            this.DateModified.Text = "Date Modified";
+            this.DateModified.Width = 140;
+            // 
+            // labelSourcePath
+            // 
+            this.labelSourcePath.AutoSize = true;
+            this.labelSourcePath.Location = new System.Drawing.Point(12, 9);
+            this.labelSourcePath.Name = "labelSourcePath";
+            this.labelSourcePath.Size = new System.Drawing.Size(81, 13);
+            this.labelSourcePath.TabIndex = 46;
+            this.labelSourcePath.Text = "&Server location:";
+            this.labelSourcePath.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // serverLocationTextBox
+            // 
+            this.serverLocationTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.serverLocationTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.serverLocationTextBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+            this.serverLocationTextBox.Location = new System.Drawing.Point(102, 5);
+            this.serverLocationTextBox.Name = "serverLocationTextBox";
+            this.serverLocationTextBox.Size = new System.Drawing.Size(757, 20);
+            this.serverLocationTextBox.TabIndex = 45;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(865, 586);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(66, 21);
+            this.cancelButton.TabIndex = 50;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // openButton
+            // 
+            this.openButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.openButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.openButton.Location = new System.Drawing.Point(793, 586);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(66, 21);
+            this.openButton.TabIndex = 49;
+            this.openButton.Text = "&Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 590);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 48;
+            this.label1.Text = "Sample &Result ID:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // sampleResultTextBox
+            // 
+            this.sampleResultTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sampleResultTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.sampleResultTextBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+            this.sampleResultTextBox.Location = new System.Drawing.Point(102, 586);
+            this.sampleResultTextBox.Name = "sampleResultTextBox";
+            this.sampleResultTextBox.Size = new System.Drawing.Size(685, 20);
+            this.sampleResultTextBox.TabIndex = 47;
+            // 
+            // connectButton
+            // 
+            this.connectButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.connectButton.Location = new System.Drawing.Point(865, 5);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(66, 21);
+            this.connectButton.TabIndex = 51;
+            this.connectButton.Text = "&Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            // 
+            // treeViewImageList
+            // 
+            this.treeViewImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("treeViewImageList.ImageStream")));
+            this.treeViewImageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.treeViewImageList.Images.SetKeyName(0, "MyNetworkPlaces.png");
+            this.treeViewImageList.Images.SetKeyName(1, "folder.png");
+            this.treeViewImageList.Images.SetKeyName(2, "DataProcessing.png");
+            // 
+            // UnifiBrowserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 618);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sampleResultTextBox);
+            this.Controls.Add(this.labelSourcePath);
+            this.Controls.Add(this.serverLocationTextBox);
+            this.Controls.Add(this.VertSplit);
+            this.Name = "UnifiBrowserForm";
+            this.Text = "UNIFI Browser";
+            this.VertSplit.Panel1.ResumeLayout(false);
+            this.VertSplit.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.VertSplit)).EndInit();
+            this.VertSplit.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer VertSplit;
+        private System.Windows.Forms.TreeView FileTree;
+        private System.Windows.Forms.ListView FolderViewList;
+        private System.Windows.Forms.ColumnHeader SourceName;
+        private System.Windows.Forms.ColumnHeader SourceSize;
+        private System.Windows.Forms.ColumnHeader SourceType;
+        private System.Windows.Forms.ColumnHeader DateModified;
+        private System.Windows.Forms.Label labelSourcePath;
+        private System.Windows.Forms.TextBox serverLocationTextBox;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox sampleResultTextBox;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.ImageList treeViewImageList;
+
+    }
+}
---
+++ pwiz_tools/MSConvertGUI/UnifiBrowserForm.cs
@@ -0,0 +1,231 @@
+﻿//
+// $Id$
+//
+//
+// Original author: Matt Chambers <matt.chambers42 .@. gmail.com>
+//
+// Copyright 2018 Matt Chambers - Nashville, TN 37221
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+//
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using IdentityModel.Client;
+
+namespace MSConvertGUI
+{
+    public partial class UnifiBrowserForm : Form
+    {
+        private const string IdentityServerBaseAddress = "https://unifiapi.waters.com:50333/identity";
+        private const string AuthorizeEndpoint = IdentityServerBaseAddress + "/connect/authorize";
+        private const string LogoutEndpoint = IdentityServerBaseAddress + "/connect/endsession";
+        private const string TokenEndpoint = IdentityServerBaseAddress + "/connect/token";
+        private const string UserInfoEndpoint = IdentityServerBaseAddress + "/connect/userinfo";
+        private const string IdentityTokenValidationEndpoint = IdentityServerBaseAddress + "/connect/identitytokenvalidation";
+        private const string TokenRevocationEndpoint = IdentityServerBaseAddress + "/connect/revocation";
+
+        //private const string Host = "unifiapi.waters.com:50034";
+        private const string BasePath = "/unifi/v1";
+
+        private string _accessToken;
+        private string _username, _password;
+        private ImageList _nodeImages;
+        private HttpClient _httpClient;
+        private Dictionary<string, TreeNode> _nodeById;
+
+        public IEnumerable<string> SelectedSampleResults;
+
+        public UnifiBrowserForm()
+        {
+            InitializeComponent();
+
+            connectButton.Click += connectButton_Click;
+            openButton.Click += openButton_Click;
+            cancelButton.Click += cancelButton_Click;
+
+            _nodeImages = treeViewImageList;
+            FileTree.ImageList = _nodeImages;
+
+            FolderViewList.LargeImageList = FolderViewList.SmallImageList = treeViewImageList;
+            FolderViewList.Columns.Remove(SourceSize);
+
+            _httpClient = new HttpClient();
+
+            openButton.Enabled = false;
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+
+            serverLocationTextBox.Text = "unifiapi.waters.com:50034";
+            connectButton.PerformClick();
+        }
+
+        JObject GetJsonFromEndpoint(string endpoint)
+        {
+            string url = serverLocationTextBox.Text + endpoint;
+
+            //execute web api call
+            HttpResponseMessage responseMessage = _httpClient.GetAsync(url).Result;
+            if (!responseMessage.IsSuccessStatusCode)
+            {
+                Console.WriteLine(responseMessage.ToString());
+                throw new Exception("error getting response from UNIFI server: " + responseMessage.ToString());
+            }
+
+            string responseBody = responseMessage.Content.ReadAsStringAsync().Result;
+
+            return JObject.Parse(responseBody);
+        }
+
+        void GetFolders()
+        {
+            JObject jobject = GetJsonFromEndpoint("/folders");
+            JArray folders = jobject["value"] as JArray;
+            _nodeById = new Dictionary<string, TreeNode>();
+            foreach (JObject folder in folders)
+            {
+                var path = folder.Property("path").Value.ToString();
+                var id = folder.Property("id").Value.ToString();
+                JToken parentIdProperty;
+                if (!folder.TryGetValue("parentId", out parentIdProperty) || (parentIdProperty as JValue).Value == null)
+                {
+                    _nodeById[id] = FileTree.TopNode.Nodes.Add(id, System.IO.Path.GetFileName(path), 1);
+                }
+                else
+                {
+                    var parentId = (parentIdProperty as JValue).Value.ToString();
+                    var parentNode = _nodeById[parentId];
+                    _nodeById[id] = parentNode.Nodes.Add(id, System.IO.Path.GetFileName(path), 1);
+                    _nodeById[id].Tag = "folder";
+                }
+            }
+        }
+
+        void connectButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                while (true)
+                {
+                    var loginForm = new LoginForm() { StartPosition = FormStartPosition.CenterParent };
+                    if (loginForm.ShowDialog(this) == DialogResult.Cancel)
+                        return;
+                    _username = loginForm.usernameTextBox.Text;
+                    _password = loginForm.passwordTextBox.Text;
+
+                    TokenClient client = new TokenClient(TokenEndpoint, "resourceownerclient", "secret");
+                    TokenResponse response = client.RequestResourceOwnerPasswordAsync(_username, _password, "unifi").Result;
+                    if (response.IsError)
+                    {
+                        if (MessageBox.Show(this, "Username or password are not correct!", "Error", MessageBoxButtons.RetryCancel) == DialogResult.Cancel)
+                            return;
+                    }
+                    else
+                    {
+                        _accessToken = response.AccessToken;
+                        _httpClient.SetBearerToken(_accessToken);
+                        _httpClient.DefaultRequestHeaders.Remove("Accept");
+                        _httpClient.DefaultRequestHeaders.Add("Accept", "application/json;odata.metadata=minimal");
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                MessageBox.Show(this, "Server is not available", "Login Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            string url = serverLocationTextBox.Text;
+            if (!url.StartsWith("http"))
+                url = "https://" + url;
+            if (!url.EndsWith(BasePath))
+                url += BasePath;
+            serverLocationTextBox.Text = url;
+            serverLocationTextBox.ReadOnly = true;
+
+            FileTree.Nodes.Clear();
+            FileTree.Nodes.Add("host", new Uri(url).Host, 0);
+            GetFolders();
+            FileTree.ExpandAll();
+        }
+
+        void openButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+
+            SelectedSampleResults = FolderViewList.SelectedItems.Cast<ListViewItem>().Select(o => serverLocationTextBox.Text.Replace("://", String.Format("://{0}:{1}@", _username, _password)) + (o.Tag as string));
+        }
+
+        void cancelButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+        }
+
+        private void FileTree_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if(e.Node.Tag == null)
+                return;
+
+            if (e.Node.Tag.ToString() == "folder")
+            {
+                FolderViewList.Items.Clear();
+                JObject jobject = GetJsonFromEndpoint(String.Format("/folders({0})/items", e.Node.Name));
+                JArray items = jobject["value"] as JArray;
+                foreach (JObject item in items)
+                {
+                    var type = item.Property("type").Value.ToString();
+
+                    // only display sample results
+                    if (type != "SampleResult")
+                        continue;
+
+                    var name = item.Property("name").Value.ToString();
+                    var id = item.Property("id").Value.ToString();
+                    var created = item.Property("createdAt").Value.ToString();
+
+                    FolderViewList.Items.Add(new ListViewItem(new string[] { name, type, created }, 2) { Tag = String.Format("/sampleresults({0})", id) });
+                }
+            }
+        }
+
+        private void FolderViewList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            openButton.Enabled = true;
+
+            sampleResultTextBox.Text = "\"" + String.Join("\" \"", FolderViewList.SelectedItems.Cast<ListViewItem>().Select(o => o.Tag as string)) + "\"";
+        }
+
+        private void FolderViewList_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            openButton_Click(sender, e);
+        }
+    }
+}
