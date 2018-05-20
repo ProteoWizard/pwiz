105ecc183b7297910149ae1e66413cf6b9ecb167
Nicholas Shulman <nicksh@uw.edu>
2018-05-20 14:44:10 -0700

    Creating a pull request to check whether the "UNIFI_PASSWORD" environment variable is set on the TeamCity computers.

---
+++ pwiz_tools/Skyline/TestA/TestUnifiPassword.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pwiz.SkylineTestUtil;
+
+namespace pwiz.SkylineTestA
+{
+    [TestClass]
+    public class TestUnifiPassword : AbstractUnitTest
+    {
+        /// <summary>
+        /// Test which checks whether the environment variable "UNIFI_PASSWORD" is set.
+        /// I am hoping this test fails on Team City since it will indicate that the server
+        /// is set up for me to write my unifi tests in another branch.
+        /// </summary>
+        [TestMethod]
+        public void TestUnifiPasswordEnvironmentVariable()
+        {
+            Assert.IsNull(Environment.GetEnvironmentVariable("UNIFI_PASSWORD"));
+        }
+    }
+}
