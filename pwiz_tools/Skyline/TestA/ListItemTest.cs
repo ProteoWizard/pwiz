64dacabc441cead5e2fa90a015a348180f981839
Nicholas Shulman <nicksh@uw.edu>
2018-07-13 17:02:26 -0700

    Fix redundant using statements.

--- pwiz_tools/Skyline/TestA/ListItemTest.cs
+++ pwiz_tools/Skyline/TestA/ListItemTest.cs
@@ -1,10 +1,23 @@
-﻿using System.Collections.Generic;
+﻿/*
+ * Original author: Nicholas Shulman <nicksh .at. u.washington.edu>,
+ *                  MacCoss Lab, Department of Genome Sciences, UW
+ *
+ * Copyright 2018 University of Washington - Seattle, WA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using pwiz.Common.DataBinding;
-using pwiz.Skyline.Model;
-using pwiz.Skyline.Model.Databinding;
 using pwiz.Skyline.Model.Lists;
-using pwiz.Skyline.Properties;
 using pwiz.SkylineTestUtil;
 
 namespace pwiz.SkylineTestA
@@ -19,16 +32,6 @@ namespace pwiz.SkylineTestA
             const string listName = "My&Li.s&/\\t  \t\r\n";
             var type = listItemTypes.GetListItemType(listName);
             Assert.IsNotNull(type);
-//            var constructor =
-//            Assert.IsNotNull(constructor);
-//            var listItemId = new ListItemId(100);
-//            var listData = new ListData(new ListDef(listName), new ColumnData[0]);
-//            var instance = constructor.Invoke(new object[] {listData, listItemId});
-//            Assert.IsNotNull(instance);
-//            Assert.IsInstanceOfType(instance, typeof(ListItem));
-//            Assert.AreNotEqual(typeof(ListItem), instance.GetType());
-//            var listItem = (ListItem) instance;
-//            Assert.AreEqual(listName, listItemTypes.GetListName(listItem.GetType()));
         }
     }
 }
