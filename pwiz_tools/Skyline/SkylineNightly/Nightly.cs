bc6d497912263df8fd30f54a23cdddc7087b0cf6
Brian Pratt <bspratt@proteinms.net>
2018-08-02 10:59:06 -0700

    SkylineTester: change in strategy to avoid reporting handle leaks (this currently confuses the test server) - jsut raise the threshold in TestRunner rather than tweaking the log parsing in SkylineNightly. If we don't report any leaks we won't cause trouble at the server, so this doesn't require a general redeployment of SkylineNightly.exe (though that's less troublesome with the SkylineNIghtlyShim work in my previous commit).

--- pwiz_tools/Skyline/SkylineNightly/Nightly.cs
+++ pwiz_tools/Skyline/SkylineNightly/Nightly.cs
@@ -39,8 +39,6 @@ namespace SkylineNightly
     // ReSharper disable NonLocalizedString
     public class Nightly
     {
-        private const bool SUPPORT_HANDLE_LEAK_TRACKING = false; // TODO restore this once test server is updated to deal with handle leak info
-
 
         private const string NIGHTLY_TASK_NAME = "Skyline nightly build";
 
@@ -605,13 +603,10 @@ namespace SkylineNightly
                     test["duration"] = duration;
                     test["managed"] = managed;
                     test["total"] = total;
-                    if (SUPPORT_HANDLE_LEAK_TRACKING) // TODO restore this once test server is updated to deal with handle leak info
-                    {
-                        if (!string.IsNullOrEmpty(user))
-                            test["user"] = user;
-                        if (!string.IsNullOrEmpty(gdi))
-                            test["gdi"] = gdi;
-                    }
+                    if (!string.IsNullOrEmpty(user))
+                        test["user"] = user;
+                    if (!string.IsNullOrEmpty(gdi))
+                        test["gdi"] = gdi;
                 }
 
                 testCount++;
@@ -658,16 +653,13 @@ namespace SkylineNightly
                 leak["name"] = match.Groups[1].Value;
                 leak["bytes"] = match.Groups[2].Value;
             }
-            if (SUPPORT_HANDLE_LEAK_TRACKING) // TODO restore this once test server is updated to deal with handle leak info
+            var leakHandlesPattern = new Regex(@"!!! (\S+) HANDLE-LEAKED ([.0-9]+) (\S+)", RegexOptions.Compiled);
+            for (var match = leakHandlesPattern.Match(log); match.Success; match = match.NextMatch())
             {
-                var leakHandlesPattern = new Regex(@"!!! (\S+) HANDLE-LEAKED ([.0-9]+) (\S+)", RegexOptions.Compiled);
-                for (var match = leakHandlesPattern.Match(log); match.Success; match = match.NextMatch())
-                {
-                    var leak = _leaks.Append("leak");
-                    leak["name"] = match.Groups[1].Value;
-                    leak["handles"] = match.Groups[2].Value;
-                    leak["type"] = match.Groups[3].Value;
-                }
+                var leak = _leaks.Append("leak");
+                leak["name"] = match.Groups[1].Value;
+                leak["handles"] = match.Groups[2].Value;
+                leak["type"] = match.Groups[3].Value;
             }
         }
 
