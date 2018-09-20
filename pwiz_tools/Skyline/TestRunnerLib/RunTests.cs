db23311499b5e211ad004160329906f19ed270a9
brendanx67 <brendanx@proteinms.net>
2018-09-20 15:31:35 -0700

    Revert "Skyline - enumerating handles"
    This reverts commit 81ce5aac1c2416ac6fb89b0c21f4f696ae39652c.

--- pwiz_tools/Skyline/TestRunnerLib/RunTests.cs
+++ pwiz_tools/Skyline/TestRunnerLib/RunTests.cs
@@ -29,7 +29,6 @@ using System.Runtime.InteropServices;
 using System.Threading;
 using log4net;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using pwiz.Common.Collections;
 using pwiz.Common.SystemUtil;
 
 namespace TestRunnerLib
@@ -158,12 +157,6 @@ namespace TestRunnerLib
             return Path.Combine(runnerExeDirectory, assembly);
         }
 
-        public IEnumerable<T> Enumerate<T>(IEnumerator<T> enumerator)
-        {
-            while (enumerator.MoveNext())
-                yield return enumerator.Current;
-        }
-
         public bool Run(TestInfo test, int pass, int testNumber)
         {
             if (_showStatus)
@@ -276,11 +269,6 @@ namespace TestRunnerLib
                     LastTotalHandleCount,
                     LastTestDuration);
 //                Log("# Heaps " + string.Join("\t", heapCounts.Select(s => s.ToString())) + "\r\n");
-
-                HandleEnumeratorWrapper handleEnumerator = new HandleEnumeratorWrapper();
-                var counts = Enumerate(handleEnumerator).GroupBy(h => h.Type).OrderBy(g => g.Key);
-                Log(string.Join("," + Environment.NewLine, counts.Select(c => c.Key + ": " + c.Count())) + Environment.NewLine);
-
                 if (crtLeakedBytes > CheckCrtLeaks)
                     Log("!!! {0} CRT-LEAKED {1} bytes\r\n", test.TestMethod.Name, crtLeakedBytes);
 
