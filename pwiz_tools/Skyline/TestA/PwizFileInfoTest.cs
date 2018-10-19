c6d8132720d8c6e3914f1c34cc9d78b7b9b09397
Matt Chambers <matt.chambers42@gmail.com>
2018-10-19 12:45:29 -0400

    Update WIFF2 SDK to SciexToolkit (#270)
    * - updated WIFF2 SDK to SciexToolkit which avoids us needing to maintain weird application .config files (the DLL now creates those on the fly); unfortunately this update has 2 drawbacks: 1) cannot get SIM targets, and 2) WIFF1 files are not closed properly (a problem which I solved with `pwiz::util::force_close_handles_for_filepath()`); a future SDK update will fix these issues
    - fixed Analysis.yep file remaining locked after closing MSData
    * reverted System.Data.SQLite.dll to 1.0.98 to avoid loading multiple versions of SQLite into same AppDomain
    * edited UIMF DLLs to point at reverted SQLite DLL
    * fixed WIFF2 files not being included in Reader_ABI_Test
    * set Skyline to always use msparser.dll (msparserD.dll should only be used with a full C++ debug build)
    * set AutoGenerateBindingRedirects=false for Skyline project files, although this may not be necessary with everything using the same SQLite version
    * added pwiz::util::force_close_handles_for_filepath()
    * fixed some compiler warnings about dtor exceptions
    * fixed 32-bit build not including TIMS support (I could swear I already did this but can't find it!)
    * moved RawFile::Lock to cpp_cli_utilities.hpp
    * changed locked files to be a fatal error in VendorReaderTestHarness (no vendor should be leaving locked files now)
    * removed VC10 DLLs from WiX installers
    
    * * removed preload library
    
    * * reverted SQLite x86 DLL to 1.0.98.0
    * updated WiX templates to SciexToolKit DLLs
    
    * * fixed Linux compile
    * restored SQLite DLLs in subdirectory for SciexToolKit; still not sure whether they're really needed or not, but in any case SciexToolKit does not work on x86
    * added GetFileHandleTypeNumber() since file handle types differ between different versions of Windows; it's a heuristic because I haven't found a definitive list of them
    
    * - reverted to previous Sciex SDK for WIFF1 files, which restores 32-bit support and avoid file locking issues (WIFF2 only works on 64-bit though)
    * disabled WIFF2 unit tests in 32-bit builds
    * fixed Linux compile
    
    * * switched from GetFinalPathNameByHandleW to GetMappedFileNameW because I could not get the former to work on Windows Server 2012 (TeamCity)
    * fixed filename comparison case sensitivity when copying assemblies (was causing SQLite copy errors)
    
    * - added check for French-style number settings which currently break the WIFF2 SciexToolKit API
    * changed Skyline's WIFF2 test to be conditioned on CultureInfo.NumberFormat (until 2019 when the test will start running unconditionally)
    
    * Update file list for Skyline Adminstrator Installer.
    
    * Skyline: Turn off wiff2 test when Skyline is offscreen (until 2019) to avoid the Thread and Event handle leak it causes

--- pwiz_tools/Skyline/TestA/PwizFileInfoTest.cs
+++ pwiz_tools/Skyline/TestA/PwizFileInfoTest.cs
@@ -17,9 +17,11 @@
  * limitations under the License.
  */
 
+using System.Globalization;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using pwiz.ProteowizardWrapper;
+using pwiz.Skyline;
 using pwiz.SkylineTestUtil;
 
 namespace pwiz.SkylineTestA
@@ -52,6 +54,12 @@ namespace pwiz.SkylineTestA
             {
                 VerifyInstrumentInfo(testFilesDir.GetTestPath("051309_digestion.wiff"),
                     "4000 QTRAP", "electrospray ionization", "quadrupole/quadrupole/axial ejection linear ion trap", "electron multiplier");
+                if (System.DateTime.Now.Year > 2018 /* start failing after the new year */ ||
+                    (System.Environment.Is64BitProcess && !Program.SkylineOffscreen &&  /* wiff2 access leaks thread and event handles, so avoid it during nightly tests when offscreen */
+                     (CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator != "," || /* wiff2 access fails under french language settings */
+                      CultureInfo.CurrentCulture.NumberFormat.NumberGroupSeparator != "\xA0")) /* no break space */ )
+                    VerifyInstrumentInfo(testFilesDir.GetTestPath("OnyxTOFMS.wiff2"),
+                        "TripleTOF 5600", "electrospray ionization", "quadrupole/quadrupole/time-of-flight", "electron multiplier");
             }
 
             // MzWiff generated mzXML files
