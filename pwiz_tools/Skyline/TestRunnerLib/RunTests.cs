656342f568a604f803472aefb87931ef6f2ba35f
Matt Chambers <matt.chambers42@gmail.com>
2018-10-30 16:03:09 -0400

    * refactored TeamCity status messages as functions

--- pwiz_tools/Skyline/TestRunnerLib/RunTests.cs
+++ pwiz_tools/Skyline/TestRunnerLib/RunTests.cs
@@ -175,8 +175,7 @@ namespace TestRunnerLib
 
         public bool Run(TestInfo test, int pass, int testNumber, string dmpDir)
         {
-            if (TeamCityTestDecoration)
-                Console.WriteLine(@"##teamcity[testStarted name='{0}' captureStandardOutput='true']", test.TestMethod.Name + "-" + Language.TwoLetterISOLanguageName);
+            TeamCityStartTest(test);
 
             if (_showStatus)
                 Log("#@ Running {0} ({1})...\n", test.TestMethod.Name, Language.TwoLetterISOLanguageName);
@@ -352,8 +351,7 @@ namespace TestRunnerLib
                 if (crtLeakedBytes > CheckCrtLeaks)
                     Log("!!! {0} CRT-LEAKED {1} bytes\r\n", test.TestMethod.Name, crtLeakedBytes);
 
-                if (TeamCityTestDecoration)
-                    Console.WriteLine(@"##teamcity[testFinished name='{0}' duration='{1}']", test.TestMethod.Name + "-" + Language.TwoLetterISOLanguageName, LastTestDuration);
+                TeamCityFinishTest(test);
 
                 using (var writer = new FileStream("TestRunnerMemory.log", FileMode.Append, FileAccess.Write, FileShare.Read))
                 using (var stringWriter = new StreamWriter(writer))
@@ -379,22 +377,7 @@ namespace TestRunnerLib
             else
                 ErrorCounts[failureInfo] = 1;
 
-            if (TeamCityTestDecoration)
-            {
-                // ReSharper disable LocalizableElement
-                var tcMessage = new System.Text.StringBuilder(message);
-                tcMessage.AppendLine();
-                tcMessage.AppendLine(stackTrace);
-                tcMessage.Replace("'", "|'");
-                tcMessage.Replace("\n", "|n");
-                tcMessage.Replace("\r", "|r");
-                tcMessage.Replace("|", "||");
-                tcMessage.Replace("[", "|[");
-                tcMessage.Replace("]", "|]");
-                Console.WriteLine("##teamcity[testFailed name='{0}' message='{1}']", test.TestMethod.Name + "-" + Language.TwoLetterISOLanguageName, tcMessage);
-                Console.WriteLine("##teamcity[testFinished name='{0}' duration='{1}']", test.TestMethod.Name + "-" + Language.TwoLetterISOLanguageName, LastTestDuration * 1000);
-                // ReSharper enable LocalizableElement
-            }
+            TeamCityFinishTest(test, message + '\n' + stackTrace);
 
             Log("{0,3} failures, {1:F2}/{2:F2}/{3:F1} MB, {4}/{5} handles, {6} sec.\r\n\r\n!!! {7} FAILED\r\n{8}\r\n{9}\r\n!!!\r\n\r\n",
                 FailureCount,
@@ -594,6 +577,34 @@ namespace TestRunnerLib
                 _log.Write(info, args);
                 _log.Flush();
             }
+        }
+
+        public void TeamCityStartTest(TestInfo test)
+        {
+            if (TeamCityTestDecoration)
+                Console.WriteLine(@"##teamcity[testStarted name='{0}' captureStandardOutput='true']", test.TestMethod.Name + "-" + Language.TwoLetterISOLanguageName);
+        }
+
+        public void TeamCityFinishTest(TestInfo test, string errorMessage = null)
+        {
+            if (!TeamCityTestDecoration)
+                return;
+
+            if (errorMessage?.Length > 0)
+            {
+                // ReSharper disable LocalizableElement
+                var tcMessage = new System.Text.StringBuilder(errorMessage);
+                tcMessage.Replace("|", "||");
+                tcMessage.Replace("'", "|'");
+                tcMessage.Replace("\n", "|n");
+                tcMessage.Replace("\r", "|r");
+                tcMessage.Replace("[", "|[");
+                tcMessage.Replace("]", "|]");
+                Console.WriteLine("##teamcity[testFailed name='{0}' message='{1}']", test.TestMethod.Name + "-" + Language.TwoLetterISOLanguageName, tcMessage);
+                // ReSharper enable LocalizableElement
+            }
+
+            Console.WriteLine(@"##teamcity[testFinished name='{0}' duration='{1}']", test.TestMethod.Name + "-" + Language.TwoLetterISOLanguageName, LastTestDuration * 1000);
         }
 
         public static IEnumerable<TestInfo> GetTestInfos(string testDll)
