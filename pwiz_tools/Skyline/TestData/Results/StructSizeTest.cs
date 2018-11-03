c0d18b44d03937be8512d20c77384666ad5f1c6d
brendanx67 <brendanx@proteinms.net>
2018-11-02 17:08:34 -0700

    Skyline: Rename TestA to TestData in preparation for separating test which use MsDataFile from those that don't

---
+++ pwiz_tools/Skyline/TestData/Results/StructSizeTest.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Original author: Nicholas Shulman <nicksh .at. u.washington.edu>,
+ *                  MacCoss Lab, Department of Genome Sciences, UW
+ *
+ * Copyright 2017 University of Washington - Seattle, WA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pwiz.Skyline.Model.Results;
+using pwiz.SkylineTestUtil;
+
+namespace pwiz.SkylineTestData.Results
+{
+    [TestClass]
+    public class StructSizeTest : AbstractUnitTest
+    {
+        /// <summary>
+        /// Verifies that the offsets of fields within the struct ChromPeak have not changed
+        /// over the versions.
+        /// </summary>
+        [TestMethod]
+        public void TestChromPeakOffsets()
+        {
+            Assert.AreEqual((IntPtr) 0, Marshal.OffsetOf<ChromPeak>("_retentionTime"));
+            Assert.AreEqual((IntPtr) 4, Marshal.OffsetOf<ChromPeak>("_startTime"));
+            Assert.AreEqual((IntPtr) 8, Marshal.OffsetOf<ChromPeak>("_endTime"));
+            Assert.AreEqual((IntPtr)12, Marshal.OffsetOf<ChromPeak>("_area"));
+            Assert.AreEqual((IntPtr)16, Marshal.OffsetOf<ChromPeak>("_backgroundArea"));
+            Assert.AreEqual((IntPtr)20, Marshal.OffsetOf<ChromPeak>("_height"));
+            Assert.AreEqual((IntPtr)24, Marshal.OffsetOf<ChromPeak>("_fwhm"));
+            Assert.AreEqual((IntPtr)28, Marshal.OffsetOf<ChromPeak>("_flagBits"));
+            Assert.AreEqual((IntPtr)32, Marshal.OffsetOf<ChromPeak>("_pointsAcross"));
+            Assert.AreEqual(32, ChromPeak.GetStructSize(CacheFormatVersion.Eleven));
+            Assert.AreEqual(36, ChromPeak.GetStructSize(CacheFormatVersion.Twelve));
+            Assert.AreEqual(Marshal.SizeOf<ChromPeak>(), ChromPeak.GetStructSize(CacheFormatVersion.CURRENT));
+        }
+    }
+}
