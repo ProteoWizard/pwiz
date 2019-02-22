859df30828ef1488696b69b88276b69d6d44999e
Nicholas Shulman <nicksh@uw.edu>
2019-02-22 15:10:45 -0800

    Change default share type to "Complete". Used to be "minimal". Clean up ShareTypeDlg so that it has less text on it. There are a couple of tooltips that say that choosing "Minimize libraries" will remove the entries that are not in the Document.

--- pwiz_tools/Skyline/Alerts/ShareTypeDlg.Designer.cs
+++ pwiz_tools/Skyline/Alerts/ShareTypeDlg.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ShareTypeDlg));
             this.btnCancel = new System.Windows.Forms.Button();
             this.panelButtonBar = new System.Windows.Forms.FlowLayoutPanel();
@@ -36,21 +37,15 @@
             this.comboSkylineVersion = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.groupBoxShareType = new System.Windows.Forms.GroupBox();
-            this.flowLayoutPanelMinimalComplete = new System.Windows.Forms.FlowLayoutPanel();
-            this.radioMinimal = new System.Windows.Forms.RadioButton();
             this.radioComplete = new System.Windows.Forms.RadioButton();
-            this.lblShareTypeFooter = new System.Windows.Forms.Label();
-            this.lblLibraries = new System.Windows.Forms.Label();
-            this.lblRetentionTimeCalculator = new System.Windows.Forms.Label();
-            this.lblBackgroundProteome = new System.Windows.Forms.Label();
-            this.lblShareTypeHeader = new System.Windows.Forms.Label();
+            this.radioMinimal = new System.Windows.Forms.RadioButton();
             this.label3 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panelFileFormat = new System.Windows.Forms.Panel();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.panelButtonBar.SuspendLayout();
             this.groupBoxFileFormat.SuspendLayout();
             this.groupBoxShareType.SuspendLayout();
-            this.flowLayoutPanelMinimalComplete.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panelFileFormat.SuspendLayout();
             this.SuspendLayout();
@@ -100,60 +95,26 @@
             // groupBoxShareType
             // 
             resources.ApplyResources(this.groupBoxShareType, "groupBoxShareType");
-            this.groupBoxShareType.Controls.Add(this.flowLayoutPanelMinimalComplete);
-            this.groupBoxShareType.Controls.Add(this.lblShareTypeFooter);
-            this.groupBoxShareType.Controls.Add(this.lblLibraries);
-            this.groupBoxShareType.Controls.Add(this.lblRetentionTimeCalculator);
-            this.groupBoxShareType.Controls.Add(this.lblBackgroundProteome);
-            this.groupBoxShareType.Controls.Add(this.lblShareTypeHeader);
+            this.groupBoxShareType.Controls.Add(this.radioComplete);
+            this.groupBoxShareType.Controls.Add(this.radioMinimal);
             this.groupBoxShareType.Name = "groupBoxShareType";
             this.groupBoxShareType.TabStop = false;
-            // 
-            // flowLayoutPanelMinimalComplete
-            // 
-            resources.ApplyResources(this.flowLayoutPanelMinimalComplete, "flowLayoutPanelMinimalComplete");
-            this.flowLayoutPanelMinimalComplete.Controls.Add(this.radioMinimal);
-            this.flowLayoutPanelMinimalComplete.Controls.Add(this.radioComplete);
-            this.flowLayoutPanelMinimalComplete.Name = "flowLayoutPanelMinimalComplete";
-            // 
-            // radioMinimal
-            // 
-            resources.ApplyResources(this.radioMinimal, "radioMinimal");
-            this.radioMinimal.Name = "radioMinimal";
-            this.radioMinimal.TabStop = true;
-            this.radioMinimal.UseVisualStyleBackColor = true;
             // 
             // radioComplete
             // 
             resources.ApplyResources(this.radioComplete, "radioComplete");
             this.radioComplete.Name = "radioComplete";
             this.radioComplete.TabStop = true;
+            this.toolTip1.SetToolTip(this.radioComplete, resources.GetString("radioComplete.ToolTip"));
             this.radioComplete.UseVisualStyleBackColor = true;
             // 
-            // lblShareTypeFooter
+            // radioMinimal
             // 
-            resources.ApplyResources(this.lblShareTypeFooter, "lblShareTypeFooter");
-            this.lblShareTypeFooter.Name = "lblShareTypeFooter";
-            // 
-            // lblLibraries
-            // 
-            resources.ApplyResources(this.lblLibraries, "lblLibraries");
-            this.lblLibraries.Name = "lblLibraries";
-            // 
-            // lblRetentionTimeCalculator
-            // 
-            resources.ApplyResources(this.lblRetentionTimeCalculator, "lblRetentionTimeCalculator");
-            this.lblRetentionTimeCalculator.Name = "lblRetentionTimeCalculator";
-            // 
-            // lblBackgroundProteome
-            // 
-            resources.ApplyResources(this.lblBackgroundProteome, "lblBackgroundProteome");
-            this.lblBackgroundProteome.Name = "lblBackgroundProteome";
-            // 
-            // lblShareTypeHeader
-            // 
-            resources.ApplyResources(this.lblShareTypeHeader, "lblShareTypeHeader");
-            this.lblShareTypeHeader.Name = "lblShareTypeHeader";
+            resources.ApplyResources(this.radioMinimal, "radioMinimal");
+            this.radioMinimal.Name = "radioMinimal";
+            this.radioMinimal.TabStop = true;
+            this.toolTip1.SetToolTip(this.radioMinimal, resources.GetString("radioMinimal.ToolTip"));
+            this.radioMinimal.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
@@ -178,7 +139,7 @@
             this.AcceptButton = this.btnShare;
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.CancelButton = this.btnCancel;
             this.Controls.Add(this.panelFileFormat);
             this.Controls.Add(this.panel2);
@@ -193,8 +154,6 @@
             this.groupBoxFileFormat.PerformLayout();
             this.groupBoxShareType.ResumeLayout(false);
             this.groupBoxShareType.PerformLayout();
-            this.flowLayoutPanelMinimalComplete.ResumeLayout(false);
-            this.flowLayoutPanelMinimalComplete.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.panelFileFormat.ResumeLayout(false);
@@ -216,13 +175,8 @@
         private System.Windows.Forms.RadioButton radioMinimal;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label lblLibraries;
-        private System.Windows.Forms.Label lblRetentionTimeCalculator;
-        private System.Windows.Forms.Label lblBackgroundProteome;
-        private System.Windows.Forms.Label lblShareTypeHeader;
         private System.Windows.Forms.ComboBox comboSkylineVersion;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelMinimalComplete;
-        private System.Windows.Forms.Label lblShareTypeFooter;
         private System.Windows.Forms.Panel panelFileFormat;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
