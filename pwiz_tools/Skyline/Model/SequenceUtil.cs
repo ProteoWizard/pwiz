e8c8444208fbc4326be70212463a3877ba7780b7
Nicholas Shulman <nicksh@uw.edu>
2019-03-13 11:09:24 -0700

    Fix problem where modifications with negative mass cannot be found in iRT databases. (Reported by Anatoly)

--- pwiz_tools/Skyline/Model/SequenceUtil.cs
+++ pwiz_tools/Skyline/Model/SequenceUtil.cs
@@ -754,7 +754,7 @@ namespace pwiz.Skyline.Model
                 else
                 {
                     var massdiff2 = strMassDiff.TrimStart('+', '-');
-                    normalizedSeq.Append(string.Format(CultureInfo.InvariantCulture, @"[{0}{1}]", massDiff > 0 ? @"+" : string.Empty, massdiff2));
+                    normalizedSeq.Append(string.Format(CultureInfo.InvariantCulture, "[{0}{1}]", massDiff > 0 ? "+" : "-", massdiff2)); // Not L10N
                 }
                 ichLast = ichCloseBracket + 1;
             }
