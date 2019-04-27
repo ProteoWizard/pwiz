84f87c47f0f0cb2b01b9ab805c3abb77254c4b5c
Nicholas Shulman <nicksh@uw.edu>
2019-04-26 17:20:02 -0700

    Fix InspectCode

--- pwiz_tools/Shared/Common/DataBinding/ViewSpecLayout.cs
+++ pwiz_tools/Shared/Common/DataBinding/ViewSpecLayout.cs
@@ -87,7 +87,7 @@ namespace pwiz.Common.DataBinding
         {
             if (ReferenceEquals(null, obj)) return false;
             if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
+            if (obj.GetType() != GetType()) return false;
             return Equals((ViewSpecLayout) obj);
         }
 
