8e4d2bb03df5fe50ac89de3960762994115697a2
Matt Chambers <matt.chambers42@gmail.com>
2019-05-01 10:17:52 -0400

    BPC and TIC for Bruker (#515)
    - added TIC and BPC chromatograms for Bruker BAF and TDF formats
    * moved Bruker test data out of tarball
    * added tests for TIC chromatogram to Skyline's PwizFileInfoTest
    * fixed tar.jam optional parameters
    * fixed generate_*_mzml.bat files to allow extra tar args (excluding mzML files when updating Bruker tarball) or avoid tarball entirely (Waters and Shimadzu)
    * fixed VendorReaderTestHarness.cpp to error when no tests are run (raw data is missing or predicate is faulty)
    * fixed WIFF test condition

--- scripts/misc/clean_vendor_targets.bat
+++ scripts/misc/clean_vendor_targets.bat
@@ -21,38 +21,45 @@ IF EXIST build-nt-x86\pwiz_aux\msrc\utility\vendor_api rmdir /s /q build-nt-x86\
 IF EXIST build-nt-x86\pwiz\analysis\spectrum_processing rmdir /s /q build-nt-x86\pwiz\analysis\spectrum_processing
 IF EXIST build-nt-x86\pwiz\analysis\chromatogram_processing rmdir /s /q build-nt-x86\pwiz\analysis\chromatogram_processing
 
-del /q pwiz_aux\msrc\utility\vendor_api\ABI\*.dll > nul 2>&1
-del /q pwiz_aux\msrc\utility\vendor_api\ABI\LicenseKey.h > nul 2>&1
-del /q pwiz_aux\msrc\utility\vendor_api\Agilent\*.dll > nul 2>&1
+del /f /q pwiz_aux\msrc\utility\vendor_api\ABI\*.dll > nul 2>&1
+del /f /q pwiz_aux\msrc\utility\vendor_api\ABI\LicenseKey.h > nul 2>&1
+rmdir /s /q pwiz_aux\msrc\utility\vendor_api\ABI\vc10 > nul 2>&1
+rmdir /s /q pwiz_aux\msrc\utility\vendor_api\ABI\vc9 > nul 2>&1
+
+del /f /q pwiz_aux\msrc\utility\vendor_api\Agilent\*.dll > nul 2>&1
 rmdir /s /q pwiz_aux\msrc\utility\vendor_api\Agilent\x86 > nul 2>&1
 rmdir /s /q pwiz_aux\msrc\utility\vendor_api\Agilent\x64 > nul 2>&1
-del /q pwiz_aux\msrc\utility\vendor_api\Agilent\EULA.* > nul 2>&1
+del /f /q pwiz_aux\msrc\utility\vendor_api\Agilent\EULA.* > nul 2>&1
 rmdir /s /q pwiz_aux\msrc\utility\vendor_api\Agilent\Documents > nul 2>&1
+del /f /q pwiz_aux\msrc\utility\vendor_api\Bruker\*.manifest > nul 2>&1
+del /f /q pwiz_aux\msrc\utility\vendor_api\Bruker\baf2sql_c.h > nul 2>&1
+del /f /q pwiz_aux\msrc\utility\vendor_api\Bruker\baf2sql_cpp.h > nul 2>&1
+del /f /q pwiz_aux\msrc\utility\vendor_api\Bruker\schema.h > nul 2>&1
+rmdir /s /q pwiz_aux\msrc\utility\vendor_api\Bruker\install_pwiz_vendor_api_bruker_stub > nul 2>&1
 rmdir /s /q pwiz_aux\msrc\utility\vendor_api\Bruker\x86 > nul 2>&1
 rmdir /s /q pwiz_aux\msrc\utility\vendor_api\Bruker\x64 > nul 2>&1
-del /q pwiz_aux\msrc\utility\vendor_api\Shimadzu\*.dll > nul 2>&1
+del /f /q pwiz_aux\msrc\utility\vendor_api\Shimadzu\EULA.SFCS > nul 2>&1
 rmdir /s /q pwiz_aux\msrc\utility\vendor_api\Shimadzu\x86 > nul 2>&1
 rmdir /s /q pwiz_aux\msrc\utility\vendor_api\Shimadzu\x64 > nul 2>&1
-del /q pwiz_aux\msrc\utility\vendor_api\Thermo\*.dll > nul 2>&1
-del /q pwiz_aux\msrc\utility\vendor_api\Thermo\*.manifest > nul 2>&1
-rmdir /s /q  pwiz_aux\msrc\utility\vendor_api\Thermo\x86 > nul 2>&1
+del /f /q pwiz_aux\msrc\utility\vendor_api\Shimadzu\*.dll > nul 2>&1
+rmdir /s /q pwiz_aux\msrc\utility\vendor_api\Shimadzu\ja-JP > nul 2>&1
+del /f /q pwiz_aux\msrc\utility\vendor_api\Thermo\*.dll > nul 2>&1
+del /f /q pwiz_aux\msrc\utility\vendor_api\Thermo\*.manifest > nul 2>&1
+rmdir /s /q pwiz_aux\msrc\utility\vendor_api\Thermo\x86 > nul 2>&1
 rmdir /s /q pwiz_aux\msrc\utility\vendor_api\Thermo\x64 > nul 2>&1
-del /q pwiz_aux\msrc\utility\vendor_api\Thermo\EULA.* > nul 2>&1
-rmdir /s /q pwiz_aux\msrc\utility\vendor_api\Thermo\Microsoft.VC90.MFC > nul 2>&1
-del /q /s pwiz_aux\msrc\utility\vendor_api\Waters\*.dll > nul 2>&1
-del /q /s pwiz_aux\msrc\utility\vendor_api\Waters\*.lib > nul 2>&1
-rmdir /s /q pwiz_aux\msrc\utility\vendor_api\Waters\vc9_scl-0 > nul 2>&1
-rmdir /s /q pwiz_aux\msrc\utility\vendor_api\Waters\vc9_scl-1 > nul 2>&1
+del /f /q pwiz_aux\msrc\utility\vendor_api\Thermo\EULA.* > nul 2>&1
+del /f /q /s pwiz_aux\msrc\utility\vendor_api\Waters\*.dll > nul 2>&1
+del /f /q /s pwiz_aux\msrc\utility\vendor_api\Waters\*.lib > nul 2>&1
 rmdir /s /q pwiz_aux\msrc\utility\vendor_api\Waters\vc12_x86 > nul 2>&1
 rmdir /s /q pwiz_aux\msrc\utility\vendor_api\Waters\vc12_x64 > nul 2>&1
-del /q pwiz_aux\msrc\utility\vendor_api\Waters\*.h > nul 2>&1
+del /f /q pwiz_aux\msrc\utility\vendor_api\Waters\*.h > nul 2>&1
 
 rmdir /s /q pwiz\data\vendor_readers\Thermo\Reader_Thermo_Test.data > nul 2>&1
 rmdir /s /q pwiz\data\vendor_readers\Agilent\Reader_Agilent_Test.data > nul 2>&1
 rmdir /s /q pwiz\data\vendor_readers\ABI\Reader_ABI_Test.data > nul 2>&1
 rmdir /s /q pwiz\data\vendor_readers\ABI\T2D\Reader_ABI_T2D_Test.data > nul 2>&1
-rmdir /s /q pwiz\data\vendor_readers\Waters\Reader_Waters_Test.data > nul 2>&1
-rmdir /s /q pwiz\data\vendor_readers\Bruker\Reader_Bruker_Test.data > nul 2>&1
+git clean -f -d -X pwiz\data\vendor_readers\Bruker\Reader_Bruker_Test.data
+git clean -f -d -X pwiz\data\vendor_readers\Waters\Reader_Waters_Test.data
 REM rmdir /s /q pwiz\data\vendor_readers\Bruker\Reader_Shimadzu_Test.data > nul 2>&1
 
 
