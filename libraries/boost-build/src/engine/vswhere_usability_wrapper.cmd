7848caac8f2f28e0bc9536038f89461633455772
Matt Chambers <matt.chambers42@gmail.com>
2019-07-29 10:55:55 -0400

    * added VS2019 compiler support

--- libraries/boost-build/src/engine/vswhere_usability_wrapper.cmd
+++ libraries/boost-build/src/engine/vswhere_usability_wrapper.cmd
@@ -14,7 +14,7 @@ where vswhere 2> nul > nul
 if errorlevel 1 goto :no-vswhere
 set VSWHERE_REQ=-requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64
 set VSWHERE_PRP=-property installationPath
-set VSWHERE_LMT=-version "[15.0,16.0)"
+set VSWHERE_LMT=-version "[15.0,17.0)"
 vswhere -prerelease > nul
 if "%~1"=="prerelase" set VSWHERE_WITH_PRERELASE=1
 if not errorlevel 1 if "%VSWHERE_WITH_PRERELASE%"=="1" set "VSWHERE_LMT=%VSWHERE_LMT% -prerelease"
