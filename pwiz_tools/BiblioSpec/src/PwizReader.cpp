976280bad284315a961b9abc213ab7bff804d308
Matt Chambers <matt.chambers42@gmail.com>
2019-08-14 11:53:04 -0400

    BiblioSpec peak picking and MaxQuant TIMS-DDA fix (#611)
    - added peak picking when reading spectra from vendor files in BiblioSpec
    - changed MaxQuantReader to always use embedded spectra for TIMS PASEF data
    * added BiblioSpec test actually reading WIFF file (Windows only)
    * added SpectrumList_PeakPicker::supportsVendorPeakPicking() to simplify conditionally adding a vendor peak picking filter in code

--- pwiz_tools/BiblioSpec/src/PwizReader.cpp
+++ pwiz_tools/BiblioSpec/src/PwizReader.cpp
@@ -25,8 +25,11 @@
  */
 
 #include "PwizReader.h"
+#include "pwiz/analysis/spectrum_processing/SpectrumListFactory.hpp"
+#include "pwiz/analysis/spectrum_processing/SpectrumList_PeakPicker.hpp"
 
 using namespace pwiz::msdata;
+using namespace pwiz::analysis;
 using namespace boost;
 
 PwizReader::PwizReader() : curPositionInIndexMzPairs_(0)  {
@@ -57,6 +60,8 @@ void PwizReader::openFile(const char* filename, bool mzSort){
         delete fileReader_;
         #ifdef VENDOR_READERS
         fileReader_ = new MSDataFile(fileName_, &allReaders_);
+        if (SpectrumList_PeakPicker::supportsVendorPeakPicking(fileName_))
+            SpectrumListFactory::wrap(*fileReader_, "peakPicking true 1-");
         #else
         fileReader_ = new MSDataFile(fileName_);
         #endif
