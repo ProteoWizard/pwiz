5da99b753dc72c237e732f8f66a34106c6288ef5
Tobias Rohde <tobiasr@uw.edu>
2019-09-19 12:32:51 -0700

    Update Library.cs
    Fixed typo

--- pwiz_tools/Skyline/Model/Lib/Library.cs
+++ pwiz_tools/Skyline/Model/Lib/Library.cs
@@ -2391,7 +2391,7 @@ namespace pwiz.Skyline.Model.Lib
         {
             if (ReferenceEquals(null, obj)) return false;
             if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != .GetType()) return false;
+            if (obj.GetType() != GetType()) return false;
             return Equals((LibraryChromGroup) obj);
         }
 
