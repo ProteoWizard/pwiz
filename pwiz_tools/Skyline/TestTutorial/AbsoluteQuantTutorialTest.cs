47a55ef445b549608d2e7fbd502ea6994764ee41
RitaCh <ritach@uw.edu>
2019-10-30 11:17:56 -0700

    A couple of minor changes to address the code inspection problems.

--- pwiz_tools/Skyline/TestTutorial/AbsoluteQuantTutorialTest.cs
+++ pwiz_tools/Skyline/TestTutorial/AbsoluteQuantTutorialTest.cs
@@ -17,12 +17,10 @@
  * limitations under the License.
  */
 
-using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Windows.Forms;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using pwiz.Common.DataBinding;
 using pwiz.Skyline.Controls.Databinding;
