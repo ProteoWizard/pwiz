ef2619151cd4b4f86d1e363636fc4ada2bafa90a
pvvemul <pvvemul@gmail.com>
2019-11-26 15:05:00 -0800

    Update refinement settings to refine group comparisons.

--- pwiz_tools/Skyline/Model/GroupComparisonRefinementData.cs
+++ pwiz_tools/Skyline/Model/GroupComparisonRefinementData.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using pwiz.Common.DataAnalysis;
+using pwiz.Common.DataAnalysis.Matrices;
 using pwiz.Skyline.Controls.Graphs;
 using pwiz.Skyline.Controls.GroupComparison;
 using pwiz.Skyline.Model.Databinding.Entities;
@@ -13,15 +14,25 @@ namespace pwiz.Skyline.Model
 {
     class GroupComparisonRefinementData
     {
+        private List<GroupComparisonRow> Data { get; set; }
+        private SrmDocument _document;
 
-        public GroupComparisonRefinementData(IDocumentContainer docContainer, double adjustedPValCutoff,
-            double foldChangCutoff, double msLevel, GroupComparisonDef groupComparisonDef)
+        private readonly double _adjustedPValCutoff;
+        private readonly double _foldChangeCutoff;
+        private readonly int _msLevel;
+        private readonly QrFactorizationCache _qrFactorizationCache = new QrFactorizationCache();
+
+        public GroupComparisonRefinementData(SrmDocument document, double adjustedPValCutoff,
+            double foldChangeCutoff, int msLevel, GroupComparisonDef groupComparisonDef)
         {
-            var document = docContainer.Document;
-            var groupComparer = new GroupComparer(groupComparisonDef, document, null);
+            _document = document;
+            _foldChangeCutoff = foldChangeCutoff;
+            _msLevel = msLevel;
+
+            var groupComparer = new GroupComparer(groupComparisonDef, _document, _qrFactorizationCache);
             List<GroupComparisonResult> results = new List<GroupComparisonResult>();
 
-            foreach (var protein in document.MoleculeGroups)
+            foreach (var protein in _document.MoleculeGroups)
             {
                 IEnumerable<PeptideDocNode> peptides;
                 if (groupComparer.ComparisonDef.PerProtein)
@@ -42,72 +53,48 @@ namespace pwiz.Skyline.Model
             var adjustedPValues = PValues.AdjustPValues(results.Select(
                 row => row.LinearFitResult.PValue)).ToArray();
 
-            var foldChangeList = new List<>();
+            var foldChangeList = new List<GroupComparisonRow>();
             for (int iRow = 0; iRow < results.Count; iRow++)
             {
                 var resultRow = results[iRow];
                 FoldChangeResult foldChangeResult = new FoldChangeResult(groupComparisonDef.ConfidenceLevel,
                     adjustedPValues[iRow], resultRow.LinearFitResult, double.NaN);
-                foldChangeResults.Add(foldChangeResult);
+                foldChangeList.Add(new GroupComparisonRow(resultRow.Selector.Protein, resultRow.Selector.Peptide, resultRow.Selector.MsLevel,
+                    foldChangeResult, resultRow.Selector.GroupIdentifier));
             }
 
-
-
-//                var peptideGroups = document.MoleculeGroups.ToArray();
-//                for (int i = 0; i < peptideGroups.Length; i++)
-//                {
-//                    var peptideGroup = peptideGroups[i];
-//                    IEnumerable<PeptideDocNode> peptides;
-//                    if (gcm.GroupComparer.ComparisonDef.PerProtein)
-//                    {
-//                        peptides = new PeptideDocNode[] { null };
-//                    }
-//                    else
-//                    {
-//                        peptides = peptideGroup.Molecules;
-//                    }
-//                    foreach (var peptide in peptides)
-//                    {
-//                        results.AddRange(gcm.GroupComparer.CalculateFoldChanges(peptideGroup, peptide));
-//                    }
-//                }
-//            }
-//
-//            gcm.Results = new GroupComparisonResults(gcm.GroupComparer, results, DateTime.Now, DateTime.Now);
-//
-//
-//            Dictionary<int, double> criticalValuesByDegreesOfFreedom = new Dictionary<int, double>();
-//            var groupComparisonDef = gcm.Results.GroupComparer.ComparisonDef;
-//            var adjustedPValues = PValues.AdjustPValues(gcm.Results.ResultRows.Select(
-//                row => row.LinearFitResult.PValue)).ToArray();
-//            for (int iRow = 0; iRow < gcm.Results.ResultRows.Count; iRow++)
-//            {
-//                var resultRow = gcm.Results.ResultRows[iRow];
-//                if (null != resultRow.Selector.Peptide)
-//                {
-//                    peptide = new Model.Databinding.Entities.Peptide(_skylineDataSchema,
-//                        new IdentityPath(protein.IdentityPath, resultRow.Selector.Peptide.Id));
-//                }
-//                double criticalValue;
-//                if (!criticalValuesByDegreesOfFreedom.TryGetValue(resultRow.LinearFitResult.DegreesOfFreedom,
-//                    out criticalValue))
-//                {
-//                    criticalValue = FoldChangeResult.GetCriticalValue(groupComparisonDef.ConfidenceLevel,
-//                        resultRow.LinearFitResult.DegreesOfFreedom);
-//                    criticalValuesByDegreesOfFreedom.Add(resultRow.LinearFitResult.DegreesOfFreedom, criticalValue);
-//                }
-//                FoldChangeResult foldChangeResult = new FoldChangeResult(groupComparisonDef.ConfidenceLevel,
-//                    adjustedPValues[iRow], resultRow.LinearFitResult, criticalValue);
-//                rows.Add(new FoldChangeBindingSource.FoldChangeRow(protein, peptide, resultRow.Selector.LabelType,
-//                    resultRow.Selector.MsLevel, resultRow.Selector.GroupIdentifier, resultRow.ReplicateCount, foldChangeResult));
-//            }
+            Data = foldChangeList;
         }
+
+        public SrmDocument RemoveBelowCutoffs(SrmDocument document)
+        {
+            var filterByCutoff = Data.ToArray();
+
+            var toRemove = filterByCutoff.Where(r => 
+                    (double.IsNaN(_adjustedPValCutoff) && r.FoldChangeResult.AdjustedPValue >= _adjustedPValCutoff) ||
+                    (double.IsNaN(_foldChangeCutoff) && r.FoldChangeResult.AbsLog2FoldChange <= _foldChangeCutoff) || r.MsLevel != _msLevel)
+                .Select(r => r.Peptide != null ? r.Peptide.Id.GlobalIndex : r.Protein.Id.GlobalIndex).Distinct()
+                .ToArray();
+
+            return (SrmDocument) document.RemoveAll(toRemove);
+        }
+
         private class GroupComparisonRow
         {
-            public GroupComparisonRow(PeptideGroupDocNode protein, PeptideDocNode peptide, int msLevel, FoldChangeResult result)
+            public GroupComparisonRow(PeptideGroupDocNode protein, PeptideDocNode peptide, int? msLevel, FoldChangeResult result, GroupIdentifier group)
             {
-
+                Protein = protein;
+                Peptide = peptide;
+                MsLevel = msLevel;
+                Group = group;
+                FoldChangeResult = result;
             }
+
+            public PeptideGroupDocNode Protein { get; private set; }
+            public PeptideDocNode Peptide { get; private set; }
+            public int? MsLevel { get; private set; }
+            public GroupIdentifier Group { get; private set; }
+            public FoldChangeResult FoldChangeResult { get; private set; }
         }
     }
 
