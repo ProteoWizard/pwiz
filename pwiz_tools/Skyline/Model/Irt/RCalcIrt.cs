4fa7329071bbc84dc5e602c6bf30000366c8678e
kaipo <5232643+kaipot@users.noreply.github.com>
2019-12-05 09:15:01 -0800

    remove unused using

--- pwiz_tools/Skyline/Model/Irt/RCalcIrt.cs
+++ pwiz_tools/Skyline/Model/Irt/RCalcIrt.cs
@@ -24,7 +24,6 @@ using System.Xml;
 using System.Xml.Serialization;
 using pwiz.Common.SystemUtil;
 using pwiz.Skyline.Model.DocSettings;
-using pwiz.Skyline.Model.Results;
 using pwiz.Skyline.Model.RetentionTimes;
 using pwiz.Skyline.Properties;
 using pwiz.Skyline.Util;
