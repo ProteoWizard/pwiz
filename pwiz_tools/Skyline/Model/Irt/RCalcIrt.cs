ce5bc37660f72da5ad0990720bb4f9f51e477839
kaipo <5232643+kaipot@users.noreply.github.com>
2020-01-17 16:04:25 -0800

    Allow iRT peptides to match heavy or light

--- pwiz_tools/Skyline/Model/Irt/RCalcIrt.cs
+++ pwiz_tools/Skyline/Model/Irt/RCalcIrt.cs
@@ -242,6 +242,7 @@ namespace pwiz.Skyline.Model.Irt
         public static ProcessedIrtAverages ProcessRetentionTimes(IProgressMonitor monitor,
             IRetentionTimeProvider[] providers, DbIrtPeptide[] standardPeptideList, DbIrtPeptide[] items)
         {
+            var standardPeptideListOriginal = standardPeptideList;
             var matchedStandard = IrtStandard.WhichStandard(standardPeptideList.Select(pep => pep.ModifiedTarget));
             if (matchedStandard != null && matchedStandard.HasDocument)
             {
@@ -251,10 +252,13 @@ namespace pwiz.Skyline.Model.Irt
                 foreach (var dummyPep in standardDoc.Molecules.Where(pep => pep.HasExplicitMods))
                 {
                     var standardPepIdx = standardPeptideList.IndexOf(pep => dummyPep.ModifiedTarget.Equals(pep.ModifiedTarget));
-                    standardPeptideList[standardPepIdx] = new DbIrtPeptide(standardPeptideList[standardPepIdx])
+                    if (standardPepIdx != -1)
                     {
-                        ModifiedTarget = standardDoc.Settings.GetModifiedSequence(dummyPep.ModifiedTarget, IsotopeLabelType.heavy, dummyPep.ExplicitMods)
-                    };
+                        standardPeptideList[standardPepIdx] = new DbIrtPeptide(standardPeptideList[standardPepIdx])
+                        {
+                            ModifiedTarget = standardDoc.Settings.GetModifiedSequence(dummyPep.ModifiedTarget, IsotopeLabelType.heavy, dummyPep.ExplicitMods)
+                        };
+                    }
                 }
             }
 
@@ -276,6 +280,15 @@ namespace pwiz.Skyline.Model.Irt
                 if (data.RegressionSuccess || data.CalcRegressionWith(retentionTimeProvider, standardPeptideList, items))
                 {
                     AddRetentionTimesToDict(retentionTimeProvider, data.RegressionRefined, dictPeptideAverages, standardPeptideList);
+                }
+                else
+                {
+                    var dataRetry = new RetentionTimeProviderData(retentionTimeProvider, standardPeptideListOriginal);
+                    if (dataRetry.RegressionSuccess || dataRetry.CalcRegressionWith(retentionTimeProvider, standardPeptideListOriginal, items))
+                    {
+                        AddRetentionTimesToDict(retentionTimeProvider, dataRetry.RegressionRefined, dictPeptideAverages, standardPeptideListOriginal);
+                        data = dataRetry;
+                    }
                 }
                 providerData.Add(data);
 
