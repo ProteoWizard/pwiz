66e73ac1b1fa74eb4aec8fb0eefa7bf594eb93ea
Matt Chambers <matt.chambers42@gmail.com>
2020-02-07 12:54:49 -0500

    * fixed logging crash in TestRunner from PR #900 * added TestPass1Subset test in Skyline's Jamfile so pass1 gets some testing on TeamCity * made TestInstrumentInfo, one of the TestPass1Subset tests, a ReportLeakEarly test so that code path gets tested on TeamCity

--- pwiz_tools/Skyline/TestRunner/Program.cs
+++ pwiz_tools/Skyline/TestRunner/Program.cs
@@ -76,7 +76,8 @@ namespace TestRunner
         // These tests get extra runs to meet the leak thresholds
         private static Dictionary<string, ExpandedLeakCheck> LeakCheckIterationsOverrideByTestName = new Dictionary<string, ExpandedLeakCheck>
         {
-            {"TestGroupedStudiesTutorial", new ExpandedLeakCheck(LeakCheckIterations * 4, true)}
+            {"TestGroupedStudiesTutorial", new ExpandedLeakCheck(LeakCheckIterations * 4, true)},
+            {"TestInstrumentInfo", new ExpandedLeakCheck(LeakCheckIterations * 2, true)}
         };
 
         // These tests are allowed to fail the total memory leak threshold, and extra iterations are not done to stabilize a spiky total memory distribution
@@ -670,7 +671,8 @@ namespace TestRunner
                         if (!GetLeakCheckReportEarly(test))
                         {
                             leakMessage = minDeltas.Value.GetLeakMessage(LeakThresholds, test.TestMethod.Name);
-                            runTests.Log(leakMessage);
+                            if (leakMessage != null)
+                                runTests.Log(leakMessage);
                         }
 
                         if (leakMessage != null)
