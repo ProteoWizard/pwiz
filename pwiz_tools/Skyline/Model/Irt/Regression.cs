11e43dcc5d495fd1ddc1a368ecd74f88185cbfbb
kaipo <5232643+kaipot@users.noreply.github.com>
2020-02-10 13:56:43 -0800

    Removed unused using

--- pwiz_tools/Skyline/Model/Irt/Regression.cs
+++ pwiz_tools/Skyline/Model/Irt/Regression.cs
@@ -20,7 +20,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using pwiz.Common.Collections;
 using pwiz.Common.DataAnalysis;
 using pwiz.Common.SystemUtil;
 using pwiz.Skyline.Model.DocSettings;
