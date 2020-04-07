a49d88fbde2c46c76c611ee180a5f143ccd32243
Nicholas Shulman <nicksh@proteinms.net>
2020-04-07 15:49:06 -0700

    Remove code that creates and destroys AllChromatogramsGraph asynchronously. I am still trying to track down the intermittent failure in "Dispose cannot be called during CreateHandle". This failure stopped happening after I added some extra diagnostic code in AsyncChromatogramsGraph2. Hoping this change now may get the error to start happening again in which case we will now get better diagnostics.
    Revert "Skyline: Use ActionUtil.RunAsynch() instead of a custom Thread in AllChromatogramsGraph.RemoveAsync()"
    
    This reverts commit 1090734be34866ec990aada040e92bc384150964.
    
    Revert "Skyline: Make closing the AllChromatogramsGraph asynchronous and dependent on CreateHandle() having completed successfully (#965)"
    
    This reverts commit 5fd14f59ac1e53d2878f5f12f6c441d13dc4869e.

--- pwiz_tools/Skyline/Controls/Graphs/AllChromatogramsGraph.cs
+++ pwiz_tools/Skyline/Controls/Graphs/AllChromatogramsGraph.cs
@@ -25,7 +25,6 @@ using System.Runtime.CompilerServices;
 using System.Runtime.ExceptionServices;
 using System.Security;
 using System.Text;
-using System.Threading;
 using System.Windows.Forms;
 using pwiz.Skyline.Model;
 using pwiz.Skyline.Model.AuditLog;
@@ -43,7 +42,6 @@ namespace pwiz.Skyline.Controls.Graphs
     public partial class AllChromatogramsGraph : FormEx
     {
         private readonly Stopwatch _stopwatch;
-        private ManualResetEvent _windowCreatedEvent;
         private int _selected = -1;
         private bool _selectionIsSticky;
         private readonly int _multiFileWindowWidth;
@@ -63,42 +61,9 @@ namespace pwiz.Skyline.Controls.Graphs
         public AllChromatogramsGraph()
         {
             InitializeComponent();
-
-            HandleCreated += Notification_HandleCreated;
-
             toolStrip1.Renderer = new CustomToolStripProfessionalRenderer();
             _stopwatch = new Stopwatch();
             _multiFileWindowWidth = Size.Width;
-        }
-
-        private void Notification_HandleCreated(object sender, EventArgs e)
-        {
-            _windowCreatedEvent.Set();
-        }
-
-        public void ShowSafe(IWin32Window owner)
-        {
-            if (_windowCreatedEvent == null)
-                _windowCreatedEvent = new ManualResetEvent(false);
-
-            Show(owner);
-        }
-
-        public void RemoveAsync()
-        {
-            if (_windowCreatedEvent == null)
-                Close();
-            else
-            {
-                // Avoid closing the ACG during CreateHandle()
-                ActionUtil.RunAsync(() =>
-                {
-                    _windowCreatedEvent.WaitOne();
-                    _windowCreatedEvent.Dispose();
-
-                    Invoke((Action)Close);
-                }, @"Close AllChromatogramsGraph");
-            }
         }
 
         protected override void OnLoad(EventArgs e)
