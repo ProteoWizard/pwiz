ee981cc1654448b4a5e82f5c62f71425b3778ad0
Nicholas Shulman <nicksh@proteinms.net>
2020-05-04 03:11:28 -0700

    Fix code inspection

--- pwiz_tools/Skyline/SettingsUI/PeptideGridViewDriver.cs
+++ pwiz_tools/Skyline/SettingsUI/PeptideGridViewDriver.cs
@@ -17,11 +17,9 @@
  * limitations under the License.
  */
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
-using NHibernate.Transform;
 using pwiz.Common.DataBinding;
 using pwiz.Skyline.Alerts;
 using pwiz.Skyline.Controls;
