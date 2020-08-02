89751abe73c9d29cd50a6a005d2bb50590406837
Nicholas Shulman <nicksh@proteinms.net>
2020-08-01 17:24:15 -0700

    Fix tooltips on Peptide Settings dialog. The ToolTip class gets confused if any of the controls with tooltips are no longer part of the form. The ToolTip class figures out what the top level form is by iterating through a Hashtable and taking the first Control.TopLevelControl property. If the first control in the HashTable is no longer attached to the Form, then TopLevelControl ends up being a bogus value, and the tooltips do not work in the dialog. This is particularly likely to happen when the UIMode is Small Molecules, since several pages get removed from the TabControl.
    This fix nulls out the ToolTips for all of the controls on a page when pages get removed from the tab control.

--- pwiz_tools/Skyline/SettingsUI/PeptideSettingsUI.Designer.cs
+++ pwiz_tools/Skyline/SettingsUI/PeptideSettingsUI.Designer.cs
@@ -501,6 +501,7 @@ namespace pwiz.Skyline.SettingsUI
             this.tabControl1.Controls.Add(this.tabQuantification);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.ControlRemoved += new System.Windows.Forms.ControlEventHandler(this.tabControl1_ControlRemoved);
             // 
             // tabDigestion
             // 
