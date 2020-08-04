32740895442d6b87edaf341f8bc0319a95af960f
Nicholas Shulman <nicksh@proteinms.net>
2020-08-03 21:57:09 -0700

    More comments

--- pwiz_tools/Skyline/Test/ExtractedMetadataTest.cs
+++ pwiz_tools/Skyline/Test/ExtractedMetadataTest.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿/*
+ * Original author: Nicholas Shulman <nicksh .at. u.washington.edu>,
+ *                  MacCoss Lab, Department of Genome Sciences, UW
+ *
+ * Copyright 2020 University of Washington - Seattle, WA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 using System.IO;
-using System.Text.RegularExpressions;
 using System.Xml;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using pwiz.Common.DataBinding;
-using pwiz.Skyline.Model.DocSettings;
 using pwiz.Skyline.Model.DocSettings.MetadataExtraction;
 using pwiz.Skyline.Util;
 using pwiz.SkylineTestUtil;
@@ -14,27 +29,6 @@ namespace pwiz.SkylineTest
     [TestClass]
     public class ExtractedMetadataTest : AbstractUnitTest
     {
-        [TestMethod]
-        public void TestRegex()
-        {
-            string pattern = @"\b(\w+?)\s\1\b";
-            string input = "This this is a nice day. What about this? This tastes good. I saw a a dog.";
-            foreach (Match match in Regex.Matches(input, pattern, RegexOptions.IgnoreCase))
-                Console.WriteLine("{0} (duplicates '{1}') at position {2}",
-                    match.Value, match.Groups[1].Value, match.Index);
-        }
-
-        [TestMethod]
-        public void TestReplace()
-        {
-            var input = "Now is the time for all good men to come to the aid of their country";
-
-            var replace1 = Regex.Replace(input, @"([a-z])\1", @"_$1 $1 $1_");
-            Console.WriteLine(replace1);
-            var replace2 = Regex.Replace(input, @"([a-z])\1", @"_\1 \1 \1_");
-            Console.WriteLine(replace2);
-        }
-
         [TestMethod]
         public void TestMetadataRuleSerialization()
         {
