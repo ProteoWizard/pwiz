70349e5b2a7256556e7f9c29ffa9b73290dac2e6
Nicholas Shulman <nicksh@proteinms.net>
2020-08-18 16:55:04 -0700

    Move QualitativeIonRatio to the PrecursorResult. It used to be on the PeptideResult, but based on user feedback, it needs to be a precursor level value.

--- pwiz_tools/Skyline/Model/DocSettings/AbsoluteQuantification/QuantificationResult.cs
+++ pwiz_tools/Skyline/Model/DocSettings/AbsoluteQuantification/QuantificationResult.cs
@@ -98,35 +98,41 @@ namespace pwiz.Skyline.Model.DocSettings.AbsoluteQuantification
         }
     }
 
-    public class PeptideQuantificationResult : QuantificationResult
+    public class PrecursorQuantificationResult : QuantificationResult
     {
         #region duplicate properties from base class to control the order they appear in Report Editor
         [Format(Formats.GLOBAL_STANDARD_RATIO, NullValue = TextUtil.EXCEL_NA)]
+        [InvariantDisplayName("PrecursorNormalizedArea")]
         public new double? NormalizedArea
         {
             get { return base.NormalizedArea; }
         }
 
         [Format(Formats.GLOBAL_STANDARD_RATIO, NullValue = TextUtil.EXCEL_NA)]
+        [InvariantDisplayName("PrecursorCalculatedConcentration")]
         public new double? CalculatedConcentration
         {
             get { return base.CalculatedConcentration; }
         }
 
         [Format(Formats.CV, NullValue = TextUtil.EXCEL_NA)]
+        [InvariantDisplayName("PrecursorAccuracy")]
         public new double? Accuracy
         {
             get { return base.Accuracy; }
         }
         #endregion
 
+        [InvariantDisplayName("BatchTargetQualitativeIonRatio")]
+        public double? TargetQualitativeIonRatio { get; private set; }
         public double? QualitativeIonRatio { get; private set; }
         public ValueStatus QualitativeIonRatioStatus { get; private set; }
 
-        public PeptideQuantificationResult ChangeIonRatio(double? ionRatio, ValueStatus ionRatioStatus)
+        public PrecursorQuantificationResult ChangeIonRatio(double? targetIonRatio, double? ionRatio, ValueStatus ionRatioStatus)
         {
             return ChangeProp(ImClone(this), im =>
             {
+                im.TargetQualitativeIonRatio = targetIonRatio;
                 im.QualitativeIonRatio = ionRatio;
                 im.QualitativeIonRatioStatus = ionRatioStatus;
             });
