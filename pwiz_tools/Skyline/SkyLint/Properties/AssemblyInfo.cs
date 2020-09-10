db39604c86132aaa260e7714e5a96dc2f684b9cd
bspratt <bspratt@protein.ms>
2020-09-10 13:58:11 -0700

    Skyline: adding "SkyLint", a simple program that checks for code issues that ReSharper doesn't handle to our satisfaction. In particular, inspections in generated code which ReSharper ignores.
    So why isn't this handled with git pre-commit hooks? A few reasons - GitHub doesn't support server-side hooks (as a security issue) and client side there's no agreement on how commit hooks are handled between commandline git and TortoiseGit.
    
    So, this little app is added as a Skyline pre-build step, it throws up a messagebox if it sees a prohibited string in any file matching a file mask, then exits with a non-zero code.
    
    Note that this commit is actually an intentionally broken build, as there are a couple of lingering examples of ".ImageScalingSize = new System.Drawing.Size(" in the code, which this whole project is designed to ferret out.

---
+++ pwiz_tools/Skyline/SkyLint/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SkyLint")]
+[assembly: AssemblyDescription("Build-time tool for inspecting new code for issues that ReSharper can't detect")]
+[assembly: AssemblyCompany("University of Washington")]
+[assembly: AssemblyProduct("SkyLint")]
+[assembly: AssemblyCopyright("Copyright © University of Washington 2020")]
+[assembly: AssemblyVersion("20.1.1.233")]
+[assembly: AssemblyFileVersion("20.1.1.233")]
+[assembly: AssemblyInformationalVersion("20.1.1.233-ccd64f0ad (developer build)")]
+[assembly: ComVisible(false)]
