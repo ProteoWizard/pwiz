c6032e04d01386f640576473c996fa975298362a
Matt Chambers <matt.chambers42@gmail.com>
2020-10-20 15:19:56 -0400

    Feature/sciex wiff2 data sdk update (#1207)
    * updated WiffFile2.ipp to use Sciex's new Data SDK
    - changed output WIFF sample filename to be only WIFF filename if filename contains sample name (e.g. if sample name is "Sample" and WIFF name is "WiffSample", output is "WiffSample" instead of "WiffSample-Sample")
    - added collision energy for WIFF2 and enabled it for 32-bit
    * removed restrictions on WIFF2 testing in Skyline
    * removed isolation window functions from WiffFile::Experiment
    * added INSTALLER_VENDOR_FILES.txt target for installer scripts to generate their vendor dependency file list automatically (pwiz and IDPicker installers updated, Skyline admin installer could come next)
    * added retries in read_file_header() to avoid file lock failures with the alternate WIFF1 constructor
    * added --test option to SeeMS to check that it can open a file
    * fixed read_file_header() not being able to open WIFF files when called from SpectrumList_PeakPicker::supportsVendorPeakPicking()
    * changed installer tests to copy to %TEMP% before installing due to an issue installing MSI from ImDisk RAMdisk
    * fixed 64-bit SeeMS to not crash on exit and return exit code correctly
    * fixed Skyline tests for pass0
    * changed smartBuildTrigger.py and WIX Jamfile to test installer (it works fine when I run it myself on the build agent, but not when TeamCity runs it for some reason)
    * fixed typo in TestInstrumentInfo
    * deleted msconvert.exe.config from MSConvertGUI project
    * added working test for Skyline MSI installer; TestRunner.exe and its dependencies are copied from the build directory to the installed location and tests are run from there
    * refactored Installer.sln to contain only SetupDeployProject
    * made it possible to override Skyline assembly name from command-line (currently overrides to Skyline if built with --official)
    * added CRT DLLs to percolator directory
    * templatized TestSetting files to use dynamic vendor files list
    * added tuxliketimeout.exe tool for running msiexec with a timeout to avoid hanging builds
    * added timeout for TestRunner execution (it had hung at a BlibBuild error dialog due to missing CRT files; at some future time that blocking dialog needs to be solved as well)
    * moved built artifacts to bin directory except FileList64.txt
    * made pwiz_tools/Skyline//TestData target an alias to pwiz_tools/Skyline/Executables/Installer//Test when WIX is installed (the installer test currently only runs TestData; the alias will need to be reconsidered if that changes)
    * fixed TestRunner failures due to writing to the same default log file

--- pwiz_tools/SeeMS/Manager.cs
+++ pwiz_tools/SeeMS/Manager.cs
@@ -270,27 +270,27 @@ namespace seems
             LoadDefaultAnnotationSettings();
         }
 
-        public void OpenFile(string filepath, bool closeIfOpen = false )
+        public bool OpenFile(string filepath, bool closeIfOpen = false )
         {
-            OpenFile(filepath, -1, closeIfOpen);
+            return OpenFile(filepath, -1, closeIfOpen);
         }
 
-        public void OpenFile(string filepath, int index, bool closeIfOpen = false )
+        public bool OpenFile(string filepath, int index, bool closeIfOpen = false )
         {
-            OpenFile(filepath, index > 0 ? new List<object> { index } : null, null, closeIfOpen);
+            return OpenFile(filepath, index > 0 ? new List<object> { index } : null, null, closeIfOpen);
         }
 
-        public void OpenFile(string filepath, string id, bool closeIfOpen = false )
+        public bool OpenFile(string filepath, string id, bool closeIfOpen = false )
         {
-            OpenFile(filepath, new List<object> { id }, null, closeIfOpen);
+            return OpenFile(filepath, new List<object> { id }, null, closeIfOpen);
         }
 
-        public void OpenFile (string filepath, IList<object> idOrIndexList, IAnnotation annotation, bool closeIfOpen = false )
+        public bool OpenFile(string filepath, IList<object> idOrIndexList, IAnnotation annotation, bool closeIfOpen = false )
         {
-            OpenFile(filepath, idOrIndexList, annotation, "", closeIfOpen);
+            return OpenFile(filepath, idOrIndexList, annotation, "", closeIfOpen);
         }
 
-        public void OpenFile(string filepathOrMsDataRunPath, IList<object> idOrIndexList, IAnnotation annotation, string spectrumListFilters, bool closeIfOpen = false)
+        public bool OpenFile(string filepathOrMsDataRunPath, IList<object> idOrIndexList, IAnnotation annotation, string spectrumListFilters, bool closeIfOpen = false)
         {
             var msDataRunPath = new OpenDataSourceDialog.MSDataRunPath(filepathOrMsDataRunPath);
             string filepath = msDataRunPath.ToString();
@@ -417,16 +417,12 @@ namespace seems
                     }
 				}
 
+			    return true;
 			} catch( Exception ex )
 			{
-				string message = ex.Message;
-				if( ex.InnerException != null )
-					message += "\n\nAdditional information: " + ex.InnerException.Message;
-				MessageBox.Show( message,
-								"Error opening source file",
-								MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1,
-								0, false );
+				Program.HandleException("Error opening source file", ex);
                 OnLoadDataSourceProgress("Failed to load data: " + ex.Message, 100);
+			    return false;
 			}
 		}
 
@@ -626,13 +622,7 @@ namespace seems
 
 			} catch( Exception ex )
 			{
-                string message = "SeeMS encountered an error reading metadata from \"" + managedDataSource.Source.CurrentFilepath + "\" (" + ex.ToString() + ")";
-				if( ex.InnerException != null )
-                    message += "\n\nAdditional information: " + ex.InnerException.ToString();
-				MessageBox.Show( message,
-								"Error reading source metadata",
-								MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1,
-								0, false );
+			    Program.HandleException("Error reading source metadata", ex);
 				OnLoadDataSourceProgress("Failed to load data: " + ex.Message, 100);
 			}
 		}
