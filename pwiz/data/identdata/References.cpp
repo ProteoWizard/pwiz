bd595607ec111ec2bee73184b2a3ebb0cdca4604
Matt Chambers <matt.chambers42@gmail.com>
2020-10-30 13:30:15 -0400

    * optimized SequenceCollection's resolve() to use binary searches

--- pwiz/data/identdata/References.cpp
+++ pwiz/data/identdata/References.cpp
@@ -51,7 +51,7 @@ void resolve(shared_ptr<object_type>& reference,
     if (!reference.get() || reference->id.empty())
         return; 
 
-    typename vector< shared_ptr<object_type> >::const_iterator it = 
+    typename vector< shared_ptr<object_type> >::const_iterator it =
         find_if(referentList.begin(), referentList.end(), HasID<object_type>(reference->id));
 
     if (it == referentList.end())
@@ -61,8 +61,8 @@ void resolve(shared_ptr<object_type>& reference,
             << "  object type: " << typeid(object_type).name() << endl
             << "  reference id: " << reference->id << endl
             << "  referent list: " << referentList.size() << endl;
-        for (typename vector< shared_ptr<object_type> >::const_iterator it=referentList.begin();
-             it!=referentList.end(); ++it)
+        for (typename vector< shared_ptr<object_type> >::const_iterator it = referentList.begin();
+            it != referentList.end(); ++it)
             oss << "    " << (*it)->id << endl;
         throw runtime_error(oss.str().c_str());
     }
@@ -188,7 +188,7 @@ PWIZ_API_DECL void resolve(PeptideEvidencePtr& pe, const IdentData& mzid)
     if (!pe->peptidePtr || pe->peptidePtr->peptideSequence.empty())
         resolve(pe->peptidePtr, mzid.sequenceCollection.peptides);
 
-    if (!pe->dbSequencePtr || pe->dbSequencePtr->length == 0)
+    if (!pe->dbSequencePtr || (pe->dbSequencePtr->seq.empty() && pe->dbSequencePtr->length == 0))
         resolve(pe->dbSequencePtr, mzid.sequenceCollection.dbSequences);
 
     // TODO construct a collection of TranslationTable's from all the
@@ -248,10 +248,59 @@ PWIZ_API_DECL void resolve(SequenceCollection& sc, IdentData& mzid)
     BOOST_FOREACH(DBSequencePtr& dbs, sc.dbSequences)
         resolve(dbs->searchDatabasePtr, mzid.dataCollection.inputs.searchDatabase);
     
-    ResolvePE rpe(&mzid);
-    for_each(sc.peptideEvidence.begin(),
-             sc.peptideEvidence.end(),
-             rpe);
+    // look for unresolved PeptidePtr and DBSequencePtr in PeptideEvidences
+
+    bool needPeptideResolved = false, needDbSeqResolved = false;
+    for (const auto& pe : sc.peptideEvidence)
+    {
+        if (!pe) throw runtime_error("NULL PeptideEvidencePtr");
+        if (!pe->dbSequencePtr) throw runtime_error("NULL dbSequencePtr for PeptideEvidence (no id to resolve)");
+        if (!pe->peptidePtr) throw runtime_error("NULL peptidePtr for PeptideEvidence (no id to resolve)");
+
+        if (pe->dbSequencePtr->seq.empty() && pe->dbSequencePtr->length == 0)
+            needDbSeqResolved = true;
+        if (pe->peptidePtr->peptideSequence.empty())
+            needPeptideResolved = true;
+
+        if (needDbSeqResolved && needPeptideResolved)
+            break;
+    }
+
+    map<string, const DBSequencePtr*> dbSeqById;
+    if (needDbSeqResolved)
+        for (const auto& s : sc.dbSequences)
+            if (s)
+                dbSeqById[s->id] = &s;
+
+    map<string, const PeptidePtr*> peptideById;
+    if (needPeptideResolved)
+        for (const auto& p : sc.peptides)
+            if (p)
+                peptideById[p->id] = &p;
+
+    for (auto& pe : sc.peptideEvidence)
+    {
+        // null check still needed in case loop above broke early
+        if (!pe) throw runtime_error("NULL PeptideEvidencePtr");
+        if (!pe->dbSequencePtr) throw runtime_error("NULL dbSequencePtr for PeptideEvidence (no id to resolve)");
+        if (!pe->peptidePtr) throw runtime_error("NULL peptidePtr for PeptideEvidence (no id to resolve)");
+
+        if (needDbSeqResolved && pe->dbSequencePtr->seq.empty() && pe->dbSequencePtr->length == 0)
+        {
+            auto findItr = dbSeqById.find(pe->dbSequencePtr->id);
+            if (findItr == dbSeqById.end())
+                throw runtime_error("dBSequence_ref for PeptideEvidence " + pe->id + " does not resolve to a DBSequence element");
+            pe->dbSequencePtr = *findItr->second;
+        }
+
+        if (needPeptideResolved && pe->peptidePtr->peptideSequence.empty())
+        {
+            auto findItr = peptideById.find(pe->peptidePtr->id);
+            if (findItr == peptideById.end())
+                throw runtime_error("peptide_ref for PeptideEvidence " + pe->id + " does not resolve to a Peptide element");
+            pe->peptidePtr = *findItr->second;
+        }
+    }
 
     // Create a single Enzyme referent list
     vector<EnzymePtr> enzymePtrs;
