01553155d4e061a52b1a10acb8e06e8c80fc5dbd
Nicholas Shulman <nicksh@proteinms.net>
2020-11-21 00:05:21 -0800

    Change report that gets used for example of a dataset with more than one column group.

--- pwiz_tools/Skyline/TestFunctional/ClusteredHeatMapTest.cs
+++ pwiz_tools/Skyline/TestFunctional/ClusteredHeatMapTest.cs
@@ -1,12 +1,6 @@
-﻿using log4net.Core;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using pwiz.Common.DataBinding;
-using pwiz.Common.DataBinding.Controls.Editor;
-using pwiz.Skyline.Controls;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using pwiz.Skyline.Controls.Clustering;
 using pwiz.Skyline.Controls.Databinding;
-using pwiz.Skyline.Model;
-using pwiz.Skyline.Model.Databinding.Entities;
 using pwiz.SkylineTestUtil;
 
 namespace pwiz.SkylineTestFunctional
@@ -38,33 +32,11 @@ namespace pwiz.SkylineTestFunctional
             PauseForScreenShot("Normal heat map");
             OkDialog(heatMap, heatMap.Close);
 
-            // Use the Results Grid to show a weird heat map where two distinct sets of columns have been pivoted
-            RunUI(()=>
-            {
-                SkylineWindow.ShowResultsGrid(true);
-                SkylineWindow.SelectedPath = SkylineWindow.Document.GetPathTo((int) SrmDocument.Level.Transitions, 0);
-            });
-            var resultsGrid = FindOpenForm<LiveResultsGrid>();
-            WaitForCondition(() => resultsGrid.IsComplete);
-            // Add columns which are pivoted on two different axes.
-            // There will be the columns "Area" and "Background" which both come from "Transition.Results!*.Value"
-            // And there will be "ProductMz" which comes from Transition.Precursor.Transitions!*
-            RunDlg<ViewEditor>(resultsGrid.NavBar.CustomizeView, viewEditor =>
-            {
-                viewEditor.ViewName = "ViewWithMultiplePivotedColumnSets";
-                var chooseColumnsTab = viewEditor.ChooseColumnsTab;
-                chooseColumnsTab.AddColumn(PropertyPath.Parse("PrecursorResult.PeptideResult.ResultFile.Replicate"));
-                // Add these two columns which are pivoted along the Transition.Results!*.Value axis
-                chooseColumnsTab.AddColumn(PropertyPath.Parse("Transition.Results!*.Value.Area"));
-                chooseColumnsTab.AddColumn(PropertyPath.Parse("Transition.Results!*.Value.Background"));
-                // Add this one column which is pivoted along the Transition.Precursor.Transitions!* axis
-                chooseColumnsTab.AddColumn(PropertyPath.Parse("Transition.Precursor.Transitions!*.ProductMz"));
-                viewEditor.OkDialog();
-            });
-            WaitForCondition(() => resultsGrid.IsComplete);
-            heatMap = ShowDialog<HierarchicalClusterGraph>(() => resultsGrid.DataboundGridControl.ShowHeatMap());
-            Assert.IsNotNull(heatMap);
-            PauseForScreenShot("Heat map with two dendrograms above the column");
+            RunUI(()=>documentGrid.ChooseView("ThreeColumnGroups"));
+            WaitForCondition(() => documentGrid.IsComplete);
+            heatMap = ShowDialog<HierarchicalClusterGraph>(()=>documentGrid.DataboundGridControl.ShowHeatMap());
+            PauseForScreenShot("Heat map with three column groups");
+            Assert.AreEqual(3, heatMap.Results.ColumnGroupDendrograms.Count);
             OkDialog(heatMap, heatMap.Close);
         }
     }
