1fd21180389678a072e34de04b3ca0e7cf3f1f67
Nicholas Shulman <nicksh@proteinms.net>
2021-01-05 15:15:57 -0800

    DeconvolutedChromatogram

--- pwiz_tools/Skyline/Model/Results/Deconvolution/DeconvolutedChromatogram.cs
+++ pwiz_tools/Skyline/Model/Results/Deconvolution/DeconvolutedChromatogram.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace pwiz.Skyline.Model.Results.Deconvolution
+﻿namespace pwiz.Skyline.Model.Results.Deconvolution
 {
     public class DeconvolutedChromatogram
     {
