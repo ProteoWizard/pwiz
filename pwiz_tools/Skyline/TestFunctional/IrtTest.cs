10850cfa8828be85352c9e1277dfa5a851ebf38d
kaipo <5232643+kaipot@users.noreply.github.com>
2021-03-03 17:17:03 -0800

    Add output to help figure out irt test failures

--- pwiz_tools/Skyline/TestFunctional/IrtTest.cs
+++ pwiz_tools/Skyline/TestFunctional/IrtTest.cs
@@ -415,6 +415,7 @@ namespace pwiz.SkylineTestFunctional
             {
                 //Get 11 peptides from the document (all of them) and go back to calculator dialog
                 calibrateDlg2.Recalculate(SkylineWindow.Document, 11);
+                calibrateDlg2.WriteFixedPointPeptides(); // diagnostic
                 calibrateDlg2.StandardName = "Document2";
                 calibrateDlg2.OkDialog();
             });
