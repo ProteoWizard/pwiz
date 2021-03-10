5d721c8bb0be6ed59b07a647b025338c7188cacf
Matt Chambers <matt.chambers42@gmail.com>
2021-03-10 13:02:31 -0500

    * fixed installer test and Mobilion chromatogram 32-bit test

--- pwiz_tools/Skyline/TestData/PwizFileInfoTest.cs
+++ pwiz_tools/Skyline/TestData/PwizFileInfoTest.cs
@@ -95,7 +95,7 @@ namespace pwiz.SkylineTestData
             VerifyTicChromatogram(TestFilesDir.GetVendorTestData(TestFilesDir.VendorDir.Agilent, "ImsSynthAllIons.d"), 49, 369032);
             VerifyTicChromatogram(TestFilesDir.GetVendorTestData(TestFilesDir.VendorDir.Agilent, "GFb_4Scan_TimeSegs_1530_100ng.d"), 63, 56163792);
             VerifyTicChromatogram(TestFilesDir.GetVendorTestData(TestFilesDir.VendorDir.Bruker, "Hela_QC_PASEF_Slot1-first-6-frames.d"), 1, 23340182);
-            VerifyTicChromatogram(TestFilesDir.GetVendorTestData(TestFilesDir.VendorDir.Mobilion, "ExampleTuneMix_binned5.mbi"), 29, 61158643);
+            VerifyTicChromatogram(TestFilesDir.GetVendorTestData(TestFilesDir.VendorDir.Mobilion, "ExampleTuneMix_binned5" + ExtensionTestContext.ExtMobilionRaw), 29, 61158643);
             VerifyTicChromatogram(TestFilesDir.GetVendorTestData(TestFilesDir.VendorDir.Thermo, "090701-LTQVelos-unittest-01.raw"), 30, 32992246);
             VerifyTicChromatogram(TestFilesDir.GetVendorTestData(TestFilesDir.VendorDir.Waters, "MSe_Short.raw"), 2, 3286253);
             VerifyTicChromatogram(TestFilesDir.GetVendorTestData(TestFilesDir.VendorDir.Waters, "HDMRM_Short_noLM.raw"), 0, 0);
@@ -230,7 +230,7 @@ namespace pwiz.SkylineTestData
             {
                 var tic = msDataFile.GetTotalIonCurrent();
                 Assert.AreEqual(count, tic.Length);
-                Assert.AreEqual(maxIntensity, tic.Length > 0 ? tic.Max() : 0);
+                Assert.AreEqual(maxIntensity, tic.Length > 0 ? tic.Max() : 0, maxIntensity * 1e-7);
             }
         }
 
