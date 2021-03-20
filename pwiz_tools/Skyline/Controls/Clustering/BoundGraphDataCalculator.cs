4663e7e04cecca93fb09ecc05bfdb4beb06446b6
Nicholas Shulman <nicksh@proteinms.net>
2021-03-20 15:17:54 -0700

    File header comment.

--- pwiz_tools/Skyline/Controls/Clustering/BoundGraphDataCalculator.cs
+++ pwiz_tools/Skyline/Controls/Clustering/BoundGraphDataCalculator.cs
@@ -1,10 +1,33 @@
-﻿using System.Threading;
+﻿/*
+ * Original author: Nicholas Shulman <nicksh .at. u.washington.edu>,
+ *                  MacCoss Lab, Department of Genome Sciences, UW
+ *
+ * Copyright 2021 University of Washington - Seattle, WA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System.Threading;
 using pwiz.Common.DataBinding;
 using pwiz.Skyline.Controls.Graphs;
 using ZedGraph;
 
 namespace pwiz.Skyline.Controls.Clustering
 {
+    /// <summary>
+    /// GraphDataCalculator which gets its input data from a DataSchema.
+    /// When working with data from a DataSchema on a background thread, you must always
+    /// make sure to have called <see cref="QueryLock.GetReadLock"/>.
+    /// </summary>
     public abstract class BoundGraphDataCalculator<TInput, TResults> : GraphDataCalculator<TInput, TResults> where TInput : DataSchemaInput
     {
         protected BoundGraphDataCalculator(CancellationToken parentCancellationToken, ZedGraphControl zedGraphControl) : base(parentCancellationToken, zedGraphControl)
