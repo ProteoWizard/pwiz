098f903a384ec31906fd3b1e4d62de19ed67aa00
Nicholas Shulman <nicksh@proteinms.net>
2021-04-01 20:10:20 -0700

    Make SortRESX.exe get call as part of Skyline pre build events.

---
+++ pwiz_tools/Skyline/PreBuildEvents.bat
@@ -0,0 +1,4 @@
+echo "%~dp0Executables\SortRESX\SortRESX.exe" "%~dp0..\.."
+"%~dp0Executables\SortRESX\SortRESX.exe" "%~dp0..\.."
+echo "%~dp0ProtocolBuffers\generatecode.bat"
+"%~dp0ProtocolBuffers\generatecode.bat"
