b0bd63a1c6497e3d40607cd9eb695ac58d2f6c74
bspratt <bspratt@protein.ms>
2021-04-04 15:09:19 -0700

    relocate the console test for "Only .sky files are accepted with the --in argument" (rejecting .sky.zip) to an area where a .sky.zip file is actually available for test purposes

--- pwiz_tools/Skyline/TestData/CommandLineTest.cs
+++ pwiz_tools/Skyline/TestData/CommandLineTest.cs
@@ -117,6 +117,14 @@ namespace pwiz.SkylineTestData
 
             var outFilesDir = new TestFilesDir(TestContext, outPath);
             AssertEx.FileExists(outFilesDir.GetTestPath("BSA_Protea_label_free_20100323_meth3_multi.sky"));
+
+            // Only .sky files are accepted with the --in argument
+            var output = RunCommand("--in=" + outPath);
+            Assert.IsTrue(output.Contains(string.Format(
+                Resources
+                    .CommandArgs_ARG_IN_The_specified_input_file__0__is_not_supported__Only_Skyline_files_with_the__1__extension_are_accepted_,
+                outPath,
+                SrmDocument.EXT)));
         }
 
         [TestMethod]
@@ -849,16 +857,8 @@ namespace pwiz.SkylineTestData
             string rawPath = testFilesDir.GetTestPath("ah_20101011y_BSA_MS-MS_only_5-2" +
                 ExtensionTestContext.ExtThermoRaw);
 
-            // Only .sky files are accepted with the --in argument
-            var output = RunCommand("--in=" + bogusPath + ".zip");
-            Assert.IsTrue(output.Contains(string.Format(
-                Resources
-                    .CommandArgs_ARG_IN_The_specified_input_file__0__is_not_supported__Only_Skyline_files_with_the__1__extension_are_accepted_,
-                bogusPath + ".zip",
-                SrmDocument.EXT)));
-
             //Error: file does not exist
-            output = RunCommand("--in=" + bogusPath);
+            var output = RunCommand("--in=" + bogusPath);
             Assert.IsTrue(output.Contains(string.Format(Resources.CommandLine_OpenSkyFile_Error__The_Skyline_file__0__does_not_exist_, bogusPath)));
 
             //Error: no raw file
