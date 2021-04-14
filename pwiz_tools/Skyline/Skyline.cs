5226d6d516aef8afd9f434556adec97cc17bbb3c
nickshulman <nicksh@proteinms.net>
2021-04-14 07:59:51 -0700

    Fix opening zipped file warning and some Heat Map bugs (#1536)
    Fix case where "Opening a document inside a ZIP file is not supported" message would not be shown if the file in the .zip was nested under multiple folders.
    
    Reported by tun-li shen
    
    * Fix a couple of problems with the Heat Map graph:
    1. The labels on the Y-axis sometimes get rotated 90 degrees
    2. The PaneProgressBar sometimes remains visible after the calculations are all done.
    (reported by Brendan)

--- pwiz_tools/Skyline/Skyline.cs
+++ pwiz_tools/Skyline/Skyline.cs
@@ -290,11 +290,10 @@ namespace pwiz.Skyline
             }
             // If the parent directory ends with .zip and lives in AppData\Local\Temp
             // then the user has double-clicked a file in Windows Explorer inside a ZIP file
-            if (parentDir != null && PathEx.HasExtension(parentDir, @".zip") &&
-                parentDir.ToLower().Contains(@"appdata\local\temp"))
+            if (DirectoryEx.IsTempZipFolder(parentDir, out string zipFileName))
             {
                 MessageDlg.Show(this, TextUtil.LineSeparate(Resources.SkylineWindow_HasFileToOpen_Opening_a_document_inside_a_ZIP_file_is_not_supported_,
-                    string.Format(Resources.SkylineWindow_HasFileToOpen_Unzip_the_file__0__first_and_then_open_the_extracted_file__1__, Path.GetFileName(parentDir), Path.GetFileName(_fileToOpen))));
+                    string.Format(Resources.SkylineWindow_HasFileToOpen_Unzip_the_file__0__first_and_then_open_the_extracted_file__1__, zipFileName, Path.GetFileName(_fileToOpen))));
                 return false;
             }
 
