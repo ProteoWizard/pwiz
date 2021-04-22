432e46b219292f43b0fab6fea0a9f1a043f8d2eb
nickshulman <nicksh@proteinms.net>
2021-04-21 23:08:41 -0700

    Fix RTScheduleGraphPane exceptions (#1551) (#1554)
    Fix Exception "Scheduling failure (no targets?)" displaying RTScheduleGraphPane if Bruker template file is specified and the document is empty.
    (Reported on the exception web)
    
    Fix NullReferenceException trying to display Scheduling Graph from Export Method dialog.
    (Discovered by Kaipo)
    
    Fix Overflow exception if user sets the scheduling window to "NaN" (not a number).
    (Found by Nick. I'll do a separate PR to make it so that MessageBoxHelper prevents the user from being able to type NaN into a DecimalListTextBox).
    
    Also, added a step to TestExportMethodDlg which brings up ExportMethodScheduleGraph

--- pwiz_tools/Skyline/Controls/Graphs/RTScheduleGraphPane.cs
+++ pwiz_tools/Skyline/Controls/Graphs/RTScheduleGraphPane.cs
@@ -77,12 +77,14 @@ namespace pwiz.Skyline.Controls.Graphs
         public BrukerTimsTofMethodExporter.Metrics BrukerMetrics { get; set; }
 
         private bool _exportMethodDlg;
+        private ZedGraphControl _graphControl;
 
-        public RTScheduleGraphPane(GraphSummary graphSummary, bool isExportMethodDlg = false)
+        public RTScheduleGraphPane(GraphSummary graphSummary, ZedGraphControl graphControl, bool isExportMethodDlg = false)
             : base(graphSummary)
         {
             _exportMethodDlg = isExportMethodDlg;
-            _dataCalculator = new SchedulingDataCalculator(this);
+            _graphControl = graphControl;
+            _dataCalculator = new SchedulingDataCalculator(this, graphControl);
 
             XAxis.Title.Text = Resources.RTScheduleGraphPane_RTScheduleGraphPane_Scheduled_Time;
             YAxis.Scale.MinAuto = false;
@@ -148,7 +150,7 @@ namespace pwiz.Skyline.Controls.Graphs
             }
 
             AxisChange();
-            GraphSummary.GraphControl.Invalidate();
+            _graphControl.Invalidate();
         }
 
         public void AddSchedulingCurve(string label, IPointList points, Color color)
@@ -250,12 +252,14 @@ namespace pwiz.Skyline.Controls.Graphs
 
         private class SchedulingDataCalculator : GraphDataCalculator<InputData, Results>
         {
-            public SchedulingDataCalculator(RTScheduleGraphPane graphPane) : base(CancellationToken.None, graphPane.GraphSummary.GraphControl)
+            public SchedulingDataCalculator(RTScheduleGraphPane graphPane, ZedGraphControl graphControl) : base(CancellationToken.None, graphControl, graphPane)
             {
-                GraphPane = graphPane;
             }
 
-            public new RTScheduleGraphPane GraphPane { get; }
+            public new RTScheduleGraphPane GraphPane
+            {
+                get { return (RTScheduleGraphPane) base.GraphPane; }
+            }
 
             protected override Results CalculateResults(InputData input, CancellationToken cancellationToken)
             {
@@ -295,9 +299,18 @@ namespace pwiz.Skyline.Controls.Graphs
                     }
                     else
                     {
-                        BrukerTimsTofMethodExporter.GetScheduling(document,
-                            new ExportDlgProperties(new ExportMethodDlg(document, ExportFileType.Method), new CancellationToken()) { MethodType = ExportMethodType.Scheduled },
-                            input.BrukerTemplateFileValue, new SilentProgressMonitor(cancellationToken), out brukerPoints);
+                        try
+                        {
+                            BrukerTimsTofMethodExporter.GetScheduling(document,
+                                new ExportDlgProperties(new ExportMethodDlg(document, ExportFileType.Method),
+                                    new CancellationToken()) {MethodType = ExportMethodType.Scheduled},
+                                input.BrukerTemplateFileValue, new SilentProgressMonitor(cancellationToken),
+                                out brukerPoints);
+                        }
+                        catch (Exception)
+                        {
+                            // ignore "Scheduling failure (no points)" error
+                        }
                     }
 
                     return brukerPoints;
@@ -355,7 +368,7 @@ namespace pwiz.Skyline.Controls.Graphs
             public static IEnumerable<KeyValuePair<double, double>> GetOverlapCounts(
                 IEnumerable<PrecursorScheduleBase> schedules, double minTime, double maxTime, double stepSize)
             {
-                if (maxTime < minTime)
+                if (maxTime < minTime || double.IsNaN(maxTime) || double.IsNaN(minTime))
                 {
                     return Enumerable.Empty<KeyValuePair<double, double>>();
                 }
