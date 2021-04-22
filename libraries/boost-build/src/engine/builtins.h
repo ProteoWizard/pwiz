dd05870e3a8a83accf60dbd05e641fda0488ea9d
Matt Chambers <matt.chambers42@gmail.com>
2021-04-22 17:30:18 -0400

    * restored Boost.Build customizations (keep this commit separate and use it as a patch for future Boost.Build updates)

--- libraries/boost-build/src/engine/builtins.h
+++ libraries/boost-build/src/engine/builtins.h
@@ -65,6 +65,7 @@ LIST *builtin_precious( FRAME * frame, int flags );
 LIST *builtin_self_path( FRAME * frame, int flags );
 LIST *builtin_makedir( FRAME * frame, int flags );
 LIST *builtin_readlink( FRAME * frame, int flags );
+LIST *builtin_rescan( FRAME *frame, int flags );
 LIST *builtin_glob_archive( FRAME * frame, int flags );
 LIST *builtin_debug_print_helper( FRAME * frame, int flags );
 
