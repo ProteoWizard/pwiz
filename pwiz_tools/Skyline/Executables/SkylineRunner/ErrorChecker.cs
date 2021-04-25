39a6ffb1c7e6b6047b1a0447ad6160d95e22b1f1
vagisha <vagisha@gmail.com>
2021-04-25 07:44:48 -0700

    Skyline: Always output a message beginning with "Error:" when returning a non-zero exit code from CommandLine. (#1531)
    * Skyline: Always output a message beginning with "Error:" when returning a non-zero exit code from CommandLine.
    - Attempted improvements at the new logic for SkylineRunner error exit codes
    - make SkylineRunner error exit codes more robust to output with --timestamp and --memstamp
    
    Co-authored-by: Brendan MacLean <brendanx67@users.noreply.github.com>
    Co-authored-by: brendanx67 <brendanx@proteinms.net>

---
+++ pwiz_tools/Skyline/Executables/SkylineRunner/ErrorChecker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace pwiz.SkylineRunner
+{
+    public class ErrorChecker
+    {
+        // We want to be able to distribute SkylineRunner as a single EXE file. So, requiring
+        // resource DLLs for language translation is not possible.
+        private static readonly string[] INTL_ERROR_PREFIXES =
+        {
+            "エラー：", // ja
+            "错误："    // zh-CHS
+        };
+
+        public static bool IsErrorLine(string line)
+        {
+            // The English prefix can happen in any culture when running Skyline-daily with a new
+            // untranslated error message.
+            if (HasErrorPrefix(line, "Error:", StringComparison.InvariantCulture))
+                return true;
+
+            return INTL_ERROR_PREFIXES.Any(p => HasErrorPrefix(line, p, StringComparison.CurrentCulture));
+        }
+
+
+        private static bool HasErrorPrefix(string line, string prefix, StringComparison comparisonType)
+        {
+            int prefixIndex = line.IndexOf(prefix, comparisonType);
+            if (prefixIndex == -1)
+                return false;
+            // The prefix could start the line or it could be preceded by a tab character
+            // if the output includes a timestamp or memory stamp.
+            return prefixIndex == 0 || line[prefixIndex - 1] == '\t';
+        }
+    }
+}
