d32acfb833c49c892a3be6973437d07c3e248087
Matt Chambers <matt.chambers42@gmail.com>
2021-04-29 17:30:30 -0400

    * fixed ObjectDisposedException

--- pwiz_tools/Skyline/FileUI/PeptideSearch/DDASearchControl.cs
+++ pwiz_tools/Skyline/FileUI/PeptideSearch/DDASearchControl.cs
@@ -73,7 +73,7 @@ namespace pwiz.Skyline.FileUI.PeptideSearch
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
-            cancelToken.Cancel();
+            cancelToken?.Cancel();
 
             if (disposing && (components != null))
             {
