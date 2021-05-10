c1b0017c990adfe5434fbcaf07317af6e58cd8c2
Chris Ashwood <chrashwood@gmail.com>
2021-05-10 15:28:43 -0500

    Update PwizReader.cpp
    Fixed tabs to space indentation.

--- pwiz_tools/BiblioSpec/src/PwizReader.cpp
+++ pwiz_tools/BiblioSpec/src/PwizReader.cpp
@@ -413,7 +413,7 @@ void PwizReader::transferSpec(BiblioSpec::SpecData& returnData,
     
     returnData.id = specInfo->scanNumber;
     returnData.retentionTime = specInfo->retentionTime/60;  // seconds to minutes
-	returnData.mz = specInfo->precursors[0].mz;
+    returnData.mz = specInfo->precursors[0].mz;
     returnData.numPeaks = specInfo->data.size();
     
     if( returnData.numPeaks > 0 ){
@@ -440,7 +440,7 @@ void PwizReader::transferSpectrum(BiblioSpec::Spectrum& returnSpectrum,
     returnSpectrum.setScanNumber(specInfo->scanNumber);
     returnSpectrum.setMz(specInfo->precursors[0].mz);
     addCharges(returnSpectrum, foundSpec);
-	returnSpectrum.setRetentionTime(specInfo->retentionTime/60);
+    returnSpectrum.setRetentionTime(specInfo->retentionTime/60);
     
     if( specInfo->data.size() > 0 ){
         vector<BiblioSpec::PEAK_T> peaks;
