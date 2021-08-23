6ce878246365d9d7c23ad46883d7870582a969dd
brendanx67 <brendanx@proteinms.net>
2021-08-23 10:42:29 -0700

    - Finalize DIA tutorials and pass all tests

--- pwiz_tools/Skyline/TestPerf/DdaTutorialTest.cs
+++ pwiz_tools/Skyline/TestPerf/DdaTutorialTest.cs
@@ -62,6 +62,11 @@ namespace TestPerf
             };
             RunFunctionalTest();
         }
+        public bool IsRecordMode
+        {
+            get { return false; }
+        }
+
         private const string HEAVY_R = "Label:13C(6)15N(4) (C-term R)";
         private const string HEAVY_K = "Label:13C(6)15N(2) (C-term K)";
         private const string OXIDATION_M = "Oxidation (M)";
