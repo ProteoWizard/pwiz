37288d39b3bee76c1899f2ba56b7d1a30236c845
Matt Chambers <matt.chambers42@gmail.com>
2021-08-26 16:21:26 -0400

    * changed msvc_setup_script.bat to take an argument for platform toolset version (i.e. 141, 142, 143) so the version of Visual Studio tools is explicitly chosen; otherwise Boost.Build might be using one toolset and msvc_setup_script might set up a different one (it might either use the newest VS it finds, or the oldest) * refactored MSVC_CURRENT_SETUP_SCRIPT as a function instead of a constant in order to give current toolset to msvc_setup_script.bat * refactored MSFileReader copies as Boost.Build targets instead of shell commands in order to use new MSVC_CURRENT_SETUP_SCRIPT *

--- libraries/msvc_setup_script.bat
+++ libraries/msvc_setup_script.bat
@@ -1,3 +1,8 @@
 CALL "%~dp0boost-build\src\engine\vswhere_usability_wrapper.cmd"
-IF EXIST "%VS160COMNTOOLS%VsDevCmd.bat" CALL "%VS160COMNTOOLS%VsDevCmd.bat" -arch=%1 && exit /b
-IF EXIST "%VS150COMNTOOLS%VsDevCmd.bat" CALL "%VS150COMNTOOLS%VsDevCmd.bat" -arch=%1 && exit /b
+IF "%2"=="" echo Must specify platform toolset version without period (e.g. 141, 142, 143)
+IF "%2"=="141" IF EXIST "%VS150COMNTOOLS%VsDevCmd.bat" (CALL "%VS150COMNTOOLS%VsDevCmd.bat" -arch=%1 && exit /b 0) ELSE echo VS2017 not found. && exit /b 1
+IF "%2"=="142" IF EXIST "%VS160COMNTOOLS%VsDevCmd.bat" (CALL "%VS160COMNTOOLS%VsDevCmd.bat" -arch=%1 && exit /b 0) ELSE echo VS2019 not found. && exit /b 1
+IF "%2"=="143" IF EXIST "%VS170COMNTOOLS%VsDevCmd.bat" (CALL "%VS170COMNTOOLS%VsDevCmd.bat" -arch=%1 && exit /b 0) ELSE echo VS2022 not found. && exit /b 1
+
+echo Unsupported requested platform toolset version %2; current supported versions: 141, 142, 143
+exit /b 1
