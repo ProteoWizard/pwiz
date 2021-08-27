9f001b423dc21c46fdf665484ffea28f0e756892
Nicholas Shulman <nicksh@proteinms.net>
2021-08-27 16:56:51 -0700

    Uncomment assert about peak areas in ReimportResultsTest.

--- pwiz_tools/Skyline/TestFunctional/ReimportResultsTest.cs
+++ pwiz_tools/Skyline/TestFunctional/ReimportResultsTest.cs
@@ -145,10 +145,8 @@ namespace pwiz.SkylineTestFunctional
             transitionChromInfo = SkylineWindow.Document.MoleculeTransitions.Skip(1).First().Results[0].First();
             Assert.AreEqual(peak2.Item1, transitionChromInfo.StartRetentionTime, deltaRetentionTime);
             Assert.AreEqual(peak2.Item2, transitionChromInfo.EndRetentionTime, deltaRetentionTime);
-            // TODO(nicksh): this next assert should succeed, but it seems that PeakIntegrator.IntegratePeakWithoutBackground
-            // calculates the completely wrong number
-            // Assert.AreEqual(expectedReimportedArea2 + expectedReimportedBackground2, transitionChromInfo.Area, 10);
-            Assert.AreEqual(0, transitionChromInfo.BackgroundArea, 10);
+            Assert.AreEqual(expectedReimportedArea2 + expectedReimportedBackground2, transitionChromInfo.Area, 10);
+            Assert.AreEqual(0, transitionChromInfo.BackgroundArea, 100);
         }
 
         private static Tuple<double, double> ChangePeakBounds(GraphChromatogram graphChromatogram, double startTime, double endTime)
