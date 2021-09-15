19ff6dbac02afb8b0e8b2e4d9535715d24fafe61
Matt Chambers <matt.chambers42@gmail.com>
2021-09-15 14:23:55 -0400

    * disabled 64-bit MSFileReader registration (new TC agent doesn't even have it)

--- pwiz_tools/reg-controls.bat
+++ pwiz_tools/reg-controls.bat
@@ -7,17 +7,7 @@ REM # Bracketing causes problems, because of the quoting around cmd /c "...(x86)
 REM # So use gotos instead
 
 if "%1" NEQ "64" goto reg32
-
-	IF EXIST "c:\Program Files (x86)\Thermo\MSFileReader\XRawfile2.dll" regsvr32 /s /u "c:\Program Files (x86)\Thermo\MSFileReader\XRawfile2.dll"
-	REM # regsvr32 must be called through cmd /c for it to impact %ERRORLEVEL% with the /s option
-	IF EXIST "c:\Program Files\Thermo\MSFileReader\XRawfile2_x64.dll" cmd /c "regsvr32 /s "c:\Program Files\Thermo\MSFileReader\XRawfile2_x64.dll""
-	if %ERRORLEVEL% NEQ 0 (
-	    echo     *** Couldn't register 64-bit MSFileReader
-	    set ERRORLEVEL=1
-	    exit /b 1
-	)
-	echo     *** Registered 64-bit MSFileReader
-
+REM No more MSFileReader for 64-bit.
 goto success
 
 :reg32
