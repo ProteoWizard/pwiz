df519410c06e3b2c0f7ce10975d97ad10096a9f9
alinmarsh <alimarsh@mit.edu>
2021-10-17 19:14:50 -0700

    Remove Panorama browse button

--- pwiz_tools/Skyline/Executables/SkylineBatch/SkylineBatch/RemoteFileControl.Designer.cs
+++ pwiz_tools/Skyline/Executables/SkylineBatch/SkylineBatch/RemoteFileControl.Designer.cs
@@ -28,32 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnBrowse = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.textRelativePath = new System.Windows.Forms.TextBox();
             this.comboRemoteFileSource = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // btnBrowse
-            // 
-            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnBrowse.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.btnBrowse.Location = new System.Drawing.Point(350, 64);
-            this.btnBrowse.Margin = new System.Windows.Forms.Padding(4);
-            this.btnBrowse.Name = "btnBrowse";
-            this.btnBrowse.Size = new System.Drawing.Size(32, 23);
-            this.btnBrowse.TabIndex = 26;
-            this.btnBrowse.Text = "...";
-            this.btnBrowse.UseVisualStyleBackColor = true;
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.label2.Location = new System.Drawing.Point(-3, 50);
+            this.label2.Location = new System.Drawing.Point(-6, 96);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.Size = new System.Drawing.Size(144, 25);
             this.label2.TabIndex = 25;
             this.label2.Text = "Relative path:";
             // 
@@ -61,9 +49,10 @@
             // 
             this.textRelativePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textRelativePath.Location = new System.Drawing.Point(0, 66);
+            this.textRelativePath.Location = new System.Drawing.Point(0, 127);
+            this.textRelativePath.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textRelativePath.Name = "textRelativePath";
-            this.textRelativePath.Size = new System.Drawing.Size(343, 20);
+            this.textRelativePath.Size = new System.Drawing.Size(758, 31);
             this.textRelativePath.TabIndex = 24;
             // 
             // comboRemoteFileSource
@@ -72,9 +61,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.comboRemoteFileSource.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboRemoteFileSource.FormattingEnabled = true;
-            this.comboRemoteFileSource.Location = new System.Drawing.Point(0, 15);
+            this.comboRemoteFileSource.Location = new System.Drawing.Point(0, 29);
+            this.comboRemoteFileSource.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.comboRemoteFileSource.Name = "comboRemoteFileSource";
-            this.comboRemoteFileSource.Size = new System.Drawing.Size(381, 21);
+            this.comboRemoteFileSource.Size = new System.Drawing.Size(758, 33);
             this.comboRemoteFileSource.TabIndex = 23;
             this.comboRemoteFileSource.SelectedIndexChanged += new System.EventHandler(this.comboRemoteFileSource_SelectedIndexChanged);
             // 
@@ -82,31 +72,30 @@
             // 
             this.label1.AutoSize = true;
             this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.label1.Location = new System.Drawing.Point(-3, -1);
+            this.label1.Location = new System.Drawing.Point(-6, -2);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.Size = new System.Drawing.Size(163, 25);
             this.label1.TabIndex = 22;
             this.label1.Text = "Remote source:";
             // 
             // RemoteFileControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.btnBrowse);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textRelativePath);
             this.Controls.Add(this.comboRemoteFileSource);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "RemoteFileControl";
-            this.Size = new System.Drawing.Size(382, 89);
+            this.Size = new System.Drawing.Size(764, 171);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnBrowse;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textRelativePath;
         private System.Windows.Forms.ComboBox comboRemoteFileSource;
