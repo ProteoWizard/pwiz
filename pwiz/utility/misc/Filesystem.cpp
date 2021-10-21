d73b7b9b2a9fb15cc92d1ba30136f2b95803c08c
Matt Chambers <matt.chambers42@gmail.com>
2021-10-21 16:32:03 -0400

    - disabled path length checks for HTTP URLs - added warning for empty MGF output explaining possible reasons

--- pwiz/utility/misc/Filesystem.cpp
+++ pwiz/utility/misc/Filesystem.cpp
@@ -26,6 +26,9 @@
 #include "Filesystem.hpp"
 
 #ifdef _MSC_VER
+    #ifdef _WIN32_WINNT
+        #undef _WIN32_WINNT
+    #endif
     #define _WIN32_WINNT 0x0600
     #define WIN32_LEAN_AND_MEAN
     #define NOMINMAX
@@ -649,7 +652,7 @@ PWIZ_API_DECL bool isHTTP(const string& s)
     //sregex uriRegex = sregex::compile("^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?");
     //return regex_match(s, uriRegex);
 
-    return bal::istarts_with(s, "http://") || bal::istarts_with(s, "https://");
+    return bal::istarts_with(s, "http:") || bal::istarts_with(s, "https:");
 }
 
 
@@ -716,6 +719,7 @@ PWIZ_API_DECL string read_file_header(const string& filepath, size_t length)
 PWIZ_API_DECL void check_path_length(const string& path)
 {
 #ifdef WIN32
+    if (isHTTP(path)) return;
     std::wstring wide_path = boost::locale::conv::utf_to_utf<wchar_t>(bfs::absolute(path).string());
     if (wide_path.length() > 250)
         throw std::invalid_argument("path is too long (must be 250 characters or less): " + bfs::absolute(path).string());
