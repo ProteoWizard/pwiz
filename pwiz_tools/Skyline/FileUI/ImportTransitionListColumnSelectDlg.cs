81418a165c2059e155bfab42e9093118b9237b9c
Henry Sanford <henrytsanford@gmail.com>
2021-11-22 13:59:14 -0800

    Remove unneeded using statement

--- pwiz_tools/Skyline/FileUI/ImportTransitionListColumnSelectDlg.cs
+++ pwiz_tools/Skyline/FileUI/ImportTransitionListColumnSelectDlg.cs
@@ -31,7 +31,6 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Drawing;
-using System.IO;
 using System.Linq;
 using System.Windows.Forms;
 using Protein = pwiz.ProteomeDatabase.API.Protein;
