d42a65fde39c808e62f6b7db51f64e7320a68ff3
Nicholas Shulman <nicksh@proteinms.net>
2021-12-04 17:56:53 -0800

    Turn off Log Scale if user choose Global Standards or Ratio To Label normalization.

--- pwiz_tools/Skyline/TestFunctional/ConstrainNormalizationMethodTest.cs
+++ pwiz_tools/Skyline/TestFunctional/ConstrainNormalizationMethodTest.cs
@@ -1,4 +1,22 @@
-﻿using System.Collections.Generic;
+﻿/*
+ * Original author: Nicholas Shulman <nicksh .at. u.washington.edu>,
+ *                  MacCoss Lab, Department of Genome Sciences, UW
+ *
+ * Copyright 2021 University of Washington - Seattle, WA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
@@ -11,11 +29,15 @@ using pwiz.SkylineTestUtil;
 
 namespace pwiz.SkylineTestFunctional
 {
+    /// <summary>
+    /// Tests that the chosen normalization method changes to an appropriate value if
+    /// the current option is not supported by the document.
+    /// </summary>
     [TestClass]
     public class ConstrainNormalizationMethodTest : AbstractFunctionalTest
     {
         [TestMethod]
-        public void TestContrainNormalizationMethod()
+        public void TestConstrainNormalizationMethod()
         {
             TestFilesZip = @"TestFunctional\ConstrainNormalizationMethodTest.zip";
             RunFunctionalTest();
@@ -30,6 +52,8 @@ namespace pwiz.SkylineTestFunctional
                     new IdentityPath(pair.NodeMoleculeGroup.PeptideGroup, pair.NodeMolecule.Peptide)).ToList();
             Assert.AreNotEqual(0, globalStandardPaths.Count);
             Assert.IsTrue(SkylineWindow.Document.Settings.HasGlobalStandardArea);
+
+            // Modify the current document so that it has no global standards, and save the document as "NoGlobalStandards.sky"
             RunUI(()=>
             {
                 SkylineWindow.SequenceTree.SelectedPaths = globalStandardPaths;
@@ -38,43 +62,63 @@ namespace pwiz.SkylineTestFunctional
             Assert.IsFalse(SkylineWindow.Document.Settings.HasGlobalStandardArea);
             string pathNoGlobalStandardsNoHeavy = TestFilesDir.GetTestPath("NoGlobalStandards.sky");
             RunUI(()=>SkylineWindow.SaveDocument(pathNoGlobalStandardsNoHeavy));
+
+            // Open a document which has heavy standards and choose "Normalize To Heavy"
             string pathHasHeavy = TestFilesDir.GetTestPath("Human_plasma.sky");
             var heavyNormalizeOption = NormalizeOption.FromIsotopeLabelType(IsotopeLabelType.heavy);
             RunUI(()=>
             {
                 SkylineWindow.OpenFile(pathHasHeavy);
                 SkylineWindow.ShowPeakAreaReplicateComparison();
+                // Choose "Normalize To Heavy" normalization method
                 SkylineWindow.SetNormalizationMethod(heavyNormalizeOption);
             });
             WaitForGraphs();
-            var areaReplicateGraphPane = GetAreaReplicateGraphPane();
+            var areaReplicateGraphPane = FindAreaReplicateGraphPane();
             var options = GetNormalizationOptionMenuItems(areaReplicateGraphPane, heavyNormalizeOption);
+            CollectionAssert.Contains(options, heavyNormalizeOption);
             CollectionAssert.DoesNotContain(options, NormalizeOption.GLOBAL_STANDARDS);
+
+            // Open a document which has no heavy standards
             RunUI(() =>
             {
                 SkylineWindow.OpenFile(pathGlobalStandardsNoHeavy);
                 SkylineWindow.ShowPeakAreaReplicateComparison();
             });
             WaitForGraphs();
-            areaReplicateGraphPane = GetAreaReplicateGraphPane();
+            areaReplicateGraphPane = FindAreaReplicateGraphPane();
+
+            // The currently selected normalization option should have changed to "DEFAULT" since there
+            // are no global standards
             options = GetNormalizationOptionMenuItems(areaReplicateGraphPane, NormalizeOption.DEFAULT);
             CollectionAssert.DoesNotContain(options, heavyNormalizeOption);
+            CollectionAssert.Contains(options, NormalizeOption.GLOBAL_STANDARDS);
+
+            // Choose Global Standard normalization
             RunUI(()=>SkylineWindow.SetNormalizationMethod(NormalizeOption.GLOBAL_STANDARDS));
             WaitForGraphs();
             GetNormalizationOptionMenuItems(areaReplicateGraphPane, NormalizeOption.GLOBAL_STANDARDS);
 
+            // Open a document which does not have global standards
             RunUI(()=>
             {
                 SkylineWindow.OpenFile(pathNoGlobalStandardsNoHeavy);
                 SkylineWindow.ShowPeakAreaReplicateComparison();
             });
             WaitForGraphs();
-            areaReplicateGraphPane = GetAreaReplicateGraphPane();
+            areaReplicateGraphPane = FindAreaReplicateGraphPane();
+
+            // Verify that normalization option has changed to "DEFAULT".
             options = GetNormalizationOptionMenuItems(areaReplicateGraphPane, NormalizeOption.DEFAULT);
             CollectionAssert.DoesNotContain(options, NormalizeOption.GLOBAL_STANDARDS);
             CollectionAssert.DoesNotContain(options, heavyNormalizeOption);
         }
 
+        /// <summary>
+        /// Returns the set of normalization menu items that were found on the graph pane's context menu.
+        /// </summary>
+        /// <param name="graphPane">Graph pane to </param>
+        /// <param name="expectedSelected">The NormalizeOption which is expected to have a checkmark next to it</param>
         private List<NormalizeOption> GetNormalizationOptionMenuItems(SummaryGraphPane graphPane, NormalizeOption expectedSelected)
         {
             List<NormalizeOption> list = null;
@@ -121,7 +165,7 @@ namespace pwiz.SkylineTestFunctional
             return list;
         }
 
-        private AreaReplicateGraphPane GetAreaReplicateGraphPane()
+        private AreaReplicateGraphPane FindAreaReplicateGraphPane()
         {
             return FormUtil.OpenForms.OfType<GraphSummary>().Select(summary => summary.GraphControl.GraphPane)
                 .OfType<AreaReplicateGraphPane>().FirstOrDefault();
