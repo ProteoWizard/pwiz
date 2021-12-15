ed8ae4eac715c09549931eeaccb03ee26d51e41f
RitaCh <ritach@uw.edu>
2021-12-15 15:29:40 -0800

    Use the setting to decide if the spectrum should be scaled or not.

--- pwiz_tools/Skyline/Controls/Graphs/GraphFullScan.cs
+++ pwiz_tools/Skyline/Controls/Graphs/GraphFullScan.cs
@@ -1161,7 +1161,7 @@ namespace pwiz.Skyline.Controls.Graphs
                 }
             }
 
-            LoadScan(false, false);
+            LoadScan(false, Settings.Default.LockYAxis);
         }
 
         private void comboBoxScanType_SelectedIndexChanged(object sender, EventArgs e)
