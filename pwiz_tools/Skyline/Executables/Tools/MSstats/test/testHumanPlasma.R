b48f83a0ea2554f91b7fcb46228d400e2f0010ce
Nicholas Shulman <nicksh@proteinms.net>
2022-01-10 14:39:28 -0800

    Files missing from last checkin

--- pwiz_tools/Skyline/Executables/Tools/MSstats/test/testHumanPlasma.R
+++
@@ -1,6 +0,0 @@
-source("../MSstatsExternalTool.R")
-inputFile <- normalizePath("Human_plasma.csv")
-outputdir <- file.path(getwd(), "TestResult");
-outputdir <- paste(outputdir, "/", sep="")
-dir.create(outputdir)
-GroupComparison(inputFile, FALSE, "all", address=outputdir)
--- pwiz_tools/Skyline/Executables/Tools/MSstats/tool-inf/InstallPackages.r
+++ pwiz_tools/Skyline/Executables/Tools/MSstats/tool-inf/InstallPackages.r
@@ -1,7 +1,3 @@
-#Install MSstats Packages
-#a <- installed.packages()
-#packages <- a[, 1]
-
 install.packages(c("gplots", "lme4", "reshape", "reshape2",
                    "ggplot2", "ggrepel", "data.table", "dplyr", "tidyr",
                    "survival", "doSNOW", "snow", "foreach", 'stringr',
@@ -12,16 +8,3 @@ install.packages(c("gplots", "lme4", "reshape", "reshape2",
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager", repos='http://cran.us.r-project.org')
 BiocManager::install("MSstats")
-
-
-
-#if (!is.element("MSstats",packages) || packageVersion("MSstats") < "3.3.11" ){
-#directory <- tempdir()
-#directory<-gsub("\\", "/", directory, fixed = TRUE)
-#filename <- "MSstats_3.3.11.tar.gz"
-#path <- file.path(directory, filename)
-#MSstatsPackage <-normalizePath(path)
-#download.file("http://www.stat.purdue.edu/~choi67/MSstats_3.3.11.tar.gz",path)
-
-#install.packages(path, repos = NULL, type="source")
-#}
