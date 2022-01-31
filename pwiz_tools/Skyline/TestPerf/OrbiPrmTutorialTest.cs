70b3031f95248339a41846a84096440964e6f1ff
brendanx67 <brendanx@proteinms.net>
2022-01-30 16:15:06 -0800

    - checkpoint including the Targets view ratio issue Nick is going to fix

--- pwiz_tools/Skyline/TestPerf/OrbiPrmTutorialTest.cs
+++ pwiz_tools/Skyline/TestPerf/OrbiPrmTutorialTest.cs
@@ -24,15 +24,20 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pwiz.Common.DataBinding;
+using pwiz.Common.DataBinding.Controls.Editor;
 using pwiz.ProteomeDatabase.API;
 using pwiz.Skyline;
 using pwiz.Skyline.Alerts;
+using pwiz.Skyline.Controls.Databinding;
 using pwiz.Skyline.Controls.Startup;
 using pwiz.Skyline.EditUI;
+using pwiz.Skyline.FileUI;
 using pwiz.Skyline.Model;
 using pwiz.Skyline.Model.DocSettings;
 using pwiz.Skyline.Model.GroupComparison;
 using pwiz.Skyline.Model.Lib;
+using pwiz.Skyline.Model.Results;
 using pwiz.Skyline.Properties;
 using pwiz.Skyline.SettingsUI;
 using pwiz.Skyline.Util;
@@ -48,6 +53,11 @@ namespace TestPerf
     public class OrbiPrmTutorialTest : AbstractFunctionalTestEx
     {
         private const string EXT_ZIP = ".zip";
+        private const string ROOT_DIR = "PRM-Orbi";
+        private static string LIBRARY_DIR = Path.Combine(ROOT_DIR, "Heavy Library");
+        private static string DATA_DIR = Path.Combine(ROOT_DIR, "PRM data");
+        private static string SAMPLES_DIR = Path.Combine(DATA_DIR, "Samples");
+        private static string STANDARDS_DIR = Path.Combine(DATA_DIR, "Standards");
 
         [TestMethod]
         public void TestOrbiPrmTutorial()
@@ -65,27 +75,22 @@ namespace TestPerf
                 @"TestPerf\OrbiPrmViews.zip",
             };
 
-            const string rootName = "PRM-Orbi";
-            string libraryDir = Path.Combine(rootName, "Heavy Library");
-            string dataDir = Path.Combine(rootName, "PRM data");
-            string samplesDir = Path.Combine(dataDir, "Samples");
-            string standardsDir = Path.Combine(dataDir, "Standards");
             TestFilesPersistent = new[]
             {
-                Path.Combine(libraryDir, "heavy-01.mzXML"),
-                Path.Combine(libraryDir, "heavy-01.pep.xml"),
-                Path.Combine(libraryDir, "heavy-02.mzXML"),
-                Path.Combine(libraryDir, "heavy-02.pep.xml"),
-                Path.Combine(samplesDir, "G1_rep1.mzXML"),
-                Path.Combine(samplesDir, "G1_rep2.mzXML"),
-                Path.Combine(samplesDir, "G1_rep3.mzXML"),
-                Path.Combine(samplesDir, "G2M_rep1.mzXML"),
-                Path.Combine(samplesDir, "G2M_rep2.mzXML"),
-                Path.Combine(samplesDir, "G2M_rep3.mzXML"),
-                Path.Combine(samplesDir, "S_rep1.mzXML"),
-                Path.Combine(samplesDir, "S_rep2.mzXML"),
-                Path.Combine(samplesDir, "S_rep3.mzXML"),
-                Path.Combine(standardsDir, "heavy-PRM.mzXML"),
+                Path.Combine(LIBRARY_DIR, "heavy-01.mzXML"),
+                Path.Combine(LIBRARY_DIR, "heavy-01.pep.xml"),
+                Path.Combine(LIBRARY_DIR, "heavy-02.mzXML"),
+                Path.Combine(LIBRARY_DIR, "heavy-02.pep.xml"),
+                Path.Combine(SAMPLES_DIR, "G1_rep1.mzXML"),
+                Path.Combine(SAMPLES_DIR, "G1_rep2.mzXML"),
+                Path.Combine(SAMPLES_DIR, "G1_rep3.mzXML"),
+                Path.Combine(SAMPLES_DIR, "G2M_rep1.mzXML"),
+                Path.Combine(SAMPLES_DIR, "G2M_rep2.mzXML"),
+                Path.Combine(SAMPLES_DIR, "G2M_rep3.mzXML"),
+                Path.Combine(SAMPLES_DIR, "S_rep1.mzXML"),
+                Path.Combine(SAMPLES_DIR, "S_rep2.mzXML"),
+                Path.Combine(SAMPLES_DIR, "S_rep3.mzXML"),
+                Path.Combine(STANDARDS_DIR, "heavy-PRM.mzXML"),
             };
 
             RunFunctionalTest();            
@@ -95,7 +100,9 @@ namespace TestPerf
 
         private string GetTestPath(string relativePath)
         {
-            return TestFilesDirs.First().GetTestPath(Path.Combine("PRM-Orbi", relativePath));
+            if (!relativePath.StartsWith(ROOT_DIR))
+                relativePath = Path.Combine(ROOT_DIR, relativePath);
+            return TestFilesDirs.First().GetTestPath(relativePath);
         }
 
         private const string HEAVY_LIBRARY = "heavy";
@@ -105,6 +112,102 @@ namespace TestPerf
         private const string HEAVY_R = "Label:13C(6)15N(4) (C-term R)";
 
         protected override void DoTest()
+        {
+            PrepareTargets();
+            ExportMethodReport();
+        }
+
+        private void ExportMethodReport()
+        {
+            const string customReportName = "PRM_precursor_list";
+            var exportReportDlg = ShowDialog<ExportLiveReportDlg>(SkylineWindow.ShowExportReportDialog);
+            var editReportListDlg = ShowDialog<ManageViewsForm>(exportReportDlg.EditList);
+            var viewEditor = ShowDialog<ViewEditor>(editReportListDlg.AddView);
+            RunUI(() => viewEditor.ViewName = customReportName);
+            RunUI(() =>
+            {
+                viewEditor.ChooseColumnsTab.ExpandPropertyPath(PropertyPath.Parse("Proteins!*.Peptides!*.Precursors!*"), true);
+                // Make the view editor bigger so that these expanded nodes can be seen in the next screenshot
+                viewEditor.Height = Math.Min(viewEditor.Height, 434);
+                var columnsToAdd = new[]
+                { 
+                    PropertyPath.Parse("Proteins!*.Peptides!*.Precursors!*.ModifiedSequence"),
+                    PropertyPath.Parse("Proteins!*.Peptides!*.Precursors!*.Charge"),
+                    PropertyPath.Parse("Proteins!*.Peptides!*.Precursors!*.Mz"),
+                };
+                foreach (var id in columnsToAdd)
+                {
+                    Assert.IsTrue(viewEditor.ChooseColumnsTab.TrySelect(id), "Unable to select {0}", id);
+                    viewEditor.ChooseColumnsTab.AddSelectedColumn();
+                }
+                Assert.AreEqual(3, viewEditor.ChooseColumnsTab.ColumnCount);
+                // viewEditor.ChooseColumnsTab.ScrollTreeToTop();
+            });
+            PauseForScreenShot<ViewEditor.ChooseColumnsView>("Edit Report form", 18);
+
+            var previewReportDlg = ShowDialog<DocumentGridForm>(viewEditor.ShowPreview);
+            WaitForConditionUI(() => previewReportDlg.IsComplete);
+            RunUI(() =>
+            {
+                Assert.AreEqual(106, previewReportDlg.RowCount);
+                Assert.AreEqual(3, previewReportDlg.ColumnCount);
+            });
+            OkDialog(previewReportDlg, previewReportDlg.Close);
+            OkDialog(viewEditor, viewEditor.OkDialog);
+            OkDialog(editReportListDlg, editReportListDlg.OkDialog);
+            OkDialog(exportReportDlg, () =>
+            {
+                exportReportDlg.ReportName = customReportName;
+                exportReportDlg.OkDialog(GetTestPath(customReportName + TextUtil.EXT_CSV), TextUtil.SEPARATOR_CSV); // Not L10N
+            });
+
+            var importResultsDlg = ShowDialog<ImportResultsDlg>(SkylineWindow.ImportResults);
+            PauseForScreenShot<ImportResultsDlg>("Import Results form", 19);
+
+            using (new WaitDocumentChange(null, true))
+            {
+                const string schedulingFilename = "heavy-PRM.mzXML";
+                RunUI(() =>
+                {
+                    var filePaths = new[] { GetTestPath(Path.Combine(STANDARDS_DIR, schedulingFilename)) };
+                    importResultsDlg.NamedPathSets =
+                        importResultsDlg.GetDataSourcePathsFileReplicates(filePaths.Select(MsDataFileUri.Parse));
+                });
+                OkDialog(importResultsDlg, importResultsDlg.OkDialog);
+            }
+            RunUI(SkylineWindow.RemoveMissingResults);
+            AssertEx.IsDocumentState(SkylineWindow.Document, null, 19, 31, 62, 518);
+            RunUI(() =>
+            {
+                SkylineWindow.ShowPeakAreaReplicateComparison();
+                SkylineWindow.ShowRTReplicateGraph();
+                SkylineWindow.Height = 790;
+
+            });
+            RestoreViewOnScreen(20);
+
+            // TODO: Remove this after Nick fixes Targets view ratios
+            RunUI(() => SkylineWindow.NormalizeAreaGraphTo(NormalizeOption.RatioToFirstStandard(SkylineWindow.Document.Settings)));
+            WaitForGraphs();
+            RunUI(() => SkylineWindow.NormalizeAreaGraphTo(NormalizeOption.NONE));
+            WaitForGraphs();
+            RunUI(() =>
+            {
+                string lightNodeText = SkylineWindow.SequenceTree.Nodes[0].Nodes[0].Nodes[0].Text;
+                AssertEx.Contains(lightNodeText, "rdotp",
+                    string.Format(Resources.TransitionGroupTreeNode_GetResultsText_total_ratio__0__, 0.0002));
+                string heavyNodeText = SkylineWindow.SequenceTree.Nodes[0].Nodes[0].Nodes[1].Text;
+                Assert.IsFalse(heavyNodeText.Contains("rdotp"));
+            });
+
+            PauseForScreenShot<SkylineWindow>("Skyline main window", 20);
+            RunUI(SkylineWindow.CollapsePeptides);
+
+            string documentFile = TestContext.GetTestPath("PRM_Scheduled" + SrmDocument.EXT);
+            RunUI(() => SkylineWindow.SaveDocument(documentFile));
+        }
+
+        private void PrepareTargets()
         {
             // Have to open the start page from within Skyline to ensure audit logging starts up correctly
             var startPage = ShowDialog<StartPage>(SkylineWindow.OpenStartPage);
@@ -178,14 +281,14 @@ namespace TestPerf
 
             RunDlg<BuildLibraryDlg>(peptideSettingsUI.ShowBuildLibraryDlg, buildLibraryDlg =>
             {
-                buildLibraryDlg.LibraryPath = GetTestPath(@"Heavy Library") + "\\";
+                buildLibraryDlg.LibraryPath = GetTestPath(LIBRARY_DIR) + "\\";
                 buildLibraryDlg.LibraryName = HEAVY_LIBRARY;
                 buildLibraryDlg.LibraryCutoff = 0.9;
                 buildLibraryDlg.OkWizardPage();
                 IList<string> inputPaths = new List<string>
                 {
-                    GetTestPath(Path.Combine("Heavy Library", "heavy-01.pep.xml")),
-                    GetTestPath(Path.Combine("Heavy Library", "heavy-02.pep.xml")),
+                    GetTestPath(Path.Combine(LIBRARY_DIR, "heavy-01.pep.xml")),
+                    GetTestPath(Path.Combine(LIBRARY_DIR, "heavy-02.pep.xml")),
                 };
                 buildLibraryDlg.AddInputFiles(inputPaths);
                 buildLibraryDlg.OkWizardPage();
@@ -313,7 +416,7 @@ namespace TestPerf
             RunUI(() => SkylineWindow.Size = new Size(1100, 657));
             PauseForScreenShot<SkylineWindow>("Main window with peptide selected and Library Match view", 17);
 
-            string documentFile = TestContext.GetTestPath("PRM_Scheduled" + SrmDocument.EXT);
+            string documentFile = TestContext.GetTestPath("PRM_Proteome" + SrmDocument.EXT);
             RunUI(() => SkylineWindow.SaveDocument(documentFile));
         }
 
