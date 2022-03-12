6430cbfbc767f99de643e96bcc7b22b65e793e5e
Nicholas Shulman <nicksh@proteinms.net>
2022-03-12 12:12:56 -0800

    Fix DdaPeakIntegrationTest

--- pwiz_tools/Skyline/TestFunctional/DdaPeakIntegrationTest.cs
+++ pwiz_tools/Skyline/TestFunctional/DdaPeakIntegrationTest.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+ * Original author: Nicholas Shulman <nicksh .at. u.washington.edu>,
+ *                  MacCoss Lab, Department of Genome Sciences, UW
+ *
+ * Copyright 2022 University of Washington - Seattle, WA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using pwiz.Skyline.Controls.Graphs;
@@ -67,7 +85,7 @@ namespace pwiz.SkylineTestFunctional
                             // TODO(nicksh): PointsAcrossPeak reports its value as "null" instead of "zero" for backwards compatibility reasons
                             // Re-enable this assert when that behavior is fixed (we never anticipated that someone would need to have a peak 
                             // with zero points in it, but this happens all the time with DDA MS2).
-                            Assert.IsNotNull(transitionChromInfo.PointsAcrossPeak);
+                            // Assert.IsNotNull(transitionChromInfo.PointsAcrossPeak);
 
                             var chromatogramInfo = chromatogramGroupInfo.GetTransitionInfo(transitionDocNode, tolerance,
                                 TransformChrom.raw, null);
