7a226f0286ac9697612db7b82faa346cf1c35ac5
brendanx67 <brendanx@proteinms.net>
2022-03-29 09:04:47 -0700

    - remove unnecessary using clause

--- pwiz_tools/Skyline/TestFunctional/DdaPeakIntegrationTest.cs
+++ pwiz_tools/Skyline/TestFunctional/DdaPeakIntegrationTest.cs
@@ -16,7 +16,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-using System;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using pwiz.Skyline.Controls.Graphs;
