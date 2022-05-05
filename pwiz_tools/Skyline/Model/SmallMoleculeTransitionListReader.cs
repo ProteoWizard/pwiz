e387fcdfd600e6e1e48c929a393350245be0a3e6
Brian Pratt <bspratt@proteinms.net>
2022-05-05 10:07:05 -0700

    be a little more lenient with oddly (but not incorrectly) formed adducts SortSiblingsByMass, to match what we were doing before

--- pwiz_tools/Skyline/Model/SmallMoleculeTransitionListReader.cs
+++ pwiz_tools/Skyline/Model/SmallMoleculeTransitionListReader.cs
@@ -440,10 +440,7 @@ namespace pwiz.Skyline.Model
                         var smallestMassRow = r;
                         var zString = GetCellTrimmed(row, INDEX_PRECURSOR_ADDUCT) ?? 
                                       GetCellTrimmed(row, INDEX_PRECURSOR_CHARGE) ?? string.Empty;
-                        if (!Adduct.TryParse(zString, out var adductInferred, Adduct.ADDUCT_TYPE.charge_only, true))
-                        {
-                            return; // Badly formed adduct description, it will be flagged elsewhere
-                        }
+                        Adduct.TryParse(zString, out var adductInferred, Adduct.ADDUCT_TYPE.charge_only);
                         var smallestMass = adductInferred.MassFromMz(mzParsed, MassType.Monoisotopic);
 
                         for (var r2 = r + 1; r2 < Rows.Count; r2++)
@@ -457,10 +454,7 @@ namespace pwiz.Skyline.Model
                                 {
                                     var zString2 = GetCellTrimmed(row2, INDEX_PRECURSOR_ADDUCT) ?? 
                                                    GetCellTrimmed(row2, INDEX_PRECURSOR_CHARGE) ?? string.Empty;
-                                    if (!Adduct.TryParse(zString2, out var adduct2, Adduct.ADDUCT_TYPE.charge_only, true))
-                                    {
-                                        return; // Badly formed adduct description, it will be flagged elsewhere
-                                    }
+                                    Adduct.TryParse(zString2, out var adduct2, Adduct.ADDUCT_TYPE.charge_only);
                                     var mass2 = adduct2.MassFromMz(mzParsed2, MassType.Monoisotopic);
                                     visited.Add(row2);
                                     if (mass2 < smallestMass)
