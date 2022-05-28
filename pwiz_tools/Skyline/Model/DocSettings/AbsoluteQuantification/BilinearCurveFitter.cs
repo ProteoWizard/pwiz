dfc2504ac506cf0c6763a7af47081484154e2998
Nicholas Shulman <nicksh@uw.edu>
2022-05-28 14:40:40 -0700

    OptimizeTransitions

--- pwiz_tools/Skyline/Model/DocSettings/AbsoluteQuantification/BilinearCurveFitter.cs
+++ pwiz_tools/Skyline/Model/DocSettings/AbsoluteQuantification/BilinearCurveFitter.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using EnvDTE;
 using MathNet.Numerics.Statistics;
-using NHibernate.Linq.GroupJoin;
 using Statistics = pwiz.Skyline.Util.Statistics;
 
 namespace pwiz.Skyline.Model.DocSettings.AbsoluteQuantification
@@ -18,12 +19,16 @@ namespace pwiz.Skyline.Model.DocSettings.AbsoluteQuantification
             MinSameLoqCountForAccept = 25;
             GridSize = 100;
             CvThreshold = .2;
+            Random = new Random();
+            MinNumTransitions = 5;
         }
         public int MaxBootstrapIterations { get; set; }
         public int MinBootstrapIterations { get; set; }
         public int MinSameLoqCountForAccept { get; set; }
         public int GridSize { get; set; }
         public double CvThreshold { get; set; }
+        public Random Random { get; set; }
+        public int MinNumTransitions { get; set; }
 
         public BilinearCurveFit FitBilinearCurve(IEnumerable<WeightedPoint> points)
         {
@@ -74,14 +79,14 @@ namespace pwiz.Skyline.Model.DocSettings.AbsoluteQuantification
             return BilinearCurveFit.FromLinearFit(linearFit, baselineStats, totalError);
         }
 
-        public BilinearCurveFit ComputeBootstrapParams(Random random, IList<WeightedPoint> points)
+        public BilinearCurveFit ComputeBootstrapParams(IList<WeightedPoint> points)
         {
             var randomPoints = Enumerable.Range(0, points.Count)
-                .Select(i => points[random.Next(points.Count)]).ToList();
+                .Select(i => points[Random.Next(points.Count)]).ToList();
             return FitBilinearCurve(randomPoints);
         }
 
-        public double ComputeBootstrappedLoq(Random random, IList<WeightedPoint> points)
+        public double ComputeBootstrappedLoq(IList<WeightedPoint> points)
         {
             var lod = ComputeLod(points);
             var maxConcentration = points.Max(pt => pt.X);
@@ -92,7 +97,7 @@ namespace pwiz.Skyline.Model.DocSettings.AbsoluteQuantification
             double lastLoq = maxConcentration;
             for (int i = 0; i < MaxBootstrapIterations; i++)
             {
-                var p = ComputeBootstrapParams(random, points);
+                var p = ComputeBootstrapParams(points);
                 for (int iConcentration = 0; iConcentration < concentrationValues.Count; iConcentration++)
                 {
                     var area = p.GetY(concentrationValues[iConcentration]);
@@ -180,5 +185,131 @@ namespace pwiz.Skyline.Model.DocSettings.AbsoluteQuantification
             return lodConc;
         }
 
+        public QuantLimit ComputeQuantLimits(IList<WeightedPoint> areas)
+        {
+            return new QuantLimit(ComputeLod(areas), ComputeBootstrappedLoq(areas));
+        }
+
+        public Tuple<IList<int>, QuantLimit> OptimizeTransitions(OptimizeType optimizeType, IList<IList<WeightedPoint>> areas)
+        {
+            var quantLimits = new List<Tuple<int, QuantLimit>>();
+
+            OptimizeType otherOptimizeType;
+            if (optimizeType == OptimizeType.LOD)
+            {
+                otherOptimizeType = OptimizeType.LOQ;
+            }
+            else
+            {
+                otherOptimizeType = OptimizeType.LOD;
+            }
+
+            for (int iTransition = 0; iTransition < areas.Count; iTransition++)
+            {
+                quantLimits.Add(Tuple.Create(iTransition, ComputeQuantLimits(areas[iTransition])));
+            }
+
+            var maxConcentration = areas.SelectMany(list => list).Max(point => point.X);
+            var lowestLimits = new Dictionary<OptimizeType, double>()
+            {
+                {OptimizeType.LOD, quantLimits.Min(q=>q.Item2.Lod)},
+                {OptimizeType.LOQ, quantLimits.Min(q=>q.Item2.Loq)}
+            };
+            if (lowestLimits[optimizeType] == maxConcentration && lowestLimits[otherOptimizeType] < maxConcentration)
+            {
+                quantLimits = quantLimits.OrderBy(q => q.Item2.GetQuantLimit(otherOptimizeType)).ToList();
+            }
+            else
+            {
+                quantLimits = quantLimits.OrderBy(q => q.Item2.GetQuantLimit(optimizeType)).ToList();
+            }
+
+            IList<int> acceptedFragmentIndices = new List<int>();
+            var acceptedAreas = areas.First().Select(pt => new WeightedPoint(pt.X, 0, pt.Weight)).ToList();
+            foreach (var quantLimit in quantLimits.Take(MinNumTransitions))
+            {
+                if (acceptedFragmentIndices.Count > 0 && quantLimit.Item2.GetQuantLimit(optimizeType) >= maxConcentration)
+                {
+                    break;
+                }
+                acceptedFragmentIndices.Add(quantLimit.Item1);
+                acceptedAreas = acceptedAreas.Zip(areas[quantLimit.Item1],
+                    (pt1, pt2) => new WeightedPoint(pt1.X, pt1.Y + pt2.Y, pt1.Weight)).ToList();
+            }
+
+            var optimizedQuantLimit = ComputeQuantLimits(acceptedAreas);
+            int startIndex = Math.Min(acceptedFragmentIndices.Count, MinNumTransitions);
+            var rejectedItems = new List<Tuple<int, QuantLimit>>();
+            foreach (var quantLimitAndIndex in quantLimits.Skip(startIndex))
+            {
+                var fragmentIndex = quantLimitAndIndex.Item1;
+                var possibleNewAreas = acceptedAreas.Zip(areas[fragmentIndex],
+                    (pt1, pt2) => new WeightedPoint(pt1.X, pt1.Y + pt2.Y, pt1.Weight)).ToList();
+                var prospectiveQuantLimit = ComputeQuantLimits(possibleNewAreas);
+                // accept this transition if it helped the result
+                if (prospectiveQuantLimit.GetQuantLimit(optimizeType) < optimizedQuantLimit.GetQuantLimit(optimizeType))
+                {
+                    optimizedQuantLimit = prospectiveQuantLimit;
+                    acceptedAreas = possibleNewAreas;
+                    acceptedFragmentIndices.Add(fragmentIndex);
+                }
+                else
+                {
+                    // save the limits in case we don't have enough limits at the end of this
+                    rejectedItems.Add(Tuple.Create(fragmentIndex, prospectiveQuantLimit));
+                    lowestLimits[OptimizeType.LOD] = Math.Min(lowestLimits[OptimizeType.LOD],
+                        prospectiveQuantLimit.GetQuantLimit(OptimizeType.LOD));
+                    lowestLimits[OptimizeType.LOQ] = Math.Min(lowestLimits[OptimizeType.LOQ],
+                        prospectiveQuantLimit.GetQuantLimit(OptimizeType.LOQ));
+                }
+            }
+            // if we still don't have enough transitions, for the case where there were transitions at the maximum limit
+            if (acceptedFragmentIndices.Count < MinNumTransitions && rejectedItems.Any())
+            {
+                if (lowestLimits[optimizeType] == maxConcentration &&
+                    lowestLimits[otherOptimizeType] < maxConcentration)
+                {
+                    rejectedItems = rejectedItems.OrderBy(item => item.Item2.GetQuantLimit(otherOptimizeType)).ToList();
+                }
+                else
+                {
+                    rejectedItems = rejectedItems.OrderBy(item => item.Item2.GetQuantLimit(optimizeType)).ToList();
+                }
+
+                int numTransitionsNeeded = MinNumTransitions - acceptedFragmentIndices.Count;
+                foreach (var item in rejectedItems.Take(numTransitionsNeeded))
+                {
+                    acceptedFragmentIndices.Add(item.Item1);
+                    acceptedAreas = acceptedAreas.Zip(areas[item.Item1],
+                        (pt1, pt2) => new WeightedPoint(pt1.X, pt1.Y + pt2.Y, pt1.Weight)).ToList();
+                }
+
+                optimizedQuantLimit = ComputeQuantLimits(acceptedAreas);
+            }
+
+            return Tuple.Create(acceptedFragmentIndices, optimizedQuantLimit);
+        }
+
+        public class QuantLimit
+        {
+            public QuantLimit(double lod, double loq)
+            {
+                Lod = lod;
+                Loq = loq;
+            }
+
+            public double Lod { get; }
+            public double Loq { get; }
+
+            public double GetQuantLimit(OptimizeType optimizeType)
+            {
+                if (optimizeType == OptimizeType.LOD)
+                {
+                    return Lod;
+                }
+
+                return Loq;
+            }
+        }
     }
 }
---
+++ pwiz_tools/Skyline/Model/DocSettings/AbsoluteQuantification/OptimizeType.cs
@@ -0,0 +1,18 @@
+﻿namespace pwiz.Skyline.Model.DocSettings.AbsoluteQuantification
+{
+    public class OptimizeType
+    {
+        public static readonly OptimizeType LOD = new OptimizeType("lod");
+        public static readonly OptimizeType LOQ = new OptimizeType("loq");
+        private OptimizeType(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
