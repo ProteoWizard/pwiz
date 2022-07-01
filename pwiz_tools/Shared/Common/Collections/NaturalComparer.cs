3f070125afcac32856ece160256dbe1dd9fd9717
Clark Brace <clarkbrace@gmail.com>
2022-07-01 15:55:44 -0700

    Refactored and used windows natural sort functionality. Updated tests to also account for the slightly different sorting algorithm.

--- pwiz_tools/Shared/Common/Collections/NaturalComparer.cs
+++ pwiz_tools/Shared/Common/Collections/NaturalComparer.cs
@@ -18,85 +18,22 @@
  */
 
 using System;
+using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 
 namespace pwiz.Common.Collections
 {
     /// <summary>
     /// Added by Clark Brace (cbrace3)
-    /// Code sourced form Justin.Jones as part of (CPOL)
-    /// https://www.codeproject.com/Articles/22517/Natural-Sort-Comparer
+    /// https://www.pinvoke.net/default.aspx/shlwapi.strcmplogicalw
     /// </summary>
     public class NaturalComparer
     {
-
+        [DllImport("shlwapi.dll", CharSet = CharSet.Unicode, ExactSpelling = true)]
+        private static extern int StrCmpLogicalW(string x, string y);
         public static int Compare(string x, string y)
         {
-            if (x == null && y == null)
-            {
-                return 0;
-            }
-
-            if (x == null)
-            {
-                return 1;
-            }
-
-            if (y == null)
-            {
-                return -1;
-            }
-
-
-            var x1 = Regex.Split(x, @"([0-9]+)");
-
-            var y1 = Regex.Split(y, @"([0-9]+)");
-
-
-            for (var i = 0; i < x1.Length && i < y1.Length; i++)
-            {
-                var left = x1[i];
-                var right = y1[i];
-                // If both values are numbers
-                if (int.TryParse(left, out var l) && int.TryParse(right, out var r))
-                {
-                    var comp  =l.CompareTo(r);
-                    if (comp != 0)
-                    {
-                        return comp;
-                    }
-                }
-                else if (!int.TryParse(left, out _) || !int.TryParse(right, out _)) // If both are letters
-                {
-                    var comp = string.Compare(left, right, StringComparison.CurrentCulture);
-                    if (comp != 0)
-                    {
-                        return comp;
-                    }
-                }
-                else if(int.TryParse(left, out _)) // If right is a number, right wins
-                {
-                    return 1;
-                }
-                else if (int.TryParse(right, out _)) //If left is a number, left wins
-                {
-                    return -1; 
-                }
-            }
-            //If all else equal go by length. If length is the same they are identical
-            if (y1.Length > x1.Length)
-            {
-                return 1;
-            }
-            else if (x1.Length > y1.Length)
-            {
-                return -1;
-            }
-            else
-            {
-                return 0;
-            }
-            
+            return StrCmpLogicalW(x, y);
         }
     }
 }
