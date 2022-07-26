e7d3120195b853f13e698090b2378f723a2a636f
Nicholas Shulman <nicksh@uw.edu>
2022-07-26 11:40:03 -0700

    Add comments

--- pwiz_tools/Skyline/Test/ProtocolBuffers/LegacyProtocolBufferFormatsTest.cs
+++ pwiz_tools/Skyline/Test/ProtocolBuffers/LegacyProtocolBufferFormatsTest.cs
@@ -1,10 +1,33 @@
-﻿using Google.Protobuf;
+﻿/*
+ * Original author: Nicholas Shulman <nicksh .at. u.washington.edu>,
+ *                  MacCoss Lab, Department of Genome Sciences, UW
+ *
+ * Copyright 2022 University of Washington - Seattle, WA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using Google.Protobuf;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using pwiz.Skyline.Model.Serialization;
 using pwiz.SkylineTest.ProtocolBuffers.GeneratedCode;
 
 namespace pwiz.SkylineTest.ProtocolBuffers
 {
+    /// <summary>
+    /// Verifies that using the types in the Google.Protobuf.WellKnownTypes package
+    /// are binary compatible with the OptionalDouble protocol buffer messages that
+    /// we used to use.
+    /// </summary>
     [TestClass]
     public class LegacyProtocolBufferFormatsTest
     {
