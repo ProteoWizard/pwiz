2c4ed283fa25315b887610725d23584b39492d8d
Nicholas Shulman <nicksh@uw.edu>
2022-08-19 15:53:52 -0700

    Make it so that the only .resx files that get sorted are the ones in "pwiz_tools\Skyline\Model\Databinding\Entities" (i.e. column captions and tooltips).

--- pwiz_tools/Skyline/PreBuildEvents.bat
+++ pwiz_tools/Skyline/PreBuildEvents.bat
@@ -1,5 +1,5 @@
-echo "%~dp0Executables\SortRESX\SortRESX.exe" "%~dp0..\..\pwiz_tools\Skyline\EditUI" "%~dp0..\..\pwiz_tools\Shared\Common"
-"%~dp0Executables\SortRESX\SortRESX.exe" "%~dp0..\..\pwiz_tools\Skyline" "%~dp0..\..\pwiz_tools\Shared\Common"
+echo "%~dp0Executables\SortRESX\SortRESX.exe" "%~dp0..\..\pwiz_tools\Skyline\Model\Databinding\Entities"
+"%~dp0Executables\SortRESX\SortRESX.exe" "%~dp0..\..\pwiz_tools\Skyline\Model\Databinding\Entities"
 
 echo "%~dp0ProtocolBuffers\generatecode.bat"
 "%~dp0ProtocolBuffers\generatecode.bat"
