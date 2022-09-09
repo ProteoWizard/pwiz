c3f4440c9c40c16f981dfb96ad2a557ffe98b483
Nicholas Shulman <nicksh@uw.edu>
2022-09-09 11:40:44 -0700

    Revert change which suppressed error message if you choose "Edit Current" when the current imsdb is "None".

--- pwiz_tools/Skyline/SettingsUI/IonMobility/EditIonMobilityLibraryDlg.cs
+++ pwiz_tools/Skyline/SettingsUI/IonMobility/EditIonMobilityLibraryDlg.cs
@@ -263,11 +263,8 @@ namespace pwiz.Skyline.SettingsUI.IonMobility
         {
             if (!File.Exists(path))
             {
-                if (!string.IsNullOrEmpty(path))
-                {
-                    MessageDlg.Show(this, String.Format(Resources.EditIonMobilityLibraryDlg_OpenDatabase_The_file__0__does_not_exist__Click_the_Create_button_to_create_a_new_ion_mobility_library_or_click_the_Open_button_to_find_the_missing_file_,
-                        path));
-                }
+                MessageDlg.Show(this, String.Format(Resources.EditIonMobilityLibraryDlg_OpenDatabase_The_file__0__does_not_exist__Click_the_Create_button_to_create_a_new_ion_mobility_library_or_click_the_Open_button_to_find_the_missing_file_,
+                                                    path));
                 return;
             }
 
