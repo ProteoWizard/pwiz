34e493616a82c8b8d8fd8592faac31851b816bf9
Brian Pratt <bspratt@proteinms.net>
2022-09-26 10:42:52 -0700

    Extend this test to include an expected failure, and use of optional library name argument. Also convert from AbstractFunctionalTestEx to AbstractUnitTestEx

--- pwiz_tools/Skyline/TestPerf/PerfCommandlineCreateImsDbTest.cs
+++ pwiz_tools/Skyline/TestPerf/PerfCommandlineCreateImsDbTest.cs
@@ -30,7 +30,7 @@ namespace TestPerf // Note: tests in the "TestPerf" namespace only run when the 
     /// Verify commandline handling for ion mobility library building
     /// </summary>
     [TestClass]
-    public class TestCommandlineCreateImsDbPerf : AbstractFunctionalTestEx
+    public class TestCommandlineCreateImsDbPerf : AbstractUnitTestEx
     {
 
         private const string rawFile = "010521_Enamine_U6601911_A1_f100_pos_1_1_1086.d";
@@ -38,62 +38,72 @@ namespace TestPerf // Note: tests in the "TestPerf" namespace only run when the 
         [TestMethod]
         public void CommandlineCreateImsDbPerfTest()
         {
+
             TestFilesZip = GetPerfTestDataURL(@"PerfCommandlineCreateImsDbTest.zip");
             TestFilesPersistent = new[] {  rawFile }; // list of files that we'd like to unzip alongside parent zipFile, and (re)use in place
+            TestFilesDir = new TestFilesDir(TestContext, TestFilesZip, ".", TestFilesPersistent);
 
-            RunFunctionalTest();
-        }
-        protected override void DoTest()
-        {
-            var template = TestFilesDirs[0].GetTestPath("Scripps_IMS_Template.sky");
-            var inFile = TestFilesDirs[0].GetTestPath(rawFile);
-            var outSky = TestFilesDirs[0].GetTestPath("ImsDbTest.sky");
-            var transitionList = TestFilesDirs[0].GetTestPath("test_run_1_transition_list.csv");
-            var imsdb = TestFilesDirs[0].GetTestPath("ImsDbTest.imsdb");
-            var outCSV = TestFilesDirs[0].GetTestPath("ImsDbTest.csv");
-            var output = RunCommand($"--in={template}",
-                $"--out={outSky}",
-                $"--import-transition-list={transitionList}",
-                $"--import-file={inFile}",
-                $"--ionmobility-library-create={imsdb}",
-                "--ionmobility-library-name=ImsDbTest",
-                "--report-name=Precursor CCS",
-                $"--report-file={outCSV}");
-            AssertEx.FileExists(imsdb);
-
-            // Compare to expected result
-            var expected = File.ReadAllLines(TestFilesDirs[0].GetTestPath("ImsDbTest_expected.csv"));
-            var actual = File.ReadAllLines(outCSV);
-            AssertEx.AreEqual(expected.Length, actual.Length, $"Report difference - expected same line count");
-
-
-            var i = 0;
-
-            string Localize(string s)
+            var badName = @"blorf";
+            foreach (var libName in new[]{@"_myname", string.Empty, badName}) // Try with and without explicit library name, and with an illegal name
             {
-                return s.Replace(TextUtil.SEPARATOR_CSV, TextUtil.SEPARATOR_CSV_INTL).
-                    Replace(CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator, CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
-            }
-
-            foreach (var line in expected)
-            {
-                var expectedLine = line;
-                if (CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator !=
-                    CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)
+                var template = TestFilesDirs[0].GetTestPath("Scripps_IMS_Template.sky");
+                var inFile = TestFilesDirs[0].GetTestPath(rawFile);
+                var root = $"ImsDbTest{libName}";
+                var outSky = TestFilesDirs[0].GetTestPath($"{root}.sky");
+                var transitionList = TestFilesDirs[0].GetTestPath("test_run_1_transition_list.csv");
+                var imsdb = TestFilesDirs[0].GetTestPath($"{(root.EndsWith(badName) ? root + @":?" : root)}.imsdb");
+                var outCSV = TestFilesDirs[0].GetTestPath($"{root}.csv");
+                var output = RunCommand($"--in={template}",
+                    $"--out={outSky}",
+                    $"--import-transition-list={transitionList}",
+                    $"--import-file={inFile}",
+                    $"--ionmobility-library-create={imsdb}",
+                    string.IsNullOrEmpty(libName) ? string.Empty : $"--ionmobility-library-name={libName}",
+                    "--report-name=Precursor CCS",
+                    $"--report-file={outCSV}");
+                if (Equals(libName, badName))
                 {
-                    if (line.StartsWith("\""))
-                    {
-                        var parts = line.Split('"');
-                        expectedLine = parts[1] + Localize(parts[2]);
-                    }
-                    else
-                    {
-                        expectedLine = Localize(expectedLine);
-                    }
+                    // Expect failure because of illegal filename characters
+                    AssertEx.Contains(output, $"--ionmobility-library-create failed");
+                    continue;
+                }
+                AssertEx.FileExists(imsdb);
+                AssertEx.Contains(output, $"Ion mobility library changed from \"None\" to \"{(string.IsNullOrEmpty(libName) ? root : libName)}\"");
+
+                // Compare to expected result
+                var expected = File.ReadAllLines(TestFilesDirs[0].GetTestPath("ImsDbTest_expected.csv"));
+                var actual = File.ReadAllLines(outCSV);
+                AssertEx.AreEqual(expected.Length, actual.Length, $"Report difference - expected same line count");
+
+                var i = 0;
+
+                string Localize(string s)
+                {
+                    return s.Replace(TextUtil.SEPARATOR_CSV, TextUtil.SEPARATOR_CSV_INTL).
+                        Replace(CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator, CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
                 }
 
-                AssertEx.AreEqual(expectedLine, actual[i++], $"Report difference at line {i}");
+                foreach (var line in expected)
+                {
+                    var expectedLine = line;
+                    if (CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator !=
+                        CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)
+                    {
+                        if (line.StartsWith("\""))
+                        {
+                            var parts = line.Split('"');
+                            expectedLine = parts[1] + Localize(parts[2]);
+                        }
+                        else
+                        {
+                            expectedLine = Localize(expectedLine);
+                        }
+                    }
+
+                    AssertEx.AreEqual(expectedLine, actual[i++], $"Report difference at line {i}");
+                }
             }
+
         }
 
     }
