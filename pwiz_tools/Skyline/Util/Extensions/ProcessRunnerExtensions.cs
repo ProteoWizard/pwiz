31badcb69fba9a73cbee93f8e70998c60e00de89
Nicholas Shulman <nicksh@uw.edu>
2022-12-01 14:27:29 -0800

    ProcessRunnerExtensions

--- pwiz_tools/Skyline/Util/Extensions/ProcessRunnerExtensions.cs
+++ pwiz_tools/Skyline/Util/Extensions/ProcessRunnerExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using System.Diagnostics;
 using System.IO;
 using System.Threading;
-using pwiz.Common.Progress;
 using pwiz.Common.SystemUtil;
 
 namespace pwiz.Skyline.Util.Extensions
@@ -19,16 +18,7 @@ namespace pwiz.Skyline.Util.Extensions
             TextWriter writer,
             ProcessPriorityClass priorityClass = ProcessPriorityClass.Normal)
         {
-            if (progressMonitor == null)
-            {
-                processRunner.Run(psi, stdin, SilentProgress.INSTANCE, priorityClass);
-            }
-            else
-            {
-                progressMonitor.CallWithProgress(CancellationToken.None, ref status, progress => processRunner.Run(psi, stdin, progress, writer, priorityClass));
-            }
+            progressMonitor.CallWithProgress(CancellationToken.None, ref status, progress => processRunner.Run(psi, stdin, progress, writer, priorityClass));
         }
-
-
     }
 }
