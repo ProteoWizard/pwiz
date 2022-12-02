9dfc6b2969fa7fdfe39297a1ca0432dc8bf28bfc
Nicholas Shulman <nicksh@uw.edu>
2022-12-01 18:13:45 -0800

    Remove unused import

--- pwiz_tools/Skyline/Test/FindNodeTest.cs
+++ pwiz_tools/Skyline/Test/FindNodeTest.cs
@@ -19,7 +19,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using pwiz.Common.Progress;
 using pwiz.Skyline.Controls.SeqNode;
