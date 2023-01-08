aa4648b6b51f86050fcc1a82c1b823e0a8e66dd9
Nicholas Shulman <nicksh@uw.edu>
2023-01-08 11:04:30 -0800

    Remove "FilterChromatogramGroupInfo"

--- pwiz_tools/Skyline/Model/Results/Spectra/SpectrumClassFilter.cs
+++ pwiz_tools/Skyline/Model/Results/Spectra/SpectrumClassFilter.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Xml;
 using System.Xml.Schema;
@@ -10,7 +9,6 @@ using pwiz.Common.DataBinding;
 using pwiz.Common.Spectra;
 using pwiz.Common.SystemUtil;
 using pwiz.Skyline.Model.Databinding;
-using pwiz.Skyline.Model.DocSettings;
 using pwiz.Skyline.Util;
 using pwiz.Skyline.Util.Extensions;
 
@@ -255,98 +253,6 @@ namespace pwiz.Skyline.Model.Results.Spectra
             }
 
             return string.Join(" AND ", clauses);
-        }
-
-        public ChromatogramGroupInfo FilterChromatogramGroupInfo(SrmSettings srmSettings, ChromatogramGroupInfo chromatogramGroupInfo)
-        {
-            var resultFileMetaData = srmSettings.MeasuredResults?.GetResultFileMetaData(chromatogramGroupInfo.FilePath);
-            if (resultFileMetaData == null)
-            {
-                return chromatogramGroupInfo;
-            }
-
-            var header = chromatogramGroupInfo.Header;
-            header = new ChromGroupHeaderInfo(header.Precursor, 0, header.NumTransitions, 0, header.NumPeaks, 0,
-                0, header.MaxPeakIndex, header.NumPeaks, 0, 0, 0, header.Flags,
-                header.StartTime, header.EndTime, header.CollisionalCrossSection, header.IonMobilityUnits);
-            var chromTransitions = Enumerable.Range(0, header.NumTransitions)
-                .Select(chromatogramGroupInfo.GetChromTransitionLocal).ToList();
-            var timeIntensitiesGroup =
-                FilterTimeIntensitiesGroup(resultFileMetaData, chromTransitions, chromatogramGroupInfo.TimeIntensitiesGroup);
-            var peaks = Enumerable.Range(0, header.NumTransitions).SelectMany(chromatogramGroupInfo.GetPeaks).ToList();
-            return new ChromatogramGroupInfo(header, chromatogramGroupInfo.CachedFile, chromTransitions, peaks, timeIntensitiesGroup);
-        }
-
-        public TimeIntensitiesGroup FilterTimeIntensitiesGroup(ResultFileMetaData resultFileMetaData,
-            IList<ChromTransition> chromTransitions,
-            TimeIntensitiesGroup timeIntensitiesGroup)
-        {
-            var predicate = MakePredicate();
-            var timeIntensitiesList = new List<TimeIntensities>();
-            if (chromTransitions != null)
-            {
-                Assume.AreEqual(chromTransitions.Count, timeIntensitiesGroup.TransitionTimeIntensities.Count);
-            }
-            for (int iTransition = 0; iTransition < timeIntensitiesGroup.TransitionTimeIntensities.Count; iTransition++)
-            {
-                var timeIntensities = timeIntensitiesGroup.TransitionTimeIntensities[iTransition];
-                if (chromTransitions != null)
-                {
-                    var chromTransition = chromTransitions[iTransition];
-                    if (chromTransition.Source == ChromSource.ms1)
-                    {
-                        timeIntensitiesList.Add(timeIntensities);
-                        continue;
-                    }
-                }
-                timeIntensitiesList.Add(FilterTimeIntensities(resultFileMetaData, predicate, timeIntensities));
-            }
-            if (timeIntensitiesGroup is RawTimeIntensities rawTimeIntensities)
-            {
-                return new RawTimeIntensities(timeIntensitiesList, rawTimeIntensities.InterpolationParams)
-                    .ChangeTimeIntervals(rawTimeIntensities.TimeIntervals);
-            }
-
-            if (timeIntensitiesGroup is InterpolatedTimeIntensities interpolatedTimeIntensities)
-            {
-                return new InterpolatedTimeIntensities(timeIntensitiesList,
-                    interpolatedTimeIntensities.TransitionChromSources);
-            }
-
-            return new RawTimeIntensities(timeIntensitiesList, null);
-        }
-
-        public static TimeIntensities FilterTimeIntensities(ResultFileMetaData resultFileMetaData,
-            Predicate<SpectrumMetadata> predicate,
-            TimeIntensities timeIntensities)
-        {
-            if (timeIntensities.ScanIds == null)
-            {
-                return timeIntensities;
-            }
-
-            var times = new List<float>();
-            var intensities = new List<float>();
-            List<float> massErrors = null;
-            if (timeIntensities.MassErrors != null)
-            {
-                massErrors = new List<float>();
-            }
-            var scanIds = new List<int>();
-            for (int i = 0; i < timeIntensities.NumPoints; i++)
-            {
-                var spectrumMetadata = resultFileMetaData.SpectrumMetadatas[timeIntensities.ScanIds[i]];
-                if (spectrumMetadata == null || !predicate(spectrumMetadata))
-                {
-                    continue;
-                }
-                times.Add(timeIntensities.Times[i]);
-                intensities.Add(timeIntensities.Intensities[i]);
-                massErrors?.Add(timeIntensities.MassErrors[i]);
-                scanIds.Add(timeIntensities.ScanIds[i]);
-            }
-
-            return new TimeIntensities(times, intensities, massErrors, scanIds);
         }
     }
 }
