51d2ce1e639aee62a68e2e16c8cedb1500be0a3b
Brian Pratt <bspratt@proteinms.net>
2023-01-17 09:06:27 -0800

    restore an accidentally deleted [TestMethod] attribute

--- pwiz_tools/Skyline/TestFunctional/RunToRunRegressionTest.cs
+++ pwiz_tools/Skyline/TestFunctional/RunToRunRegressionTest.cs
@@ -33,6 +33,7 @@ namespace pwiz.SkylineTestFunctional
     {
         private const int REPLICATES = 5;
 
+        [TestMethod]
         public void TestRunToRunRegression()
         {
             TestFilesZip = @"TestFunctional\RunToRunRegressionTest.zip";
