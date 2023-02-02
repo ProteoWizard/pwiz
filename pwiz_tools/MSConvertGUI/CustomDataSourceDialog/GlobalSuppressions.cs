e7e276ad2066aad1d0f2d197972d0244c5af51e5
Matt Chambers <matt.chambers42@gmail.com>
2023-02-02 14:51:45 -0500

    * added missing suppression filse * switched MSConvertGUI and SeeMS back to include-style projects instead of the default include-all exclude-explicit

---
+++ pwiz_tools/MSConvertGUI/CustomDataSourceDialog/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Build", "CA1416:This call site is reachable on all platforms. 'RegistryKey.OpenSubKey(string)' is only supported on: 'windows'.", Justification = "<Pending>", Scope = "member", Target = "~M:CustomDataSourceDialog.OpenDataSourceDialog.OpenDataSourceDialogue_Load(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Build", "CA1416:This call site is reachable on all platforms. 'Graphics.DrawLine(Pen, int, int, int, int)' is only supported on: 'windows'.", Justification = "<Pending>", Scope = "member", Target = "~M:CustomDataSourceDialog.SplitButton.OnPaint(System.Windows.Forms.PaintEventArgs)")]
---
+++ pwiz_tools/MSConvertGUI/MSConvertGUI/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Build", "CA1416:This call site is reachable on all platforms. 'Bitmap' is only supported on: 'windows'.", Justification = "<Pending>", Scope = "member", Target = "~M:CustomProgressCell.DataGridViewProgressCell.Paint(System.Drawing.Graphics,System.Drawing.Rectangle,System.Drawing.Rectangle,System.Int32,System.Windows.Forms.DataGridViewElementStates,System.Object,System.Object,System.String,System.Windows.Forms.DataGridViewCellStyle,System.Windows.Forms.DataGridViewAdvancedBorderStyle,System.Windows.Forms.DataGridViewPaintParts)")]
+[assembly: SuppressMessage("Build", "CA1416:This call site is reachable on all platforms. 'Thread.SetApartmentState(ApartmentState)' is only supported on: 'windows'.", Justification = "<Pending>", Scope = "member", Target = "~M:MSConvertGUI.MainLogic.RunQueue")]
