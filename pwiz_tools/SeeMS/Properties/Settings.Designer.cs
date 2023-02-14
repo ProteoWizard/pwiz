d095640753985ffc7ea14bdd94c4bb16d4cce38d
nickshulman <nicksh@proteinms.net>
2023-02-14 09:06:07 -0800

    Revert "Update install requirements to Net 4.8 (#2490)" (#2497)
    This reverts commit fe7298f30c2ceb7ab41d259d8701a68ccc74b7eb.
    
    Revert "Change framework version to .Net 4.8 (#2467)"
    
    This reverts commit bb43af7581e1fd668c5921516e6284f066881c28.
    
    Co-authored-by: Nicholas Shulman <nicksh@uw.edu>

--- pwiz_tools/SeeMS/Properties/Settings.Designer.cs
+++ pwiz_tools/SeeMS/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace seems.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.4.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -346,15 +346,18 @@ namespace seems.Properties {
                 this["SrmAsSpectra"] = value;
             }
         }
-        
+
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("C:\\")]
-        public string LastBrowseToFileLocation {
-            get {
+        public string LastBrowseToFileLocation
+        {
+            get
+            {
                 return ((string)(this["LastBrowseToFileLocation"]));
             }
-            set {
+            set
+            {
                 this["LastBrowseToFileLocation"] = value;
             }
         }
