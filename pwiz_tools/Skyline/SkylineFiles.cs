0b5e404f33fc0d1252c3ba5aa9acb715d631f81f
Sophie Pallanck <pallans@wwu.edu>
2023-03-22 12:49:48 -0700

    Reverted changes to "Resources.resx", "Resources.designer.cs", and "PanoramaUtil.cs"

--- pwiz_tools/Skyline/SkylineFiles.cs
+++ pwiz_tools/Skyline/SkylineFiles.cs
@@ -3329,11 +3329,11 @@ namespace pwiz.Skyline
                     return false;
                 }
 
-                folderPathNoCtx = folderPath.Remove(0, @"/labkey".Length); 
+                folderPathNoCtx = folderPath.Remove(0, @"/labkey".Length);
                 try
                 {
                     folders =
-                        publishClient.GetInfoForFolders(server, folderPathNoCtx.TrimEnd('/').TrimStart('/')); 
+                        publishClient.GetInfoForFolders(server, folderPathNoCtx.TrimEnd('/').TrimStart('/'));
                 }
                 catch (Exception)
                 {
@@ -3342,6 +3342,11 @@ namespace pwiz.Skyline
             }
             catch (PanoramaServerException)
             {
+                return false;
+            }
+            catch (Exception e)
+            {
+                MessageDlg.ShowWithException(this, TextUtil.LineSeparate(Resources.RemoteSession_FetchContents_There_was_an_error_communicating_with_the_server__, e.Message), e);
                 return false;
             }
 
