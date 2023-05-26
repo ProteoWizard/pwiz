7e1704495c80bad2d8ba4494f984e7bb5369fb64
Nicholas Shulman <nicksh@uw.edu>
2023-05-25 18:39:32 -0700

    Add assert that verifies that version of msparser.dll is at least 2.8.1

--- pwiz_tools/Skyline/TestFunctional/NewMascotDatFileTest.cs
+++ pwiz_tools/Skyline/TestFunctional/NewMascotDatFileTest.cs
@@ -16,8 +16,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pwiz.Skyline;
 using pwiz.Skyline.Alerts;
 using pwiz.Skyline.SettingsUI;
 using pwiz.SkylineTestUtil;
@@ -36,6 +42,7 @@ namespace pwiz.SkylineTestFunctional
 
         protected override void DoTest()
         {
+            VerifyMsParserVersion();
             const string libraryName = "MascotLibrary";
             var peptideSettingsUi = ShowDialog<PeptideSettingsUI>(SkylineWindow.ShowPeptideSettingsUI);
             RunUI(() =>
@@ -61,6 +68,22 @@ namespace pwiz.SkylineTestFunctional
             Assert.AreEqual(1, SkylineWindow.Document.Settings.PeptideSettings.Libraries.Libraries.Count);
             var library = SkylineWindow.Document.Settings.PeptideSettings.Libraries.Libraries.First();
             Assert.AreEqual(1, library.Keys.Count());
+        }
+
+        private void VerifyMsParserVersion()
+        {
+            var folder = Path.GetDirectoryName(typeof(SkylineWindow).Assembly.Location);
+            Assert.IsNotNull(folder);
+            var msParserPath = new[] { "msparser.dll", "msparserD.dll" }.Select(name => Path.Combine(folder, name))
+                .FirstOrDefault(File.Exists);
+            Assert.IsNotNull(msParserPath, "Unable to find msparser.dll or msparserD.dll in folder: {0}", folder);
+            FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(msParserPath);
+            var requiredVersion = Tuple.Create(2, 8, 1);
+            var actualVersion = Tuple.Create(versionInfo.FileMajorPart, versionInfo.FileMinorPart, versionInfo.FileBuildPart);
+            if (Comparer.Default.Compare(actualVersion, requiredVersion) < 0)
+            {
+                Assert.Fail("MSParser version needs to be at least 2.8.1. Path: {0} Version: {1}\r\nSee developer setup instructions and download latest version of Mascot Parser.", msParserPath, versionInfo);
+            }
         }
     }
 }
