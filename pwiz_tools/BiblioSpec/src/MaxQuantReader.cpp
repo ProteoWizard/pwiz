61af3bf7d184a5f2f81037697ac650f8f92cf394
Matt Chambers <matt.chambers42@gmail.com>
2023-06-02 11:12:46 -0400

    * fixed bug modifying tokenizer's buffer after tokenizing

--- pwiz_tools/BiblioSpec/src/MaxQuantReader.cpp
+++ pwiz_tools/BiblioSpec/src/MaxQuantReader.cpp
@@ -529,12 +529,12 @@ bool MaxQuantReader::openFile()
  */
 void MaxQuantReader::parseHeader(string& line)
 {
+    // remove UTF-8 byte order mark
+    bal::replace_first(line, "\xEF\xBB\xBF", "");
+
     LineParser lineParser(line, separator_);
     int colNumber = 0;
     size_t numColumns = targetColumns_.size();
-
-    // remove UTF-8 byte order mark
-    bal::replace_first(line, "\xEF\xBB\xBF", "");
 
     // for each token in the line
     for (LineParser::iterator token = lineParser.begin();
