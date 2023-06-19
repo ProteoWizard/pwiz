e5a4ceb656e09c362fa1d7fe773237f6c7587370
Nicholas Shulman <nicksh@uw.edu>
2023-06-19 12:58:05 -0700

    Fix code inspection

--- pwiz_tools/Skyline/Controls/TreeViewMSStateRestorer.cs
+++ pwiz_tools/Skyline/Controls/TreeViewMSStateRestorer.cs
@@ -21,7 +21,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using pwiz.Common.Controls;
 using pwiz.Common.SystemUtil;
 
 namespace pwiz.Skyline.Controls
