039ecad688370f9b6bfadcac982c806562a7edef
Nicholas Shulman <nicksh@uw.edu>
2023-06-19 19:24:05 -0700

    Fix code inspection

--- pwiz_tools/Skyline/Model/PeptideDocNode.cs
+++ pwiz_tools/Skyline/Model/PeptideDocNode.cs
@@ -20,7 +20,6 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Windows.Forms;
 using pwiz.Common.Collections;
 using pwiz.Common.SystemUtil;
 using pwiz.Skyline.Controls.SeqNode;
