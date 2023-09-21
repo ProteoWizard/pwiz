c72729c29144b93d99fd2a59cc13ba4b92a14875
Andre Schoengraf <Andre.Schoengraf.ext@zontal.io>
2023-09-21 22:39:02 +0200

    Tab vs. space

--- pwiz_aux/msrc/utility/vendor_api/Waters/WatersRawFile.hpp
+++ pwiz_aux/msrc/utility/vendor_api/Waters/WatersRawFile.hpp
@@ -555,7 +555,7 @@ struct PWIZ_API_DECL RawData
 
     private:
 
-	void readAnalogChromatograms()
+    void readAnalogChromatograms()
     {
         const int channels = AnalogChromatogramReader.GetChannelCount();
 
