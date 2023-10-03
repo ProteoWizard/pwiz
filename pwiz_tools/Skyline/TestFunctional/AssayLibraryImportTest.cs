854e1f41ecaef45cce7e8df7d69d1ec26580bee3
Brian Pratt <bspratt@proteinms.net>
2023-10-03 14:33:56 -0700

    fix a code inspection failure - meant to use PauseForManualTutorialStep, not the forbidden PauseTest

--- pwiz_tools/Skyline/TestFunctional/AssayLibraryImportTest.cs
+++ pwiz_tools/Skyline/TestFunctional/AssayLibraryImportTest.cs
@@ -80,7 +80,7 @@ namespace pwiz.SkylineTestFunctional
                 {
                     RunUI(action);
                 }
-                PauseTest(message);
+                PauseForManualTutorialStep(message);
             }
         }
 
