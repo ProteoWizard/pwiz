539aff88557d387567a87f0b88d397080c43b990
knov <k.p.novoselov@gmail.com>
2023-11-06 19:00:11 -0500

    test github commit

--- mt_build.bat
+++ mt_build.bat
@@ -1,3 +1,4 @@
+rem run from VC command prompt
 quickbuild.bat address-model=64 --i-agree-to-the-vendor-licenses --without-bruker --without-sciex --without-shimadzu --without-waters  -j8 --abbreviate-paths pwiz msconvert
 
 
