b1e718836e37afdf78f0af44ed2f68d4ce621fee
nickshulman <nicksh@uw.edu>
2023-12-06 15:34:38 -0800

    Trying to track down why TeamCity does not notice that a StackOverflowException which causes TestRunner.exe to abruptly exit with exit code 2147943401 should be treated as a test failure.

---
+++ pwiz_tools/Skyline/TestFunctional/StackOverflowExceptionTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pwiz.SkylineTestUtil;
+
+namespace pwiz.SkylineTestFunctional
+{
+    /// <summary>
+    /// Test which causes a StackOverflowException to be thrown, causing TestRunner.exe to exit immediately.
+    ///
+    /// This test should not be merged into the master branch. The only purpose of this test is to make sure that TeamCity is properly able
+    /// to see that the tests have failed when this test is part of the suite.
+    /// </summary>
+    [TestClass]
+    public class StackOverflowExceptionTest : AbstractFunctionalTest
+    {
+        [TestMethod]
+        public void TestStackOverflowException()
+        {
+            RunFunctionalTest();
+        }
+
+        protected override void DoTest()
+        {
+            RunUI(()=>OverflowStack());
+        }
+
+        private void OverflowStack()
+        {
+            OverflowStack();
+        }
+    }
+}
