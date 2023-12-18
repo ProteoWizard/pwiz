1d5d104f198665a65ddfdec608f2f8a161b7b526
Henry Sanford <henrytsanford@gmail.com>
2023-12-18 10:21:19 -0800

    Move CheckRunCommandOutputContains to the class AbstractUnitTestEx

--- pwiz_tools/Skyline/TestData/ConsoleVerboseExceptionsTest.cs
+++ pwiz_tools/Skyline/TestData/ConsoleVerboseExceptionsTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using pwiz.SkylineTestUtil;
 
 namespace pwiz.SkylineTestData
@@ -9,18 +10,27 @@ namespace pwiz.SkylineTestData
         [TestMethod]
         public void TestConsoleVerboseExceptions()
         {
+            var newDocumentPath = TestContext.GetTestPath("out.sky");
+            var folder = Path.GetDirectoryName(newDocumentPath);
+            Assert.IsNotNull(folder);
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            string output;
+            using (File.OpenWrite(newDocumentPath))
+            {
+                // Throw an exception with verbose errors
+                output = RunCommand("--overwrite", "--new=" + newDocumentPath,
+                    "--verbose-errors");
+            }
             const string exceptionLocation =
                 @"at pwiz.Skyline.CommandLine.HandleExceptions[T](CommandArgs commandArgs, Func`1 func, Action`1 outputFunc)";
-            var newDocumentPath = TestContext.GetTestPath("out.sky");
-            // Throw an exception with verbose errors
-            var output = RunCommand("--new=" + newDocumentPath,
-                "--verbose-errors",
-                "--exception");
             // The stack trace should be reported
-            CommandLineTest.CheckRunCommandOutputContains(exceptionLocation, output);
+            CheckRunCommandOutputContains(exceptionLocation, output);
             // Throw an exception without verbose errors
-            output = RunCommand("--new=" + newDocumentPath,
-                "--exception");
+            output = RunCommand("overwrite", "--new=" + newDocumentPath);
             // The stack trace should not be reported
             Assert.IsFalse(output.Contains(exceptionLocation));
         }
