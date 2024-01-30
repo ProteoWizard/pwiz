e678ffd71e7e5773a853477abb2b131dfce80837
Nicholas Shulman <nicksh@uw.edu>
2024-01-30 07:28:21 -0800

    Remove unused imports

--- pwiz_tools/Skyline/Controls/Graphs/SummaryPeptideGraphPane.cs
+++ pwiz_tools/Skyline/Controls/Graphs/SummaryPeptideGraphPane.cs
@@ -24,7 +24,6 @@ using System.Linq;
 using pwiz.Common.SystemUtil;
 using pwiz.Skyline.Controls.SeqNode;
 using pwiz.Skyline.Model;
-using pwiz.Skyline.Model.DocSettings;
 using pwiz.Skyline.Model.Results;
 using pwiz.Skyline.Model.RetentionTimes;
 using pwiz.Skyline.Properties;
--- pwiz_tools/Skyline/Model/Irt/Regression.cs
+++ pwiz_tools/Skyline/Model/Irt/Regression.cs
@@ -21,7 +21,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using pwiz.Common.Collections;
 using pwiz.Common.SystemUtil;
 using pwiz.Skyline.Model.DocSettings;
 using pwiz.Skyline.Model.RetentionTimes;
