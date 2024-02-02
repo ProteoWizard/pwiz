5c28d3b3251f5baa2939bb116a3247f34f610348
Nicholas Shulman <nicksh@uw.edu>
2024-02-01 17:47:58 -0800

    Remove unused import

--- pwiz_tools/Skyline/ToolsUI/ToolStoreDlg.cs
+++ pwiz_tools/Skyline/ToolsUI/ToolStoreDlg.cs
@@ -24,7 +24,6 @@ using System.Linq;
 using System.Net;
 using System.Net.Http.Headers;
 using System.Reflection;
-using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using Ionic.Zip;
 using JetBrains.Annotations;
