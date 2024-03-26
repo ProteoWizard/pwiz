ProteoWizard/pwiz

commit a151f9fd3e7310a64a0b5c727741827d8b87801b

When doing "Equalize Medians" in the Peak Area Replicate graph, move the work of creating "NormalizationData" to a background thread (#2853)
Fixed Skyline unresponsive when doing "Equalize Medians" in huge documents (reported by Mike)

This change makes it so the AreaReplicateGraphPane asks for the "NormalizedValueCalculator" on a background thread and updates itself after that data is available.
This adds some classes in the "CommonUtil\SystemUtil\Caching" folder that take care of waiting for objects to be available.
Also changes the DetectionsPlotData and AreaCVGraphData to use this same caching mechanism.

pwiz_tools/Skyline/Controls/Graphs/DetectionsByReplicatePane.cs
@@ -33,9 +33,7 @@ namespace pwiz.Skyline.Controls.Graphs
 {
 
     public class DetectionsByReplicatePane : DetectionsPlotPane
-
     {
-
         public DetectionsByReplicatePane(GraphSummary graphSummary) : base(graphSummary)
         {
             XAxis.Type = AxisType.Text;
@@ -77,12 +75,17 @@ namespace pwiz.Skyline.Controls.Graphs
             GraphObjList.Clear();
             CurveList.Clear();
             Legend.IsVisible = false;
-            if (!DetectionPlotData.GetDataCache().TryGet(
-                GraphSummary.DocumentUIContainer.DocumentUI, Settings.QValueCutoff, this.DataCallback,
-                out _detectionData))
+
+            if (!Receiver.TryGetProduct(new DetectionPlotData.WorkOrderParam(GraphSummary.DocumentUIContainer.DocumentUI, Settings.QValueCutoff), out _detectionData))
+            {
                 return;
+            }
 
             AddLabels();
+            if (!_detectionData.IsValid)
+            {
+                return;
+            }
             BarSettings.Type = BarType.SortedOverlay;
             BarSettings.MinClusterGap = 0.3f;
             Legend.IsVisible = Settings.ShowLegend;

pwiz_tools/Skyline/Controls/Graphs/DetectionsHistogramPane.cs
@@ -49,11 +49,15 @@ namespace pwiz.Skyline.Controls.Graphs
             CurveList.Clear();
             Legend.IsVisible = false;
 
-            if (!DetectionPlotData.GetDataCache().TryGet(
-                GraphSummary.DocumentUIContainer.DocumentUI, Settings.QValueCutoff, this.DataCallback,
-                out _detectionData))
+            if (!Receiver.TryGetProduct(new DetectionPlotData.WorkOrderParam(GraphSummary.DocumentUIContainer.DocumentUI, Settings.QValueCutoff), out _detectionData))
+            {
                 return;
+            }
             AddLabels();
+            if (!_detectionData.IsValid)
+            {
+                return;
+            }
 
             BarSettings.Type = BarType.SortedOverlay;
             BarSettings.MinClusterGap = 0.3f;

pwiz_tools/Shared/CommonUtil/CommonUtil.csproj
@@ -162,6 +162,10 @@
       <DesignTime>True</DesignTime>
       <DependentUpon>Images.resx</DependentUpon>
     </Compile>
+    <Compile Include="SystemUtil\Caching\ProductionFacility.cs" />
+    <Compile Include="SystemUtil\Caching\Production.cs" />
+    <Compile Include="SystemUtil\Caching\Receiver.cs" />
+    <Compile Include="SystemUtil\Caching\Producer.cs" />
     <Compile Include="SystemUtil\CommonFormEx.cs">
       <SubType>Form</SubType>
     </Compile>
@@ -272,9 +276,7 @@
   <ItemGroup>
     <Content Include="CommonResources\Copy.bmp" />
   </ItemGroup>
-  <ItemGroup>
-    <Folder Include="GraphicalUserInterface\" />
-  </ItemGroup>
+  <ItemGroup />
   <Import Project="$(MSBuildToolsPath)\Microsoft.CSharp.targets" />
   <!-- To modify your build process, add your task inside one of the targets below and uncomment it. 
        Other similar extension points exist, see Microsoft.Common.targets.

pwiz_tools/Shared/CommonUtil/SystemUtil/Caching/Producer.cs
@@ -1,0 +1,132 @@
+﻿/*
+ * Original author: Nicholas Shulman <nicksh .at. u.washington.edu>,
+ *                  MacCoss Lab, Department of Genome Sciences, UW
+ *
+ * Copyright 2024 University of Washington - Seattle, WA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace pwiz.Common.SystemUtil.Caching
+{
+    public interface IProducer
+    {
+        object ProduceResult(ProductionMonitor productionMonitor, object workParameter, IDictionary<WorkOrder, object> inputs);
+        /// <summary>
+        /// Returns the list of other things that are needed to produce the products.
+        /// These are intermediate results which might also be needed by other producers.
+        /// </summary>
+        IEnumerable<WorkOrder> GetInputs(object workParameter);
+    }
+
+    public interface IProducer<in TParameter, out TResult> : IProducer
+    {
+        public TResult ProduceResult(ProductionMonitor productionMonitor, TParameter workParameter,
+            IDictionary<WorkOrder, object> dependencies);
+    }
+
+    public abstract class Producer : IProducer
+    {
+        public static Producer<TParameter, TResult> FromFunction<TParameter, TResult>(Func<ProductionMonitor, TParameter, TResult> func)
+        {
+            return Producer<TParameter, TResult>.FromFunction(func);
+        }
+        
+        protected Producer(Type parameterType, Type valueType)
+        {
+            ParameterType = parameterType;
+            ValueType = valueType;
+        }
+        public abstract object ProduceResult(ProductionMonitor productionMonitor, object workParameter, IDictionary<WorkOrder, object> dependencies);
+        public Type ParameterType { get; }
+        public Type ValueType { get; }
+        public virtual IEnumerable<WorkOrder> GetInputs(object workParameter)
+        {
+            return Array.Empty<WorkOrder>();
+        }
+
+    }
+
+    public abstract class Producer<TParameter, TResult> : Producer, IProducer<TParameter, TResult>
+    {
+        public static Producer<TParameter, TResult> FromFunction(Func<ProductionMonitor, TParameter, TResult> func)
+        {
+            return new Impl(func);
+        }
+
+        protected Producer() :base(typeof(TParameter), typeof(TResult))
+        {
+        }
+
+        public sealed override object ProduceResult(ProductionMonitor productionMonitor, object workParameter, IDictionary<WorkOrder, object> inputs)
+        {
+            return ProduceResult(productionMonitor, (TParameter)workParameter, inputs);
+        }
+
+        public abstract TResult ProduceResult(ProductionMonitor productionMonitor, TParameter parameter,
+            IDictionary<WorkOrder, object> inputs);
+
+        public sealed override IEnumerable<WorkOrder> GetInputs(object workParameter)
+        {
+            return GetInputs((TParameter)workParameter);
+        }
+
+        public virtual IEnumerable<WorkOrder> GetInputs(TParameter parameter)
+        {
+            return Array.Empty<WorkOrder>();
+        }
+
+        private class Impl : Producer<TParameter, TResult>
+        {
+            private Func<ProductionMonitor, TParameter, TResult> _impl;
+            public Impl(Func<ProductionMonitor, TParameter, TResult> impl)
+            {
+                _impl = impl;
+            }
+
+            public override TResult ProduceResult(ProductionMonitor productionMonitor, TParameter parameter, IDictionary<WorkOrder, object> dependencies)
+            {
+                return _impl(productionMonitor, parameter);
+            }
+        }
+
+        public TResult GetResult(IDictionary<WorkOrder, object> results, TParameter parameter)
+        {
+            if (results.TryGetValue(MakeWorkOrder(parameter), out var result))
+            {
+                return (TResult)result;
+            }
+
+            return default;
+        }
+
+        public WorkOrder MakeWorkOrder(TParameter workParameter)
+        {
+            return new WorkOrder(this, workParameter);
+        }
+
+        public Receiver<TParameter, TResult> RegisterCustomer(Control ownerControl, Action productAvailableAction)
+        {
+            var customer = new Receiver<TParameter, TResult>(ProductionFacility.DEFAULT, ownerControl, this);
+            if (productAvailableAction != null)
+            {
+                customer.ProductAvailable += productAvailableAction;
+            }
+
+            return customer;
+        }
+    }
+}

pwiz_tools/Shared/CommonUtil/SystemUtil/Caching/Production.cs
@@ -1,0 +1,116 @@
+﻿/*
+ * Original author: Nicholas Shulman <nicksh .at. u.washington.edu>,
+ *                  MacCoss Lab, Department of Genome Sciences, UW
+ *
+ * Copyright 2024 University of Washington - Seattle, WA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace pwiz.Common.SystemUtil.Caching
+{
+    public class WorkOrder
+    {
+        private int _hashCode;
+        public WorkOrder(Producer producer, object parameter)
+        {
+            Producer = producer;
+            WorkParameter = parameter;
+            unchecked
+            {
+                _hashCode = Producer.GetHashCode() * 397 ^ EqualityComparer<object>.Default.GetHashCode(WorkParameter);
+            }
+        }
+
+        public Producer Producer { get; }
+        public object WorkParameter { get; private set; }
+        public Type ValueType
+        {
+            get { return Producer.ValueType; }
+        }
+
+        protected bool Equals(WorkOrder other)
+        {
+            return Equals(Producer, other.Producer) && Equals(WorkParameter, other.WorkParameter);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((WorkOrder)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return _hashCode;
+        }
+
+        public IEnumerable<WorkOrder> GetInputs()
+        {
+            return Producer.GetInputs(WorkParameter);
+        }
+    }
+    public interface IProductionListener
+    {
+        public void OnProductAvailable(WorkOrder key, ProductionResult result);
+        public void OnProductStatusChanged(WorkOrder key, int progress);
+        public bool HasPendingNotifications { get; }
+    }
+    public class ProductionResult
+    {
+        private ProductionResult()
+        {
+        }
+
+        public static ProductionResult Success(object value)
+        {
+            return new ProductionResult
+            {
+                Value = value
+            };
+        }
+
+        public static ProductionResult Error(Exception exception)
+        {
+            if (exception == null)
+            {
+                throw new ArgumentNullException(nameof(exception));
+            }
+            return new ProductionResult { Exception = exception };
+        }
+
+        public object Value { get; private set; }
+        public Exception Exception { get; private set; }
+    }
+
+    public class ProductionMonitor
+    {
+        private Action<int> _progressChange;
+        public ProductionMonitor(CancellationToken cancellationToken, Action<int> progressChange)
+        {
+            CancellationToken = cancellationToken;
+            _progressChange = progressChange;
+        }
+        public CancellationToken CancellationToken { get; }
+
+        public void SetProgress(int progress)
+        {
+            _progressChange?.Invoke(progress);
+        }
+    }
+}

pwiz_tools/Shared/CommonUtil/SystemUtil/Caching/ProductionFacility.cs
@@ -1,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace pwiz.Common.SystemUtil.Caching
+{
+    public class ProductionFacility
+    {
+        public static readonly ProductionFacility DEFAULT = new ProductionFacility();
+        private Dictionary<WorkOrder, Entry> _entries = new Dictionary<WorkOrder, Entry>();
+
+        public void Listen(WorkOrder key, IProductionListener listener)
+        {
+            if (key == null)
+            {
+                return;
+            }
+            lock (this)
+            {
+                GetOrCreateEntry(key).AddListener(listener);
+            }
+        }
+
+        public ProductionResult GetResult(WorkOrder key)
+        {
+            lock (this)
+            {
+                _entries.TryGetValue(key, out var entry);
+                return entry?.Result;
+            }
+        }
+
+        public void Unlisten(WorkOrder key, IProductionListener listener)
+        {
+            if (key == null)
+            {
+                return;
+            }
+            lock (this)
+            {
+                GetEntry(key).RemoveListener(listener);
+            }
+        }
+
+        public int GetProgressValue(WorkOrder key)
+        {
+            lock (this)
+            {
+                _entries.TryGetValue(key, out var entry);
+                return entry?.ProgressValue??0;
+            }
+        }
+
+        private Entry GetEntry(WorkOrder key)
+        {
+            lock (this)
+            {
+                if (!_entries.TryGetValue(key, out var entry))
+                {
+                    throw new InvalidOperationException(string.Format("Entry {0} not found", key));
+                }
+
+                return entry;
+            }
+        }
+
+        private Entry GetOrCreateEntry(WorkOrder key)
+        {
+            lock (this)
+            {
+                if (_entries.TryGetValue(key, out var entry))
+                {
+                    return entry;
+                }
+
+                entry = new Entry(this, key);
+                _entries.Add(key, entry);
+                return entry;
+            }
+        }
+
+        private void RemoveEntry(Entry entry)
+        {
+            lock (this)
+            {
+                if (!_entries.Remove(entry.Key))
+                {
+                    throw new ArgumentException("Entry not found");
+                }
+            }
+        }
+        
+        private class Entry : IProductionListener
+        {
+            private HashSet<WorkOrder> _dependencies;
+            private Dictionary<WorkOrder, object> _dependencyResultValues = new Dictionary<WorkOrder, object>();
+            private List<IProductionListener> _listeners = new List<IProductionListener>();
+
+            private CancellationTokenSource _cancellationTokenSource;
+            public Entry(ProductionFacility cache, WorkOrder key)
+            {
+                Cache = cache;
+                Key = key;
+                _dependencies = key.GetInputs().ToHashSet();
+            }
+
+            public ProductionFacility Cache { get; }
+            public WorkOrder Key { get; }
+            public void OnProductAvailable(WorkOrder key, ProductionResult result)
+            {
+                lock (this)
+                {
+                    if (Result != null)
+                    {
+                        return;
+                    }
+
+                    if (result.Exception != null || Equals(key, Key))
+                    {
+                        NotifyResultAvailable(result);
+                        return;
+                    }
+                    _dependencyResultValues[key] = result.Value;
+                    TryStartCalculation();
+                }
+            }
+
+            public void AddListener(IProductionListener listener)
+            {
+                lock (Cache)
+                {
+                    if (_listeners.Contains(listener))
+                    {
+                        throw new ArgumentException(@"Listener already added");
+                    }
+
+                    if (_listeners.Count == 0)
+                    {
+                        BeforeFirstListenerAdded();
+                    }
+                    _listeners.Add(listener);
+                }
+            }
+
+            public void RemoveListener(IProductionListener listener)
+            {
+                lock (Cache)
+                {
+                    if (!_listeners.Remove(listener))
+                    {
+                        throw new ArgumentException(@"Listener not added");
+                    }
+
+                    if (_listeners.Count == 0)
+                    {
+                        AfterLastListenerRemoved();
+                    }
+                }
+            }
+
+            private void BeforeFirstListenerAdded()
+            {
+                lock (Cache)
+                {
+                    foreach (var input in _dependencies)
+                    {
+                        Cache.GetOrCreateEntry(input).AddListener(this);
+                    }
+                    TryStartCalculation();
+                }
+            }
+
+            private void AfterLastListenerRemoved()
+            {
+                lock (Cache)
+                {
+                    _cancellationTokenSource?.Cancel();
+                    foreach (var input in _dependencies)
+                    {
+                        Cache.GetEntry(input).RemoveListener(this);
+                    }
+                    Cache.RemoveEntry(this);
+                }
+            }
+
+            private void NotifyResultAvailable(ProductionResult result)
+            {
+                IProductionListener[] listeners;
+                lock (Cache)
+                {
+                    Result = result;
+                    _cancellationTokenSource?.Cancel();
+                    listeners = _listeners.ToArray();
+                }
+
+                foreach (var listener in listeners)
+                {
+                    listener.OnProductAvailable(Key, result);
+                }
+            }
+
+            private void TryStartCalculation()
+            {
+                lock (Cache)
+                {
+                    if (_cancellationTokenSource != null)
+                    {
+                        return;
+                    }
+                    
+                    foreach (var dependency in _dependencies.Except(_dependencyResultValues.Keys))
+                    {
+                        var result = Cache.GetResult(dependency);
+                        if (result != null)
+                        {
+                            if (result.Exception != null)
+                            {
+                                NotifyResultAvailable(result);
+                                return;
+                            }
+                            _dependencyResultValues.Add(dependency, result.Value);
+                        }
+                    }
+                    if (_dependencies.Count > _dependencyResultValues.Count)
+                    {
+                        return;
+                    }
+
+                    _cancellationTokenSource = new CancellationTokenSource();
+                    var progressCallback = new ProductionMonitor(_cancellationTokenSource.Token, OnMyProgressChanged);
+                    CommonActionUtil.RunAsync(() =>
+                    {
+                        try
+                        {
+                            Cache.IncrementWaitingCount();
+                            object value = Key.Producer.ProduceResult(progressCallback, Key.WorkParameter,
+                                _dependencyResultValues);
+                            NotifyResultAvailable(ProductionResult.Success(value));
+                        }
+                        catch (Exception ex)
+                        {
+                            NotifyResultAvailable(ProductionResult.Error(ex));
+                        }
+                        finally
+                        {
+                            Cache.DecrementWaitingCount();
+                        }
+                    });
+                }
+            }
+
+            public ProductionResult Result { get; private set; }
+
+            public bool HasPendingNotifications
+            {
+                get
+                {
+                    lock (Cache)
+                    {
+                        return _listeners.Any(listener => listener.HasPendingNotifications);
+                    }
+
+                }
+            }
+
+            public void OnProductStatusChanged(WorkOrder key, int progress)
+            {
+                IProductionListener[] listeners;
+                lock (Cache)
+                {
+                    ProgressValue = progress;
+                    listeners = _listeners.ToArray();
+                }
+
+                foreach (var listener in listeners)
+                {
+                    listener.OnProductStatusChanged(key, progress);
+                }
+            }
+
+            private void OnMyProgressChanged(int progress)
+            {
+                OnProductStatusChanged(Key, progress);
+            }
+            
+            public int ProgressValue { get; private set; }
+        }
+
+        public bool IsWaiting()
+        {
+            lock (this)
+            {
+                return _waitingCount != 0 || _entries.Values.Any(entry=>entry.HasPendingNotifications);
+            }
+        }
+
+        private int _waitingCount;
+        public int GetWaitingCount()
+        {
+            return _waitingCount;
+        }
+        public void IncrementWaitingCount()
+        {
+            Interlocked.Increment(ref _waitingCount);
+        }
+
+        public void DecrementWaitingCount()
+        {
+            Interlocked.Decrement(ref _waitingCount);
+        }
+    }
+}

commit c8f1460e992908734f7a7493ef60b4454794b6ed

Fix Detections Plots Fixed unhandled error displaying Detections Histogram and Detections Replicate Comparison This bug was introduced by PR #2853 and does not yet appear in an official Skyline-daily

pwiz_tools/Skyline/Controls/Graphs/DetectionsByReplicatePane.cs
@@ -78,9 +78,8 @@ namespace pwiz.Skyline.Controls.Graphs
 
             if (!Receiver.TryGetProduct(new DetectionPlotData.WorkOrderParam(GraphSummary.DocumentUIContainer.DocumentUI, Settings.QValueCutoff), out _detectionData))
             {
-                return;
+                _detectionData = DetectionPlotData.INVALID;
             }
-
             AddLabels();
             if (!_detectionData.IsValid)
             {

pwiz_tools/Skyline/Controls/Graphs/DetectionsHistogramPane.cs
@@ -51,7 +51,7 @@ namespace pwiz.Skyline.Controls.Graphs
 
             if (!Receiver.TryGetProduct(new DetectionPlotData.WorkOrderParam(GraphSummary.DocumentUIContainer.DocumentUI, Settings.QValueCutoff), out _detectionData))
             {
-                return;
+                _detectionData = DetectionPlotData.INVALID;
             }
             AddLabels();
             if (!_detectionData.IsValid)

pwiz_tools/Skyline/TestFunctional/TestFunctional.csproj
@@ -181,6 +181,7 @@
     <Compile Include="CrosslinkImsTest.cs" />
     <Compile Include="DdaPeakIntegrationTest.cs" />
     <Compile Include="DdaScoringTest.cs" />
+    <Compile Include="DetectionsPlotErrorTest.cs" />
     <Compile Include="DocumentGridExportTest.cs" />
     <Compile Include="DocumentGridUndoTest.cs" />
     <Compile Include="EditNeutralLossesTest.cs" />

pwiz_tools/Skyline/TestFunctional/DetectionsPlotErrorTest.cs
@@ -1,0 +1,81 @@
+﻿/*
+ * Original author: Nicholas Shulman <nicksh .at. u.washington.edu>,
+ *                  MacCoss Lab, Department of Genome Sciences, UW
+ *
+ * Copyright 2024 University of Washington - Seattle, WA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pwiz.Skyline.Controls.Graphs;
+using pwiz.SkylineTestUtil;
+using ZedGraph;
+
+namespace pwiz.SkylineTestFunctional
+{
+    /// <summary>
+    /// Tests that the "Detection - Histogram" and "Detection - Replicate Comparison" windows
+    /// display errors correctly.
+    /// </summary>
+    [TestClass]
+    public class DetectionsPlotErrorTest : AbstractFunctionalTestEx
+    {
+        [TestMethod]
+        public void TestDetectionsPlotError()
+        {
+            TestFilesZip = @"TestFunctional/DetectionsPlotTest.zip";
+            RunFunctionalTest();
+        }
+
+        protected override void DoTest()
+        {
+            OpenDocument(TestFilesDir.GetTestPath(@"DIA-TTOF-tutorial.sky"));
+            WaitForDocumentLoaded();
+            // Delete all the peptides from the document so that the detections plots will show errors
+            RunUI(()=>
+            {
+                SkylineWindow.SelectAll();
+                SkylineWindow.EditDelete();
+            });
+            // Verify that the Detections Histogram Graph displays 
+            RunUI(()=>
+            {
+                SkylineWindow.ShowDetectionsHistogramGraph();
+            });
+            WaitForGraphs();
+            var graphHistogram = SkylineWindow.DetectionsPlot;
+            Assert.IsTrue(graphHistogram.TryGetGraphPane(out DetectionsHistogramPane paneHistogram));
+            RunUI(() =>
+            {
+                Assert.AreEqual(GraphsResources.DetectionPlotPane_EmptyPlotError_Label, paneHistogram.Title.Text);
+                var subTitle = paneHistogram.GraphObjList.FirstOrDefault() as TextObj;
+                Assert.IsNotNull(subTitle, "Unable to find SubTitle text object");
+                Assert.AreEqual(GraphsResources.DetectionPlotData_NoResults_Label, subTitle.Text);
+            });
+            OkDialog(graphHistogram, graphHistogram.Close);
+
+            RunUI(()=>SkylineWindow.ShowDetectionsReplicateComparisonGraph());
+            WaitForGraphs();
+            GraphSummary graphReplicateComparison = SkylineWindow.DetectionsPlot;
+            Assert.IsTrue(graphReplicateComparison.TryGetGraphPane(out DetectionsByReplicatePane detectionsByReplicatePane));
+            RunUI(() =>
+            {
+                Assert.AreEqual(GraphsResources.DetectionPlotPane_EmptyPlotError_Label, detectionsByReplicatePane.Title.Text);
+                var subTitle = detectionsByReplicatePane.GraphObjList.FirstOrDefault() as TextObj;
+                Assert.IsNotNull(subTitle, "Unable to find SubTitle text object");
+                Assert.AreEqual(GraphsResources.DetectionPlotData_NoResults_Label, subTitle.Text);
+            });
+        }
+    }
+}