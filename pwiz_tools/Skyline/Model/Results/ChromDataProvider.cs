ProteoWizard/pwiz

commit 421138333f4c9d2dd507913ce4a6cc10b8494c91

Fix Agilent CE optimization scenario (#2931)
Fixed optimization step incorrect for some Agilent collision optimization data (reported by Marilyn)

"ChromatogramDataProvider.SetOptStepsForGroup" now checks to see whether the chromatograms have the same Q3 value and, if so, tries to use the CE value to figure out which chromatogram corresponds to which optimization step.

pwiz_tools/Skyline/Model/Results/ChromDataProvider.cs
@@ -25,6 +25,7 @@ using pwiz.Common.Collections;
 using pwiz.Common.SystemUtil;
 using pwiz.ProteowizardWrapper;
 using pwiz.Skyline.Model.DocSettings;
+using pwiz.Skyline.Model.Optimization;
 using pwiz.Skyline.Model.Results.Spectra;
 using pwiz.Skyline.Util;
 
@@ -334,6 +335,14 @@ namespace pwiz.Skyline.Model.Results
                     continue;
 
                 var chromKey = ChromKey.FromId(id, fixCEOptForShimadzu);
+                if (chromKey.CollisionEnergy == 0)
+                {
+                    var collisionEnergy = dataFile.GetChromatogramCollisionEnergy(i);
+                    if (collisionEnergy.HasValue)
+                    {
+                        chromKey = chromKey.ChangeCollisionEnergy((float)collisionEnergy);
+                    }
+                }
                 if (chromKey.Precursor != lastPrecursor)
                 {
                     lastPrecursor = chromKey.Precursor;
@@ -420,14 +429,13 @@ namespace pwiz.Skyline.Model.Results
                                 {
                                     optimizationSpacing = true;
                                 }
-                                // TODO(nicksh): Populate ChromKey.CollisionEnergy so that we can guarantee that these
-                                // ChromKeys are sorted correctly by collision energy
                             }
                         }
 
                         if (!optimizationSpacing)
                         {
-                            SetOptStepsForGroup(idToIndex, matchingGroup.Key?.NodeGroup, curGroup);
+                            SetOptStepsForGroup(doc, idToIndex, matchingGroup.Key, curGroup);
+                            curGroup.Clear();
                             firstChromKey = null;
                         }
                     }
@@ -439,7 +447,7 @@ namespace pwiz.Skyline.Model.Results
 
                 if (lastChromKey != null)
                 {
-                    SetOptStepsForGroup(idToIndex, matchingGroup.Key?.NodeGroup, curGroup);
+                    SetOptStepsForGroup(doc, idToIndex, matchingGroup.Key, curGroup);
                 }
             }
         }
@@ -477,32 +485,73 @@ namespace pwiz.Skyline.Model.Results
             }
         }
 
-        private void SetOptStepsForGroup(IReadOnlyDictionary<int, int> idToIndex, TransitionGroupDocNode transitionGroupDocNode, IList<ChromData> chromDatas)
+        /// <summary>
+        /// Sets the optimization steps for a group of chromatograms.
+        /// The chromatograms will all have the same Q1 value and the Q3 values will differ by no more than
+        /// <see cref="ChromatogramInfo.OPTIMIZE_SHIFT_SIZE"/>.
+        /// </summary>
+        private void SetOptStepsForGroup(SrmDocument document, IReadOnlyDictionary<int, int> idToIndex, ChromCacheBuilder.PeptidePrecursorMz peptidePrecursorMz, IList<ChromData> chromDatas)
         {
             if (chromDatas.Count <= 1)
             {
-                chromDatas.Clear();
                 return;
             }
 
+            var groupedByProductMz = chromDatas.GroupBy(data => data.Key.Product).ToList();
+            // Check to see whether there is a full set of optimization steps which all have identical Q3 values
+            if (groupedByProductMz.Count > 1 &&
+                chromDatas.Count > _optimizableRegression.StepCount * 2 + 1 &&
+                groupedByProductMz.Any(g => g.Count() > _optimizableRegression.StepCount))
+            {
+                // If the total number of chromatograms is more than one complete set of optimization steps (stepCount * 2 + 1)
+                // and there are more than half a complete set with identical Q3 values (stepCount)
+                // then assume that this is more than one group of chromatograms which did not use OPTIMIZE_SHIFT_SIZE
+                // and process each unique Q3 value separately
+                foreach (var group in groupedByProductMz)
+                {
+                    SetOptStepsForGroup(document, idToIndex, peptidePrecursorMz, group.ToList());
+                }
+                return;
+            }
+
+            int uniqueCeCount = 0;
+            if (_optimizableRegression.OptType == OptimizationType.collision_energy)
+            {
+                uniqueCeCount = chromDatas.Select(chromData => chromData.Key.CollisionEnergy).Distinct().Count();
+            }
+            int uniqueMzCount = chromDatas.Select(chromData => chromData.Key.Product).Distinct().Count();
+            // If some of the chromatograms have the same Q3 value, then the CE value should be used instead
+            // if the CE value uniquely identifies the chromatogram
+            bool useCeValues = uniqueCeCount == chromDatas.Count && uniqueCeCount > uniqueMzCount;
+            if (useCeValues)
+            {
+                chromDatas.Sort(Comparer<ChromData>.Create((a,b)=>a.Key.CollisionEnergy.CompareTo(b.Key.CollisionEnergy)));
+            }
             int centerIdx = (chromDatas.Count + 1) / 2 - 1;
-            if (transitionGroupDocNode != null)
+            if (peptidePrecursorMz?.NodeGroup != null)
             {
                 var centerMz = chromDatas[centerIdx].Key.Product;
-                var closestTransition = transitionGroupDocNode.Transitions.OrderBy(t => Math.Abs(t.Mz - centerMz))
+                var closestTransition = peptidePrecursorMz.NodeGroup.Transitions.OrderBy(t => Math.Abs(t.Mz - centerMz))
                     .FirstOrDefault();
-                if (closestTransition != null)
+                if (useCeValues)
                 {
-                    centerIdx = OptStepChromatograms.IndexOfCenter(closestTransition.Mz, chromDatas.Select(c => c.Key.Product),
+                    var centerCe = document.GetCollisionEnergy(peptidePrecursorMz.NodePeptide,
+                        peptidePrecursorMz.NodeGroup, closestTransition, 0);
+                    centerIdx = OptStepChromatograms.IndexOfCenterCe((float)centerCe,
+                        chromDatas.Select(chromData => chromData.Key.CollisionEnergy),
                         _optimizableRegression.StepCount);
+                }
+                else if (closestTransition != null)
+                {
+                    centerIdx = OptStepChromatograms.IndexOfCenterMz(closestTransition.Mz,
+                        chromDatas.Select(c => c.Key.Product), _optimizableRegression.StepCount);
                 }
             }
             for (var i = 0; i < chromDatas.Count; i++)
             {
-                SetOptimizationStep(idToIndex[chromDatas[i].ProviderId], i - centerIdx, chromDatas[centerIdx].Key.Product);
+                SetOptimizationStep(idToIndex[chromDatas[i].ProviderId], i - centerIdx,
+                    chromDatas[centerIdx].Key.Product);
             }
-
-            chromDatas.Clear();
         }
 
         private float GetCE(int i)

pwiz_tools/Shared/ProteowizardWrapper/MsDataFileImpl.cs
@@ -724,6 +724,12 @@ namespace pwiz.ProteowizardWrapper
 
         private static readonly string[] msLevelOrFunctionArrayNames = { "ms level", "function" };
 
+        public double? GetChromatogramCollisionEnergy(int chromIndex)
+        {
+            using var chrom = ChromatogramList.chromatogram(chromIndex, DetailLevel.FullMetadata);
+            return chrom.precursor?.activation?.cvParam(CVID.MS_collision_energy)?.value;
+        }
+
         public void GetChromatogram(int chromIndex, out string id,
             out float[] timeArray, out float[] intensityArray, bool onlyMs1OrFunction1 = false)
         {

pwiz_tools/Skyline/Model/Results/ChromData.cs
@@ -304,11 +304,11 @@ namespace pwiz.Skyline.Model.Results
         {
             get
             {
-                if (Key.OptimizationStep == 0)
+                if (Key.OptimizationStep == 0 && Key.CollisionEnergy == 0)
                 {
                     return Key;
                 }
-                return Key.ChangeOptimizationStep(0, null);
+                return Key.ChangeOptimizationStep(0, null).ChangeCollisionEnergy(0);
             }
         }
 

pwiz_tools/Skyline/Model/Results/ChromHeaderInfo.cs
@@ -1934,6 +1934,11 @@ namespace pwiz.Skyline.Model.Results
             });
         }
 
+        public ChromKey ChangeCollisionEnergy(float collisionEnergy)
+        {
+            return ChangeProp(ImClone(this), im => im.CollisionEnergy = collisionEnergy);
+        }
+
         /// <summary>
         /// For debugging only
         /// </summary>

pwiz_tools/Skyline/Model/Results/ChromatogramCache.cs
@@ -1466,7 +1466,7 @@ namespace pwiz.Skyline.Model.Results
 
             var productMzs = Enumerable.Range(startIdx, endIdx - startIdx).Select(i =>
                 new SignedMz(transitions[i].Product, transitionDocNode.Mz.IsNegative));
-            int centerIdx = startIdx + OptStepChromatograms.IndexOfCenter(
+            int centerIdx = startIdx + OptStepChromatograms.IndexOfCenterMz(
                 transitionDocNode.Mz, productMzs, optimizableRegression.StepCount);
             // Update optimization steps.
             for (var i = startIdx; i < endIdx; i++)

pwiz_tools/Skyline/Model/Results/OptStepChromatograms.cs
@@ -100,9 +100,18 @@ namespace pwiz.Skyline.Model.Results
         /// assume that the middle number is step 0. Otherwise, return the index of the productMz which is closest
         /// to the target m/z.
         /// </summary>
-        public static int IndexOfCenter(SignedMz targetMz, IEnumerable<SignedMz> productMzs, int stepCount)
+        public static int IndexOfCenterMz(SignedMz targetMz, IEnumerable<SignedMz> productMzs, int stepCount)
         {
-            var list = productMzs.ToList();
+            return IndexOfCenterValue(targetMz, productMzs.Select(v => (double) v).ToList(), stepCount);
+        }
+
+        public static int IndexOfCenterCe(float targetCe, IEnumerable<float> ceValues, int stepCount)
+        {
+            return IndexOfCenterValue(targetCe, ceValues.Select(v => (double)v).ToList(), stepCount);
+        }
+
+        private static int IndexOfCenterValue(double target, IList<double> list, int stepCount)
+        {
             if (list.Count == stepCount * 2 + 1)
             {
                 return stepCount;
@@ -112,11 +121,11 @@ namespace pwiz.Skyline.Model.Results
             {
                 return 0;
             }
-            double minDelta = Math.Abs(list[0] - targetMz);
+            double minDelta = Math.Abs(list[0] - target);
             int closestIndex = 0;
             for (int i = 1; i < list.Count; i++)
             {
-                double delta = Math.Abs(list[i] - targetMz);
+                double delta = Math.Abs(list[i] - target);
                 if (delta < minDelta)
                 {
                     closestIndex = i;

pwiz_tools/Skyline/TestFunctional/MissingOptStepsTest.cs
@@ -1,0 +1,91 @@
+﻿/*
+ * Original author: Nicholas Shulman <nicksh .at. u.washington.edu>,
+ *                  MacCoss Lab, Department of Genome Sciences, UW
+ *
+ * Copyright 2024 University of Washington - Seattle, WA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pwiz.Skyline.FileUI;
+using pwiz.Skyline.Model;
+using pwiz.Skyline.Model.Results;
+using pwiz.SkylineTestUtil;
+
+namespace pwiz.SkylineTestFunctional
+{
+    /// <summary>
+    /// Tests that when a transition is missing the full set of optimization step chromatograms,
+    /// the chromatograms get assigned to the correct step numbers by
+    /// <see cref="ChromatogramDataProvider.SetOptStepsForGroup"/>.
+    /// </summary>
+    [TestClass]
+    public class MissingOptStepsTest : AbstractFunctionalTest
+    {
+        [TestMethod]
+        public void TestMissingOptSteps()
+        {
+            TestFilesZip = @"TestFunctional\MissingOptStepsTest.zip";
+            RunFunctionalTest();
+        }
+
+        protected override void DoTest()
+        {
+            RunUI(()=>SkylineWindow.OpenFile(TestFilesDir.GetTestPath("MissingOptStepsTest.sky")));
+            RunDlg<ImportResultsDlg>(SkylineWindow.ImportResults, importResultsDlg=>
+            {
+                importResultsDlg.OptimizationName = ExportOptimize.CE;
+                importResultsDlg.RadioAddNewChecked = true;
+                importResultsDlg.NamedPathSets = new[]
+                {
+                    new KeyValuePair<string, MsDataFileUri[]>("Chromatograms", new[]
+                    {
+                        new MsDataFilePath(TestFilesDir.GetTestPath("Step-B_MethodB_0001.mzML")),
+                        new MsDataFilePath(TestFilesDir.GetTestPath("Step-B_MethodB_0002.mzML"))
+                    })
+                };
+                importResultsDlg.OkDialog();
+            });
+            WaitForDocumentLoaded();
+
+            // Verify that all the transitions have the expected number of optimization steps
+            foreach (var peptideDocNode in SkylineWindow.Document.Molecules)
+            {
+                // We expect all peptides except for one to have the full set of optimization steps
+                var expectedMinOptStep = -3;
+                if (peptideDocNode.Peptide.Sequence == "ATEHLSTLSEK")
+                {
+                    expectedMinOptStep = -2;
+                }
+                var expectedMaxOptStep = 3;
+                foreach (var precursor in peptideDocNode.TransitionGroups)
+                {
+                    foreach (var transition in precursor.Transitions)
+                    {
+                        var nonEmptyResults = transition.Results[0].Where(chromInfo => !chromInfo.IsEmpty).ToList();
+                        var minOptStep = nonEmptyResults
+                            .Min(transitionChromInfo => transitionChromInfo.OptimizationStep);
+                        var maxOptStep = nonEmptyResults
+                            .Max(transitionChromInfo => transitionChromInfo.OptimizationStep);
+                        string message = string.Format("Peptide: {0} Precursor: {1} Transition: {2}", 
+                            peptideDocNode, precursor, transition.Transition);
+                        Assert.AreEqual(expectedMinOptStep, minOptStep, message);
+                        Assert.AreEqual(expectedMaxOptStep, maxOptStep, message);
+                    }
+                }
+            }
+        }
+    }
+}

commit 3f56ca848923cd84df86850fa21b81f1c4c358b2

Speed up reading SRM chromatograms Reduce performance problems introduced by PR #2931. Only call "MsDataFileImpl.GetCollisionEnergy" if importing with collision energy optimization. This will speed up tests such as "TestLabelPeakPicking" which have SRM chromatograms but do not do CE optimization

pwiz_tools/Skyline/Model/Results/ChromDataProvider.cs
@@ -335,7 +335,8 @@ namespace pwiz.Skyline.Model.Results
                     continue;
 
                 var chromKey = ChromKey.FromId(id, fixCEOptForShimadzu);
-                if (chromKey.CollisionEnergy == 0)
+                if (_optimizableRegression?.OptType == OptimizationType.collision_energy
+                    && chromKey.CollisionEnergy == 0)
                 {
                     var collisionEnergy = dataFile.GetChromatogramCollisionEnergy(i);
                     if (collisionEnergy.HasValue)