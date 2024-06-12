266e8c76042822f1d23ff881395dac05deedbf4b
Nicholas Shulman <nicksh@uw.edu>
2024-06-11 17:27:08 -0700

    Fix "Settings.settings". "Settings.Designer.cs" had been manually edited and "Settings.settings" was not up to date with the Prosit -> Koina rename.

--- pwiz_tools/Skyline/Properties/Settings.Designer.cs
+++ pwiz_tools/Skyline/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace pwiz.Skyline.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.10.0.0")]
     public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
