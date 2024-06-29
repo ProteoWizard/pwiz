9c2af647da482508359d876cb37e7103a147ea5b
Nicholas Shulman <nicksh@uw.edu>
2024-06-29 12:23:06 -0700

    Add README.md

--- pwiz_tools/Skyline/Executables/DevTools/ResourcesOrganizer/scripts/generateLocalizationCsvFiles.bat
+++ pwiz_tools/Skyline/Executables/DevTools/ResourcesOrganizer/scripts/generateLocalizationCsvFiles.bat
@@ -1,3 +1,4 @@
+@echo off
 REM Syntax used in this batch file:
 REM "~dp0": directory containing the executing batch file
 
@@ -6,6 +7,5 @@ if not exist pwiz_tools (
 	exit /b 1
 )
 
-call %~dp0readResxFilesIntoDb.bat
-%~dp0exe\ResourcesOrganizer.exe exportLocalizationCsv --language ja JapaneseLocalization.csv
-%~dp0exe\ResourcesOrganizer.exe exportLocalizationCsv --language zh-CHS ChineseLocalization.csv
+call %~dp0readResxFiles.bat
+%~dp0exe\ResourcesOrganizer.exe exportLocalizationCsv --language ja zh-CHS
