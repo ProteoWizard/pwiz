122420591631dd2af9ddf18f88390a7467717503
Jason Wang <xgwang@uw.edu>
2024-07-11 12:37:57 -0700

    Add empty FastaExporter class to use for skyline document -> fasta file export later

---
+++ pwiz_tools/Skyline/FastaExporter.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * A class that exports a Skyline document to a .fasta file
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pwiz.Skyline
+{
+    internal class FastaExporter
+    {
+    }
+}
