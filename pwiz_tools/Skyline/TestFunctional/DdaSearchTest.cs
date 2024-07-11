12bb5f7480bc03fda3e3a2f72f668d5c6723e175
Matt Chambers <matt.chambers42@gmail.com>
2024-07-11 11:26:12 -0400

    * fixed code inspection

--- pwiz_tools/Skyline/TestFunctional/DdaSearchTest.cs
+++ pwiz_tools/Skyline/TestFunctional/DdaSearchTest.cs
@@ -21,8 +21,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using pwiz.Common.Chemistry;
 using pwiz.Skyline.Alerts;
