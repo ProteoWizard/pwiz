e2b999428e0b683147fe1803e0e6b535a253e9a7
Konstantin Novoselov <knov@apmaldi.com>
2024-08-04 17:52:47 -0400

    copy script has been added. j8 option has been removed

--- mt_build.bat
+++ mt_build.bat
@@ -1,4 +1,4 @@
 rem run from VC command prompt
-quickbuild.bat address-model=64 --i-agree-to-the-vendor-licenses --without-bruker --without-shimadzu --without-waters --without-mascot -j8 --abbreviate-paths pwiz msconvert
+quickbuild.bat address-model=64 --i-agree-to-the-vendor-licenses --without-bruker --without-shimadzu --without-waters --without-mascot --abbreviate-paths pwiz msconvert
 
 
--- mt_build_debug.bat
+++ mt_build_debug.bat
@@ -1,2 +1,2 @@
-quickbuild.bat address-model=64 variant=debug --i-agree-to-the-vendor-licenses --without-bruker --without-shimadzu --without-waters --without-mascot -j8 --abbreviate-paths pwiz msconvert 
+quickbuild.bat address-model=64 variant=debug --i-agree-to-the-vendor-licenses --without-bruker --without-shimadzu --without-waters --without-mascot --abbreviate-paths pwiz msconvert 
 
