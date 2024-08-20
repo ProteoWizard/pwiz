c685999789bf91ee6b01d4e43a822cedc30bc832
vagisha <vagisha@gmail.com>
2024-08-20 13:11:23 -0700

    Removed duplicated resource strings

--- pwiz_tools/Skyline/Executables/AutoQC/AutoQC/AnnotationsFileWatcher.cs
+++ pwiz_tools/Skyline/Executables/AutoQC/AutoQC/AnnotationsFileWatcher.cs
@@ -1,22 +1,4 @@
-﻿/*
- * Original author: Vagisha Sharma <vsharma .at. uw.edu>,
- *                  MacCoss Lab, Department of Genome Sciences, UW
- * Copyright 2015 University of Washington - Seattle, WA
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-using System.IO;
+﻿using System.IO;
 using SharedBatch;
 
 namespace AutoQC
@@ -60,7 +42,6 @@ namespace AutoQC
 
         public void StartWatching()
         {
-            // Begin watching.
             _fileWatcher.EnableRaisingEvents = true;
         }
 
