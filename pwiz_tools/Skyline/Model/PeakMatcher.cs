e334336b471ce8b32a51ee78ea22d3d46cd16515
Nicholas Shulman <nicksh@uw.edu>
2024-08-28 09:52:51 -0700

    Revert "Fix IndexOutOfRangeException in ApplyPeak (#3136)"
    This reverts commit 1749cf783a42af5e18e43fd93b41bcaff6a82da4.

--- pwiz_tools/Skyline/Model/PeakMatcher.cs
+++ pwiz_tools/Skyline/Model/PeakMatcher.cs
@@ -263,8 +263,7 @@ namespace pwiz.Skyline.Model
 
                     float scale = (chromGroupMaxTime - chromGroupMinTime)/(referenceTarget.MaxTime - referenceTarget.MinTime);
                     manualMatch = MakePeakMatchBetween(scale, referenceTarget, prev, next);
-                    if (chromGroupMinTime >= manualMatch.EndTime || manualMatch.StartTime >= chromGroupMaxTime ||
-                        manualMatch.EndTime <= manualMatch.StartTime)
+                    if (chromGroupMinTime >= manualMatch.EndTime || manualMatch.StartTime >= chromGroupMaxTime)
                         manualMatch = null;
 
                     float curMinTime = prev == null ? chromGroupMinTime : prev.AlignedPeak.RetentionTime;
