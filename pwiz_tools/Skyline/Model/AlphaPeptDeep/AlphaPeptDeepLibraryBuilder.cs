622c47aae4a99d229e3578d1f907271d02d382c5
Jason Wang <xgwang@uw.edu>
2024-09-04 15:47:13 -0700

    AlphapeptdeepLibraryBuilder - prepare settings.yaml file and execute alphapeptdeep cli to build library

--- pwiz_tools/Skyline/Model/AlphaPeptDeep/AlphaPeptDeepLibraryBuilder.cs
+++ pwiz_tools/Skyline/Model/AlphaPeptDeep/AlphaPeptDeepLibraryBuilder.cs
@@ -16,6 +16,7 @@ namespace pwiz.Skyline.Model.AlphaPeptDeep
     {
         private const string PEPTDEEP_EXECUTABLE = "peptdeep.exe";
         private const string SEMICOLON = TextUtil.SEMICOLON;
+        private const string SPACE = TextUtil.SPACE;
         private const string ALPHAPEPTDEEP = @"alphapeptdeep";
         private const string INPUT = @"input";
         private const string OUTPUT = @"output";
@@ -30,6 +31,10 @@ namespace pwiz.Skyline.Model.AlphaPeptDeep
         private const string OUTPUT_MODELS = @"output_models";
         private const string OUTPUT_SPECTRAL_LIBS = @"output_spectral_libs";
         private const string OUTPUT_SPECTRAL_LIB_FILE_NAME = @"predict.speclib.tsv";
+        private const string EXPORT_SETTINGS_COMMAND = @"export-settings";
+        private const string LIBRARY_COMMAND = @"library";
+        private const string CMD_FLOW_COMMAND = @"cmd-flow";
+        private const string BLIB_BUILD = "BlibBuild";
 
         /// <summary>
         /// key: unimod ID, value: modification name supported by Alphapeptdeep
@@ -40,7 +45,6 @@ namespace pwiz.Skyline.Model.AlphaPeptDeep
             {21, @"Phospho@S"},
             {35, @"Oxidation@M"},
         };
-
         private static readonly IEnumerable<string> PrecursorTableColumnNames = new[] { SEQUENCE, MODS, MOD_SITES, CHARGE };
 
         private string PythonVirtualEnvironmentScriptsDir { get; }
@@ -53,6 +57,24 @@ namespace pwiz.Skyline.Model.AlphaPeptDeep
         private string OutputSpectralLibsDir => Path.Combine(RootDir, OUTPUT_SPECTRAL_LIBS);
         private string OutputSpectraLibFilepath => Path.Combine(OutputSpectralLibsDir, OUTPUT_SPECTRAL_LIB_FILE_NAME);
         private SrmDocument Document { get; }
+        /// <summary>
+        /// The peptdeep cmd-flow command is how we can pass arguments that will override the settings.yaml file.
+        /// This is how the peptdeep CLI supports command line arguments.
+        /// </summary>
+        private Dictionary<string, string> CmdFlowCommandArguments =>
+            new Dictionary<string, string>()
+            {
+                {@"--task_workflow", @"library"},
+                {@"--settings_yaml", SettingsFilePath},
+                {@"--PEPTDEEP_HOME", RootDir},
+                {@"--transfer--model_output_folder", OutputModelsDir},
+                {@"--library--infile_type", @"precursor_table"},
+                {@"--library--infiles", InputFilePath},
+                {@"--library--output_folder", OutputSpectralLibsDir},
+                {@"--library--output_tsv--enabled", @"True"},
+                {@"--library--output_tsv--translate_mod_to_unimod_id", @"True"},
+                {@"--library--decoy", @"diann"}
+            };
 
         public AlphapeptdeepLibraryBuilder(string libName, string libOutPath, string pythonVirtualEnvironmentScriptsDir, SrmDocument document)
         { 
@@ -60,7 +82,6 @@ namespace pwiz.Skyline.Model.AlphaPeptDeep
             PythonVirtualEnvironmentScriptsDir = pythonVirtualEnvironmentScriptsDir;
             Document = document;
             CreateDirIfNotExist(RootDir);
-
         }
 
         public string AmbiguousMatchesMessage
@@ -94,9 +115,9 @@ namespace pwiz.Skyline.Model.AlphaPeptDeep
             IProgressStatus progressStatus = new ProgressStatus();
             try
             {
-                var result = RunAlphapeptdeep(progress, ref progressStatus);
+                RunAlphapeptdeep(progress, ref progressStatus);
                 progress.UpdateProgress(progressStatus = progressStatus.Complete());
-                return result;
+                return true;
             }
             catch (Exception exception)
             {
@@ -114,38 +135,23 @@ namespace pwiz.Skyline.Model.AlphaPeptDeep
             return dir;
         }
 
-        private bool RunAlphapeptdeep(IProgressMonitor progress, ref IProgressStatus progressStatus)
+        private void RunAlphapeptdeep(IProgressMonitor progress, ref IProgressStatus progressStatus)
         {
             progressStatus = progressStatus.ChangeSegments(0, 4);
 
-            if (!PrepareInputFile(progress, ref progressStatus))
-            {
-                return false;
-            }
+            PrepareInputFile(progress, ref progressStatus);
             progressStatus = progressStatus.NextSegment();
 
-            // WIP
-            if (!PrepareSettingsFile(progress, ref progressStatus))
-            {
-                return false;
-            }
+            PrepareSettingsFile(progress, ref progressStatus);
             progressStatus = progressStatus.NextSegment();
 
-            if (!ExecutePeptdeep(progress, ref progressStatus))
-            {
-                return false;
-            }
+            ExecutePeptdeep(progress, ref progressStatus);
             progressStatus = progressStatus.NextSegment();
 
-            if (!ImportSpectralLibrary(progress, ref progressStatus))
-            {
-                return false;
-            }
-
-            return true;
+            ImportSpectralLibrary(progress, ref progressStatus);
         }
 
-        private bool PrepareInputFile(IProgressMonitor progress, ref IProgressStatus progressStatus)
+        private void PrepareInputFile(IProgressMonitor progress, ref IProgressStatus progressStatus)
         {
             progress.UpdateProgress(progressStatus = progressStatus
                 .ChangeMessage(@"Preparing input file")
@@ -156,7 +162,6 @@ namespace pwiz.Skyline.Model.AlphaPeptDeep
 
             progress.UpdateProgress(progressStatus = progressStatus
                 .ChangePercentComplete(100));
-            return true;
         }
 
         private IEnumerable<string> GetPrecursorTable()
@@ -193,7 +198,7 @@ namespace pwiz.Skyline.Model.AlphaPeptDeep
 
                     }
                     modsBuilder.Append(modName);
-                    modSitesBuilder.Append(mod.IndexAA.ToString());
+                    modSitesBuilder.Append((mod.IndexAA + 1).ToString()); // + 1 because alphapeptdeep mod_site number starts from 1 as the first amino acid
                     if (i != modifiedSequence.ExplicitMods.Count - 1)
                     {
                         modsBuilder.Append(SEMICOLON);
@@ -215,29 +220,77 @@ namespace pwiz.Skyline.Model.AlphaPeptDeep
             return result;
         }
 
-        private bool PrepareSettingsFile(IProgressMonitor progress, ref IProgressStatus progressStatus)
+        private void PrepareSettingsFile(IProgressMonitor progress, ref IProgressStatus progressStatus)
         {
             progress.UpdateProgress(progressStatus = progressStatus
                 .ChangeMessage(@"Preparing settings file")
                 .ChangePercentComplete(0));
-            //TODO(xgwang): implement
+
+            // generate template settings.yaml file
+            var pr = new ProcessRunner();
+            var psi = new ProcessStartInfo(PeptdeepExecutablePath, $@"{EXPORT_SETTINGS_COMMAND} {SettingsFilePath}")
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = false
+            };
+            try
+            {
+                pr.Run(psi, string.Empty, progress, ref progressStatus, ProcessPriorityClass.BelowNormal, true);
+            }
+            catch (Exception ex)
+            {
+                // TODO(xgwang): update this exception to an Alphapeptdeep specific one
+                throw new Exception(@"Failed to generate settings.yaml file by executing the peptdeep export-settings command.", ex);
+            }
+
             progress.UpdateProgress(progressStatus = progressStatus
                 .ChangePercentComplete(100));
-            return true;
         }
 
-        private bool ExecutePeptdeep(IProgressMonitor progress, ref IProgressStatus progressStatus)
+        private void ExecutePeptdeep(IProgressMonitor progress, ref IProgressStatus progressStatus)
         {
             progress.UpdateProgress(progressStatus = progressStatus
                 .ChangeMessage(@"Executing peptdeep")
                 .ChangePercentComplete(0));
-            //TODO(xgwang): implement
+
+            // compose peptdeep cmd-flow command arguments to build library
+            var args = new StringBuilder();
+            foreach (var arg in CmdFlowCommandArguments)
+            {
+                args.Append(arg.Key);
+                args.Append(SPACE);
+                args.Append(arg.Value);
+                args.Append(SPACE);
+            }
+
+            // execute command
+            var pr = new ProcessRunner();
+            var psi = new ProcessStartInfo(PeptdeepExecutablePath, $@"{CMD_FLOW_COMMAND} {args}")
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = false
+            };
+            try
+            {
+                pr.Run(psi, string.Empty, progress, ref progressStatus, ProcessPriorityClass.BelowNormal, true);
+            }
+            catch (Exception ex)
+            {
+                // TODO(xgwang): update this exception to an Alphapeptdeep specific one
+                throw new Exception(@"Failed to build library by executing the peptdeep cmd-flow command.", ex);
+            }
+
             progress.UpdateProgress(progressStatus = progressStatus
                 .ChangePercentComplete(100));
-            return true;
         }
 
-        private bool ImportSpectralLibrary(IProgressMonitor progress, ref IProgressStatus progressStatus)
+        private void ImportSpectralLibrary(IProgressMonitor progress, ref IProgressStatus progressStatus)
         {
             progress.UpdateProgress(progressStatus = progressStatus
                 .ChangeMessage(@"Importing spectral library")
@@ -245,7 +298,6 @@ namespace pwiz.Skyline.Model.AlphaPeptDeep
             //TODO(xgwang): implement
             progress.UpdateProgress(progressStatus = progressStatus
                 .ChangePercentComplete(100));
-            return true;
         }
 
         //TODO(xgwang): implement
