1fcb84783f07d1c272b51b87f1b613a5f092c17e
Nicholas Shulman <nicksh@proteinms.net>
2024-09-19 15:10:50 -0700

    Remove one level of folder nesting for TutorialLocalization

--- pwiz_tools/Skyline/Executables/DevTools/TutorialLocalization/TutorialLocalization/TutorialLocalizer.cs
+++ pwiz_tools/Skyline/Executables/DevTools/TutorialLocalization/TutorialLocalization/TutorialLocalizer.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+ * Original author: Nicholas Shulman <nicksh .at. u.washington.edu>,
+ *                  MacCoss Lab, Department of Genome Sciences, UW
+ *
+ * Copyright 2024 University of Washington - Seattle, WA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -20,6 +38,8 @@ namespace TutorialLocalization
                 yield return "/html/body/h2";
                 yield return "/html/body/ul/li";
                 yield return "/html/body/ol/li";
+                yield return "/html/body/table/tr/td";
+                yield return "/html/body/div/table/tr/td";
             }
         }
 
@@ -175,7 +195,7 @@ namespace TutorialLocalization
             public string XPath { get; }
             public string EnglishText { get; }
 
-            protected bool Equals(StringKey other)
+            private bool Equals(StringKey other)
             {
                 return XPath == other.XPath && EnglishText == other.EnglishText;
             }
@@ -256,6 +276,5 @@ namespace TutorialLocalization
         {
             return node.InnerText.Any(ch => !char.IsWhiteSpace(ch));
         }
-
     }
 }
--- pwiz_tools/Skyline/Executables/DevTools/TutorialLocalization/TutorialLocalization/TutorialsLocalizer.cs
+++ pwiz_tools/Skyline/Executables/DevTools/TutorialLocalization/TutorialLocalization/TutorialsLocalizer.cs
@@ -1,6 +1,22 @@
-﻿using System;
+﻿/*
+ * Original author: Nicholas Shulman <nicksh .at. u.washington.edu>,
+ *                  MacCoss Lab, Department of Genome Sciences, UW
+ *
+ * Copyright 2024 University of Washington - Seattle, WA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Text;
@@ -23,6 +39,11 @@ namespace TutorialLocalization
 
         public void AddTutorialFolder(string folderPath, string relativePath)
         {
+            if (relativePath == "shared")
+            {
+                AddFilesRecursive(folderPath, relativePath);
+                return;
+            }
             var englishPath = Path.Combine(folderPath, "en");
             if (!Directory.Exists(englishPath))
             {
@@ -45,6 +66,15 @@ namespace TutorialLocalization
 
                 var tutorialLocalizer = new TutorialLocalizer(this, folderPath, relativePath, folderName);
                 tutorialLocalizer.Localize();
+            }
+        }
+
+        private void AddFilesRecursive(string folderPath, string relativePath)
+        {
+            AddFilesInFolder(folderPath, relativePath);
+            foreach (var subFolder in Directory.GetDirectories(folderPath))
+            {
+                AddFilesInFolder(subFolder, Path.Combine(relativePath, Path.GetDirectoryName(subFolder) ?? string.Empty));
             }
         }
 
