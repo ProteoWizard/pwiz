71c918b8d88b712ca2cdd01e299613ad2b5d8d9e
Nicholas Shulman <nicksh@proteinms.net>
2024-10-09 09:07:14 -0700

    Add "ProteinAssociationTest"

---
+++ pwiz_tools/Skyline/Test/ProteinAssociationTest.cs
@@ -0,0 +1,117 @@
+﻿/*
+ * Original author: Nicholas Shulman <nicksh .at. u.washington.edu>,
+ *                  MacCoss Lab, Department of Genome Sciences, UW
+ *
+ * Copyright 2024 University of Washington - Seattle, WA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pwiz.Skyline.Model;
+using pwiz.Skyline.Model.DocSettings;
+using pwiz.Skyline.Model.Proteome;
+using pwiz.Skyline.Properties;
+using pwiz.Skyline.Util;
+using pwiz.SkylineTestUtil;
+
+namespace pwiz.SkylineTest
+{
+    [TestClass]
+    public class ProteinAssociationTest : AbstractUnitTest
+    {
+        [TestMethod]
+        public void TestTrypticProteinAssociation()
+        {
+            TestFilesDir = new TestFilesDir(TestContext, @"Test\ProteinAssociationTest.data");
+            var peptides = new[]
+            {
+                "ADLINNLGTIAK", "ELISNASDALDKIR", "FAFQAEVNR", "IDIIPNPQER", "LIINSLYK",
+                "LISLTDENALSGNEELTVK", "NKEIFLR", "NLLHVTDTGVGMTR", "SGTSEFLNK", "TDDEVVQREEEAIQLDGLNASQIR",
+                "KYSQFINFPIYVWSSK"
+            };
+            var document = CreateDocumentWithPeptides(peptides);
+            string fastaFilePath = TestFilesDir.GetTestPath("TwoProteins.fasta");
+
+            // Associate proteins using Trypsin. The peptide "NKEIFLR" is only tryptic for the first protein
+            var trypsin = EnzymeList.GetDefault();
+            var trypsinAssociatedProteins = AssociateProteins(document, fastaFilePath, trypsin);
+            CollectionAssert.Contains(trypsinAssociatedProteins["Protein1"], "NKEIFLR");
+            CollectionAssert.DoesNotContain(trypsinAssociatedProteins["Protein2"], "NKEIFLR");
+            CollectionAssert.AreEquivalent(new[] { "ADLINNLGTIAK", "ELISNASDALDKIR", "IDIIPNPQER" },
+                trypsinAssociatedProteins["Protein2"]);
+
+            // Now associate proteins using Chymotrypsin. The peptide "NKEIFLR" is not chymotryptic for either protein
+            var chymotrypsin = new Enzyme("Chymotrypsin", "FWYL", "P");
+            var chymotrypsinAssociatedProteins = AssociateProteins(document, fastaFilePath, chymotrypsin);
+            CollectionAssert.Contains(trypsinAssociatedProteins["Protein1"], "NKEIFLR");
+            CollectionAssert.Contains(trypsinAssociatedProteins["Protein2"], "NKEIFLR");
+            CollectionAssert.AreEquivalent(new[] { "ADLINNLGTIAK", "ELISNASDALDKIR", "IDIIPNPQER", "NKEIFLR" },
+                chymotrypsinAssociatedProteins["Protein2"]);
+        }
+
+        private static Dictionary<string, List<string>> AssociateProteins(SrmDocument document, string fastaFilePath, Enzyme enzyme)
+        {
+            var lenientDigestSettings = new DigestSettings(DigestSettings.MAX_MISSED_CLEAVAGES, false);
+            var proteinAssociation = new ProteinAssociation(document, new LongWaitBrokerImpl());
+            proteinAssociation.UseFastaFile(fastaFilePath, proteinSequence => enzyme.Digest(proteinSequence, lenientDigestSettings), new LongWaitBrokerImpl());
+            return proteinAssociation.AssociatedProteins.ToDictionary(
+                kvp => kvp.Key.Sequence.Name, kvp => kvp.Value.Peptides.Select(p => p.Peptide.Sequence).ToList());
+        }
+
+        private static SrmDocument CreateDocumentWithPeptides(IEnumerable<string> peptides)
+        {
+            var settings = SrmSettingsList.GetDefault();
+            var peptideDocNodes = new List<PeptideDocNode>();
+            foreach (var peptideSequence in peptides)
+            {
+                var peptideDocNode =
+                    new PeptideDocNode(new Peptide(peptideSequence)).ChangeSettings(settings, SrmSettingsDiff.ALL);
+                peptideDocNodes.Add(peptideDocNode);
+            }
+
+            var peptideGroupDocNode = new PeptideGroupDocNode(new PeptideGroup(), Annotations.EMPTY, "Peptide List",
+                null, peptideDocNodes.ToArray());
+            return (SrmDocument)new SrmDocument(settings).ChangeChildren(new DocNode[] { peptideGroupDocNode });
+        }
+
+        private class LongWaitBrokerImpl : ILongWaitBroker
+        {
+            public bool IsCanceled
+            {
+                get { return false; }
+            }
+            public int ProgressValue { get; set; }
+            public string Message { get; set; }
+            public bool IsDocumentChanged(SrmDocument docOrig)
+            {
+                return false;
+            }
+
+            public DialogResult ShowDialog(Func<IWin32Window, DialogResult> show)
+            {
+                throw new InvalidOperationException();
+            }
+
+            public void SetProgressCheckCancel(int step, int totalSteps)
+            {
+            }
+
+            public CancellationToken CancellationToken => CancellationToken.None;
+        }
+}
+}
