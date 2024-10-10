3ab4cffc5110cd1087a6e77ba4b00eb63237bb6b
Andre Schoengraf <asedv@yahoo.de>
2024-10-11 01:16:32 +0200

    Bugfix

--- pwiz/data/vendor_readers/Bruker/SpectrumList_Bruker.cpp
+++ pwiz/data/vendor_readers/Bruker/SpectrumList_Bruker.cpp
@@ -495,17 +495,20 @@ PWIZ_API_DECL SpectrumPtr SpectrumList_Bruker::spectrum(size_t index, DetailLeve
         {
             auto mz = result->getMZArray()->data;
 
-            auto it = std::min_element(mz.begin(), mz.end());
-            std::size_t index1 = std::distance(mz.begin(), it);
+            if (mz.size() > 0)
+            {
+                auto it = std::min_element(mz.begin(), mz.end());
+                std::size_t index1 = std::distance(mz.begin(), it);
 
-            it = std::max_element(mz.begin(), mz.end());
-            std::size_t index2 = std::distance(mz.begin(), it);
+                it = std::max_element(mz.begin(), mz.end());
+                std::size_t index2 = std::distance(mz.begin(), it);
 
-            if(mz.size()>index1)
-                result->set(MS_lowest_observed_m_z, mz[index1], MS_m_z);
+                if (mz.size() > index1)
+                    result->set(MS_lowest_observed_m_z, mz[index1], MS_m_z);
 
-            if (mz.size() > index2)
-                result->set(MS_highest_observed_m_z, mz[index2], MS_m_z);
+                if (mz.size() > index2)
+                    result->set(MS_highest_observed_m_z, mz[index2], MS_m_z);
+            }
         }
     /*}
     catch (_com_error& e) // not caught by either std::exception or '...'
