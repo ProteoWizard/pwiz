8021dee2013ab053e84211a464c85364f5e65abe
Matt Chambers <matt.chambers42@gmail.com>
2024-10-21 16:25:08 -0400

    * added Orbitrap IQ-X and Exploris GC 240 instrument configurations (needed because CV update with new Thermo instruments would break tests that make sure instrument configurations are populated for all Thermo instruments) * fixed Reader_Mobilion getCvType()

--- pwiz/data/vendor_readers/Mobilion/Reader_Mobilion.hpp
+++ pwiz/data/vendor_readers/Mobilion/Reader_Mobilion.hpp
@@ -60,7 +60,7 @@ class PWIZ_API_DECL Reader_Mobilion : public Reader
     }
 
     virtual const char * getType() const {return "Mobilion MBI";}
-    virtual CVID getCvType() const {return MS_file_format;}
+    virtual CVID getCvType() const {return MS_Mobilion_MBI_format;}
     virtual std::vector<std::string> getFileExtensions() const {return {".mbi"};}
 };
 
