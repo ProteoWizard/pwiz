d382f2bd251a86f052239d2db60d9ed60f93a8c8
brendanx67 <brendanx@proteinms.net>
2024-11-12 14:17:36 -0800

    - remove missing using clause from TargetedMsmsTutorialTest

--- pwiz_tools/Skyline/TestTutorial/TargetedMSMSTutorialTest.cs
+++ pwiz_tools/Skyline/TestTutorial/TargetedMSMSTutorialTest.cs
@@ -21,7 +21,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.Globalization;
 using System.IO;
 using System.Linq;
