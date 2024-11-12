a48e8ad4d64ccb248a775271cadd00ef46bf8c1b
danjasuw <djaschob@uw.edu>
2024-11-12 12:57:37 -0800

    Skyline: Refactor OpenDataSourceDialog: Finish updates
    As part of previous commit and this commit:
    Rename OpenDataSourceDialog to BaseFileDialogNE
    Add new empty classes OpenFileDialogNE and OpenDataSourceDialog
    New inheritance path: BaseFileDialogNE > OpenFileDialogNE > OpenDataSourceDialog
    Move Open() method to class OpenFileDialogNE

---
+++ pwiz_tools/Skyline/FileUI/OpenDataSourceDialog.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Original author: Matt Chambers <matt.chambers .@. vanderbilt.edu>
+ *
+ * Copyright 2009 Vanderbilt University - Nashville, TN 37232
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System.Collections.Generic;
+using pwiz.Skyline.Model.Results;
+using pwiz.Skyline.Model.Results.RemoteApi;
+using pwiz.Skyline.Util;
+
+
+namespace pwiz.Skyline.FileUI
+{
+    public class OpenDataSourceDialog : OpenFileDialogNE
+    {
+        private static readonly string[] SOURCE_TYPES =
+        {
+            FileUIResources.OpenDataSourceDialog_OpenDataSourceDialog_Any_spectra_format,
+            DataSourceUtil.TYPE_WIFF,
+            DataSourceUtil.TYPE_WIFF2,
+            DataSourceUtil.TYPE_AGILENT,
+            DataSourceUtil.TYPE_BRUKER,
+            DataSourceUtil.TYPE_MBI,
+            DataSourceUtil.TYPE_SHIMADZU,
+            DataSourceUtil.TYPE_THERMO_RAW,
+            DataSourceUtil.TYPE_WATERS_RAW,
+            DataSourceUtil.TYPE_MZML,
+            DataSourceUtil.TYPE_MZXML,
+            DataSourceUtil.TYPE_MZ5,
+            DataSourceUtil.TYPE_UIMF
+        };
+
+        /// <summary>
+        /// File picker which is aware of mass spec "files" that are really directories
+        /// </summary>
+        /// <param name="remoteAccounts">For UNIFI</param>
+        /// <param name="specificDataSourceFilter">Optional list of specific files the user needs to located, ignoring the rest</param>
+        public OpenDataSourceDialog(IList<RemoteAccount> remoteAccounts, IList<string> specificDataSourceFilter = null)
+            : base(SOURCE_TYPES, remoteAccounts, specificDataSourceFilter )
+        {
+        }
+
+        public MsDataFileUri DataSource => FileName;
+        public MsDataFileUri[] DataSources => FileNames;
+    }
+}
