039fd70e6efa2c9378e985c17aa1e8861167ee17
David Shteynberg <dshteyn@proteinms.net>
2024-12-05 18:29:21 -0800

    Alphapeptdeep integration test to verify no duplicate accessions exist in the list of modifications

---
+++ pwiz_tools/Skyline/Test/AlphapeptdeepLibraryBuilderTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pwiz.Skyline.Model.AlphaPeptDeep;
+using pwiz.SkylineTestUtil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pwiz.SkylineTest
+{
+    [TestClass]
+    public class AlphapeptdeepLibraryBuilderTest : AbstractUnitTest
+    {
+        [TestMethod]
+        public void TestModificationInfo()
+        {
+            var groupedByAccession = AlphapeptdeepLibraryBuilder.AlphapeptdeepModificationName.GroupBy(item => item.Accession);
+            foreach (var group in groupedByAccession)
+            {
+                Assert.AreEqual(1, group.Count(), "Duplicate accession {0}", group.Key);
+            }
+        }
+    }
+}
