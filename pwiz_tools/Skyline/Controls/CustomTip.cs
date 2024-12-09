ae7a2acbce54b0732800421032c583ea7160fe13
Eddie O'Neil <ekoneil@gmail.com>
2024-12-09 14:20:06 -0800

    Remove unused methods annotated with DllImport from CustomTip => User32. No need to move unused stuff.

--- pwiz_tools/Skyline/Controls/CustomTip.cs
+++ pwiz_tools/Skyline/Controls/CustomTip.cs
@@ -1281,8 +1281,6 @@ namespace pwiz.Skyline.Controls
         [DllImport("User32.dll", CharSet=CharSet.Auto)]
         internal static extern bool ClientToScreen(IntPtr hWnd, ref POINT pt);
         [DllImport("User32.dll", CharSet=CharSet.Auto)]
-        internal static extern bool DrawFocusRect(IntPtr hWnd, ref RECT rect);
-        [DllImport("User32.dll", CharSet=CharSet.Auto)]
         internal static extern bool EndPaint(IntPtr hWnd, ref PAINTSTRUCT ps);
         [DllImport("User32.dll", CharSet=CharSet.Auto)]
         internal static extern IntPtr GetDC(IntPtr hWnd);
@@ -1293,29 +1291,7 @@ namespace pwiz.Skyline.Controls
         [DllImport("User32.dll", CharSet = CharSet.Auto)]
         internal static extern IntPtr GetFocus();
         [DllImport("User32.dll", CharSet=CharSet.Auto)]
-        internal static extern ushort GetKeyState(int virtKey);
-        [DllImport("User32.dll", CharSet=CharSet.Auto)]
-        internal static extern IntPtr GetParent(IntPtr hWnd);
-        [DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)]
-        public static extern bool GetClientRect(IntPtr hWnd, [In, Out] ref RECT rect);
-        [DllImport("User32.dll", CharSet=CharSet.Auto)]
-        internal static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-        [DllImport("User32.dll", CharSet=CharSet.Auto)]
-        internal static extern IntPtr GetWindow(IntPtr hWnd, int cmd);
-        [DllImport("User32.dll", CharSet=CharSet.Auto)]
         internal static extern bool GetWindowRect(IntPtr hWnd, ref RECT rect);
-        [DllImport("User32.dll", CharSet=CharSet.Auto)]
-        internal static extern bool HideCaret(IntPtr hWnd);
-        [DllImport("User32.dll", CharSet=CharSet.Auto)]
-        internal static extern bool InvalidateRect(IntPtr hWnd, ref RECT rect, bool erase);
-        [DllImport("User32.dll", CharSet=CharSet.Auto)]
-        internal static extern IntPtr LoadCursor(IntPtr hInstance, uint cursor);
-        [DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)]
-        public static extern int MapWindowPoints(IntPtr hWndFrom, IntPtr hWndTo, [In, Out] ref RECT rect, int cPoints);
-        [DllImport("User32.dll", CharSet=CharSet.Auto)]
-        internal static extern bool MoveWindow(IntPtr hWnd, int x, int y, int width, int height, bool repaint);
-        [DllImport("User32.dll", CharSet=CharSet.Auto)]
-        internal static extern bool PostMessage(IntPtr hWnd, int Msg, uint wParam, uint lParam);
         [DllImport("User32.dll", CharSet=CharSet.Auto)]
         internal static extern bool ReleaseCapture();
         [DllImport("User32.dll", CharSet=CharSet.Auto)]
@@ -1323,31 +1299,13 @@ namespace pwiz.Skyline.Controls
         [DllImport("User32.dll", CharSet=CharSet.Auto)]
         internal static extern bool ScreenToClient(IntPtr hWnd, ref POINT pt);
         [DllImport("User32.dll", CharSet=CharSet.Auto)]
-        internal static extern IntPtr SetCursor(IntPtr hCursor);
-        [DllImport("User32.dll", CharSet=CharSet.Auto)]
-        internal static extern IntPtr SetFocus(IntPtr hWnd);
-        [DllImport("User32.dll", CharSet=CharSet.Auto)]
-        internal static extern int SetWindowLong(IntPtr hWnd, int nIndex, int newLong);
-        [DllImport("User32.dll", CharSet=CharSet.Auto)]
         internal static extern int SetWindowPos(IntPtr hWnd, IntPtr hWndAfter, int X, int Y, int Width, int Height, uint flags);
-        [DllImport("User32.dll", CharSet=CharSet.Auto)]
-        internal static extern bool SetWindowRgn(IntPtr hWnd, IntPtr hRgn, bool redraw);
-        [DllImport("User32.dll", CharSet=CharSet.Auto)]
-        internal static extern bool ShowCaret(IntPtr hWnd);
         [DllImport("User32.dll", CharSet=CharSet.Auto)]
         internal static extern bool SetCapture(IntPtr hWnd);
         [DllImport("User32.dll", CharSet=CharSet.Auto)]
         internal static extern int ShowWindow(IntPtr hWnd, short cmdShow);
         [DllImport("User32.dll", CharSet=CharSet.Auto)]
-        internal static extern bool SystemParametersInfo(uint uiAction, uint uiParam, ref int bRetValue, uint fWinINI);
-        [DllImport("User32.dll", CharSet=CharSet.Auto)]
-        internal static extern bool TrackMouseEvent(ref TRACKMOUSEEVENTS tme);
-        [DllImport("User32.dll", CharSet=CharSet.Auto)]
         internal static extern bool UpdateLayeredWindow(IntPtr hwnd, IntPtr hdcDst, ref POINT pptDst, ref SIZE psize, IntPtr hdcSrc, ref POINT pprSrc, int crKey, ref BLENDFUNCTION pblend, int dwFlags);
-        [DllImport("User32.dll", CharSet=CharSet.Auto)]
-        internal static extern bool UpdateWindow(IntPtr hwnd);
-        [DllImport("User32.dll", CharSet=CharSet.Auto)]
-        internal static extern bool WaitMessage();
         [DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)]
         public static extern bool AdjustWindowRectEx(ref RECT lpRect, int dwStyle, bool bMenu, int dwExStyle);
         [DllImport("User32.dll", CharSet = CharSet.Auto)]
