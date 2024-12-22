7f6191b9d97f9b173f32c9ef5e4f313a28c10f67
Brendan MacLean <brendan.maclean@gmail.com>
2024-12-21 20:11:59 -0500

    - exclude Skyline/Executables/DevTools from code inspection for localization

--- pwiz_tools/Skyline/Test/CodeInspectionTest.cs
+++ pwiz_tools/Skyline/Test/CodeInspectionTest.cs
@@ -1060,6 +1060,7 @@ namespace pwiz.SkylineTest
             result.Add("Executables\\Tools\\SProCoP");
             result.Add("Executables\\Tools\\TestArgCollector");
             result.Add("Executables\\Tools\\ExampleInteractiveTool");
+            result.Add("Executables\\Tools\\DevTools");
             return result.ToArray();
         }
 
