632765340307ec604dc874721aadce672f79e6d0
dshteyn <david.shteynberg@proton.me>
2025-01-08 06:23:09 -0800

    Remove unnecessary using statements found by ReSharper

--- pwiz_tools/Skyline/TestFunctional/CarafeBuildLibraryTest.cs
+++ pwiz_tools/Skyline/TestFunctional/CarafeBuildLibraryTest.cs
@@ -2,10 +2,8 @@
 using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using pwiz.Common.SystemUtil;
-using pwiz.Skyline.Alerts;
 using pwiz.Skyline.Model.Tools;
 using pwiz.Skyline.SettingsUI;
-using pwiz.Skyline.ToolsUI;
 using pwiz.Skyline.Util;
 using pwiz.SkylineTestUtil;
 
