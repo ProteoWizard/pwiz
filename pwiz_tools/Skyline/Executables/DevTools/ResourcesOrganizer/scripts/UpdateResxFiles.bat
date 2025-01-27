ac23cc3173dc55610464b5fc8fe9431c292d4fde
Nicholas Shulman <nicksh@proteinms.net>
2025-01-26 16:09:41 -0800

    Fix mistakes in .jam and .bat files

--- pwiz_tools/Skyline/Executables/DevTools/ResourcesOrganizer/scripts/UpdateResxFiles.bat
+++ pwiz_tools/Skyline/Executables/DevTools/ResourcesOrganizer/scripts/UpdateResxFiles.bat
@@ -1,27 +1,25 @@
 @setlocal
-
+echo on
 if "%1" == "final" (
-	set EXPORTARGS=--overrideAll
-) 
-else if "%1" == "incremental" (
-	set EXPORTARGS=
-)
-else (
-	echo Usage %~nx0 final^|incrementa
-	set ERRORLEVEL=1
-	goto end
+    set EXPORTARGS=--overrideAll
+) else if "%1" == "incremental" (
+    set EXPORTARGS=
+) else (
+    echo Usage %~nx0 final^|incremental
+    set ERRORLEVEL=1
+    goto end
 )
 
 call %~dp0SetVariables.bat
 if %ERRORLEVEL% neq 0 (
-	goto end
+    goto end
 )
 
 set LASTVERSIONDB=%PWIZ_ROOT%\pwiz_tools\Skyline\Translation\LastReleaseResources.db
 if not exist %LASTVERSIONDB% (
-	echo %LASTVERSIONDB% does not exist
-	set ERRORLEVEL=1
-	goto end
+    echo %LASTVERSIONDB% does not exist
+    set ERRORLEVEL=1
+    goto end
 )
 pushd %PWIZ_ROOT%
 call %~dp0MakeResourcesDb.bat %WORKDIR%\CurrentRelease.db
@@ -29,17 +27,17 @@ popd
 pushd %WORKDIR%
 %RESORGANIZER% importLastVersion --db CurrentRelease.db %LASTVERSIONDB% --output MergedResources.db --language ja zh-CHS
 if %ERRORLEVEL% neq 0 (
-	goto error
+    goto error
 )
 %RESORGANIZER% exportResx --db MergedResources.db MergedResxFiles.zip
 if %ERRORLEVEL% neq 0 (
-	goto error
+    goto error
 )
 popd
 pushd %PWIZ_ROOT%
 libraries\7za.exe x -y %WORKDIR%\MergedResxFiles.zip
 if %ERRORLEVEL% neq 0 (
-	goto error
+    goto error
 )
 popd
 
