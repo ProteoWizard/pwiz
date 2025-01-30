875bc268c5ec11928a155f81c97eada2d6a81851
Nicholas Shulman <nicksh@proteinms.net>
2025-01-29 21:56:12 -0800

    PeakImputationTest

--- pwiz_tools/Skyline/TestFunctional/PeakImputationTest.cs
+++ pwiz_tools/Skyline/TestFunctional/PeakImputationTest.cs
@@ -1,4 +1,22 @@
-﻿using System.Linq;
+﻿/*
+ * Original author: Nicholas Shulman <nicksh .at. u.washington.edu>,
+ *                  MacCoss Lab, Department of Genome Sciences, UW
+ *
+ * Copyright 2025 University of Washington - Seattle, WA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using pwiz.Skyline.Controls.Graphs;
 using pwiz.Skyline.EditUI;
@@ -24,7 +42,7 @@ namespace pwiz.SkylineTestFunctional
                 SkylineWindow.OpenFile(TestFilesDir.GetTestPath("PeakImputationTest.sky"));
                 SkylineWindow.ShowPeakImputation();
             });
-
+            WaitForDocumentLoaded();
             var peakImputationForm = FindOpenForm<PeakImputationForm>();
             RunUI(() =>
             {
@@ -59,6 +77,35 @@ namespace pwiz.SkylineTestFunctional
                 peakImputationForm.AlignAllGraphs = true;
             });
             WaitForGraphs();
+            RunUI(() =>
+            {
+                peakImputationForm.RtCalculatorName = RtValueType.PSM_TIMES.Name;
+                peakImputationForm.DocumentWide = true;
+                peakImputationForm.MaxRTDeviation = null;
+                peakImputationForm.MaxPeakWidthVariation = null;
+            });
+            WaitForConditionUI(() => peakImputationForm.IsComplete);
+            RunUI(() =>
+            {
+                Assert.AreEqual(SkylineWindow.Document.MoleculeCount, peakImputationForm.DataboundGridControl.RowCount);
+                Assert.AreEqual(SkylineWindow.Document.MoleculeCount, peakImputationForm.ExemplaryCount);
+                Assert.AreEqual(0, peakImputationForm.NeedAdjustmentCount);
+                Assert.AreEqual(0, peakImputationForm.NeedRemovalCount);
+
+                peakImputationForm.MaxRTDeviation = 1;
+                Assert.AreEqual(1.0, SkylineWindow.Document.Settings.PeptideSettings.Imputation.MaxRtShift);
+            });
+            WaitForConditionUI(() => peakImputationForm.IsComplete);
+            RunUI(() =>
+            {
+                Assert.AreNotEqual(0, peakImputationForm.NeedAdjustmentCount);
+                peakImputationForm.ImputeBoundariesForAllRows();
+            });
+            WaitForConditionUI(() => peakImputationForm.IsComplete);
+            RunUI(() =>
+            {
+                Assert.AreEqual(0, peakImputationForm.NeedAdjustmentCount);
+            });
         }
     }
 }
