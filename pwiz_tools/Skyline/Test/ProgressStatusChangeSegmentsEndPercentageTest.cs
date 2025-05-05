6a20fa00f1470620ea2619cd62995b7f88280025
dshteyn <dshteyn@proteinms.net>
2025-05-05 13:37:37 -0700

    cruft

--- pwiz_tools/Skyline/Test/ProgressStatusChangeSegmentsEndPercentageTest.cs
+++ pwiz_tools/Skyline/Test/ProgressStatusChangeSegmentsEndPercentageTest.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Original author: David Shteynberg dshteyn .at. proteinms.net>,
+ * Original author: David Shteynberg <dshteyn .at. proteinms.net>,
  *                  MacCoss Lab, Department of Genome Sciences, UW
  *
  * Copyright 2025 University of Washington - Seattle, WA
