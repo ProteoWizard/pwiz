94df7d56182ae892bc2f8c1e8291c288fbc2cb4d
Nicholas Shulman <nickshulman@hotmail.com>
2025-05-30 13:37:42 -0700

    Add MaxTime property to PeptideChromDataSets which is the maximum time of all of the ChromDataSet's

--- pwiz_tools/Skyline/TestFunctional/ChromatogramExtractionRangeTest.cs
+++ pwiz_tools/Skyline/TestFunctional/ChromatogramExtractionRangeTest.cs
@@ -22,6 +22,11 @@ using pwiz.SkylineTestUtil;
 
 namespace pwiz.SkylineTestFunctional
 {
+    /// <summary>
+    /// Tests chromatogram extraction when the same light peptide appears multiple times in a document,
+    /// and has different heavy peptides which have different MS2 ID times.
+    /// Validates that Skyline does not choke extracting chromatograms.
+    /// </summary>
     [TestClass]
     public class ChromatogramExtractionRangeTest : AbstractFunctionalTest
     {
@@ -36,6 +41,20 @@ namespace pwiz.SkylineTestFunctional
         {
             RunUI(()=>SkylineWindow.OpenFile(TestFilesDir.GetTestPath("ExtractionRangeTest.sky")));
             ImportResultsFile(TestFilesDir.GetTestPath("S_1.mzML"));
+            var document = SkylineWindow.Document;
+            Assert.AreEqual(2, document.MoleculeCount);
+            foreach (var molecule in document.Molecules)
+            {
+                foreach (var transitionGroup in molecule.TransitionGroups)
+                {
+                    var measuredResults = document.MeasuredResults;
+                    float tolerance = (float)document.Settings.TransitionSettings.Instrument.MzMatchTolerance;
+                    Assert.IsNotNull(measuredResults);
+                    Assert.IsTrue(measuredResults.TryLoadChromatogram(measuredResults.Chromatograms[0], molecule, transitionGroup, tolerance, out var chromatogramGroupInfos));
+                    Assert.AreEqual(1, chromatogramGroupInfos.Length);
+                    Assert.IsNotNull(chromatogramGroupInfos[0]);
+                }
+            }
         }
     }
 }
