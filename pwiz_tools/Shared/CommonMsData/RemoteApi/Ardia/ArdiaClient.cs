d3900ece8b91a1d406f5671e996aa2ef97a6a046
ekoneil <ekoneil@gmail.com>
2025-06-24 16:21:20 -0700

    Fix code inspection

--- pwiz_tools/Shared/CommonMsData/RemoteApi/Ardia/ArdiaClient.cs
+++ pwiz_tools/Shared/CommonMsData/RemoteApi/Ardia/ArdiaClient.cs
@@ -24,7 +24,6 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using pwiz.Common.Collections;
 using pwiz.Common.SystemUtil;
