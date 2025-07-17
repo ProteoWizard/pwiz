c405201b44e7c0f66490aa8df0232cc9e79a5e29
Nicholas Shulman <nickshulman@hotmail.com>
2025-07-16 19:52:07 -0700

    Remove bogus "PauseTest"

--- pwiz_tools/Skyline/TestPerf/DiaSwathTutorialTest.cs
+++ pwiz_tools/Skyline/TestPerf/DiaSwathTutorialTest.cs
@@ -970,7 +970,6 @@ namespace TestPerf
                 Console.WriteLine();
 
             var peptidesPerProteinDlg = ShowDialog<AssociateProteinsDlg>(() => importPeptideSearchDlg.ClickNextButton());
-            PauseTest();
             WaitForCondition(() => peptidesPerProteinDlg.DocumentFinalCalculated);
             if (IsRecordMode)
                 Console.WriteLine();    // Line break after test run information
