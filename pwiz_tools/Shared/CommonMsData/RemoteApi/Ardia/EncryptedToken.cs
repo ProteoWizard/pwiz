e75aa92cd6d8ccd428490754c5d11a2054dca50c
Aaron Banse <acbanse@icloud.com>
2025-07-22 13:21:35 -0700

    Improved EncryptedToken style and organization based on recommendations

--- pwiz_tools/Shared/CommonMsData/RemoteApi/Ardia/EncryptedToken.cs
+++ pwiz_tools/Shared/CommonMsData/RemoteApi/Ardia/EncryptedToken.cs
@@ -1,30 +1,39 @@
-﻿using pwiz.Common.Collections;
+﻿/*
+ * Copyright 2025 University of Washington - Seattle, WA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using pwiz.Common.Collections;
 using pwiz.Common.SystemUtil;
 
 namespace pwiz.CommonMsData.RemoteApi.Ardia
 {
     public class EncryptedToken
     {
-        public string Encrypted { get; private set; }
-        public string Decrypted => !IsNullOrEmpty(this) ? CommonTextUtil.DecryptString(Encrypted) : "";
+        public string Encrypted { get; }
+        public string Decrypted => !IsNullOrEmpty(this) ? CommonTextUtil.DecryptString(Encrypted) : string.Empty;
         public static bool IsNullOrEmpty(EncryptedToken token) => token == null || token.Encrypted.IsNullOrEmpty();
 
-        private EncryptedToken(string token)
+        private EncryptedToken(string encryptedToken)
         {
-            Encrypted = CommonTextUtil.EncryptString(token);
+            Encrypted = encryptedToken;
         }
 
-        public static EncryptedToken FromString(string token) => new EncryptedToken(token);
+        public static EncryptedToken FromString(string token) => new EncryptedToken(CommonTextUtil.EncryptString(token));
 
-        public static EncryptedToken FromEncryptedString(string encryptedToken)
-        {
-            var token = new EncryptedToken("");
-            {
-                token.Encrypted = encryptedToken;
-            };
-            return token;
-        }
+        public static EncryptedToken FromEncryptedString(string encryptedToken) => new EncryptedToken(encryptedToken);
 
-        public static EncryptedToken Empty => new EncryptedToken("");
+        public static EncryptedToken Empty => new EncryptedToken(string.Empty);
     }
 }
