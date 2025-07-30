6580f173f54a812a6e23a7e100dcac405939d73c
David Shteynberg <dshteyn@proteinms.net>
2025-07-29 19:34:54 -0700

    Need to download updated zip file to pass TC tests.

--- pwiz_tools/Skyline/TestPerf/CarafeBuildLibraryTest.cs
+++ pwiz_tools/Skyline/TestPerf/CarafeBuildLibraryTest.cs
@@ -110,6 +110,9 @@ namespace TestPerf
             {
                 GetPerfTestDataURL($@"{TESTDATA_FILE}")
             };
+
+            FreshenTestDataDownloads();
+
             //DownloadTestDataPackage(new SilentProgressMonitor());
 
             var originalInstallationState = PythonInstaller.SimulatedInstallationState;
