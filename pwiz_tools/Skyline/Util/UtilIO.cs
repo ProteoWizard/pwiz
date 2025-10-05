f7041fa2853ba191e80b03ceba5fd2439be1f392
Nicholas Shulman <nicksh@uw.edu>
2025-10-04 23:16:09 -0700

    Remove logging statements Remove logging statements that had been added to track down TestInternationalFilenames intermittent failure.

--- pwiz_tools/Skyline/Util/UtilIO.cs
+++ pwiz_tools/Skyline/Util/UtilIO.cs
@@ -244,7 +244,6 @@ namespace pwiz.Skyline.Util
                 IDisposable connection;
                 if (!_connections.TryGetValue(id.GlobalIndex, out connection))
                     return;
-                (id as PooledSqliteConnection)?.LogEvent(@"Disconnect");
                 _connections.Remove(id.GlobalIndex);
                 // Disconnect inside lock, since a new attempt to connect
                 // may fail if the old connection is not fully disconnected.
