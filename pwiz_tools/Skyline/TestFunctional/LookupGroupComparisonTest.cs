50a991089bd30e7afc25fe02fbefb53b5c0bd676
Nicholas Shulman <nickshulman@hotmail.com>
2025-10-13 00:45:53 -0700

    Test sorting and grouping on Area Replicate Graph Pane

--- pwiz_tools/Skyline/TestFunctional/LookupGroupComparisonTest.cs
+++ pwiz_tools/Skyline/TestFunctional/LookupGroupComparisonTest.cs
@@ -16,8 +16,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pwiz.Skyline.Controls.Graphs;
 using pwiz.Skyline.Controls.GroupComparison;
+using pwiz.Skyline.Model.Results;
 using pwiz.SkylineTestUtil;
 
 namespace pwiz.SkylineTestFunctional
@@ -61,6 +67,65 @@ namespace pwiz.SkylineTestFunctional
             {
                 Assert.AreEqual(125, groupComparisonGrid.DataboundGridControl.RowCount);
             });
+
+            // Test sorting and grouping area graph
+            RunUI(()=>
+            {
+                SkylineWindow.ShowPeakAreaReplicateComparison();
+            });
+            // Should start out in document order
+            var chromatograms = SkylineWindow.Document.MeasuredResults.Chromatograms;
+            CollectionAssert.AreEqual(chromatograms.Select(c=>c.Name).ToList(), GetAreaAxisLabels());
+            
+            // Order by acquired time
+            RunUI(()=>SkylineWindow.ShowReplicateOrder(SummaryReplicateOrder.time));
+            WaitForGraphs();
+            var orderedByRunStartTime =
+                chromatograms.OrderBy(c => c.MSDataFileInfos.First().RunStartTime).ToList();
+            CollectionAssert.AreNotEqual(chromatograms.ToList(), orderedByRunStartTime);
+            CollectionAssert.AreEqual(orderedByRunStartTime.Select(c=>c.Name).ToList(), GetAreaAxisLabels());
+            
+            // Group by Name
+            var annotationSubjectId =
+                SkylineWindow.Document.Settings.DataSettings.AnnotationDefs.First(annotation =>
+                    annotation.Name == "SubjectID");
+            var replicateValueSubjectId = new ReplicateValue.Annotation(annotationSubjectId);
+            var listDef = SkylineWindow.Document.Settings.DataSettings.Lists.First().ListDef;
+            var replicateValueName = new ReplicateValue.Lookup(replicateValueSubjectId,
+                listDef.Properties.First(def => def.Name == "Name"));
+            RunUI(()=>SkylineWindow.GroupByReplicateValue(replicateValueName));
+            WaitForGraphs();
+            var ratNamesByRunStartTime = orderedByRunStartTime.Select(GetRatName).Distinct().ToList();
+            CollectionAssert.AreEqual(ratNamesByRunStartTime, GetAreaAxisLabels());
+            
+            // Order by rat name
+            RunUI(()=>SkylineWindow.OrderByReplicateAnnotation(replicateValueName));
+            WaitForGraphs();
+            var alphabeticalRatNames =
+                ratNamesByRunStartTime.OrderBy(name => name, StringComparer.CurrentCultureIgnoreCase).ToList();
+            CollectionAssert.AreEqual(alphabeticalRatNames, GetAreaAxisLabels());
+        }
+
+        private List<string> GetAreaAxisLabels()
+        {
+            return CallUI(() =>
+            {
+                var graphSummary = FindGraphSummaryByGraphType<AreaReplicateGraphPane>();
+                Assert.IsNotNull(graphSummary);
+                Assert.IsTrue(graphSummary.TryGetGraphPane(out AreaReplicateGraphPane areaReplicateGraphPane));
+                return areaReplicateGraphPane.XAxis.Scale.TextLabels.ToList();
+            });
+        }
+
+        private string GetRatName(ChromatogramSet chromatogramSet)
+        {
+            var subjectId = chromatogramSet.Annotations.GetAnnotation("SubjectID");
+            var listData = SkylineWindow.Document.Settings.DataSettings.Lists.Single();
+            var rowIndex = listData.RowIndexOfPrimaryKey(subjectId);
+            Assert.AreNotEqual(-1, rowIndex, "No row {0}", subjectId);
+            int indexName = listData.FindColumnIndex("Name");
+            Assert.AreNotEqual(-1, indexName, "No column Name");
+            return listData.Columns[indexName].GetValue(rowIndex)?.ToString();
         }
     }
 }
