f4fe76b80e464cf78770263b973a396729d87392
Nicholas Shulman <nickshulman@hotmail.com>
2025-10-15 07:14:53 -0700

    Remove unused using statements

--- pwiz_tools/Skyline/TestFunctional/CarafeEmptyNameTest.cs
+++ pwiz_tools/Skyline/TestFunctional/CarafeEmptyNameTest.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿/*
+ * Original author: Nicholas Shulman <nicksh .at. u.washington.edu>,
+ *                  MacCoss Lab, Department of Genome Sciences, UW
+ *
+ * Copyright 2025 University of Washington - Seattle, WA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using pwiz.Skyline.Alerts;
 using pwiz.Skyline.SettingsUI;
 using pwiz.SkylineTestUtil;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using pwiz.Skyline.Properties;
 
 namespace pwiz.SkylineTestFunctional
@@ -59,7 +72,7 @@ namespace pwiz.SkylineTestFunctional
 
             RunDlg<AlertDlg>(buildLibraryDlg.OkWizardPage, dlg =>
             {
-                Assert.AreEqual(String.Format(Resources.MessageBoxHelper_ValidateNameTextBox__0__cannot_be_empty, "Name"),
+                Assert.AreEqual(string.Format(Resources.MessageBoxHelper_ValidateNameTextBox__0__cannot_be_empty, "Name"),
                     dlg.Message);
                 dlg.OkDialog();
             });
@@ -67,6 +80,5 @@ namespace pwiz.SkylineTestFunctional
             OkDialog(buildLibraryDlg, buildLibraryDlg.CancelDialog);
             OkDialog(peptideSettings, peptideSettings.OkDialog);
         }
-
     }
 }
