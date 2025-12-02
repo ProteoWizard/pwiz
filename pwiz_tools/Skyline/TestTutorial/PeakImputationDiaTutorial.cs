3f2ad5f4180374c0bd439a507b826563ace73b2f
Nicholas Shulman <nickshulman@hotmail.com>
2025-12-01 21:28:57 -0800

    Show how peak imputation changes the volcano plot

--- pwiz_tools/Skyline/TestTutorial/PeakImputationDiaTutorial.cs
+++ pwiz_tools/Skyline/TestTutorial/PeakImputationDiaTutorial.cs
@@ -1,6 +1,8 @@
+using DigitalRune.Windows.Docking;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using pwiz.Skyline.Controls.Databinding;
 using pwiz.Skyline.Controls.GroupComparison;
+using pwiz.Skyline.SettingsUI;
 using pwiz.SkylineTestUtil;
 
 namespace pwiz.SkylineTestTutorial
@@ -36,6 +38,47 @@ namespace pwiz.SkylineTestTutorial
             var foldChangeGrid = FindOpenForm<FoldChangeGrid>();
             WaitForConditionUI(() => foldChangeGrid.IsComplete);
             PauseForScreenShot(foldChangeGrid);
+            RunUI(()=>foldChangeGrid.ShowVolcanoPlot());
+            var foldChangeVolcanoPlot = FindOpenForm<FoldChangeVolcanoPlot>();
+            var floatingWindow = ScreenshotManager.FindParent<FloatingWindow>(foldChangeGrid);
+            Assert.IsNotNull(floatingWindow);
+            Assert.AreSame(floatingWindow, ScreenshotManager.FindParent<FloatingWindow>(foldChangeVolcanoPlot));
+            RunUI(()=>
+            {
+                floatingWindow.Width = 1024;
+            });
+            PauseForScreenShot(floatingWindow);
+            RunUI(()=>foldChangeGrid.ShowChangeSettings());
+            var editGroupComparisonDlg = FindOpenForm<EditGroupComparisonDlg>();
+            RunUI(() =>
+            {
+                editGroupComparisonDlg.ShowAdvanced(true);
+                editGroupComparisonDlg.GroupComparisonDef =
+                    editGroupComparisonDlg.GroupComparisonDef.ChangeUseZeroForMissingPeaks(true);
+            });
+            WaitForConditionUI(() => foldChangeVolcanoPlot.IsComplete);
+            PauseForScreenShot(editGroupComparisonDlg);
+            OkDialog(editGroupComparisonDlg, editGroupComparisonDlg.Close);
+            PauseForScreenShot(floatingWindow);
+            RunUI(()=>
+            {
+                foldChangeVolcanoPlot.Close();
+                foldChangeGrid.ShowVolcanoPlot();
+            });
+            foldChangeVolcanoPlot = FindOpenForm<FoldChangeVolcanoPlot>();
+            WaitForConditionUI(() => foldChangeVolcanoPlot.IsComplete);
+            PauseForScreenShot(floatingWindow);
+            RunLongDlg<PeptideSettingsUI>(SkylineWindow.ShowPeptideSettingsUI, peptideSettingsUi =>
+            {
+                RunUI(()=>
+                {
+                    peptideSettingsUi.SelectedTab = PeptideSettingsUI.TABS.Prediction;
+                    peptideSettingsUi.ImputeMissingPeaks = true;
+                });
+                PauseForScreenShot(peptideSettingsUi);
+            }, peptideSettingsUi=>peptideSettingsUi.OkDialog());
+            WaitForConditionUI(() => foldChangeVolcanoPlot.IsComplete);
+            PauseForScreenShot(floatingWindow);
         }
     }
 }
