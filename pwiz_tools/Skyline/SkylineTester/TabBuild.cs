6b6f17d970aec40c80ec4416c0f350672f5350a4
Brian Pratt <bspratt@proteinms.net>
2025-12-09 22:29:36 -0800

    make sure SkylineTester runs the debug build when desired

--- pwiz_tools/Skyline/SkylineTester/TabBuild.cs
+++ pwiz_tools/Skyline/SkylineTester/TabBuild.cs
@@ -215,12 +215,14 @@ namespace SkylineTester
             commandShell.Add("cd {0}", buildRoot.Quote());
 
             // Do a debug build if desired
-            if (Environment.GetEnvironmentVariables().Contains("DEBUG_SKYLINE_NIGHTLY"))
+            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DEBUG_SKYLINE_NIGHTLY")))
             {
                 var msg = " Environment variable DEBUG_SKYLINE_NIGHTLY is set, building with debug information. This may slow or even disable some tests.";
                 commandShell.Add($@"#@ {msg}\n");
                 commandShell.Add($@"# {msg}");
                 toolsetArg += " variant=debug debug-symbols=on iterator-debugging=off runtime-debugging=off";
+
+                MainWindow.BuildDebug = true; // So SkylineTester knows to run Debug/Skyline instead of Release/Skyline
             }
 
             foreach (int architecture in architectures)
