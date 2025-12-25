5dca8c0cf277184e1b40822dd38830d67c092a5d
Nicholas Shulman <nickshulman@hotmail.com>
2025-12-25 09:42:42 -0800

    Revert to original version of Microsoft.Diagnostics.Runtime.dll

--- pwiz_tools/Skyline/TestUtil/HangDetection.cs
+++ pwiz_tools/Skyline/TestUtil/HangDetection.cs
@@ -1,20 +1,45 @@
+/*
+ * Original author: Nicholas Shulman <nicksh .at. u.washington.edu>,
+ *                  MacCoss Lab, Department of Genome Sciences, UW
+ *
+ * Copyright 2025 University of Washington - Seattle, WA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 using Microsoft.Diagnostics.Runtime;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using pwiz.Skyline.Util.Extensions;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Threading;
 
 namespace pwiz.SkylineTestUtil
 {
     public static class HangDetection
     {
+        /// <summary>
+        /// If action takes more than 30 minutes to complete, interrupt this thread and write threads to console.
+        /// </summary>
         public static void InterruptWhenHung(Action action)
         {
             InterruptAfter(action, TimeSpan.FromSeconds(1), 1800);
         }
         
+        /// <summary>
+        /// If action takes longer than <paramref name="cycleCount"/> times <paramref name="cycleDuration"/> then
+        /// interrupt this thread and dump callstacks to the console.
+        /// </summary>
         public static void InterruptAfter(Action action, TimeSpan cycleDuration, int cycleCount)
         {
             bool[] completed = new bool[1];
@@ -38,11 +63,17 @@ namespace pwiz.SkylineTestUtil
             }
             catch (ThreadInterruptedException interruptedException)
             {
-                var stringWriter = new StringWriter();
-                stringWriter.WriteLine();
-                stringWriter.WriteLine(GetThreadDump(Process.GetCurrentProcess().Id));
-                stringWriter.WriteLine("*** End of thread dump");
-                Console.Out.WriteLine("*** Hang detected. Thread dump:{0}", stringWriter);
+                try
+                {
+                    var threadDumpLines = new List<string> { "*** Hang detected. Thread dump:" };
+                    threadDumpLines.AddRange(GetAllThreadsCallstacks(Process.GetCurrentProcess().Id));
+                    threadDumpLines.Add("*** End of thread dump");
+                    Console.Out.WriteLine(TextUtil.LineSeparate(threadDumpLines));
+                }
+                catch (Exception ex)
+                {
+                    Console.Out.WriteLine("Unable to get thread dump: {0}", ex);
+                }
                 throw new AssertFailedException(string.Format("Timeout waiting {0} times {1} for action to complete", cycleDuration, cycleCount), interruptedException);
             }
         }
@@ -71,33 +102,24 @@ namespace pwiz.SkylineTestUtil
             }
         }
 
-        public static string GetThreadDump(int processId)
+        public static IEnumerable<string> GetAllThreadsCallstacks(int processId)
         {
-            var stringWriter = new StringWriter();
-            try
-            {
-                using var dataTarget = DataTarget.AttachToProcess(processId, 60000, AttachFlag.Passive);
-                var runtime = dataTarget.ClrVersions[0].CreateRuntime();
+            using var dataTarget = DataTarget.AttachToProcess(processId, 60000, AttachFlag.Passive);
+            var runtime = dataTarget.ClrVersions[0].CreateRuntime();
 
-                foreach (var thread in runtime.Threads)
+            foreach (var thread in runtime.Threads)
+            {
+                if (!thread.IsAlive) continue;
+
+                yield return $"Thread {thread.OSThreadId:X} (Managed ID: {thread.ManagedThreadId})";
+
+                foreach (var frame in thread.EnumerateStackTrace())
                 {
-                    if (!thread.IsAlive) continue;
-
-                    stringWriter.WriteLine($"Thread {thread.OSThreadId:X} (Managed ID: {thread.ManagedThreadId})");
-
-                    foreach (var frame in thread.EnumerateStackTrace())
-                    {
-                        stringWriter.WriteLine($"  {frame.Method?.Type?.Name}.{frame.Method?.Name ?? "[Unknown]"}");
-                    }
-                    stringWriter.WriteLine();
+                    yield return $"  {frame.Method?.Type?.Name}.{frame.Method?.Name ?? "[Unknown]"}";
                 }
-            }
-            catch (Exception exception)
-            {
-                stringWriter.WriteLine("Unable to dump all threads: {0}", exception);
-            }
 
-            return stringWriter.ToString();
+                yield return string.Empty;
+            }
         }
     }
 }
