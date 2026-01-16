7aeefda2f290b28b4013278415123ab4ba84c11d
Nicholas Shulman <nicksh@uw.edu>
2026-01-16 10:59:53 -0800

    Fix FilesDirTest, TestFastaImport, TestKeyboardShortcutHelp, TestManagingSearchTools, TestManagingSearchTools

--- pwiz_tools/Skyline/Test/UtilTest.cs
+++ pwiz_tools/Skyline/Test/UtilTest.cs
@@ -26,6 +26,7 @@ using System.Text;
 using pwiz.Skyline.Util;
 using pwiz.Skyline.Util.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pwiz.Common.CommonResources;
 using pwiz.Common.SystemUtil;
 using pwiz.Skyline.Model;
 using pwiz.SkylineTestUtil;
@@ -496,7 +497,7 @@ namespace pwiz.SkylineTest
             // NB: We really do not want to see the error "was locked but has since been deleted"
             //     since we know the file is locked and cannot have been deleted.
             AssertEx.ThrowsException<IOException>(() => FileLockingProcessFinder.DeleteDirectoryWithFileLockingDetails(dirPath),
-                x => AssertEx.Contains(x.Message, lockedFile, "this process"));
+                x => AssertEx.Contains(x.Message, lockedFile, MessageResources.FileLockingProcessFinder_ToFileLockingException_this_process));
         }
     }
 }
