4cfc5a0774224db61a4691f68bb3ed58f8d00086
Nicholas Shulman <nickshulman@hotmail.com>
2026-01-23 16:13:04 -0800

    Add ManyTransitionsTest.cs

---
+++ pwiz_tools/Skyline/TestFunctional/ManyTransitionsTest.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pwiz.SkylineTestUtil;
+
+namespace pwiz.SkylineTestFunctional
+{
+    [TestClass]
+    public class ManyTransitionsTest : AbstractFunctionalTest
+    {
+        [TestMethod]
+        public void TestManyTransitions()
+        {
+            TestFilesZip = @"TestFunctional/ManyTransitionsTest.data";
+            RunFunctionalTest();
+        }
+
+        protected override void DoTest()
+        {
+            RunUI(() => SkylineWindow.OpenFile(TestFilesDir.GetTestPath("mb_0000.sky")));
+            WaitForDocumentLoaded();
+            WaitForGraphs();
+            RunUI(()=>SkylineWindow.SelectedPath = SkylineWindow.Document.GetPathTo(2, 0));
+            WaitForGraphs();
+            RunUI(() => SkylineWindow.SelectedPath = SkylineWindow.Document.GetPathTo(3, 0));
+            WaitForGraphs();
+        }
+    }
+}
