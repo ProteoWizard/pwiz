4fe65b847dbbd509ebe523b2daa17cee1b43485c
Nicholas Shulman <nickshulman@hotmail.com>
2026-02-09 16:05:19 -0800

    Different way of fixing bug: Always exclude missing values when run-to-run. This is more consistent with the fact that DictionaryRetentionScoreCalculator is not set up to display missing values on the graph since its value for "UnknownScore" is NaN

--- pwiz_tools/Skyline/Model/RetentionTimes/RetentionTimeRegressionGraphData.cs
+++ pwiz_tools/Skyline/Model/RetentionTimes/RetentionTimeRegressionGraphData.cs
@@ -267,9 +267,17 @@ namespace pwiz.Skyline.Model.RetentionTimes
 
                 if (IsRunToRun)
                 {
+                    var targetTimesDict = pointInfos.Where(pt => pt.Y.HasValue)
+                        .ToDictionary(pt => pt.ModifiedTarget, pt => pt.Y.Value);
                     var origTimesDict = pointInfos.Where(pt => pt.X.HasValue)
                         .ToDictionary(pt => pt.ModifiedTarget, pt => pt.X.Value);
                     _calculator = new DictionaryRetentionScoreCalculator(XmlNamedElement.NAME_INTERNAL, DocumentRetentionTimes.ConvertToMeasuredRetentionTimes(origTimesDict));
+                    var alignedRetentionTimes = AlignedRetentionTimes.AlignLibraryRetentionTimes(targetTimesDict, origTimesDict, refine ? RegressionSettings.Threshold : 0, RegressionSettings.RegressionMethod, token);
+                    if (alignedRetentionTimes != null)
+                    {
+                        _regressionAll = alignedRetentionTimes.Regression;
+                        _statisticsAll = alignedRetentionTimes.RegressionStatistics;
+                    }
                 }
                 else
                 {
@@ -294,31 +302,30 @@ namespace pwiz.Skyline.Model.RetentionTimes
                         // CONSIDER: If another calculator type is added that requires loading and
                         //           could be unavailable, this name check would need to be
                         //           generalized for that type.
-                        _calculator = calcName is RtCalculatorOption.Irt irtOption
+                        var calc = calcName is RtCalculatorOption.Irt irtOption
                             ? usableCalculators.FirstOrDefault(c => c.Name == irtOption.Name)
                             : usableCalculators.FirstOrDefault();
+                        if (calc != null)
+                        {
+                            _regressionAll = RetentionTimeRegression.CalcSingleRegression(XmlNamedElement.NAME_INTERNAL,
+                                calc,
+                                targetTimes,
+                                null,
+                                true,
+                                RegressionSettings.RegressionMethod,
+                                out _statisticsAll,
+                                out _,
+                                token);
+                        }
+                        token.ThrowIfCancellationRequested();
+                        _calculator = calc;
                     }
-                }
 
-                if (_calculator != null && _regressionAll == null)
-                {
-                    _regressionAll = RetentionTimeRegression.CalcSingleRegression(XmlNamedElement.NAME_INTERNAL,
-                        _calculator,
-                        targetTimes,
-                        null,
-                        true,
-                        RegressionSettings.RegressionMethod,
-                        out _statisticsAll,
-                        out _,
-                        token);
-                }
-
-                token.ThrowIfCancellationRequested();
-
-                if (_calculator != null)
-                {
-                    pointInfos = pointInfos.Select(pt =>
-                        pt.ChangeX(_calculator.ScoreSequence(pt.ModifiedTarget))).ToList();
+                    if (_calculator != null)
+                    {
+                        pointInfos = pointInfos.Select(pt =>
+                            pt.ChangeX(_calculator.ScoreSequence(pt.ModifiedTarget))).ToList();
+                    }
                 }
 
                 _regressionPredict = (IsRunToRun || RegressionSettings.RegressionMethod != RegressionMethodRT.linear)  ? null : document.Settings.PeptideSettings.Prediction.RetentionTime;
@@ -407,7 +414,7 @@ namespace pwiz.Skyline.Model.RetentionTimes
                     {
                         validPoints.Add(pt);
                     }
-                    else if (_regressionIncludesMissingValues)
+                    else if (_regressionIncludesMissingValues && !IsRunToRun)
                     {
                         validPoints.Add(pt.ChangeX(pt.X ?? _calculator?.UnknownScore ?? 0).ChangeY(pt.Y ?? 0));
                     }
