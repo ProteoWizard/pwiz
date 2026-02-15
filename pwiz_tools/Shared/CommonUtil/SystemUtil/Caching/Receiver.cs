ProteoWizard/pwiz

commit fad394269807a690bee48a828b81b8a38ca2a237

Use TaskScheduler.FromCurrentSynchronizationContext instead of Control.BeginInvoke (or CommonActionUtil.SafeBeginInvoke) to work around problem of calling BeginInvoke after control handle has been destroyed. (#3959)
Fixed potential hang when opening a new document (not reported by anyone)

pwiz_tools/Shared/CommonUtil/SystemUtil/Caching/Receiver.cs
@@ -17,6 +17,8 @@
  * limitations under the License.
  */
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace pwiz.Common.SystemUtil.Caching
@@ -31,12 +33,18 @@ namespace pwiz.Common.SystemUtil.Caching
         private bool _progressNotificationPending;
         private WorkOrder _workOrder;
         private readonly IProductionListener _listener;
+        private TaskScheduler _taskScheduler;
         public Receiver(ProductionFacility cache, Control ownerControl, Producer factory)
         {
             Cache = cache;
             OwnerControl = ownerControl;
             Producer = factory;
             OwnerControl.HandleDestroyed += OwnerControlHandleDestroyed;
+            if (OwnerControl.InvokeRequired)
+            {
+                throw new InvalidOperationException(@"Must be constructed on event thread");
+            }
+            _taskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
             _listener = new Listener(this);
         }
 
@@ -57,13 +65,17 @@ namespace pwiz.Common.SystemUtil.Caching
                 if (!_notificationPending)
                 {
                     _notificationPending = true;
-                    CommonActionUtil.SafeBeginInvoke(OwnerControl, () =>
-                    {
+                    BeginInvoke(()=> {
                         _notificationPending = false;
                         productAvailable();
                     });
                 }
             }
+        }
+
+        private void BeginInvoke(Action action)
+        {
+            Task.Factory.StartNew(action, CancellationToken.None, TaskCreationOptions.None, _taskScheduler);
         }
 
         private void OnProductStatusChanged()
@@ -80,7 +92,7 @@ namespace pwiz.Common.SystemUtil.Caching
                 _progressNotificationPending = true;
             }
 
-            CommonActionUtil.SafeBeginInvoke(OwnerControl, () =>
+            BeginInvoke(() =>
             {
                 lock (this)
                 {

commit 55f369f47b4ece26af6adc35381d409f5db432a7

Try to fix leaks introduced by PR #3959 Use SynchronizationContext directly instead of TaskScheduler in Receiver.cs

pwiz_tools/Shared/CommonUtil/SystemUtil/Caching/Receiver.cs
@@ -18,7 +18,6 @@
  */
 using System;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace pwiz.Common.SystemUtil.Caching
@@ -33,7 +32,7 @@ namespace pwiz.Common.SystemUtil.Caching
         private bool _progressNotificationPending;
         private WorkOrder _workOrder;
         private readonly IProductionListener _listener;
-        private TaskScheduler _taskScheduler;
+        private SynchronizationContext _synchronizationContext;
         public Receiver(ProductionFacility cache, Control ownerControl, Producer factory)
         {
             Cache = cache;
@@ -44,7 +43,7 @@ namespace pwiz.Common.SystemUtil.Caching
             {
                 throw new InvalidOperationException(@"Must be constructed on event thread");
             }
-            _taskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+            _synchronizationContext = SynchronizationContext.Current;
             _listener = new Listener(this);
         }
 
@@ -75,7 +74,7 @@ namespace pwiz.Common.SystemUtil.Caching
 
         private void BeginInvoke(Action action)
         {
-            Task.Factory.StartNew(action, CancellationToken.None, TaskCreationOptions.None, _taskScheduler);
+            _synchronizationContext?.Post(_ => action(), null);
         }
 
         private void OnProductStatusChanged()
@@ -158,6 +157,7 @@ namespace pwiz.Common.SystemUtil.Caching
             {
                 Cache.Unlisten(_workOrder, _listener);
                 _workOrder = null;
+                _synchronizationContext = null;
                 if (OwnerControl != null)
                 {
                     OwnerControl.HandleDestroyed -= OwnerControlHandleDestroyed;