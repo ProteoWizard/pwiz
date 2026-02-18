486fbb1361afd82f0ff58b5ec0092b7f5573f6ee
Brian Pratt <bspratt@proteinms.net>
2026-02-18 10:15:34 -0800

    A couple more places where we need to check for zero length arrays

--- pwiz/data/msdata/IO.cpp
+++ pwiz/data/msdata/IO.cpp
@@ -1643,7 +1643,10 @@ void writeMzMLbExtra<BinaryDataArray>(stream<Connection_mzMLb>* mzMLb_os, string
             else dataset += "_numpress_slof";
 
             offset = (*mzMLb_os)->seek(dataset, 0, std::ios_base::cur);
-            (*mzMLb_os)->write_opaque(dataset, (const unsigned char*)&encoded[0], encoded_size);
+            if (encoded_size > 0)
+            {
+                (*mzMLb_os)->write_opaque(dataset, (const unsigned char*)&encoded[0], encoded_size);
+            }
         }
         else if (usedConfig.precision == BinaryDataEncoder::Precision_32)
         {
@@ -1730,7 +1733,10 @@ void writeMzMLbExtra<BinaryDataArray>(stream<Connection_mzMLb>* mzMLb_os, string
             dataset += "_float";
             offset = (*mzMLb_os)->seek(dataset, 0, std::ios_base::cur);
             encoded_size = float_data.size() * sizeof(float);
-            (*mzMLb_os)->write(dataset, &float_data[0], float_data.size());
+            if (encoded_size > 0)
+            {
+                (*mzMLb_os)->write(dataset, &float_data[0], float_data.size());
+            }
         }
         else
         {
