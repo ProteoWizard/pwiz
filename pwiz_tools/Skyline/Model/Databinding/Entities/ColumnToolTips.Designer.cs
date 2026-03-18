f92ae680cad5d0405ecf37b3358f9001f0d11993
nickshulman <nicksh@proteinms.net>
2026-03-18 13:42:08 -0700

    Generate separate report documentation file for each UI mode (#4074)
    Fixed Help > Documentation > Reports showing columns in "Mixed" mode.
    Generate separate report HTML documentation files for each of the possible UI modes.
    These files are Reports-proteomic.html, Reports-small_molecules.html and Reports-mixed.html

--- pwiz_tools/Skyline/Model/Databinding/Entities/ColumnToolTips.Designer.cs
+++ pwiz_tools/Skyline/Model/Databinding/Entities/ColumnToolTips.Designer.cs
@@ -19,7 +19,7 @@ namespace pwiz.Skyline.Model.Databinding.Entities {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class ColumnToolTips {
@@ -1996,7 +1996,7 @@ namespace pwiz.Skyline.Model.Databinding.Entities {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to A comma separated list of the chemical forumulas for all neutral losses from this fragment, or empty if not all losses have formulas.
+        ///   Looks up a localized string similar to A comma separated list of the chemical formulas for all neutral losses from this fragment, or empty if not all losses have formulas.
         /// </summary>
         public static string LossFormulas {
             get {
