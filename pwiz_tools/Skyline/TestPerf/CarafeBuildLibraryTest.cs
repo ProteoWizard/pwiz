ac2650875c2612c0ca5c6a4549c4f91047580c2f
Nicholas Shulman <nickshulman@hotmail.com>
2026-03-18 08:45:15 -0700

    Claude got test to compile

--- pwiz_tools/Skyline/TestPerf/CarafeBuildLibraryTest.cs
+++ pwiz_tools/Skyline/TestPerf/CarafeBuildLibraryTest.cs
@@ -28,11 +28,9 @@ using pwiz.Skyline.Model;
 using pwiz.Skyline.Model.Irt;
 using pwiz.Skyline.Model.Lib;
 using pwiz.Skyline.Model.Lib.Carafe;
-using pwiz.Skyline.Model.Tools;
 using pwiz.Skyline.Properties;
 using pwiz.Skyline.SettingsUI;
 using pwiz.Skyline.SettingsUI.Irt;
-using pwiz.Skyline.ToolsUI;
 using pwiz.Skyline.Util;
 using pwiz.SkylineTestUtil;
 using System;
@@ -47,16 +45,9 @@ namespace TestPerf
     [TestClass]
     public class CarafeBuildLibraryTest : AbstractFunctionalTestEx
     {
-        private double MZ_TOLERANCE = 1e-4; 
+        private double MZ_TOLERANCE = 1e-4;
         // private double INTENSITY_TOLERANCE = 1.5e-1;
         private double MIN_COSINE_ANGLE = 0.9;
-
-        /// <summary>
-        /// When true Python installation is forced by deleting any old installation
-        /// </summary>
-        private bool IsCleanPythonMode => false;
-
-        private bool DisableLongPathsRegistry => false;
 
         /// <summary>
         /// When true console output is added to clarify what the test has accomplished
@@ -64,15 +55,12 @@ namespace TestPerf
         public bool IsVerboseMode => false;
 
         /// <summary>
-        /// When true the test write the Python hash value for <see cref="Settings.PythonEmbeddableHash"/>
-        /// and the expected spectra in "TestPerf\CarafeBuildLibraryTestExtra.data"
+        /// When true the test writes the expected spectra in "TestPerf\CarafeBuildLibraryTestExtra.data"
         /// </summary>
         protected override bool IsRecordMode => false;
 
         public string LogOutput => TestContext.GetTestResultsPath("TestConsole.log");
         private const string TESTDATA_FILE = @"CarafeBuildLibraryTest.zip";
-
-        private string _toolName = CarafeLibraryBuilder.CARAFE;
 
         private void PrintEnvironment()
         {
@@ -118,9 +106,6 @@ namespace TestPerf
                 PrintEnvironment();
             }
 
-            if (IsCleanPythonMode)
-                AssertEx.IsTrue(PythonInstaller.DeleteToolsPythonDirectory());
-
             TestFilesZipPaths = new[]
             {
                 GetPerfTestDataURL(TESTDATA_FILE),
@@ -128,17 +113,7 @@ namespace TestPerf
             };
             TestFilesPersistent = new[] { "rawdata" };
 
-            var originalInstallationState = PythonInstaller.SimulatedInstallationState;
-            try
-            {
-                PythonInstaller.SimulatedInstallationState = PythonInstaller.eSimulatedInstallationState.NONVIDIAHARD;
-                RunFunctionalTest();
-            }
-            finally
-            {
-                PythonInstaller.SimulatedInstallationState = originalInstallationState;
-            }
-
+            RunFunctionalTest();
         }
 
         private enum TestLibrary
@@ -164,24 +139,18 @@ namespace TestPerf
             string builtLibraryBySkyIrt = null;
             if (_testLibraries.Contains(TestLibrary.LibraryTunedBySkyline))
             {
-                builtLibraryBySky = CarafeBuildLibrary(TestLibrary.LibraryTunedBySkyline, MzMLFile, "", SkyFineTuneFile, BuildLibraryDlg.BuildLibraryTargetOptions.currentSkylineDocument, BuildLibraryDlg.LearningOptions.another_doc, PythonInstaller.eSimulatedInstallationState.NONVIDIASOFT);
+                builtLibraryBySky = CarafeBuildLibrary(TestLibrary.LibraryTunedBySkyline, MzMLFile, "", SkyFineTuneFile, BuildLibraryDlg.BuildLibraryTargetOptions.currentSkylineDocument, BuildLibraryDlg.LearningOptions.another_doc);
             }
 
             if (_testLibraries.Contains(TestLibrary.LibraryTunedByDiann))
             {
-                builtLibraryByDiann = CarafeBuildLibrary(TestLibrary.LibraryTunedByDiann, MzMLFile, "", DiannFineTuneFile, BuildLibraryDlg.BuildLibraryTargetOptions.currentSkylineDocument, BuildLibraryDlg.LearningOptions.diann_report, PythonInstaller.eSimulatedInstallationState.NONVIDIAHARD);
+                builtLibraryByDiann = CarafeBuildLibrary(TestLibrary.LibraryTunedByDiann, MzMLFile, "", DiannFineTuneFile, BuildLibraryDlg.BuildLibraryTargetOptions.currentSkylineDocument, BuildLibraryDlg.LearningOptions.diann_report);
             }
 
             if (_testLibraries.Contains(TestLibrary.LibraryTunedBySkylineIrt))
             {
-                builtLibraryBySkyIrt = CarafeBuildLibrary(TestLibrary.LibraryTunedBySkylineIrt, MzMLFile, "", SkyFineTuneFile, BuildLibraryDlg.BuildLibraryTargetOptions.currentSkylineDocument, BuildLibraryDlg.LearningOptions.another_doc, PythonInstaller.eSimulatedInstallationState.NONVIDIAHARD, IrtStandard.BIOGNOSYS_11);
+                builtLibraryBySkyIrt = CarafeBuildLibrary(TestLibrary.LibraryTunedBySkylineIrt, MzMLFile, "", SkyFineTuneFile, BuildLibraryDlg.BuildLibraryTargetOptions.currentSkylineDocument, BuildLibraryDlg.LearningOptions.another_doc, IrtStandard.BIOGNOSYS_11);
             }
-
-            var fileHash = PythonInstallerUtil.GetMD5FileHash(PythonInstaller.PythonEmbeddablePackageDownloadPath);
-
-            if (IsRecordMode)
-                Console.WriteLine($@"Computed PythonEmbeddableHash: {fileHash}");
-            Assert.AreEqual(Settings.Default.PythonEmbeddableHash, fileHash);
 
             var spectralLibraryViewer = ShowDialog<ViewLibraryDlg>(SkylineWindow.ViewSpectralLibraries);
             RunUI(() =>
@@ -218,8 +187,6 @@ namespace TestPerf
             {
                 ValidateLibraryResult("cpu_test_res_fine_tuned_bySky.json", builtLibraryBySky);
             }
-            if (IsCleanPythonMode)
-                AssertEx.IsTrue(PythonInstaller.DeleteToolsPythonDirectory());
         }
 
         private void ValidateLibraryResult(string expectedFile, string resultPath)
@@ -276,25 +243,24 @@ namespace TestPerf
         }
 
         /// <summary>
-        /// Test goes through building of a Library by Carafe with or without iRT.  Returns path to library built/
+        /// Test goes through building of a Library by Carafe with or without iRT.  Returns path to library built.
         /// </summary>
         /// <param name="testLibrary">Enum value for library</param>
-        /// <param name="buildTarget">Build library target peptides, current document peptides (or FASTA database if current doc is blank)</param>
-        /// <param name="learnFrom">Source of fine tuning document</param>
-        /// <param name="simulatedInstallationState">Python Simulated State helps determine whether user is offered Nvidia install</param>
-        /// <param name="iRTtype">iRT standard type</param>
         /// <param name="mzMLFile">MS/MS Data file path</param>
         /// <param name="proteinDatabase">Protein FASTA database</param>
         /// <param name="fineTuneFile">fine tuning file path</param>
+        /// <param name="buildTarget">Build library target peptides, current document peptides (or FASTA database if current doc is blank)</param>
+        /// <param name="learnFrom">Source of fine tuning document</param>
+        /// <param name="iRTtype">iRT standard type</param>
         private string CarafeBuildLibrary(TestLibrary testLibrary,
             string mzMLFile, string proteinDatabase, string fineTuneFile,
             BuildLibraryDlg.BuildLibraryTargetOptions buildTarget, BuildLibraryDlg.LearningOptions learnFrom,
-            PythonInstaller.eSimulatedInstallationState simulatedInstallationState, IrtStandard iRTtype = null)
+            IrtStandard iRTtype = null)
         {
             var peptideSettings = ShowDialog<PeptideSettingsUI>(() =>
                 SkylineWindow.ShowPeptideSettingsUI(PeptideSettingsUI.TABS.Library));
             bool buildLibraryDlgFinished = false;
-            var buildLibraryDlg = ShowDialog<BuildLibraryDlg> (() =>
+            var buildLibraryDlg = ShowDialog<BuildLibraryDlg>(() =>
             {
                 peptideSettings.ShowBuildLibraryDlg();
                 buildLibraryDlgFinished = true;
@@ -306,7 +272,7 @@ namespace TestPerf
                 buildLibraryDlg.ComboBuildLibraryTarget = buildTarget;
                 buildLibraryDlg.ComboLearnFrom = learnFrom;
                 buildLibraryDlg.Carafe = true;
-                if (iRTtype != null) 
+                if (iRTtype != null)
                     buildLibraryDlg.IrtStandard = iRTtype;
 
                 buildLibraryDlg.OkWizardPage();
@@ -323,45 +289,11 @@ namespace TestPerf
             Assert.AreEqual(buildLibraryDlg.ButtonNextText, @"Finish");
             Assert.IsTrue(buildLibraryDlg.ButtonNextEnabled);
             const int waitTime = 30 * 60 * 1000;
-            // Test the control path where Python needs installation and is
-            if (simulatedInstallationState == PythonInstaller.eSimulatedInstallationState.NONVIDIASOFT)
-            {
-                // TestCancelPython always uses NAIVE state so must reset state
-                TestCancelPython(buildLibraryDlg);
-                PythonInstaller.SimulatedInstallationState = simulatedInstallationState;
-                var confirmDlg = TestNvidiaInstallPython(buildLibraryDlg);
-                if (confirmDlg != null)
-                {
-                    OkDialog(confirmDlg, confirmDlg.OkDialog);
-                }
-                else
-                {
-                    PythonInstaller.SimulatedInstallationState =
-                        PythonInstaller.eSimulatedInstallationState.NONVIDIAHARD;
-                    //RunUI(buildLibraryDlg.OkWizardPage);
-                }
-            }
-            else
-            {
-                PythonInstaller.SimulatedInstallationState = simulatedInstallationState;
-                SkylineWindow.BeginInvoke(new Action(buildLibraryDlg.OkWizardPage));
-                WaitForConditionUI(waitTime, () =>
-                    buildLibraryDlgFinished || null != FindOpenForm<MultiButtonMsgDlg>() ||
-                    null != FindOpenForm<AddIrtPeptidesDlg>());
-                var confirmPythonDlg = FindOpenForm<MultiButtonMsgDlg>();
-                if (confirmPythonDlg != null)
-                {
-                    AssertEx.AreComparableStrings(ToolsUIResources.PythonInstaller_BuildPrecursorTable_Python_0_installation_is_required, confirmPythonDlg.Message);
-                    OkDialog(confirmPythonDlg, confirmPythonDlg.OkDialog);
-                }
-            }
-            WaitForCondition(waitTime, () => buildLibraryDlgFinished || null != FindOpenForm<MessageDlg>() || iRTtype != null && null != FindOpenForm<AddIrtPeptidesDlg>());
-            var messageDlg = FindOpenForm<MessageDlg>();
-            if (messageDlg != null)
-            {
-                Assert.AreEqual(ToolsUIResources.PythonInstaller_OkDialog_Successfully_set_up_Python_virtual_environment, messageDlg.Message);
-                OkDialog(messageDlg, messageDlg.OkDialog);
-            }
+
+            // Carafe 2.0 manages its own Python via PyInstaller, so no Python
+            // installation dialogs are expected. Just click Finish and wait.
+            SkylineWindow.BeginInvoke(new Action(buildLibraryDlg.OkWizardPage));
+            WaitForCondition(waitTime, () => buildLibraryDlgFinished || iRTtype != null && null != FindOpenForm<AddIrtPeptidesDlg>());
 
             if (iRTtype != null)
             {
@@ -421,170 +353,6 @@ namespace TestPerf
             OkDialog(regression, regression.CloseDialog);
         }
  
-        /// <summary>
-        /// Pretends Python needs installation then Cancels Python install
-        /// </summary>
-        /// <param name="buildLibraryDlg">Build Library dialog</param>
-        public void TestCancelPython(BuildLibraryDlg buildLibraryDlg)
-        {
-            VerboseLog("TestCarafeBuildLibrary: Start TestCancelPython() test ... ");
-            // Test the control path where Python is not installed, and the user is prompted to deal with admin access
-            PythonInstaller.SimulatedInstallationState =
-                PythonInstaller.eSimulatedInstallationState.NAIVE; // Simulates not having the needed registry settings
-            var installPythonDlg = ShowDialog<MultiButtonMsgDlg>(buildLibraryDlg.OkWizardPage); // Expect the offer to install Python
-
-            CancelDialog(installPythonDlg, installPythonDlg.CancelDialog); // Cancel it immediately
-
-            installPythonDlg =
-                ShowDialog<MultiButtonMsgDlg>(buildLibraryDlg.OkWizardPage); // Expect the offer to install Python
-
-            AssertEx.AreComparableStrings(
-                ToolsUIResources.PythonInstaller_BuildPrecursorTable_Python_0_installation_is_required,
-                installPythonDlg.Message);
-
-            var needAdminDlg = ShowDialog<MessageDlg>(installPythonDlg.OkDialog);
-
-            AssertEx.AreComparableStrings(ToolsUIResources.PythonInstaller_Requesting_Administrator_elevation,
-                needAdminDlg.Message);
-
-            CancelDialog(needAdminDlg, needAdminDlg.CancelDialog);
-            VerboseLog("TestCarafeBuildLibrary: Finish TestCancelPython() test ... ");
-        }
-
-        public MessageDlg TestNvidiaInstallPython(BuildLibraryDlg buildLibraryDlg)
-        {
-            VerboseLog("TestCarafeBuildLibrary: Start TestNvidiaInstallPython() test ... ");
-            // Test the control path where Nvidia Card is Available and Nvidia Libraries are not installed, and the user is prompted to deal with Nvidia
-            // Test for LongPaths not set and admin
-            if (PythonInstaller.IsRunningElevated() && !PythonInstaller.ValidateEnableLongpaths())
-            {
-                var adminDlg = ShowDialog<MessageDlg>(buildLibraryDlg.OkWizardPage,
-                    WAIT_TIME); // Expect request for elevated privileges 
-                // var adminDlg = WaitForOpenForm<MessageDlg>();
-                AssertEx.AreComparableStrings(ToolsUIResources.PythonInstaller_Requesting_Administrator_elevation,
-                    adminDlg.Message);
-                OkDialog(adminDlg, adminDlg.OkDialog);
-            }
-            else if (!PythonInstaller.ValidateEnableLongpaths())
-            {
-                Assert.Fail($@"Error: Cannot finish {_toolName}BuildLibraryTest because {PythonInstaller.REG_FILESYSTEM_KEY}\{PythonInstaller.REG_LONGPATHS_ENABLED} is not set and have insufficient permissions to set it");
-            }
-            else
-            {
-                ShowDialog<MessageDlg>(buildLibraryDlg.OkWizardPage, WAIT_TIME); // Expect the offer to installNvidia
-            }
-
-            var installNvidiaDlg = WaitForOpenForm<MessageDlg>();
-
-            AssertEx.AreComparableStrings(ToolsUIResources.PythonInstaller_Install_Nvidia_Library,
-                installNvidiaDlg.Message);
-
-            CancelDialog(installNvidiaDlg, installNvidiaDlg.CancelDialog);
-
-            installNvidiaDlg = ShowDialog<MessageDlg>(buildLibraryDlg.OkWizardPage, WAIT_TIME);
-            AssertEx.AreComparableStrings(ToolsUIResources.PythonInstaller_Install_Nvidia_Library,
-                installNvidiaDlg.Message);
-
-            OkDialog(installNvidiaDlg, installNvidiaDlg.ClickYes);
-
-            var needAdminDlg = WaitForOpenForm<MessageDlg>();
-
-            AssertEx.AreComparableStrings(ModelResources.NvidiaInstaller_Requesting_Administrator_elevation,
-                needAdminDlg.Message);
-
-            CancelDialog(needAdminDlg, needAdminDlg.CancelDialog); // Expect the offer to installNvidia
-            installNvidiaDlg = ShowDialog<MessageDlg>(buildLibraryDlg.OkWizardPage, WAIT_TIME); // 3 minutes 
-            AssertEx.AreComparableStrings(ToolsUIResources.PythonInstaller_Install_Nvidia_Library,
-                installNvidiaDlg.Message);
-
-            // Python installation begins when user ClickNo
-            OkDialog(installNvidiaDlg, installNvidiaDlg.ClickNo);
-
-            if (!IsCleanPythonMode)
-                return null;
-
-            var pythonConfirm = WaitForOpenForm<MessageDlg>(WAIT_TIME * 4); // 12 minutes - successful completion message
-            VerboseLog("TestCarafeBuildLibrary: Finish TestNvidiaInstallPython() test ... ");
-            return pythonConfirm;
-        }
-
-        /// <summary>
-        /// Pretends no NVIDIA hardware then Installs Python, returns true if Python installer ran (successful or not), false otherwise
-        /// </summary>
-        /// <param name="buildLibraryDlg">Build Library</param>
-        public bool InstallPython(BuildLibraryDlg buildLibraryDlg)
-        {
-            PythonInstaller.SimulatedInstallationState =
-                PythonInstaller.eSimulatedInstallationState.NONVIDIAHARD; // Normal tests systems will have registry set suitably
-            MessageDlg confirmDlg = null;
-            RunLongDlg<MultiButtonMsgDlg>(buildLibraryDlg.OkWizardPage, pythonDlg =>
-            {
-                AssertEx.AreComparableStrings(
-                    ToolsUIResources.PythonInstaller_BuildPrecursorTable_Python_0_installation_is_required,
-                    pythonDlg.Message);
-
-                if (!PythonInstaller.ValidateEnableLongpaths())
-                {
-                    var longPathDlg = ShowDialog<MessageDlg>(pythonDlg.OkDialog);
-
-                    Assert.AreEqual(
-                        string.Format(ToolsUIResources.PythonInstaller_Requesting_Administrator_elevation),
-                        longPathDlg.Message);
-
-                    if (PythonInstaller.IsRunningElevated())
-                    {
-                        confirmDlg = ShowDialog<MessageDlg>(pythonDlg.OkDialog, WAIT_TIME);
-                        ConfirmPythonSuccess(confirmDlg);
-                    }
-                    else
-                    {
-                        Assert.Fail($@"Error: Cannot finish {_toolName}BuildLibraryTest because {PythonInstaller.REG_FILESYSTEM_KEY}\{PythonInstaller.REG_LONGPATHS_ENABLED} is not set and have insufficient permissions to set it");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine(@"Info: LongPathsEnabled registry key is already set to 1");
-                    OkDialog(pythonDlg, pythonDlg.OkDialog);
-                    confirmDlg = ShowDialog<MessageDlg>(pythonDlg.OkDialog, WAIT_TIME);
-                    ConfirmPythonSuccess(confirmDlg);
-                }
-
-
-            }, dlg => dlg.Close());
-            if (DisableLongPathsRegistry)
-            {
-                PythonInstaller.EnableWindowsLongPaths(false);
-            }
-
-            return true;
-        }
-
-        /// <summary>
-        /// Confirms Python installation success
-        /// </summary>
-        /// <param name="confirmDlg">Message dialog success</param>
-        private void ConfirmPythonSuccess(AlertDlg confirmDlg)
-        {
-            ConfirmPython(confirmDlg);
-        }
-        
-        /// <summary>
-        /// Confirms Python installation
-        /// </summary>
-        /// <param name="confirmDlg">Alert dialog </param>
-        /// <param name="confirmSuccess">true for success, false for failure</param>
-        public void ConfirmPython(AlertDlg confirmDlg, bool confirmSuccess = true)
-        {
-            var expectMsg = string.Format(ToolsUIResources
-                .PythonInstaller_OkDialog_Failed_to_set_up_Python_virtual_environment);
-            if (confirmSuccess)
-                expectMsg = string.Format(ToolsUIResources
-                    .PythonInstaller_OkDialog_Successfully_set_up_Python_virtual_environment);
-
-            Assert.AreEqual(expectMsg, confirmDlg.Message);
-            confirmDlg.OkDialog();
-        }
-
         private void VerboseLog(string message)
         {
             if (IsVerboseMode)
