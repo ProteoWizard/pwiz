2c696e1e8c8ee35311dc827c2cc3cd6e787a44bb
Nicholas Shulman <nickshulman@hotmail.com>
2026-04-09 21:38:30 -0700

    Verify number of outliers in regression graph pane (currently failing)

--- pwiz_tools/Skyline/TestFunctional/RetentionTimeRegressionExceptionTest.cs
+++ pwiz_tools/Skyline/TestFunctional/RetentionTimeRegressionExceptionTest.cs
@@ -1,3 +1,22 @@
+/*
+ * Original author: Nicholas Shulman <nicksh .at. u.washington.edu>,
+ *                  MacCoss Lab, Department of Genome Sciences, UW
+ *
+ * Copyright 2026 University of Washington - Seattle, WA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using pwiz.Skyline.Properties;
 using pwiz.SkylineTestUtil;
@@ -19,11 +38,17 @@ namespace pwiz.SkylineTestFunctional
             RunUI(()=>
             {
                 SkylineWindow.OpenFile(TestFilesDir.GetTestPath("RtRegressionBugSmall.sky"));
+                SkylineWindow.SelectedResultsIndex = 1;
+                SkylineWindow.ShowSingleReplicate();
                 Settings.Default.RTCalculatorName = "TTOF_64w_iRT-C18";
             });
             WaitForDocumentLoaded();
             RunUI(()=>SkylineWindow.ShowRTRegressionGraphScoreToRun());
-            WaitForGraphs();
+            WaitForRegression();
+            var outliers = SkylineWindow.RTGraphController.Outliers;
+            // I expect only two outliers, but currently, three get reported.
+            // The peptide "GAGSSEPVTGLDAK" is not supposed to be an outlier.
+            Assert.AreEqual(2, outliers.Length);
         }
     }
 }
